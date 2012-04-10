f352f08efa781500420c4e1cf920d6180c1e6a29
Steaphan Greene <steaphan@oculus.com>
2012-04-09 18:43:43 -0700

    Port BinLog BaseDirs
    Summary:
    Add global variables to list directory for binlog files and binlog index file.
    The variables are binlog_file_basedir and binlog_index_basedir.
    
    Reference patch: https://github.com/facebook/mysql-5.6/commit/3ccbf59
    
    Differential Revision: D6155155

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -297,6 +297,7 @@ extern char *my_bind_addr_str;
 extern char *my_admin_bind_addr_str;
 extern uint mysqld_admin_port;
 extern bool listen_admin_interface_in_separate_thread;
+extern char *binlog_file_basedir_ptr, *binlog_index_basedir_ptr;
 extern char glob_hostname[HOSTNAME_LENGTH + 1];
 extern char system_time_zone_dst_on[30], system_time_zone_dst_off[30];
 extern char *opt_init_file;
