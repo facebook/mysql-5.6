879eae0a86b3a4cebfe53f4780fad819ea2ab941
rongrong <rongrong@fb.com>
2012-11-01 14:45:10 -0700

    Port Ignore innodb_thread_concurrency for Replication Thread to 5.6
    Summary:
    Port the following mysql-5.1 changes to mysql-5.6:
    Thu Feb 25 11:14:09 2010 (3a85a176d5223d3bbe6c816505884f3506bfce77) Ryan Mack
        Port 86895488: Ignore innodb_thread_concurrency for the SQL replication thread
            https://phabricator.fb.com/D92902
    
    Test Plan: mysqltest.sh
    
    Reviewers: steaphan
    
    Reviewed By: steaphan
    
    Differential Revision: https://phabricator.fb.com/D618142
    
    Task ID: 1816878
    (cherry picked from commit 774dee6831046374ab64df195f46523c31dbcdd0)
    
    Conflicts:
    
    	storage/innobase/handler/ha_innodb.cc

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -2270,6 +2270,10 @@ innobase_trx_init(
 	trx->check_unique_secondary = !thd_test_options(
 		thd, OPTION_RELAXED_UNIQUE_CHECKS);
 
+	/* The replication thread ignores innodb_thread_concurrency. */
+	if (thd_is_replication_slave_thread(thd))
+		trx->always_enter_innodb = TRUE;
+
 	DBUG_VOID_RETURN;
 }
 
