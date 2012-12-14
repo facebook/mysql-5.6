82e02b4614885d4209a06457680792f11f823a1f
Steaphan Greene <steaphan@oculus.com>
2012-12-13 16:29:07 -0800

    Replication Monitoring to 5.6
    Summary:
    Add more SHOW STATUS counters for monitoring replication:
    
    Relay_log_io_events, Relay_log_io_bytes:
    Number of events and bytes written by the IO thread
    
    Relay_log_sql_events, Relay_log_sql_bytes:
    Number of events and bytes written by the SQL thread
    
    Relay_log_sql_wait_seconds:
    Number of seconds that the SQL thread waits for the IO thread to provide
    events. This occurs when the master is idle or the network between
    master/slave is slow.
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/b209107
    
    Reviewed By: lth
    
    Differential Revision: D6851248
    
    fbshipit-source-id: 1ed2b659790

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -307,6 +307,10 @@ extern const char *binlog_error_action_list[];
 
 extern bool opt_log_slow_extra;
 
+extern ulong relay_io_events, relay_sql_events;
+extern ulonglong relay_io_bytes, relay_sql_bytes;
+extern ulonglong relay_sql_wait_time;
+
 /* SHOW STATS var: Name of current timer */
 extern const char *timer_in_use;
 /* Current timer stats */
