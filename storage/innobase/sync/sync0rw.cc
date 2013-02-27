1ef6d1f4db6e71f7905cce5b9b000cf1ba212cdb
rudradevbasak <rudradevbasak@fb.com>
2013-02-27 10:59:55 -0800

    Revert "Ported Shared PThread Data to 5.6"
    Summary: This reverts commit 603a08ac6a9585e9652f82771e40b83f08fb2c89.
    
    Test Plan: Jenkins tests pass.
    
    Reviewers: steaphan, sanpra90
    
    Reviewed By: steaphan
    
    CC: db-eng@lists
    
    Differential Revision: https://phabricator.fb.com/D720695

--- storage/innobase/sync/sync0rw.cc
+++ storage/innobase/sync/sync0rw.cc
@@ -41,7 +41,6 @@ Created 9/11/1995 Heikki Tuuri
 #include "os0sync.h" /* for INNODB_RW_LOCKS_USE_ATOMICS */
 #include "ha_prototypes.h"
 
-UNIV_INTERN ulint	rw_lock_count = 0;
 /*
 	IMPLEMENTATION OF THE RW_LOCK
 	=============================
@@ -264,8 +263,8 @@ rw_lock_create_func(
 	lock->last_x_file_name = "not yet reserved";
 	lock->last_s_line = 0;
 	lock->last_x_line = 0;
-	os_event_create2(&lock->event);
-	os_event_create2(&lock->wait_ex_event);
+	lock->event = os_event_create();
+	lock->wait_ex_event = os_event_create();
 
 	mutex_enter(&rw_lock_list_mutex);
 
@@ -273,7 +272,6 @@ rw_lock_create_func(
 	      || UT_LIST_GET_FIRST(rw_lock_list)->magic_n == RW_LOCK_MAGIC_N);
 
 	UT_LIST_ADD_FIRST(list, rw_lock_list, lock);
-	rw_lock_count++;
 
 	mutex_exit(&rw_lock_list_mutex);
 }
@@ -301,9 +299,9 @@ rw_lock_free_func(
 	mutex = rw_lock_get_mutex(lock);
 #endif /* !INNODB_RW_LOCKS_USE_ATOMICS */
 
-	os_event_free2(&lock->event);
+	os_event_free(lock->event);
 
-	os_event_free2(&lock->wait_ex_event);
+	os_event_free(lock->wait_ex_event);
 
 	ut_ad(UT_LIST_GET_PREV(list, lock) == NULL
 	      || UT_LIST_GET_PREV(list, lock)->magic_n == RW_LOCK_MAGIC_N);
@@ -311,7 +309,6 @@ rw_lock_free_func(
 	      || UT_LIST_GET_NEXT(list, lock)->magic_n == RW_LOCK_MAGIC_N);
 
 	UT_LIST_REMOVE(list, rw_lock_list, lock);
-	rw_lock_count--;
 
 	mutex_exit(&rw_lock_list_mutex);
 
