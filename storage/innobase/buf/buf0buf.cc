cc123d87bf7f5ebaa3c00a4e87ba66ebd16a40fa
Rudradev Basak <rudradevbasak@fb.com>
2013-03-04 09:45:34 -0800

    Add Facebooks's integrated version of xtrabackup
    Summary:
    This began with just the "innodb56.patch" from Percona, with some
    fixes from them as well, including:
    
    * https://bugs.launchpad.net/percona-xtrabackup/+bug/950334
    * A fix for deleted tablespaces in xtrabackup apply log (from 5.1 version)
    
    But, this is all done differently than that patch.  Instead of a
    separate patch, this is integrated into the main code.
    
    All changes are now protected via the XTRABACKUP define.
    
    This allows the same code to compile for both mysqld and xtrabackup.
    
    This should have a net ZERO effect, until the xtrabackup build is added.
    
    We build this in our tree, beside our normal out-of-dir cmake build.
    
    Basically, this can be built just by running this on a clean source tree:
    
    	cd xtrabackup; ./utils/build.sh innodb56; cd ..
    
    Note that this only works right in a clean source tree.
    But, since we build out-of-dir, our source tree is always clean.
    
    Also note that some corner-cases (Debug builds of xtrabackup, for example)
    will probably still require more build magic to work.  The real solution
    here would be to integrate this build into the main cmake build of
    mysql-5.6 itself, but we didn't go that far with this (yet).
    
    Sections not required for mysql-5.6 were removed, for simplicity.
    
    Also, some compile/build warnings/errors were cleaned up.
    
    And, innobackupex was modified.  For apply log, if xtrabackup binary is
    explicitly specified, dont try to autodetect.
    
    Also, we initialize error messages for xtrabackup, and we do not use
    thread-specific locales for error messages.
    
    Also, we create the xtrabackup_logfile in the supplied target_dir.
    
    Also, we removed the FLUSH TABLES WITH READ LOCK calls from innobackupex.
    
    Also, we added a fix to prevent an indefinite sleep of xtrabackup.
    
    Also, we added support for the O_DIRECT_NO_FSYNC option.
    
    And, we don't use O_DIRECT for xtrabackup_logfile.
    
    Test Plan:
    We created a small suite of xtrabackup tests that run within mtr.
    These are included here, and all pass.
    
    Jenkins (arc unit).  All pass.
    
    xtrabackup build works (in CLEAN source dir):
    	cd xtrabackup; ./utils/build.sh innodb56; cd ..
    
    Artificially threw some warnings in xtrabackup.
    Successfully carried out a full backup with warning (segfaulted before).
    
    Reviewers: steaphan, jtolmer, nizamordulu
    
    Reviewed By: steaphan, jtolmer, nizamordulu

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -578,11 +578,18 @@ buf_page_is_corrupted(
 	if (checksum_field1 == 0 && checksum_field2 == 0
 	    && mach_read_from_4(read_buf + FIL_PAGE_LSN) == 0) {
 		/* make sure that the page is really empty */
+
+/* Do not do this check in xtrabackup, as this check is
+incompatible with 1st newly-created tablespace pages, which
+have FIL_PAGE_FIL_FLUSH_LSN != 0, FIL_PAGE_OR_CHKSUM == 0,
+FIL_PAGE_END_LSN_OLD_CHKSUM == 0 */
+#ifndef XTRABACKUP
 		for (ulint i = 0; i < UNIV_PAGE_SIZE; i++) {
 			if (read_buf[i] != 0) {
 				return(TRUE);
 			}
 		}
+#endif /* !XTRABACKUP */
 
 		return(FALSE);
 	}
--- xtrabackup/utils/build.sh
+++ xtrabackup/utils/build.sh
@@ -8,34 +8,31 @@ MYSQL_56_VERSION=5.6.10
 PS_51_VERSION=5.1.59-13.0
 PS_55_VERSION=5.5.16-22.0
 
-AUTO_DOWNLOAD=${AUTO_DOWNLOAD:-no}
-MASTER_SITE="http://s3.amazonaws.com/percona.com/downloads/community"
-
-# Percona Server 5.5 does not build with -Werror, so ignore DEBUG for now
-if [ -n "$DEBUG" -a "$1" != "galera55" -a "$1" != "xtradb55" -a "$1" != "xtradb51" -a "$1" != "xtradb" ]
-then
-    # InnoDB extra debug flags
-    innodb_extra_debug="-DUNIV_DEBUG -DUNIV_SYNC_DEBUG -DUNIV_MEM_DEBUG \
--DUNIV_DEBUG_THREAD_CREATION -DUNIV_DEBUG_LOCK_VALIDATE -DUNIV_DEBUG_PRINT \
--DUNIV_DEBUG_FILE_ACCESS -DUNIV_SEARCH_DEBUG -DUNIV_LOG_LSN_DEBUG \
--DUNIV_ZIP_DEBUG -DUNIV_AHI_DEBUG -DUNIV_SQL_DEBUG -DUNIV_AIO_DEBUG \
--DUNIV_LRU_DEBUG -DUNIV_BUF_DEBUG -DUNIV_HASH_DEBUG -DUNIV_LIST_DEBUG -DUNIV_IBUF_DEBUG"
-    export CFLAGS="$CFLAGS -g -O0 $innodb_extra_debug -DSAFE_MUTEX -DSAFEMALLOC"
-    export CXXFLAGS="$CXXFLAGS -g -O0 $innodb_extra_debug -DSAFE_MUTEX -DSAFEMALLOC"
-    extra_config_51="--with-debug=full"
-    extra_config_55plus="-DWITH_DEBUG=ON"
+if [ -r ../CMakeCache.txt ]; then
+  echo BUILD_TYPE= ${BUILD_TYPE:="$(grep CMAKE_BUILD_TYPE:STRING= ../CMakeCache.txt | cut -f2 -d=)"}
+  echo SRC_DIR= ${SRC_DIR:="$(grep MySQL_SOURCE_DIR:STATIC= ../CMakeCache.txt | cut -f2 -d=)"}
+  echo CMAKE= ${CMAKE:="$(grep CMAKE_COMMAND:INTERNAL= ../CMakeCache.txt | cut -f2 -d=)"}
+  echo CXX= ${CXX:="$(grep CMAKE_CXX_COMPILER:FILEPATH= ../CMakeCache.txt | cut -f2 -d=) $(grep CMAKE_CXX_COMPILER_ARG1:STRING= ../CMakeCache.txt | cut -f2 -d=)"}
+  echo CC= ${CC:="$(grep CMAKE_C_COMPILER:FILEPATH= ../CMakeCache.txt | cut -f2 -d=) $(grep CMAKE_C_COMPILER_ARG1:STRING= ../CMakeCache.txt | cut -f2 -d=)"}
+  echo CXXFLAGS= ${CXXFLAGS:="$(grep CMAKE_CXX_FLAGS:STRING= ../CMakeCache.txt | cut -f2 -d=) $(grep CMAKE_C_COMPILER_ARG1:STRING= ../CMakeCache.txt | cut -f2 -d=)"}
+  echo CFLAGS= ${CFLAGS:="$(grep CMAKE_C_FLAGS:STRING= ../CMakeCache.txt | cut -f2 -d=) $(grep CMAKE_C_COMPILER_ARG1:STRING= ../CMakeCache.txt | cut -f2 -d=)"}
 else
-    export CFLAGS="$CFLAGS -g -O3"
-    export CXXFLAGS="$CXXFLAGS -g -O3"
-    extra_config_51=
-    extra_config_55plus=
+  echo BUILD_TYPE= ${BUILD_TYPE:=RelWithDebInfo}
+  echo SRC_DIR= ${SRC_DIR:=$(readlink -f ..)}
+  echo CMAKE= ${CMAKE:=cmake}
+  echo CXX= ${CXX:=g++}
+  echo CC= ${CC:=gcc}
+  echo CXXFLAGS= ${CXXFLAGS:-}
+  echo CFLAGS= ${CFLAGS:-}
 fi
 
-if [ "$1" = "innodb51_builtin" -o "$1" = "innodb50" ]
-then
-    # include/*.ic in pre-5.1-plugin InnoDB do not compile well in C++.
-    export CXXFLAGS="$CXXFLAGS -fpermissive"
-fi
+export CC="$CC"
+export CXX="$CXX"
+export CFLAGS="$CFLAGS -DXTRABACKUP"
+export CXXFLAGS="$CFLAGS -fno-rtti -fno-exceptions"
+export LDFLAGS="$LDFLAGS"
+export SRC_DIR="$SRC_DIR"
+export BUILD_TYPE="$BUILD_TYPE"
 
 MAKE_CMD=make
 if gmake --version > /dev/null 2>&1
@@ -50,50 +47,8 @@ function usage()
     echo
     echo "Usage: `basename $0` CODEBASE"
     echo "where CODEBASE can be one of the following values or aliases:"
-    echo "  innodb50         | 5.0                   build against innodb 5.1 builtin, but should be compatible with MySQL 5.0"
-    echo "  innodb51_builtin | 5.1                   build against built-in InnoDB in MySQL 5.1"
-    echo "  innodb51         | plugin                build agsinst InnoDB plugin in MySQL 5.1"
-    echo "  innodb55         | 5.5                   build against InnoDB in MySQL 5.5"
     echo "  innodb56         | 5.6                   build against InnoDB in MySQL 5.6"
-    echo "  xtradb51         | xtradb,mariadb51      build against Percona Server with XtraDB 5.1"
-    echo "                   | mariadb52,mariadb53"
-    echo "  xtradb55         | xtradb55,galera55,    build against Percona Server with XtraDB 5.5"
-    echo "                   | mariadb55"
     exit -1
-}
-
-################################################################################
-# Download files specified as arguments from $MASTER_SITE if $AUTO_DOWNLOAD is
-# "yes". Otherwise print an error message and exit.
-################################################################################
-function auto_download()
-{
-    for i in $*
-    do
-	if ! test -f $i
-	then
-	    if [ "$AUTO_DOWNLOAD" = "yes" ]
-	    then
-		wget "$MASTER_SITE"/$i
-	    else
-		echo "Put $i in $top_dir or set environment variable \
-AUTO_DOWNLOAD to \"yes\""
-		exit -1
-	    fi
-	fi
-    done
-}
-
-################################################################################
-# Unpack the tarball specified as the first argument and apply the patch
-# specified as the second argument to the resulting directory.
-################################################################################
-function unpack_and_patch()
-{
-    tar xzf $top_dir/$1
-    cd `basename "$1" ".tar.gz"`
-    patch -p1 < $top_dir/patches/$2
-    cd ..
 }
 
 ################################################################################
@@ -112,17 +67,11 @@ function build_server()
     local $type=$1
     echo "Configuring the server"
     cd $server_dir
-    BUILD/autorun.sh
+    echo eval $configure_cmd
     eval $configure_cmd
 
     echo "Building the server"
-    if [ "$type" = "innodb56" ]
-    then
-        make_dirs libmysqld
-    else
-        make_dirs include zlib strings mysys dbug extra
-        make_dirs $innodb_dir
-    fi
+    make_dirs libmysqld
     cd $top_dir
 }
 
@@ -130,8 +79,8 @@ function build_libarchive()
 {
 	echo "Building libarchive"
 	cd $top_dir/src/libarchive
-	
-	cmake  . \
+
+	${CMAKE}  . \
 	    -DENABLE_CPIO=OFF \
 	    -DENABLE_OPENSSL=OFF \
 	    -DENABLE_TAR=OFF \
@@ -172,20 +121,8 @@ function build_xtrabackup()
 function build_all()
 {
     local type=$1
-    local mysql_version_short=${mysql_version:0:3}
-    server_dir=$top_dir/mysql-$mysql_version_short
-    server_tarball=mysql-$mysql_version.tar.gz
-    innodb_dir=$server_dir/storage/$innodb_name
 
-    echo "Downloading sources"
-    auto_download $server_tarball
-
-    test -d $server_dir && rm -r $server_dir
-
-    echo "Preparing sources"
-    unpack_and_patch $server_tarball $server_patch
-    mv $top_dir/mysql-$mysql_version $server_dir
-
+    mkdir -p $server_dir
     build_server $type
 
     build_xtrabackup
@@ -202,170 +139,52 @@ top_dir=`pwd`
 
 
 case "$type" in
-"innodb51_builtin" | "5.1" | "innodb50" | "5.0")
-	mysql_version=$MYSQL_51_VERSION
-	server_patch=innodb51_builtin.patch
-	innodb_name=innobase
-	xtrabackup_target=5.1
-	configure_cmd="./configure --enable-local-infile \
-	    --enable-thread-safe-client \
-	    --with-plugins=innobase \
-	    --with-zlib-dir=bundled \
-	    --enable-shared \
-	    --with-extra-charsets=all $extra_config_51"
-
-	build_all $type
-	;;
-
-"innodb51" | "plugin")
-       mysql_version=$MYSQL_51_VERSION
-       server_patch=innodb51.patch
-       innodb_name=innodb_plugin
-       xtrabackup_target=plugin
-       configure_cmd="./configure --enable-local-infile \
-           --enable-thread-safe-client \
-           --with-plugins=innodb_plugin \
-           --with-zlib-dir=bundled \
-           --enable-shared \
-           --with-extra-charsets=all $extra_config_51"
-
-       build_all $type
-       ;;
-"innodb55" | "5.5")
-	mysql_version=$MYSQL_55_VERSION
-	server_patch=innodb55.patch
-	innodb_name=innobase
-	xtrabackup_target=5.5
-	# We need to build with partitioning due to MySQL bug #58632
-	configure_cmd="cmake . \
-		-DENABLED_LOCAL_INFILE=ON \
-		-DWITH_INNOBASE_STORAGE_ENGINE=ON \
-		-DWITH_PARTITION_STORAGE_ENGINE=ON \
-		-DWITH_ZLIB=bundled \
-		-DWITH_EXTRA_CHARSETS=all \
-		-DENABLE_DTRACE=OFF $extra_config_55plus"
-
-	build_all $type
-	;;
-
 "innodb56" | "5.6")
         mysql_version=$MYSQL_56_VERSION
         server_patch=innodb56.patch
         innodb_name=innobase
         xtrabackup_target=5.6
-        configure_cmd="cmake . \
+        mysql_version_short=${mysql_version:0:3}
+        server_dir=$top_dir/mysql-$mysql_version_short
+        configure_cmd="${CMAKE} $SRC_DIR \
                 -DWITH_INNOBASE_STORAGE_ENGINE=ON \
-                -DWITH_ZLIB=bundled \
+                -DWITH_PERFSCHEMA_STORAGE_ENGINE=ON \
+                -DMYSQL_DATADIR="/var/lib/mysql" \
+                -DMYSQL_UNIX_ADDR="/var/lib/mysql/mysql.sock" \
+                -DBUILD_CONFIG=mysql_release \
+                -DMYSQL_USER="mysql" \
+                -DWITH_FAST_MUTEXES=1 \
                 -DWITH_EXTRA_CHARSETS=all \
-                -DWITH_EMBEDDED_SERVER=1 $extra_config_55plus"
+                -DWITH_EMBEDDED_SERVER=1 \
+                -DMYSQL_MAINTAINER_MODE=1 \
+                -DMYSQL_ROOT_DIR=$server_dir \
+                -DCMAKE_BUILD_TYPE=$BUILD_TYPE"
+        if [ -n "$CMAKE_PREFIX_PATH" ]; then
+                configure_cmd+=" -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH"
+        fi
+        if [ -n "$CURSES_LIBRARY" ]; then
+                configure_cmd+=" -DCURSES_LIBRARY=$CURSES_LIBRARY"
+        fi
+        if [ -n "$CURSES_INCLUDE_PATH" ]; then
+                configure_cmd+=" -DCURSES_INCLUDE_PATH=$CURSES_INCLUDE_PATH"
+        fi
+        if [ -n "$KRB_PATH" ]; then
+                configure_cmd+=" -DWITH_KRB=$KRB_PATH"
+        fi
+        if [ -n "$SSL_PATH" ]; then
+                configure_cmd+=" -DWITH_SSL=$SSL_PATH"
+        fi
+        if [ -n "$ZLIB_PATH" ]; then
+                configure_cmd+=" -DWITH_ZLIB=$ZLIB_PATH"
+        else
+                congigure_cmd+=" -DWITH_ZLIB=bundled"
+        fi
+        if [ -n "$GLIBC_PATH" ]; then
+                configure_cmd+=" -DWITH_GLIBC=$GLIBC_PATH"
+        fi
         build_all $type
         ;;
 
-"xtradb51" | "xtradb" | "mariadb51" | "mariadb52" | "mariadb53")
-	server_dir=$top_dir/Percona-Server
-	branch_dir=percona-server-5.1-xtrabackup
-	innodb_dir=$server_dir/storage/innodb_plugin
-	xtrabackup_target=xtradb
-	configure_cmd="./configure --enable-local-infile \
-	    --enable-thread-safe-client \
-	    --with-plugins=innodb_plugin \
-	    --with-zlib-dir=bundled \
-	    --enable-shared \
-	    --with-extra-charsets=all $extra_config_51"
-	if [ "`uname -s`" = "Linux" ]
-	then
-		configure_cmd="LIBS=-lrt $configure_cmd"
-	fi
-
-
-	echo "Downloading sources"
-	
-	# Get Percona Server
-	if [ -d $branch_dir ]
-	then
-	    rm -rf $branch_dir
-	fi
-        if [ -d $branch_dir ]
-        then
-	    cd $branch_dir
-	    (bzr upgrade || true)
-	    bzr clean-tree --force --ignored
-	    bzr revert
-	    bzr pull --overwrite
-	else
-	    bzr branch -r tag:Percona-Server-$PS_51_VERSION \
-		lp:percona-server/5.1 $branch_dir
-	    cd $branch_dir
-	fi
-
-	$MAKE_CMD main
-	cd $top_dir
-	rm -rf $server_dir
-	ln -s $branch_dir/Percona-Server $server_dir
-
-	# Patch Percona Server
-	cd $server_dir
-	patch -p1 < $top_dir/patches/xtradb51.patch
-
-	build_server $type
-
-	build_xtrabackup
-
-	;;
-"xtradb55" | "galera55" | "mariadb55")
-	server_dir=$top_dir/Percona-Server-5.5
-	branch_dir=percona-server-5.5-xtrabackup
-	innodb_dir=$server_dir/storage/innobase
-	xtrabackup_target=xtradb55
-	# We need to build with partitioning due to MySQL bug #58632
-	configure_cmd="cmake . \
-		-DENABLED_LOCAL_INFILE=ON \
-		-DWITH_INNOBASE_STORAGE_ENGINE=ON \
-		-DWITH_PARTITION_STORAGE_ENGINE=ON \
-		-DWITH_ZLIB=bundled \
-		-DWITH_EXTRA_CHARSETS=all \
-		-DENABLE_DTRACE=OFF $extra_config_55plus"
-	if [ "`uname -s`" = "Linux" ]
-	then
-		configure_cmd="LIBS=-lrt $configure_cmd"
-	fi
-
-
-	echo "Downloading sources"
-	
-	# Get Percona Server
-	if [ -d $branch_dir ]
-	then
-	    rm -rf $branch_dir
-	fi
-        if [ -d $branch_dir ]
-        then
-	    cd $branch_dir
-	    yes | bzr break-lock
-	    (bzr upgrade || true)
-	    bzr clean-tree --force --ignored
-	    bzr revert
-	    bzr pull --overwrite
-	else
-	    bzr branch -r tag:Percona-Server-$PS_55_VERSION \
-		lp:percona-server $branch_dir
-	    cd $branch_dir
-	fi
-
-	$MAKE_CMD PERCONA_SERVER=Percona-Server-5.5 main
-	cd $top_dir
-	rm -rf $server_dir
-	ln -s $branch_dir/Percona-Server $server_dir
-
-	# Patch Percona Server
-	cd $server_dir
-	patch -p1 < $top_dir/patches/xtradb55.patch
-
-	build_server $type
-
-	build_xtrabackup
-
-	;;
 *)
 	usage
 	;;
