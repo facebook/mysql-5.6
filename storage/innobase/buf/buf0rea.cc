07ee2aa7f8b87a4161908b6bd8e3517462b39457
Rongrong Zhong <rongrong@fb.com>
2013-03-27 16:25:38 -0700

    Merge aio page read requests
    Summary:
    Tries to submit multiple aio page read requests together to improve read
    performance.
    
    This code adds an array to buffer aio requests in os_aio_array_t. So far
    only os_aio_read_array uses it. A new parameter (should_buffer) is added
    to indicate whether an aio request should be buffered or submitted. If
    should_submit is true, it will submit all bufferred aio requests on the
    os_aio_array.
    
    Only buf_read_ahead_linear is modified to utilize this functionality so
    far. All other call sites are setting should_submit to true. Other
    os_aio_array_t arrays will also ignore this.
    
    If one thread calling buf_read_ahead_linear is buffering io requests but
    another thread issues a normal os_aio_request, that other request will
    submit all the buffered requests from buf_read_ahead_linear. This is
    still better than nothing I suppose.
    
    Test Plan:
    Perf tests were run manually and approved by Yoshinori.
    
    Reviewers: steaphan, jtolmer, yoshinori, mcallaghan
    
    Reviewed By: steaphan, nizamordulu

--- storage/innobase/buf/buf0rea.cc
+++ storage/innobase/buf/buf0rea.cc
@@ -120,7 +120,12 @@ buf_read_page_low(
 			use to stop dangling page reads from a tablespace
 			which we have DISCARDed + IMPORTed back */
 	ulint	offset,	/*!< in: page number */
-	trx_t*	trx)
+	trx_t*	trx,
+	ibool	should_buffer)	/*!< in: whether to buffer an aio request.
+				AIO read ahead uses this. If you plan to
+				use this parameter, make sure you remember
+				to call os_aio_linux_dispatch_read_array_submit
+				when you are read to commit all your requests.*/
 {
 	buf_page_t*	bpage;
 	ulint		wake_later;
@@ -187,7 +192,8 @@ buf_read_page_low(
 			      | ignore_nonexistent_pages,
 			      sync, space, zip_size, offset, 0, zip_size,
 			      bpage->zip.data, bpage,
-			      trx ? &trx->table_io_perf : NULL);
+			      trx ? &trx->table_io_perf : NULL,
+			      should_buffer);
 	} else {
 		ut_a(buf_page_get_state(bpage) == BUF_BLOCK_FILE_PAGE);
 
@@ -195,7 +201,8 @@ buf_read_page_low(
 			      | ignore_nonexistent_pages,
 			      sync, space, 0, offset, 0, UNIV_PAGE_SIZE,
 			      ((buf_block_t*) bpage)->frame, bpage,
-			      trx ? &trx->table_io_perf : NULL);
+			      trx ? &trx->table_io_perf : NULL,
+			      should_buffer);
 	}
 
 	if (sync) {
@@ -350,7 +357,7 @@ read_ahead:
 				&err, FALSE,
 				ibuf_mode | OS_AIO_SIMULATED_WAKE_LATER,
 				space, zip_size, FALSE,
-				tablespace_version, i, trx);
+				tablespace_version, i, trx, FALSE);
 			if (err == DB_TABLESPACE_DELETED) {
 				ut_print_timestamp(stderr);
 				fprintf(stderr,
@@ -414,7 +421,7 @@ buf_read_page(
 
 	count = buf_read_page_low(&err, TRUE, BUF_READ_ANY_PAGE, space,
 				  zip_size, FALSE,
-				  tablespace_version, offset, trx);
+				  tablespace_version, offset, trx, FALSE);
 	srv_stats.buf_pool_reads.add(count);
 	if (err == DB_TABLESPACE_DELETED) {
 		ut_print_timestamp(stderr);
@@ -462,7 +469,7 @@ buf_read_page_async(
 				  | OS_AIO_SIMULATED_WAKE_LATER
 				  | BUF_READ_IGNORE_NONEXISTENT_PAGES,
 				  space, zip_size, FALSE,
-				  tablespace_version, offset, NULL);
+				  tablespace_version, offset, NULL, FALSE);
 	srv_stats.buf_pool_reads.add(count);
 
 	/* We do not increment number of I/O operations used for LRU policy
@@ -717,13 +724,12 @@ buf_read_ahead_linear(
 	for (i = low; i < high; i++) {
 		/* It is only sensible to do read-ahead in the non-sync
 		aio mode: hence FALSE as the first parameter */
-
 		if (!ibuf_bitmap_page(zip_size, i)) {
 			count += buf_read_page_low(
 				&err, FALSE,
 				ibuf_mode,
 				space, zip_size, FALSE, tablespace_version, i,
-				trx);
+				trx, TRUE);
 			if (err == DB_TABLESPACE_DELETED) {
 				ut_print_timestamp(stderr);
 				fprintf(stderr,
@@ -736,6 +742,10 @@ buf_read_ahead_linear(
 			}
 		}
 	}
+#if defined(LINUX_NATIVE_AIO)
+	/* Tell aio to submit all buffered requests. */
+	ut_a(os_aio_linux_dispatch_read_array_submit());
+#endif
 
 	/* In simulated aio we wake the aio handler threads only after
 	queuing all aio requests, in native aio the following call does
@@ -813,7 +823,7 @@ buf_read_ibuf_merge_pages(
 		buf_read_page_low(&err, sync && (i + 1 == n_stored),
 				  BUF_READ_ANY_PAGE, space_ids[i],
 				  zip_size, TRUE, space_versions[i],
-				  page_nos[i], NULL);
+				  page_nos[i], NULL, FALSE);
 
 		if (UNIV_UNLIKELY(err == DB_TABLESPACE_DELETED)) {
 tablespace_deleted:
@@ -908,13 +918,13 @@ buf_read_recv_pages(
 		if ((i + 1 == n_stored) && sync) {
 			buf_read_page_low(&err, TRUE, BUF_READ_ANY_PAGE, space,
 					  zip_size, TRUE, tablespace_version,
-					  page_nos[i], NULL);
+					  page_nos[i], NULL, FALSE);
 		} else {
 			buf_read_page_low(&err, FALSE, BUF_READ_ANY_PAGE
 					  | OS_AIO_SIMULATED_WAKE_LATER,
 					  space, zip_size, TRUE,
 					  tablespace_version, page_nos[i],
-					  NULL);
+					  NULL, FALSE);
 		}
 	}
 
