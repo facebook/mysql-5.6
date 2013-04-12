265e51d551f4ce1902689409cd5ab253ffc91f29
Rongrong Zhong <rongrong@fb.com>
2013-04-12 15:04:44 -0700

    Port Stats: Slow IOs to 5.6
    Summary:
    Uses "slow_ios" in place of "old_ios" in SHOW STATUS, SHOW INNODB STATUS
    and IS tables because the results are for slow IOs as defined here.
    
    * slow IOs are requests for which the service time is too long
      (innodb_aio_slow_usecs)
    
    * old IOs are requests that wait too long to be serviced
      (innodb_aio_old_usecs). This is only possible for async reads and
      async writes.
    
    Adds the my.cnf variable innodb_aio_slow_usecs. File reads, writes and
    fsyncs that take longer than this are counted as "slow_ios".
    
    Changes the default value for innodb_aio_old_usecs from 2 seconds to 0.5
    seconds.
    
    Adds SHOW STATUS counters:
    * data_fsync_max_seconds - slowest fsync for InnoDB
    * data_fsync_slow - number of slow fsyncs
    
    These count the number of operations that are slow:
    * data_async_read_slow_ios
    * data_sync_read_slow_ios
    * data_async_write_slow_ios
    * data_sync_write_slow_ios
    * data_log_write_slow_ios
    * data_double_write_slow_ios
    
    These count the number of operations that wait too long in request
    queues for background IO threads:
    * data_async_read_old_ios
    * data_async_write_old_ios
    
    Internal:
        Porting from 5.1 change:
          Wed May 16 10:08:40 2012 (161f6f80f03a70221dd03db6a504f25e8a3fedaa) Mark Callaghan:  Improve IO stats for InnoDB
            https://phabricator.fb.com/D473569
    
        ** Also correcting os_aio_slot_t.age from double to ulonglong.
    
    Test Plan: mysqltest.sh
    
    Reviewers: steaphan
    
    Reviewed By: steaphan
    
    Differential Revision: https://phabricator.fb.com/D618968
    
    Task ID: 1817492
    (cherry picked from commit 4e26d37aec33a11d934ce7c9dcbac7b46053fcb3)
    
    Conflicts:
    
    	storage/innobase/os/os0file.cc
    
    Conflicts:
    	sql/table_stats.cc

--- storage/innobase/srv/srv0srv.cc
+++ storage/innobase/srv/srv0srv.cc
@@ -347,6 +347,13 @@ UNIV_INTERN ulint	srv_lock_deadlocks	= 0;
 /** Number of row lock wait timeouts */
 UNIV_INTERN ulint	srv_lock_wait_timeouts	= 0;
 
+/** Count as "slow" file read, write and fsync requests that take this long */
+UNIV_INTERN ulint	srv_io_slow_usecs	= 0;
+
+/** Count as "old" file read and write requests that wait this long in bg arrays 
+before getting scheduled */
+UNIV_INTERN ulint	srv_io_old_usecs	= 0;
+
 /** Number of extra writes done in buf_flush_try_neighbors from LRU list */
 UNIV_INTERN ulint	srv_neighbors_flushed_lru	= 0;
 
@@ -1439,6 +1446,10 @@ srv_export_innodb_status(void)
 
 	export_vars.innodb_data_fsync_time = os_file_flush_time;
 
+	export_vars.innodb_data_fsync_slow = os_fsync_too_slow;
+
+	export_vars.innodb_data_fsync_max_time = os_fsync_max_time;
+
 	export_vars.innodb_data_read = srv_stats.data_read;
 
 	export_vars.innodb_data_reads = os_n_file_reads;
@@ -1455,6 +1466,12 @@ srv_export_innodb_status(void)
 	export_vars.innodb_data_async_read_svc_time =
 		os_async_read_perf.svc_time ;
 
+	export_vars.innodb_data_async_read_old_ios =
+		os_async_read_old_ios;
+
+	export_vars.innodb_data_async_read_slow_ios =
+		os_async_read_perf.slow_ios;
+
 	export_vars.innodb_data_sync_read_bytes = os_sync_read_perf.bytes;
 
 	export_vars.innodb_data_sync_read_requests =
@@ -1462,6 +1479,9 @@ srv_export_innodb_status(void)
 
 	export_vars.innodb_data_sync_read_svc_time =
 		os_sync_read_perf.svc_time;
+
+	export_vars.innodb_data_sync_read_slow_ios =
+		os_sync_read_perf.slow_ios;
 
 	export_vars.innodb_data_async_write_bytes = os_async_write_perf.bytes;
 
@@ -1471,6 +1491,12 @@ srv_export_innodb_status(void)
 	export_vars.innodb_data_async_write_svc_time =
 		os_async_write_perf.svc_time;
 
+	export_vars.innodb_data_async_write_old_ios =
+		os_async_write_old_ios;
+
+	export_vars.innodb_data_async_write_slow_ios =
+		os_async_write_perf.slow_ios;
+
 	export_vars.innodb_data_sync_write_bytes = os_sync_write_perf.bytes;
 
 	export_vars.innodb_data_sync_write_requests =
@@ -1479,11 +1505,16 @@ srv_export_innodb_status(void)
 	export_vars.innodb_data_sync_write_svc_time =
 		os_sync_write_perf.svc_time;
 
+	export_vars.innodb_data_sync_write_slow_ios =
+		os_sync_write_perf.slow_ios;
+
 	export_vars.innodb_data_log_write_bytes = os_log_write_perf.bytes;
 
 	export_vars.innodb_data_log_write_requests = os_log_write_perf.requests;
 
 	export_vars.innodb_data_log_write_svc_time = os_log_write_perf.svc_time;
+
+	export_vars.innodb_data_log_write_slow_ios = os_log_write_perf.slow_ios;
 
 	export_vars.innodb_data_double_write_bytes = os_double_write_perf.bytes;
 
@@ -1492,6 +1523,9 @@ srv_export_innodb_status(void)
 
 	export_vars.innodb_data_double_write_svc_time =
 		os_double_write_perf.svc_time;
+
+	export_vars.innodb_data_double_write_slow_ios=
+		os_double_write_perf.slow_ios;
 
 	export_vars.innodb_buffer_pool_flushed_lru = stat.n_flushed_lru;
 
