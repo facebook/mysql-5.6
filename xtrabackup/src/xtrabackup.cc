0e55b8beaf83a94d9d3c701dc4ab80447240a9fa
Rudradev Basak <rudradevbasak@fb.com>
2013-05-10 12:38:25 -0700

    Fix error message segfault in xtrabackup.
    Summary:
    Initialize error messages for xtrabackup.
    
    Also, do not use thread-specific locales for error messages.
    
    Without this, any access to error messages will cause a segfault.
    
    Test Plan:
    Artificially threw some warnings in xtrabackup.
    
    Successfully carried out a full backup with warning (segfaulted before).
    
    Reviewers: steaphan
    
    Reviewed By: steaphan

--- xtrabackup/src/xtrabackup.cc
+++ xtrabackup/src/xtrabackup.cc
@@ -102,6 +102,8 @@ extern "C" {
 #include <fcntl.h>
 #include <buf0flu.h>
 #include <buf0lru.h>
+#include <sql_locale.h>
+#include <derror.h>
 
 #ifdef INNODB_VERSION_SHORT
 #include <ibuf0ibuf.h>
@@ -2456,6 +2458,9 @@ innodb_init_param(void)
 
 	/* dummy for initialize all_charsets[] */
 	get_charset_name(0);
+
+	my_default_lc_messages = my_locale_by_name("en_US");
+	init_errmessage();
 
 #ifdef XTRADB_BASED
 	srv_page_size = 0;
