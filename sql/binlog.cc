330b4e19ae0217d2427538e113278684dafa1768
Santosh Praneeth Banda <santoshb@fb.com>
2013-05-15 19:31:32 -0700

    Raise log level for ER_BINLOG_UNSAFE_STATEMENT
    Summary:
    Currently ER_BINLOG_UNSAFE_STATEMENT warnings are written to
    error log if log_warnings level is >=1. This is changed so that the
    warnings are written only if log_warnings>=2.
    
    Test Plan: mtr test with an unsafe statement replicated in slave.
    
    Reviewers: jtolmer
    
    Reviewed By: jtolmer

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -9031,7 +9031,7 @@ void THD::issue_unsafe_warnings()
                           ER_BINLOG_UNSAFE_STATEMENT,
                           ER(ER_BINLOG_UNSAFE_STATEMENT),
                           ER(LEX::binlog_stmt_unsafe_errcode[unsafe_type]));
-      if (log_warnings)
+      if (log_warnings >= 2)
       {
         if (unsafe_type == LEX::BINLOG_STMT_UNSAFE_LIMIT)
           do_unsafe_limit_checkout( buf, unsafe_type, query());
