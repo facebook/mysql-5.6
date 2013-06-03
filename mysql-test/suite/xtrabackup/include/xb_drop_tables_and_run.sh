a57575aa72b5369292dc7b6ca6d220e685a6341c
Rudradev Basak <rudradevbasak@fb.com>
2013-06-03 13:47:11 -0700

    Add fix for deleted tablespaces in xtrabackup apply log
    Summary:
    Without this fix xtrabackup apply log may hang at the end, if it failed
    to open some .ibd file in the past.
    
    This patch was part of the old innodb51.patch supplied by Percona, but
    for some weird reason it is not part of their current innodb56.patch on
    which we have based our patches.
    
    Test Plan:
    Added test where table is modified and then dropped during backup.
    Succeeds with this patch, fails otherwise.
    
    Reviewers: steaphan
    
    Reviewed By: steaphan

---
+++ mysql-test/suite/xtrabackup/include/xb_drop_tables_and_run.sh
@@ -0,0 +1,7 @@
+# Initially creates a lot of tables.
+# Then start modifying and dropping those tables
+# while simultaneously running a backup
+
+suite/xtrabackup/include/xb_create_tables.sh $1 2>&1
+suite/xtrabackup/include/xb_modify_and_drop_tables.sh $1 2>&1 &
+suite/xtrabackup/include/xb_run.sh 2>&1
