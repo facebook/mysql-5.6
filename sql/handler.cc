c3ddf5e7ffc2d0d88a40a806d26229e515b9ca38
Rongrong Zhong <rongrong@fb.com>
2013-07-15 18:39:48 -0700

    alter table xx defragment command
    Summary:
    Add parser support for defragment command.
    This only includes parser support for "alter table defragment", actual
    functionality is not included in this diff.
    Only works for innodb tables, doesn't work for partitioned table or
    other storage engines.
    The command is not replicated.
    
    Test Plan:
    Jenkins
    Added test doesn't do much other than confirm the new command is valid.
    
    Reviewers: steaphan, santoshb, nizamordulu
    
    Reviewed By: steaphan

--- sql/handler.cc
+++ sql/handler.cc
@@ -4660,6 +4660,21 @@ handler::ha_drop_table(const char *name)
   return drop_table(name);
 }
 
+/**
+  Defragment table in the engine: public interface.
+
+  @sa handler::defragment_table()
+*/
+
+int
+handler::ha_defragment_table(const char *name)
+{
+  DBUG_ASSERT(m_lock_type == F_UNLCK);
+  mark_trx_read_write();
+
+  return defragment_table(name);
+}
+
 
 /**
   Create a table in the engine: public interface.
