4a65e9d9af31020dccd85c85b07b234ebe7a0093
Santosh Praneeth Banda <santoshb@fb.com>
2013-09-11 18:37:25 -0700

    use rli->is_fatal_error instead of thd->is_fatal_error
    Summary:
    While sql_thread is executing transactions, sometimes
    thd->is_fatal_error may get set even though the error is
    actually lock wait timeout. Avoid this behaviour by using
    ignoring thd->is_fatal_error in slave transaction
    retry logic. The fatal_error flag is getting set in
      function mysql_update() {
      ......
      if (table->file->is_fatal_error(error, HA_CHECK_DUP_KEY))
        flags|= ME_FATALERROR; /* Other handler errors are fatal */
      .....
      }
    
      virtual bool is_fatal_error(int error, uint flags)
      {
        if (!handler::is_fatal_error(error, flags) ||
            error == HA_ERR_NO_PARTITION_FOUND ||
            error == HA_ERR_NOT_IN_LOCK_PARTITIONS)
          return FALSE;
        return TRUE;
      }
    
      virtual bool is_fatal_error(int error, uint flags)
      {
        if (!error ||
           ((flags & HA_CHECK_DUP_KEY) &&
           (error == HA_ERR_FOUND_DUPP_KEY ||
           error == HA_ERR_FOUND_DUPP_UNIQUE)))
           return FALSE;
        return TRUE;
      }
    
    The thd->is_fatal_error is not checked in mysql 5.1 which was working fine.
    This diff uses a new flag rli->is_fatal_error instead of global
    thd->is_fatal_error to set correct sql_thread error.
    
    Link for public bug report http://bugs.mysql.com/bug.php?id=70318
    
    Test Plan: Tested the rpm on a admarketDb machine
    
    Reviewers: steaphan, yoshinori
    
    Reviewed By: steaphan

--- sql/rpl_slave.cc
+++ sql/rpl_slave.cc
@@ -4473,6 +4473,7 @@ static int exec_relay_log_event(THD* thd, Relay_log_info* rli)
         else
         {
           thd->is_fatal_error= 1;
+          rli->set_fatal_error();
           rli->report(ERROR_LEVEL, thd->get_stmt_da()->sql_errno(),
                       "Slave SQL thread retried transaction %lu time(s) "
                       "in vain, giving up. Consider raising the value of "
