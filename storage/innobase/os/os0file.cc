b1b4c7977136d57170f8bf500aaedba740b1c333
Steaphan Greene <steaphan@fb.com>
2013-10-11 09:48:35 -0700

    Fix typo in some comments.
    Summary:
    This was a typo in an earlier diff, which was then copied a bunch of
    times in a later diff.
    
    Test Plan: Changes to comments only.  No real code changes.
    
    Reviewers: jtolmer
    
    Reviewed By: jtolmer

--- storage/innobase/os/os0file.cc
+++ storage/innobase/os/os0file.cc
@@ -4853,7 +4853,7 @@ os_aio_func(
 				AIO read ahead uses this. If you plan to
 				use this parameter, make sure you remember
 				to call os_aio_linux_dispatch_read_array_submit
-				when you are read to commit all your requests.*/
+				when you're ready to commit all your requests.*/
 
 {
 	os_aio_array_t*	array;
