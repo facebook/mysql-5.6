e5291d87da90f8081a70403fa933ecde1881af74
Rongrong Zhong <rongrong@fb.com>
2013-11-04 11:02:58 -0800

    Remove stats histograms
    Summary:
    Revert stat histogram diffs - these need to be redone.
    
    Test Plan: arc unit
    
    Reviewers: steaphan
    
    Reviewed By: steaphan

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -66,9 +66,6 @@ bool opt_binlog_order_commits= true;
 const char *log_bin_index= 0;
 const char *log_bin_basename= 0;
 
-char *histogram_step_size_binlog_fsync = NULL;
-latency_histogram histogram_binlog_fsync;
-
 MYSQL_BIN_LOG mysql_bin_log(&sync_binlog_period);
 
 static int binlog_init(void *p);
@@ -811,9 +808,6 @@ static int binlog_init(void *p)
   binlog_hton->rollback= binlog_rollback;
   binlog_hton->prepare= binlog_prepare;
   binlog_hton->flags= HTON_NOT_USER_SELECTABLE | HTON_HIDDEN;
-
-  latency_histogram_init(&histogram_binlog_fsync,
-                         histogram_step_size_binlog_fsync);
   return 0;
 }
 
@@ -6785,23 +6779,15 @@ std::pair<bool, bool>
 MYSQL_BIN_LOG::sync_binlog_file(bool force, bool async)
 {
   bool synced= false;
-  ulonglong start_time, binlog_fsync_time;
   unsigned int sync_period= get_sync_period();
   if (force || (!async && (sync_period && ++sync_counter >= sync_period)))
   {
     sync_counter= 0;
     statistic_increment(binlog_fsync_count, &LOCK_status);
-    start_time = my_timer_now();
-    int ret = mysql_file_sync(log_file.file, MYF(MY_WME));
-    binlog_fsync_time = my_timer_since(start_time);
-    if (histogram_step_size_binlog_fsync)
-      latency_histogram_increment(&histogram_binlog_fsync,
-                                  binlog_fsync_time, 1);
-    if (ret)  {
+    if (mysql_file_sync(log_file.file, MYF(MY_WME)))
       return std::make_pair(true, synced);
-    }
     synced= true;
-   }
+  }
   return std::make_pair(false, synced);
 }
 
