5b383a8acc5af2a94111603a57f1a0cab9fd3dcf
Nizameddin Ordulu <nizamordulu@fb.com>
2013-11-25 01:02:24 -0800

    Unify the interface for compression functions.
    Summary:
    Instead of having functions page_zip_compress_zlib, page_zip_compress_bzip
    etc. we create a new interface that takes the compression type as a
    parameter.
    
    The new interface consists of comp_compress(), comp_decompress(),
    comp_mem_compress(), and, comp_mem_decompress(). The former two are used for
    compression and decompression while the latter to are used for getting the
    memory requirements for these operations. The memory functions make the memory
    computations in page0zip.cc simpler.
    
    I also realized that the table_flags in page_zip_compress() was not handled
    properly because it was actually space flags. This caused page_zip_compress()
    to always use ZLIB_STREAM as the compression type which means the tests never
    tested the new compression. I fixed this by getting getting the compression
    parameters using FSP_GET_* functions.
    
    Test Plan: mtr
    
    Reviewers: rongrong, herman
    
    Reviewed By: rongrong

--- storage/innobase/comp/comp0zlib.cc
+++ storage/innobase/comp/comp0zlib.cc
@@ -1,74 +1,104 @@
-#include "comp0zlib.h"
 #include "zlib_embedded/zlib.h"
-#include <assert.h>
+#include "comp0types.h"
 
-int comp_zlib_compress(
-	const unsigned char* in, /* input buffer with data to be compressed */
-	unsigned int avail_in, /* size of the input buffer */
-	unsigned char* out, /* output buffer */
-	unsigned int avail_out, /* available space in the output buffer */
-	unsigned long* total_out, /* size of the compressed output */
-	void* (*zalloc) (void*, unsigned int, unsigned int), /* used to allocate
-								the internal
-								state */
-	void (*zfree) (void*, void*), /* used to free the internal state */
-	void* opaque, /* private data object passed to zalloc and zfree */
-	int level, /* see zlib manual */
-	int window_bits, /* see zlib.h */
-	int strategy) /* see zlib.h */
+/* Below macros are used to calculate the memory requirement for zlib's
+compression/decompression streams */
+#define DEFLATE_MEMORY_BOUND(windowBits, memLevel) \
+    ((4UL << (windowBits)) + (512UL << (memLevel)) + (128UL << 10))
+
+#define INFLATE_MEMORY_BOUND(windowBits) \
+    ((1UL << (windowBits)) + (128UL << 10))
+
+/**********************************************************************//**
+Allocate memory for zlib. */
+static
+void*
+comp_zlib_alloc(
+/*============*/
+	void* opaque, /*!< in/out: memory heap */
+	uInt items, /*!< in: number of items to allocate */
+	uInt size) /*!< in: size of an item in bytes */
+{
+	void* m = mem_heap_alloc(static_cast<mem_heap_t*>(opaque),
+				 items * size);
+	UNIV_MEM_VALID(m, items * size);
+	return m;
+}
+
+/**********************************************************************//**
+Free memory function which does nothing. */
+static
+void
+comp_zlib_free(
+	void* opaque __attribute__((unused)), /*!< in: memory heap */
+	void* address __attribute__((unused))) /*!< in: object to free */
+{
+}
+/*****************************************************************************
+Compress the contents of the buffer comp_state->in into the output buffer
+comp_state->out using zlib. Update the avail_in, avail_out fields of comp_state
+after compression.
+@return 1 on success 0 on failure. */
+int
+comp_zlib_compress(
+	comp_state_t* comp_state) /* in: in, avail_in, avail_out, level.
+				     out: out, avail_out */
 {
 	int err;
 	z_stream c_stream;
-	c_stream.next_in = (unsigned char*)in;
-	c_stream.avail_in = avail_in;
-	c_stream.next_out = out;
-	c_stream.avail_out = avail_out;
-	c_stream.zalloc = zalloc;
-	c_stream.zfree = zfree;
-	c_stream.opaque = opaque;
-	err = deflateInit2(&c_stream, level,
-			   Z_DEFLATED, window_bits,
-			   MAX_MEM_LEVEL, strategy);
-	assert(err == Z_OK);
+	c_stream.next_in = comp_state->in;
+	c_stream.avail_in = comp_state->avail_in;
+	c_stream.next_out = comp_state->out;
+	c_stream.avail_out = comp_state->avail_out;
+	c_stream.zalloc = comp_zlib_alloc;
+	c_stream.zfree = comp_zlib_free;
+	c_stream.opaque = comp_state->heap;
+	err = deflateInit2(&c_stream, comp_state->level,
+			   Z_DEFLATED, -(int)UNIV_PAGE_SIZE_SHIFT,
+			   MAX_MEM_LEVEL, Z_DEFAULT_STRATEGY);
+	ut_a(err == Z_OK);
 	err = deflate(&c_stream, Z_FINISH);
-	assert(Z_OK == deflateEnd(&c_stream));
-	*total_out = c_stream.total_out;
+	ut_a(Z_OK == deflateEnd(&c_stream));
+	ut_a(c_stream.avail_out + c_stream.total_out == comp_state->avail_out);
+	ut_a(err != Z_STREAM_END || c_stream.avail_in == 0);
+	comp_state->avail_out = c_stream.avail_out;
+	comp_state->avail_in = c_stream.avail_in;
 	return (err == Z_STREAM_END);
 }
-
-int comp_zlib_decompress(
-	const unsigned char* in, /* input buffer with compressed data */
-	unsigned int avail_in, /* the length of the input buffer */
-	unsigned char* out, /* output buffer */
-	unsigned int avail_out, /* available space in the output buffer */
-	unsigned long* total_in, /* size of the input buffer used for
-				    decompression */
-	void* (*zalloc) (void*, unsigned int, unsigned int), /* used to allocate
-								the internal
-								state */
-	void (*zfree) (void*, void*), /* used to free the internal state */
-	void* opaque, /* private data object passed to zalloc and zfree */
-	int window_bits) /* see zlib.h */
+/*****************************************************************************
+Decompress the contents of the buffer comp_state->in into the output buffer
+comp_state->out using zlib. Update the avail_in, avail_out fields of comp_state
+after decompression. Failure of decompression means page corruption or a bug so
+this function does not have a return value. */
+void
+comp_zlib_decompress(
+	comp_state_t* comp_state) /* in: in, avail_in, avail_out, level.
+				     out: out, avail_out, avail_in */
 {
 	z_stream d_stream;
-	int err;
-	d_stream.next_in = (unsigned char*)in;
-	d_stream.avail_in = avail_in;
-	d_stream.next_out = out;
-	d_stream.avail_out = avail_out;
-	d_stream.zalloc = zalloc;
-	d_stream.zfree = zfree;
-	d_stream.opaque = opaque;
-	err = inflateInit2(&d_stream, window_bits);
-	assert(err == Z_OK);
-	err = inflate(&d_stream, Z_FINISH);
-	if (err != Z_STREAM_END) {
-		err = inflateEnd(&d_stream);
-		assert(err == Z_OK);
-		return 0;
-	}
-	err = inflateEnd(&d_stream);
-	assert(err == Z_OK);
-	*total_in = d_stream.total_in;
-	return 1;
+	d_stream.next_in = comp_state->in;
+	d_stream.avail_in = comp_state->avail_in;
+	d_stream.next_out = comp_state->out;
+	d_stream.avail_out = comp_state->avail_out;
+	d_stream.zalloc = comp_zlib_alloc;
+	d_stream.zfree = comp_zlib_free;
+	d_stream.opaque = comp_state->heap;
+	ut_a(Z_OK == inflateInit2(&d_stream, -(int)UNIV_PAGE_SIZE_SHIFT));
+	ut_a(Z_STREAM_END  == inflate(&d_stream, Z_FINISH));
+	ut_a(Z_OK == inflateEnd(&d_stream));
+	ut_a(d_stream.avail_in + d_stream.total_in == comp_state->avail_in);
+	comp_state->avail_in = d_stream.avail_in;
+	comp_state->avail_out = d_stream.avail_out;
 }
+
+/* For additional memory needed by zlib see zlib manual
+   http://www.zlib.net/manual.html */
+static comp_interface_t comp_zlib_obj = {
+	comp_zlib_compress,
+	comp_zlib_decompress,
+	DEFLATE_MEMORY_BOUND(UNIV_PAGE_SIZE_SHIFT,
+			     MAX_MEM_LEVEL),
+	INFLATE_MEMORY_BOUND(UNIV_PAGE_SIZE_SHIFT)
+};
+
+comp_interface_t* comp_zlib = &comp_zlib_obj;
