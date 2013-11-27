426f92233c20af935b4d20a48b07747fd4c71e5e
Nizameddin Ordulu <nizamordulu@fb.com>
2013-11-26 21:43:39 -0800

    Add LZ4 Compression for InnoDB.
    Summary:
    Make COMPRESSION=LZ4 work.
    Test Plan: mtr
    
    Reviewers: rongrong, herman
    
    Reviewed By: rongrong

---
+++ storage/innobase/include/lz4_embedded/lz4_prefix.h
@@ -0,0 +1,7 @@
+#ifndef lz4_prefix_h
+#define lz4_prefix_h
+#define LZ4_compress_limitedOutput innobase_LZ4_compress_limitedOutput
+#define LZ4_uncompress_unknownOutputSize \
+		innobase_LZ4_uncompress_unknownOutputSize
+#define LZ4_compressHC innobase_LZ4_compressHC
+#endif /* lz4_prefix_h */
