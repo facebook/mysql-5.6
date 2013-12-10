e5efe884adaab74b567095d8de589fad1b9862e7
Rudradev Basak <rudradevbasak@fb.com>
2013-12-09 16:16:36 -0800

    Disable ibuf operation during crash recovery
    Summary:
    Turn off ibuf operations by default during crash recovery.
    
    Add a sys var that can turn it back on if needed.
    
    Test Plan:
    Run mysql crash recovery under gdb to check that ibuf operations aren't
    being allowed. Also check with MTR crash tests.
    
    Reviewers: steaphan, nizamordulu
    
    Reviewed By: steaphan
    
    Differential Revision: https://reviews.facebook.net/D47757

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -353,6 +353,7 @@ extern ulong opt_peak_lag_sample_rate;
 extern ulong relay_io_events, relay_sql_events;
 extern ulonglong relay_io_bytes, relay_sql_bytes;
 extern ulonglong relay_sql_wait_time;
+extern my_bool recv_skip_ibuf_operations;
 
 /* SHOW STATS var: Name of current timer */
 extern const char *timer_in_use;
