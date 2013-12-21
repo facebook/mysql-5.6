83587185f15fb291ca095f3539b5ec76ea2b472c
Santosh Praneeth Banda <santoshb@fb.com>
2013-12-20 20:35:28 -0800

    slave transaction retries for MTS
    Summary:
    Coordinator thread enqueus events read from the relay log to the slave
    worker's jobs queue. Worker thread dequeus each element, executes the
    event and deletes the event. This makes slave_transaction_retries do not
    work with MTS, since there is no way of knowing all the events that were
    executed in the current group. The worker thread functionality is
    changed so that it dequeues only when the current transaction is
    successfully committed. Worker thread just reads events starting from the
    head of the jobs queue storing an index which is increased on every read
    from the queue. The index is reset to 0 when worker thread encounters a
    temporary error or when the current transaction is successfully
    committed.
    
    slave_transaction_retries is not supported in scenarios where
    coordinator thread waits for worker thread to release some memory issues
    (either if queue is full or if all the worker queues are using
    slave_pending_jobs_size_max memory). In these cases executed events in
    the current group are dequeued to signal coordinator thread.
    
    Test Plan:
    Simulated lock wait timeout error in slave and verified transactions are
    retried by the worker thread. rpl_parallel_conf_limits tests MTS
    execution when coordinator waits for worker thread due to memory issues
    (This test verifies the second scenario where slave_transaction_retries
    is not supported).
    
    Reviewers: steaphan
    
    Reviewed By: steaphan

--- sql/rpl_rli_pdb.h
+++ sql/rpl_rli_pdb.h
@@ -302,6 +302,12 @@ public:
   virtual ~Slave_worker();
 
   Slave_jobs_queue jobs;   // assignment queue containing events to execute
+  ulong current_event_index; // index of the event in current group
+  // number of events executed in the current group before temporary error.
+  // last_current_event_index is the maximum of current_event_index over
+  // several runs of same transaction or group.
+  ulong last_current_event_index;
+  ulong trans_retries;  // transaction retries count by this slave worker
   mysql_mutex_t jobs_lock; // mutex for the jobs queue
   mysql_cond_t  jobs_cond; // condition variable for the jobs queue
   Relay_log_info *c_rli;   // pointer to Coordinator's rli
@@ -373,7 +379,7 @@ public:
   int rli_init_info(bool);
   int flush_info(bool force= FALSE);
   static size_t get_number_worker_fields();
-  void slave_worker_ends_group(Log_event*, int);
+  void slave_worker_ends_group(Log_event*, int&, bool&);
   const char *get_master_log_name();
   ulonglong get_master_log_pos() { return master_log_pos; };
   ulonglong set_master_log_pos(ulong val) { return master_log_pos= val; };
