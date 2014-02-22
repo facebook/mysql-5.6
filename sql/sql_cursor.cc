b85cc08949c775426902fa26858f8ec2daa7e7d0
Rongrong Zhong <rongrong@fb.com>
2014-02-22 15:18:59 -0800

    Port v5.1 IS.User_Stats to 5.6
    Summary:
    Port of all of our 5.1 user stats diffs.
    
    Table columns:
        USER_NAME
        // data stats
        BINLOG_BYTES_WRITTEN - number of bytes written to the binlo
        BYTES_RECEIVED, BYTES_SENT - number of network bytes received or sent
        // commands count
        COMMANDS_DDL - number of DDL commands
        COMMANDS_DELETE - number of DELETE and TRUNCATE commands
        COMMANDS_HANDLER - number of HANDLER
        COMMANDS_INSERT - number of INSERT, INSERT SELECT, LOAD and REPLACE commands
        COMMANDS_OTHER - number of other commands
        COMMANDS_SELECT - number of SELECT commands
        COMMANDS_TRANSACTION - number of BEGIN, COMMIT, ROLLBACK commands
        COMMANDS_UPDATE - number of UPDATE commands
        // connections
        CONNECTIONS_CONCURRENT - concurrent connections for this user
        CONNECTIONS_DENIED_MAX_GLOBAL - number of connections denied because
            the global connection limit was reached
        CONNECTIONS_DENIED_MAX_USER -number of connections denied because
            the per user limit was reached
        CONNECTIONS_LOST - number of connections lost rather than properly closed
        CONNECTIONS_TOTAL - total number of connections
        // io perf
        DISK_READ_BYTES
        DISK_READ_REQUESTS
        DISK_READ_SVC_USECS
        DISK_READ_WAIT_USECS
        DISK_READ_BYTES_BLOB
        DISK_READ_REQUESTS_BLOB
        DISK_READ_SVC_USECS_BLOB
        DISK_READ_WAIT_USECS_BLOB
        // error counts
        ERRORS_ACCESS_DENIED - number of access denied errors
        ERRORS_TOTAL
        // timers
        MICROSECONDS_WALL - number of wallclock seconds running commands
        MICROSECONDS_DDL
        MICROSECONDS_DELETE
        MICROSECONDS_HANDLER
        MICROSECONDS_INSERT
        MICROSECONDS_OTHER
        MICROSECONDS_SELECT
        MICROSECONDS_TRANSACTION
        MICROSECONDS_UPDATE
        // empty query
        QUERIES_EMPTY - number of queries that return 0 rows
        // row stats
        ROWS_DELETED - number of rows deleted
        ROWS_FETCHED - number of rows sent back to a user
        ROWS_INSERTED - number of rows inserted
        ROWS_READ - number of rows read from a table
        ROWS_UPDATED - number of rows updated
        ROWS_INDEX_FIRST
        ROWS_INDEX_NEXT
        // transaction stats
        TRANSACTIONS_COMMIT - number of commits, this includes auto-commit
        TRANSACTIONS_ROLLBACK number of rollbacks
    
    Tests added:
        user_stats_bytes
        user_stats_max_conns
        user_stats_rows_primary
        user_stats_rows
        user_stats_commands
        user_stats_disk
        user_stats_errors
        user_stats_more
        user_stats_rows_fetched
        user_stats_rows_secondary
        user_stats_transactions
        rpl.rpl_user_stats_rows
        rpl.rpl_user_stats_bytes
    Tests modified:
        information_schema
        sys_vars.max_user_connections_func
    Test results changed:
        information_schema_db
        mysqlshow
        funcs_1.is_columns_is
        funcs_1.is_tables_is
    
    Test Plan: mtr
    
    Reviewers: steaphan
    
    Reviewed By: steaphan

--- sql/sql_cursor.cc
+++ sql/sql_cursor.cc
@@ -105,7 +105,9 @@ bool mysql_open_cursor(THD *thd, select_result *result,
   save_result= lex->result;
 
   lex->result= result_materialize;
-
+  ulonglong start_time, last_time;
+  start_time = my_timer_now();
+  last_time = start_time;
   MYSQL_QUERY_EXEC_START(thd->query(),
                          thd->thread_id,
                          (char *) (thd->db ? thd->db : ""),
@@ -114,7 +116,7 @@ bool mysql_open_cursor(THD *thd, select_result *result,
                          2);
   parent_locker= thd->m_statement_psi;
   thd->m_statement_psi= NULL;
-  bool rc= mysql_execute_command(thd, NULL);
+  bool rc= mysql_execute_command(thd, &start_time, &last_time);
   thd->m_statement_psi= parent_locker;
   MYSQL_QUERY_EXEC_DONE(rc);
 
