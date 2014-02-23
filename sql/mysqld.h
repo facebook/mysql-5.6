b3c9d4fd68b17e96b793485ede1e2038f3f8b385
Santosh Praneeth Banda <santoshb@fb.com>
2014-02-22 17:59:45 -0800

    Port unix datagram socket from 5.1 to 5.6
    Summary:
    This implements the same functionality of unix datagram slocket of 5.0/5.1.
    
    Test Plan:
    All test cases ported, and all pass.
    
    Reviewers: steaphan, jtolmer, zshao
    
    Reviewed By: jtolmer

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -25,6 +25,10 @@
 #include "sql_list.h"                      /* I_List */
 #include "sql_cmd.h"                       /* SQLCOM_END */
 #include "my_rdtsc.h"                      /* my_timer* */
+#include "sql_priv.h"                      /* enum_var_type */
+// for unix sockets
+#include <sys/socket.h>
+#include <sys/un.h>
 #include "atomic_stat.h"
 
 class THD;
@@ -531,6 +535,11 @@ extern ulong log_warnings;
 extern uint host_cache_size;
 void init_sql_statement_names();
 
+/* Enable logging queries to a unix local datagram socket */
+extern my_bool log_datagram;
+extern ulong log_datagram_usecs;
+extern int log_datagram_sock;
+
 /* flashcache */
 extern int cachedev_fd;
 
@@ -645,6 +654,7 @@ extern PSI_socket_key key_socket_tcpip, key_socket_unix, key_socket_client_conne
 
 void init_server_psi_keys();
 #endif /* HAVE_PSI_INTERFACE */
+bool setup_datagram_socket(sys_var *self, THD *thd, enum_var_type type);
 
 /*
   MAINTAINER: Please keep this list in order, to limit merge collisions.
