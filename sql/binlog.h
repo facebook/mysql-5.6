9b721f389ee6fba6ffac5817a606ca8f14215a73
Rongrong Zhong <rongrong@fb.com>
2014-02-26 12:01:14 -0800

    Add histograms for tracking I/O and per user latencies
    Summary:
    This diff adds support for latency stat histograms.
    
    Included are 8 histograms to track I/O latencies: async reads|writes,
    sync reads|writes, log writes, double buffer writes.
    
        * histogram_async_write
        * histogram_async_read
        * histogram_sync_read
        * histogram_sync_write
        * histogram_log_write
        * histogram_double_write
        * histogram_fsync
        * histgoram_file_flush_time
        * histogram_binlog_fsync
        * histogram_connection_create
        * histogram_update_command
        * histogram_delete_command
        * histogram_insert_command
        * histogram_select_command
        * histogram_ddl_command
        * histogram_transaction_command
        * histogram_handler_command
        * histogram_other_command
    
    These vars set the step sizes of these histograms on server start up:
    
        * Innodb_histogram_step_size_async_read
        * Innodb_histogram_step_size_async_write
        * Innodb_histogram_step_size_sync_read
        * Innodb_histogram_step_size_sync_write
        * Innodb_histogram_step_size_log_write
        * Innodb_histogram_step_size_double_write
        * Innodb_histogram_step_size_fsync
        * Innodb_histogram_step_size_file_flush_time
        * Innodb_histogram_step_size_binlog_fsync
        * Histogram_step_size_connection_create
        * Histogram_step_size_update_command
        * Histogram_step_size_delete_command
        * Histogram_step_size_insert_command
        * Histogram_step_size_select_command
        * Histogram_step_size_ddl_command
        * Histogram_step_size_transaction_command
        * Histogram_step_size_handler_command
        * Histogram_step_size_other_command
        * Histogram_step_size_connection_create
    
    Their default start value is set to 16 microseconds.
    
    The latency for synchronous I/O operations is simply the time taken by
    the calling thread to service the I/O request.
    
    In case of asynchronous I/O, there are 2 kinds of latencies:
    
    1. Latency during native I/O:
    Here, starting from the time a slot is reserved in the request array
    (reservation_time) till the time the helper thread waits to collect the
    completed I/O request from the array, is tracked by the histogram.
    
    2. Latency during simulated I/O
    Here, starting from the time a slot is reserved in the request array
    (reservation_time) till the time synchronous I/O is completed, is
    tracked by the histogram (waiting time in the request array + time taken
    to do sync I/O)
    
    This prints all the histogram data to the output of SHOW GLOBAL STATUS,
    and in SHOW ENGINE INNODB STATUS.
    
    Also adds a new information schema table called USER_LATENCY_HISTOGRAMS.
    
    Test Plan: jenkins
    
    Reviewers: santoshb, jtolmer, steaphan
    
    Reviewed By: santoshb, steaphan
    
    Differential Revision: https://reviews.facebook.net/D50751

--- sql/binlog.h
+++ sql/binlog.h
@@ -21,6 +21,8 @@
 #include "log.h"
 
 extern ulong rpl_read_size;
+extern char *histogram_step_size_binlog_fsync;
+extern latency_histogram histogram_binlog_fsync;
 
 class Relay_log_info;
 class Master_info;
