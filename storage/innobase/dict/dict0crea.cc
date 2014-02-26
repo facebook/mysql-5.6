ea6dc4aa74469f2110ca5ccd80b903a2156938e8
Steaphan Greene <steaphan@fb.com>
2014-02-25 17:22:38 -0800

    Port v5.1 InnoDB Commit and Rollback Counters
    Summary:
    Added counters for innodb commit and rollback.
    
    All of this seems totally redundant with the new perfschema stuff.
    However, I included it all anyway, so they can be compared.
    
    Added to SHOW STATUS:
    
    Innodb_transaction_commit_all:
    	Counts all commits (readwrite, readonly and autocommit-readonly).
    
    Innodb_transaction_commit_with_undo:
    	Counts commits for which undo was created.
    
    Innodb_transaction_rollback_total
    	Counts rollbacks of transactions.
    
    Innodb_transaction_rollback_parital:
    	Counts rollbacks to savepoint or from one statement.
    
    Also, display these in TRANSACTION section of SHOW ENGINE INNODB STATUS.
    
    Test Plan: Jenkins (new test included, passes without --record).
    
    Reviewers: chip, jtolmer
    
    Reviewed By: jtolmer

--- storage/innobase/dict/dict0crea.cc
+++ storage/innobase/dict/dict0crea.cc
@@ -1660,7 +1660,7 @@ dict_create_add_foreigns_to_dictionary(
 
 	trx->op_info = "committing foreign key definitions";
 
-	trx_commit(trx);
+	trx_commit(trx, TRUE);
 
 	trx->op_info = "";
 
@@ -1836,7 +1836,7 @@ dict_create_add_tablespace_to_dictionary(
 
 	if (commit) {
 		trx->op_info = "committing tablespace and datafile definition";
-		trx_commit(trx);
+		trx_commit(trx, TRUE);
 	}
 
 	trx->op_info = "";
