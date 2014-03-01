7f2f7dc6c98a217ebdada28e91be4c6870d4d37a
Rongrong Zhong <rongrong@fb.com>
2014-02-28 18:56:23 -0800

    Porting more table stats changes from 5.1 to 5.6
    Summary:
    Improved the determinism of some table stats tests.
    
    IO stats collected with table space split between secondary and PK indexes.
    
    Add an queries_empty column in information_schema.table_statistics.
    Only select queries are considered so far, as the empty queries in
    IS.user_statistics.
    
    Test Plan: mysqltest.sh
    
    Reviewers: steaphan
    
    Reviewed By: steaphan

--- storage/innobase/include/os0file.h
+++ storage/innobase/include/os0file.h
@@ -320,9 +320,10 @@ The wrapper functions have the prefix of "innodb_". */
 	pfs_os_file_close_func(file, __FILE__, __LINE__)
 
 # define os_aio(type, mode, name, file, buf, offset,			\
-		n, message1, message2, io_perf2, tab)			\
+		n, message1, message2, primary_index_id, io_perf2, tab) \
 	pfs_os_aio_func(type, mode, name, file, buf, offset,		\
-		n, message1, message2, __FILE__, __LINE__, io_perf2, tab)
+		n, message1, message2, __FILE__, __LINE__, 		\
+		primary_index_id, io_perf2, tab)
 
 # define os_file_read(file, buf, offset, n)				\
 	pfs_os_file_read_func(file, buf, offset, n, __FILE__, __LINE__)
@@ -364,9 +365,9 @@ to original un-instrumented file I/O APIs */
 # define os_file_close(file)	os_file_close_func(file)
 
 # define os_aio(type, mode, name, file, buf, offset, n,			\
-		message1, message2, io_perf2, tab)			\
+		message1, message2, primary_index_id, io_perf2, tab)	\
 	os_aio_func(type, mode, name, file, buf, offset, n,		\
-		    message1, message2, io_perf2, tab)
+		    message1, message2, primary_index_id, io_perf2, tab)
 
 # define os_file_read(file, buf, offset, n)	\
 	os_file_read_func(file, buf, offset, n)
@@ -430,6 +431,8 @@ struct os_io_perf2_struct {
 	my_io_perf_t	read;
 	my_io_perf_t	write;
 	my_io_perf_t	read_blob;
+	my_io_perf_t	read_primary;
+	my_io_perf_t	read_secondary;
 };
 typedef struct os_io_perf2_struct os_io_perf2_t;
 
@@ -439,6 +442,8 @@ struct os_io_table_perf_struct {
 	my_io_perf_t	read;			/*!< sync read */
 	my_io_perf_t	write;			/*!< sync write */
 	my_io_perf_t	read_blob;		/*!< sync blob read */
+	my_io_perf_t	read_primary;	/*!< sync read for primary index */
+	my_io_perf_t	read_secondary;/*!< sync read for secondary index */
 	longlong	index_inserts;		/*!< secondary index inserts */
 };
 typedef struct os_io_table_perf_struct os_io_table_perf_t;
@@ -806,6 +811,7 @@ pfs_os_aio_func(
                                 OS_AIO_SYNC */
 	const char*	src_file,/*!< in: file name where func invoked */
 	ulint		src_line,/*!< in: line where the func invoked */
+	ib_uint64_t*	primary_index_id,/*!< in: index_id of primary index */
 	os_io_perf2_t*	io_perf2,/*!< in: per fil_space_t performance
 					counters */
 	os_io_table_perf_t* table_io_perf);
@@ -1174,6 +1180,7 @@ os_aio_func(
 				(can be used to identify a completed
 				aio operation); ignored if mode is
 				OS_AIO_SYNC */
+	ib_uint64_t*	primary_index_id,/*!< in: index_id of primary index */
 	os_io_perf2_t*	io_perf2,/*!< in: per fil_space_t performance
 				counters */
 	os_io_table_perf_t* table_io_perf);
