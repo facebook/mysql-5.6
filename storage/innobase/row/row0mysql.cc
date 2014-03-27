26f03fafa1e6a4cfde543c7628e7227eab4e1a3a
Steaphan Greene <steaphan@fb.com>
2014-03-27 07:17:47 -0700

    Port count of lock waits and lock wait timeouts to table_statistics
    Summary:
    Port the following commit from 5.1 to 5.6 with minimal changes:
    "Add count of lock waits and lock wait timeouts to table_statistics"
    
    Test Plan:
    1. Check that patch builds.
    2. Run innodb_lock_wait* unit tests and verify that they pass.
    3. Run full test suite and ensure that the diff doesn't introduce
    regression bugs.
    4. Manually cause a lock wait e.g. using
    'select * from t1 where i=1 for update'. Verify that the innodb_row_lock_waits
    and +innodb_row_lock_wait_timeouts counter in the info scheme increased.
    
    Reviewers: rongrong
    
    Reviewed By: rongrong

--- storage/innobase/row/row0mysql.cc
+++ storage/innobase/row/row0mysql.cc
@@ -587,7 +587,8 @@ row_mysql_handle_errors(
 				function */
 	trx_t*		trx,	/*!< in: transaction */
 	que_thr_t*	thr,	/*!< in: query thread, or NULL */
-	trx_savept_t*	savept)	/*!< in: savepoint, or NULL */
+	trx_savept_t*	savept,	/*!< in: savepoint, or NULL */
+	dict_table_t*	table)	/*!< in: table */
 {
 	dberr_t	err;
 
@@ -600,6 +601,7 @@ handle_new_error:
 
 	switch (err) {
 	case DB_LOCK_WAIT_TIMEOUT:
+		fil_change_lock_wait_timeout_count(table->space, 1);
 		if (row_rollback_on_timeout) {
 			trx_rollback_to_savepoint(trx, NULL);
 			break;
@@ -627,6 +629,7 @@ handle_new_error:
 		/* MySQL will roll back the latest SQL statement */
 		break;
 	case DB_LOCK_WAIT:
+		fil_change_lock_wait_count(table->space, 1);
 		lock_wait_suspend_thread(thr);
 
 		if (trx->error_state != DB_SUCCESS) {
@@ -1128,7 +1131,8 @@ run_again:
 	if (err != DB_SUCCESS) {
 		que_thr_stop_for_mysql(thr);
 
-		was_lock_wait = row_mysql_handle_errors(&err, trx, thr, NULL);
+		was_lock_wait = row_mysql_handle_errors(&err, trx, thr, NULL,
+		    prebuilt->table);
 
 		if (was_lock_wait) {
 			goto run_again;
@@ -1209,7 +1213,8 @@ run_again:
 	if (err != DB_SUCCESS) {
 		que_thr_stop_for_mysql(thr);
 
-		was_lock_wait = row_mysql_handle_errors(&err, trx, thr, NULL);
+		was_lock_wait = row_mysql_handle_errors(&err, trx, thr, NULL,
+		    table ? table : prebuilt->table);
 
 		if (was_lock_wait) {
 			goto run_again;
@@ -1329,7 +1334,7 @@ error_exit:
 		thr->lock_state = QUE_THR_LOCK_ROW;
 
 		was_lock_wait = row_mysql_handle_errors(
-			&err, trx, thr, &savept);
+			&err, trx, thr, &savept, prebuilt->table);
 
 		thr->lock_state = QUE_THR_LOCK_NOLOCK;
 
@@ -1754,7 +1759,7 @@ run_again:
 		DEBUG_SYNC(trx->mysql_thd, "row_update_for_mysql_error");
 
 		was_lock_wait = row_mysql_handle_errors(&err, trx, thr,
-							&savept);
+				&savept, prebuilt->table);
 		thr->lock_state= QUE_THR_LOCK_NOLOCK;
 
 		if (was_lock_wait) {
@@ -3136,7 +3141,7 @@ run_again:
 			ibool	was_lock_wait;
 
 			was_lock_wait = row_mysql_handle_errors(
-				&err, trx, thr, NULL);
+				&err, trx, thr, NULL, table);
 
 			if (was_lock_wait) {
 				goto run_again;
@@ -4291,7 +4296,7 @@ check_next_foreign:
 	case DB_OUT_OF_FILE_SPACE:
 		err = DB_MUST_GET_MORE_FILE_SPACE;
 
-		row_mysql_handle_errors(&err, trx, NULL, NULL);
+		row_mysql_handle_errors(&err, trx, NULL, NULL, table);
 
 		/* raise error */
 		ut_error;
