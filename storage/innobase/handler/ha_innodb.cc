84c1dfa32c7b6877ed02f8a86a08d2432f4a2c34
Peng Tian <pengt@fb.com>
2014-05-21 17:08:39 -0700

    Added a new native DOCUMENT column type into MySQL
    Summary:
    This diff introduced a new native column type DOCUMENT into MySQL. Users can create tables with this new type, the underlying format will be plain text JSON for now. It may support BSON or customized binary JSON in the future. Only a valid JSON document can be inserted into a DOCUMENT column. A DOCUMENT column can be NULL. Its max size is 16MB. JSON functions can be applied on it. For example:
    - create table t (i int(8), a document, s text(255)) engine=innodb;
    - select a from t where json_contains_key(a, 'address', 'hoa');
    - select a from t where json_extract(a, 'address', 'zipcode') like '98762';
    - select json_extract(a, 'name'), json_extract(a, 'children') from t where json_array_length(json_extract(a, 'children')) > 2;
    
    Limitations and error handlings for now:
    - A DOCUMENT column can be NULL. The max size of document column is 16MB.
    - A warning will be returned if the JSON document is not valid during insertion and NULL will be inserted.
    - An error will be returned when trying to create a table with document column with non-innodb engine
    - An error will be returned when creating index on a column with document type
    - An error will be returned when trying to partition a table on a document column
    - Although for now document type is only supported by InnoDB, and MySQL 5.6 temp table are all created by MyISAM engine, temp tables with document columns still will work correctly since they will be treated as blobs.
    
    Test Plan:
    - Added new unit test cases json.type_document_innodb.test and json.json_func_document.test
    - Jenkins tests.
    
    Reviewers: tianx
    
    Reviewed By: tianx

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -5997,6 +5997,7 @@ innobase_mysql_cmp(
 	case MYSQL_TYPE_BLOB:
 	case MYSQL_TYPE_LONG_BLOB:
 	case MYSQL_TYPE_VARCHAR:
+	case MYSQL_TYPE_DOCUMENT:
 		/* Use the charset number to pick the right charset struct for
 		the comparison. Since the MySQL function get_charset may be
 		slow before Bar removes the mutex operation there, we first
@@ -6065,6 +6066,7 @@ innobase_get_fts_charset(
 	case MYSQL_TYPE_BLOB:
 	case MYSQL_TYPE_LONG_BLOB:
 	case MYSQL_TYPE_VARCHAR:
+	case MYSQL_TYPE_DOCUMENT:
 		/* Use the charset number to pick the right charset struct for
 		the comparison. Since the MySQL function get_charset may be
 		slow before Bar removes the mutex operation there, we first
@@ -6420,6 +6422,10 @@ get_innobase_type_from_mysql_type(
 		return(DATA_DOUBLE);
 	case MYSQL_TYPE_DECIMAL:
 		return(DATA_DECIMAL);
+
+	/* For now we don't create new type for DOCUMENT, DOCUMENT will
+	be treated as DATA_BLOB in InnoDB, just as what GEOMETRY does */
+	case MYSQL_TYPE_DOCUMENT:
 	case MYSQL_TYPE_GEOMETRY:
 	case MYSQL_TYPE_TINY_BLOB:
 	case MYSQL_TYPE_MEDIUM_BLOB:
@@ -6602,9 +6608,12 @@ ha_innobase::store_key_val_for_row(
 			|| mysql_type == MYSQL_TYPE_MEDIUM_BLOB
 			|| mysql_type == MYSQL_TYPE_BLOB
 			|| mysql_type == MYSQL_TYPE_LONG_BLOB
+			/* MYSQL_TYPE_DOCUMENT data is treated
+			as BLOB data in innodb. */
+			|| mysql_type == MYSQL_TYPE_DOCUMENT
 			/* MYSQL_TYPE_GEOMETRY data is treated
 			as BLOB data in innodb. */
-			|| mysql_type == MYSQL_TYPE_GEOMETRY) {
+		        || mysql_type == MYSQL_TYPE_GEOMETRY) {
 
 			const CHARSET_INFO* cs;
 			ulint		key_len;
@@ -14561,7 +14570,8 @@ ha_innobase::cmp_ref(
 		if (mysql_type == MYSQL_TYPE_TINY_BLOB
 			|| mysql_type == MYSQL_TYPE_MEDIUM_BLOB
 			|| mysql_type == MYSQL_TYPE_BLOB
-			|| mysql_type == MYSQL_TYPE_LONG_BLOB) {
+			|| mysql_type == MYSQL_TYPE_LONG_BLOB
+			|| mysql_type == MYSQL_TYPE_DOCUMENT) {
 
 			/* In the MySQL key value format, a column prefix of
 			a BLOB is preceded by a 2-byte length field */
