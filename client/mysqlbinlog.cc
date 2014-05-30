50f82200906c2aad2f3296930fca1d299cb4864a
Santosh Praneeth Banda <santoshb@fb.com>
2014-05-30 13:56:21 -0700

    mysqlbinlog --exclude-gtids should not require binlog name
    Summary:
    Current behavior of mysqlbinlog with --exclude-gtids requires
    a binlog file name when streaming from a mysql server. But using
    the gtid_executed set given in --exclude-gtids server can
    automatically find the required binlog file name, so no binlog
    file name to mysqlbinlog is required.
    
    Dump thread with auto_position sends a fake rotate event
    before sending real events to the slave thread. mysqlbinlog
    assumes this fake rotate event to be the end of file. This behavior is
    avoided by checking is_artificial_event(). Without this change,
    mysqlbinlog --exclude-gtid exits without printing any binlog events.
    
    This diff fixes an error message.
    
    Test Plan:
    mysqlbinlog --exclude-gtids on a devserver streaming binlogs
    from a prod udb
    
    Reviewers: jtolmer, rongrong
    
    Reviewed By: rongrong
    
    Subscribers: evanelias, webscalesql-eng@
    
    Differential Revision: https://phabricator.fb.com/D1357602
    (cherry picked from commit a8c0c4a1c198a6d33cd5fa8c9d3e1d327e6a4a2a)

--- client/mysqlbinlog.cc
+++ client/mysqlbinlog.cc
@@ -2324,8 +2324,13 @@ static Exit_status dump_remote_log_entries(PRINT_EVENT_INFO *print_event_info,
         {
           if (!to_last_remote_log)
           {
-            if ((rev->ident_len != logname_len) ||
-                memcmp(rev->new_log_ident, logname, logname_len))
+            // logname is NULL if mysqlbinlog is run with
+            // --exclude-gtids or --start-gtid. In these cases
+            // fake rotate event should not be considered as end of
+            // binlog file.
+            if ((!rev->is_artificial_event() || logname) &&
+                ((rev->ident_len != logname_len) ||
+                memcmp(rev->new_log_ident, logname, logname_len)))
             {
               DBUG_RETURN(OK_CONTINUE);
             }
@@ -2942,7 +2947,7 @@ static int args_post_process(void)
       && opt_remote_proto != BINLOG_DUMP_GTID)
   {
     error("--start-gtid requires --index-file option or "
-          "--read-from-remote-server=BINLOG_DUMP_GTID option");
+          "--read-from-remote-master=BINLOG-DUMP-GTIDS option");
     DBUG_RETURN(ERROR_STOP);
   }
 
@@ -3210,7 +3215,8 @@ int main(int argc, char** argv)
   parse_args(&argc, &argv);
 
   if (!argc && opt_find_gtid_str == NULL &&
-      opt_start_gtid_str == NULL)
+      opt_start_gtid_str == NULL &&
+      opt_exclude_gtids_str == NULL)
   {
     usage();
     free_defaults(defaults_argv);
@@ -3306,7 +3312,13 @@ int main(int argc, char** argv)
         retval = start_gtid_dump(opt_find_gtid_str, true);
     }
   }
-
+  else if (!argc && opt_exclude_gtids_str != NULL)
+  {
+    // server can automatically find binlog file name when using
+    // auto_position using --exclude-gtids. So passing binlog file
+    // name to mysqlbinlog is not necessary in this case.
+    retval = dump_log_entries(NULL);
+  }
   else
   {
     for (save_stop_position= stop_position, stop_position= ~(my_off_t)0 ;
