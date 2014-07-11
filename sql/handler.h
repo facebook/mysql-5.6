d9611d05f52e9ce9c5d04501a20d5eacaec0a21b
Sergei Petrunia <psergey@askmonty.org>
2014-07-12 00:55:19 +0400

    LevelDB Storage Engine: re-commit in git against webscale

--- sql/handler.h
+++ sql/handler.h
@@ -392,6 +392,7 @@ enum legacy_db_type
   DB_TYPE_MEMCACHE,
   DB_TYPE_FALCON,
   DB_TYPE_MARIA,
+  DB_TYPE_LEVELDB, /* Need it here for extended keys to work */
   /** Performance schema engine. */
   DB_TYPE_PERFORMANCE_SCHEMA,
   DB_TYPE_FIRST_DYNAMIC=42,
