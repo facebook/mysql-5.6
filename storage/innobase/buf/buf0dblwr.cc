6858703c5ae1adaba0a2c8e0dbb038536404ef92
Steaphan Greene <steaphan@fb.com>
2014-08-28 13:05:36 -0700

    Do not check or try to recover empty doublewrite pages
    Summary:
    The pages in the doublewrite buffer are fixed.  When the reduced mode is
    dynamically enabled, only the first page may be used, so the other pages
    are overwritten with blank pages that point to themselves.
    
    Since these pages *may* be used by the (separate) single-page flushing
    system, we still have to process them.  The idea was that these pages
    would pass the checksums, and so nothing special had to be done.
    
    There is really no point in checking a doublewrite page that points
    within the doublewrite buffer itself.  This was just done to keep the
    code simple.  So, now, we just ignore these pages entirely.
    
    Test Plan:
    Jenkins
    
    mysqltest.sh --clean --release --repeat=16 --parallel=16 --mem \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite \
    	innodb.innodb_doublewrite innodb.innodb_doublewrite
    
    Verified using extra logging info that these pages (and *only* these
    pages) are being skipped during all this testing.
    
    Reviewers: pengt
    
    Reviewed By: pengt

--- storage/innobase/buf/buf0dblwr.cc
+++ storage/innobase/buf/buf0dblwr.cc
@@ -576,7 +576,11 @@ buf_dblwr_init_or_load_pages(
 				mach_read_from_4(page + FIL_PAGE_SPACE_ID);
 			ulint page_no  =
 				mach_read_from_4(page + FIL_PAGE_OFFSET);
-			recv_dblwr.add(page, space_id, page_no);
+			/* Do not bother checking unused doublewrite pages. */
+			if (space_id != TRX_SYS_SPACE ||
+			    !buf_dblwr_page_inside(page_no)) {
+				recv_dblwr.add(page, space_id, page_no);
+			}
 		}
 
 		page += UNIV_PAGE_SIZE;
