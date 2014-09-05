07c841236ebae82a6bb0fe85147f4dd83ea2c517
Santosh Praneeth Banda <santoshb@fb.com>
2014-09-05 15:00:08 -0700

    New conf option for SO_SNDBUF in dump thread.
    Summary:
    A new configuration option rpl_send_buffer_size is added
    to control the TCP send buffer size of the socket used in dump thread.
    
    Test Plan: All mts tests passed. Yoshi verified the patch.
    
    Reviewers: yoshinori, steaphan
    
    Reviewed By: steaphan

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -4176,6 +4176,13 @@ static Sys_var_uint Sys_rpl_receive_buffer_size(
        VALID_RANGE(1024, UINT_MAX), DEFAULT(2 * 1024 * 1024),
        BLOCK_SIZE(1024));
 
+static Sys_var_uint Sys_rpl_send_buffer_size(
+       "rpl_send_buffer_size",
+       "The size of output buffer for the socket used during sending "
+       "events to a slave.",
+       GLOBAL_VAR(rpl_send_buffer_size), CMD_LINE(REQUIRED_ARG),
+       VALID_RANGE(1024, UINT_MAX), DEFAULT(2 * 1024 * 1024), BLOCK_SIZE(1024));
+
 static Sys_var_mybool Sys_slave_allow_batching(
        "slave_allow_batching", "Allow slave to batch requests",
        GLOBAL_VAR(opt_slave_allow_batching),
