8f88eb446e80a401e5637421d0007d5e6e93c528
Rongrong Zhong <rongrong@fb.com>
2014-09-05 15:40:10 -0700

    Bug #68220 innodb_rows_updated is misleading on slave
    Summary:
    Added innodb_system_rows_read/inserted/updated/deleted counters
    that are the equivalent of innodb_rows_* but that only account for
    changes made to system databases (mysql, information_schame and
    preformance_schema). These counters will be used on slaves to
    differentiated the updates made on system databases from those made on
    user databases.
    
    innodb_rows_* status counters are not updated when innodb_system_rows_*
    are updated.
    
    Test Plan:
    rpl.rpl_innodb_bug68220
    
    Reviewers: rudradevbasak, steaphan, santoshb
    
    Reviewed By: santoshb
    (Original Reviewed By: rudradevbasak, santoshb)

--- storage/innobase/api/api0api.cc
+++ storage/innobase/api/api0api.cc
@@ -1358,7 +1358,10 @@ ib_execute_insert_query_graph(
 
 		dict_table_n_rows_inc(table);
 
-		srv_stats.n_rows_inserted.inc();
+		if (table->is_system_db)
+			srv_stats.n_system_rows_inserted.inc();
+		else
+			srv_stats.n_rows_inserted.inc();
 	}
 
 	trx->op_info = "";
@@ -1712,9 +1715,15 @@ ib_execute_update_query_graph(
 
 			dict_table_n_rows_dec(table);
 
-			srv_stats.n_rows_deleted.inc();
+			if (table->is_system_db)
+				srv_stats.n_system_rows_deleted.inc();
+			else
+				srv_stats.n_rows_deleted.inc();
 		} else {
-			srv_stats.n_rows_updated.inc();
+			if (table->is_system_db)
+				srv_stats.n_system_rows_updated.inc();
+			else
+				srv_stats.n_rows_updated.inc();
 		}
 
 	} else if (err == DB_RECORD_NOT_FOUND) {
