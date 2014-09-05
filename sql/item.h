95d7d53a1ddc1bf3583296063d3b92dc793cf8d2
Tian Xia <tianx@fb.com>
2014-09-05 00:17:09 -0700

    Use FBSON as the storage format for document type
    Summary:
    This patch completes the integration of FBSON in MySQL, and now DOCUMENT type
    stores FBSON binary instead of plain JSON text. When JSON is inserted into a
    DOCUMENT column, we validate and serialize JSON into FBSON binary which is stored
    in the column.
    
    JSON builtin functions will be more efficient on DOCUMENT column, since it will
    operate directly on the FBSON bytes without going through parsing. Note, the
    output of those builtin functions will be JSON text, not FBSON binary.
    
    SELECT on DOCUMENT column will automatically convert FBSON to JSON text
    to be displayed.
    
    Also added some new tests and did some test cleanup. Please note, some of the
    test result changes are due to ordering of the DOCUMENT column, and some are
    due to white space removal after FBSON -> JSON conversion.
    
    Test Plan: All tests under suite/json.
    
    Reviewers: jtolmer
    
    Reviewed By: jtolmer

--- sql/item.h
+++ sql/item.h
@@ -1068,6 +1068,7 @@ public:
   */
   virtual bool val_bool();
   virtual String *val_nodeset(String*) { return 0; }
+  virtual String *val_doc(String *str) { return 0; }
 
 protected:
   /* Helper functions, see item_sum.cc */
@@ -2199,6 +2200,7 @@ public:
   longlong val_date_temporal();
   my_decimal *val_decimal(my_decimal *);
   String *val_str(String*);
+  String *val_doc(String*);
   double val_result();
   longlong val_int_result();
   longlong val_time_temporal_result();
--- sql/item_jsonfunc.cc
+++ sql/item_jsonfunc.cc
@@ -35,71 +35,135 @@
  * Note we assume the system charset is UTF8,
  * which is the encoding of json object in FBSON
  */
-#include "../fbson/FbsonJsonParser.h"
-#include "../fbson/FbsonUtil.h"
 
-static String*
+/*
+ * Converts FbsonValue object to string and write to str
+ * Input: pval - FbsonValue object to convert
+ *        str - output
+ *        cs - character set
+ * Output: true - success
+ */
+static bool
 ValueToString(fbson::FbsonValue *pval, String &str, const CHARSET_INFO *cs)
 {
-  String *res = &str;
+  if (!pval)
+    return false;
 
   switch (pval->type())
   {
   case fbson::FbsonType::T_False:
     {
-      res->set_int(0, true /*unsigned_flag*/, cs);
-      break;
+      str.set_int(0, true /*unsigned_flag*/, cs);
+      return true;
     }
   case fbson::FbsonType::T_True:
     {
-      res->set_int(1, true /*unsigned_flag*/, cs);
-      break;
+      str.set_int(1, true /*unsigned_flag*/, cs);
+      return true;
     }
   case fbson::FbsonType::T_Object:
   case fbson::FbsonType::T_Array:
     {
       fbson::FbsonToJson tojson;
       std::string json = tojson.json(pval);
-      res->copy(json.c_str(), json.size(), cs);
-      break;
+      str.copy(json.c_str(), json.size(), cs);
+      return true;
     }
   case fbson::FbsonType::T_String:
     {
       fbson::StringVal *str_val = (fbson::StringVal *)pval;
-      res->copy(str_val->getBlob(), str_val->getBlobLen(), cs);
-      break;
+      str.copy(str_val->getBlob(), str_val->getBlobLen(), cs);
+      return true;
     }
   case fbson::FbsonType::T_Int8:
     {
-      res->set_int(((fbson::Int8Val*)pval)->val(), false, cs);
-      break;
+      str.set_int(((fbson::Int8Val*)pval)->val(), false, cs);
+      return true;
     }
   case fbson::FbsonType::T_Int16:
     {
-      res->set_int(((fbson::Int16Val*)pval)->val(), false, cs);
-      break;
+      str.set_int(((fbson::Int16Val*)pval)->val(), false, cs);
+      return true;
     }
   case fbson::FbsonType::T_Int32:
     {
-      res->set_int(((fbson::Int32Val*)pval)->val(), false, cs);
-      break;
+      str.set_int(((fbson::Int32Val*)pval)->val(), false, cs);
+      return true;
     }
   case fbson::FbsonType::T_Int64:
     {
-      res->set_int(((fbson::Int64Val*)pval)->val(), false, cs);
-      break;
+      str.set_int(((fbson::Int64Val*)pval)->val(), false, cs);
+      return true;
     }
   case fbson::FbsonType::T_Double:
     {
-      res->set_real(((fbson::DoubleVal*)pval)->val(), NOT_FIXED_DEC, cs);
-      break;
+      str.set_real(((fbson::DoubleVal*)pval)->val(), NOT_FIXED_DEC, cs);
+      return true;
     }
   case fbson::FbsonType::T_Null:
   default:
-      res = nullptr;
+      return false;
   }
 
-  return res;
+  return false;
+}
+
+/*
+ * Checks if item is a document
+ * Input: item - input item
+ *        json - output, json string or fbson blob
+ * Output: true - output is fbson blob (doc type)
+ *         false - output is json string
+ */
+static bool is_doc_type(Item *item, String *&json, String *buffer)
+{
+  bool doc_type = (item->field_type() == MYSQL_TYPE_DOCUMENT);
+  if (doc_type)
+    json = item->val_doc(buffer); // this is an FBSON blob
+  else
+    json = item->val_str(buffer);
+
+  return doc_type;
+}
+
+/*
+ * Parses JSON c_str into FBSON value object
+ * Input: c_str - JSON string (null terminated)
+ *        os - output stream storing FBSON packed bytes
+ * Output: FbsonValue object.
+ *         NULL if JSON is invalid
+ */
+static fbson::FbsonValue *get_fbson_val(const char *c_str,
+                                        fbson::FbsonOutStream &os)
+{
+  // try parsing input as JSON
+  fbson::FbsonValue *pval = nullptr;
+  fbson::FbsonJsonParser parser(os);
+  if (parser.parse(c_str))
+  {
+    pval = fbson::FbsonDocument::createValue(
+        os.getBuffer(), os.getSize());
+    DBUG_ASSERT(pval);
+  }
+  else
+    my_error(ER_INVALID_JSON, MYF(0), c_str);
+
+  return pval;
+}
+
+/*
+ * Gets FBSON value object directly from str (FBSON blob)
+ * Input: str - FBSON blob
+ *        len - blob length
+ * Output: FbsonValue object.
+ */
+static fbson::FbsonValue *get_fbson_val(const char *str, unsigned int len)
+{
+  // this is an FBSON blob
+  fbson::FbsonValue *pval = fbson::FbsonDocument::createValue(str, len);
+  DBUG_ASSERT(pval);
+
+  return pval;
 }
 
 /*
@@ -110,22 +174,75 @@ bool Item_func_json_valid::val_bool()
 {
   DBUG_ASSERT(fixed);
 
+  null_value = 0;
   String buffer;
-  String *json = args[0]->val_str(&buffer);
-  null_value= (json ? 0:1);
+  String *json = nullptr;
+
+  bool doc_type = is_doc_type(args[0], json, &buffer);
+
   if (json)
   {
+    if (doc_type)
+      return true; // FBSON blob
+
     fbson::FbsonJsonParser parser;
-    if (parser.parse(json->c_ptr_safe()))
-      return true;
+    return parser.parse(json->c_ptr_safe());
   }
 
+  null_value = 1;
   return false;
 }
 
 longlong Item_func_json_valid::val_int()
 {
   return (val_bool() ? 1 : 0);
+}
+
+/*
+ * Extracts key path (stored in args) from pval
+ * Input: args - path arguments
+ *        arg_count - # of path elements
+ *        pval - FBSON value object to extract from
+ *        str - string buffer
+ * Output: FbsonValue object pointed by key path.
+ *         NULL if path is invalid
+ */
+static fbson::FbsonValue*
+json_extract_helper(Item **args,
+                    uint arg_count,
+                    fbson::FbsonValue *pval, /* in: fbson value object */
+                    String *str) /* out: string buffer */
+{
+  for (unsigned i = 1; i < arg_count && pval; ++i)
+  {
+    String *pstr;
+    if (pval->isObject())
+    {
+      if ( (pstr = args[i]->val_str(str)) )
+        pval = ((fbson::ObjectVal*)pval)->find(pstr->c_ptr_safe());
+      else
+        pval = nullptr;
+    }
+    else if (pval->isArray())
+    {
+      if ( (pstr = args[i]->val_str(str)) )
+      {
+        // array index parameter is 0-based
+        char *end = nullptr;
+        int index = strtol(pstr->c_ptr_safe(), &end, 0);
+        if (end && !*end)
+          pval = ((fbson::ArrayVal*)pval)->get(index);
+        else
+          pval = nullptr;
+      }
+      else
+        pval = nullptr;
+    }
+    else
+      pval = nullptr;
+  }
+
+  return pval;
 }
 
 /*
@@ -137,54 +254,31 @@ String *Item_func_json_extract::val_str(String *str)
   DBUG_ASSERT(fixed);
 
   null_value = 0;
-  String *res = nullptr;
-  String *pstr = args[0]->val_str(str);
+  String *pstr = nullptr;
+
+  bool doc_type = is_doc_type(args[0], pstr, str);
   if (pstr)
   {
-    fbson::FbsonJsonParser parser;
-    if (parser.parse(pstr->c_ptr_safe()))
+    fbson::FbsonValue *pval = nullptr;
+    if (doc_type)
     {
-      fbson::FbsonOutStream *out = parser.getWriter().getOutput();
-      fbson::FbsonValue *pval = fbson::FbsonDocument::createValue(
-          out->getBuffer(), out->getSize());
-      DBUG_ASSERT(pval);
-
-      unsigned i = 1;
-      for (; i < arg_count && pval && pstr; ++i)
-      {
-        if (pval->isObject())
-        {
-          if ( (pstr = args[i]->val_str(str)) )
-            pval = ((fbson::ObjectVal*)pval)->find(pstr->c_ptr_safe());
-        }
-        else if (pval->isArray())
-        {
-          if ( (pstr = args[i]->val_str(str)) )
-          {
-            // array index parameter is 0-based
-            char *end = nullptr;
-            int index = strtol(pstr->c_ptr_safe(), &end, 0);
-            if (end && !*end)
-              pval = ((fbson::ArrayVal*)pval)->get(index);
-            else
-              pval = nullptr;
-          }
-        }
-        else
-          pval = nullptr;
-      }
-
-      if (pval && pstr)
-        res = ValueToString(pval,*str,collation.collation);
+      pval = get_fbson_val(pstr->ptr(), pstr->length());
+      pval = json_extract_helper(args, arg_count, pval, str);
+      if (ValueToString(pval,*str,collation.collation))
+        return str;
     }
     else
-      my_error(ER_INVALID_JSON, MYF(0), pstr->c_ptr_safe());
+    {
+      fbson::FbsonOutStream os;
+      pval = get_fbson_val(pstr->c_ptr_safe(), os);
+      pval = json_extract_helper(args, arg_count, pval, str);
+      if (ValueToString(pval,*str,collation.collation))
+        return str;
+    }
   }
 
-  if (!res)
-    null_value = 1;
-
-  return res;
+  null_value = 1;
+  return nullptr;
 }
 
 void Item_func_json_extract::fix_length_and_dec()
@@ -202,59 +296,52 @@ bool Item_func_json_contains_key::val_bool()
 {
   DBUG_ASSERT(fixed);
 
-  bool res = false;
-
+  null_value = 0;
   String buffer;
-  String *pstr = args[0]->val_str(&buffer);
-  null_value = (pstr? 0:1);
+  String *pstr = nullptr;
+
+  bool doc_type = is_doc_type(args[0], pstr, &buffer);
   if (pstr)
   {
-    fbson::FbsonJsonParser parser;
-    if (parser.parse(pstr->c_ptr_safe()))
+    fbson::FbsonValue *pval = nullptr;
+    if (doc_type)
     {
-      fbson::FbsonOutStream *out = parser.getWriter().getOutput();
-      fbson::FbsonValue *pval = fbson::FbsonDocument::createValue(
-          out->getBuffer(), out->getSize());
-      DBUG_ASSERT(pval);
-
-      unsigned i = 1;
-      for (; i < arg_count && pval && pstr; ++i)
-      {
-        if (pval->isObject())
-        {
-          if ( (pstr = args[i]->val_str(&buffer)) )
-            pval = ((fbson::ObjectVal*)pval)->find(pstr->c_ptr_safe());
-        }
-        else if (pval->isArray())
-        {
-          if ( (pstr = args[i]->val_str(&buffer)) )
-          {
-            // array index parameter is 0-based
-            char *end = nullptr;
-            int index = strtol(pstr->c_ptr_safe(), &end, 0);
-            if (end && !*end)
-              pval = ((fbson::ArrayVal*)pval)->get(index);
-            else
-              pval = nullptr;
-          }
-        }
-        else
-          pval = nullptr;
-      }
-
-      if (pval && pstr)
-        res = true;
+      pval = get_fbson_val(pstr->ptr(), pstr->length());
+      return json_extract_helper(args, arg_count, pval, &buffer) != nullptr;
     }
     else
-      my_error(ER_INVALID_JSON, MYF(0), pstr->c_ptr_safe());
+    {
+      fbson::FbsonOutStream os;
+      pval = get_fbson_val(pstr->c_ptr_safe(), os);
+      return json_extract_helper(args, arg_count, pval, &buffer) != nullptr;
+    }
   }
 
-  return res;
+  null_value = 1;
+  return false;
 }
 
 longlong Item_func_json_contains_key::val_int()
 {
   return (val_bool() ? 1 : 0);
+}
+
+/*
+ * Gets array length from FbsonValue object
+ * Input: pval - FbsonValue object (array)
+ *        json - original JSON string
+ * Output: number of array elements (array length)
+ *         0 if pval is not an array object
+ */
+static longlong
+json_array_length_helper(fbson::FbsonValue *pval, const char *json)
+{
+  if (pval && pval->isArray())
+    return ((fbson::ArrayVal*)pval)->numElem();
+  else
+    my_error(ER_INVALID_JSON_ARRAY, MYF(0), json);
+
+  return 0;
 }
 
 /*
@@ -265,29 +352,28 @@ longlong Item_func_json_array_length::val_int()
 {
   DBUG_ASSERT(fixed);
 
-  longlong res = 0;
-
+  null_value = 0;
   String buffer;
-  String *pstr = args[0]->val_str(&buffer);
-  null_value = (pstr? 0:1);
+  String *pstr = nullptr;
+
+  bool doc_type = is_doc_type(args[0], pstr, &buffer);
+
   if (pstr)
   {
-    fbson::FbsonJsonParser parser;
-    if (parser.parse(pstr->c_ptr_safe()))
+    fbson::FbsonValue *pval = nullptr;
+    if (doc_type)
     {
-      fbson::FbsonOutStream *out = parser.getWriter().getOutput();
-      fbson::FbsonValue *pval = fbson::FbsonDocument::createValue(
-          out->getBuffer(), out->getSize());
-      DBUG_ASSERT(pval);
-
-      if (pval->isArray())
-        res = ((fbson::ArrayVal*)pval)->numElem();
-      else
-        my_error(ER_INVALID_JSON_ARRAY, MYF(0), pstr->c_ptr_safe());
+      pval = get_fbson_val(pstr->ptr(), pstr->length());
+      return json_array_length_helper(pval, pstr->c_ptr_safe());
     }
     else
-      my_error(ER_INVALID_JSON, MYF(0), pstr->c_ptr_safe());
+    {
+      fbson::FbsonOutStream os;
+      pval = get_fbson_val(pstr->c_ptr_safe(), os);
+      return json_array_length_helper(pval, pstr->c_ptr_safe());
+    }
   }
 
-  return res;
+  null_value = 1;
+  return 0;
 }
