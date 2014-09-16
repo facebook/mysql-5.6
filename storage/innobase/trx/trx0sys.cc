12c7c39b0ef639b9ee6c6e3d7ab52fa944589523
Abhishek Kona <abhishekk@fb.com>
2014-09-15 18:10:15 -0700

    Port Fix Locality of trx_ts to 5.6
    Summary:
    Previously, trx_t's were allocated at random heap locations.
    This causes performance issues when iterating over all open transactions
    (such as when creating a new transaction's list of visible
    transactions). This change allocates larger blocks of trx_t's and
    tracks the next usable trx_t with a singly linked list. As blocks are
    filled, new blocks are allocated. This results in a 50% improvement in
    performance in the 2000 transaction test case.
    
    Test Plan: All unit tests pass, including those in valgrind.
    
    Reviewers: chip, steaphan
    
    Reviewed By: chip, steaphan

--- storage/innobase/trx/trx0sys.cc
+++ storage/innobase/trx/trx0sys.cc
@@ -592,6 +592,7 @@ trx_sys_create(void)
 	trx_sys = static_cast<trx_sys_t*>(mem_zalloc(sizeof(*trx_sys)));
 
 	mutex_create(trx_sys_mutex_key, &trx_sys->mutex, SYNC_TRX_SYS);
+	mutex_create(trx_sys_mutex_key, &trx_sys->trx_memory_mutex, SYNC_TRX);
 }
 
 /*****************************************************************//**
@@ -1243,8 +1244,13 @@ trx_sys_close(void)
 	mutex_exit(&trx_sys->mutex);
 
 	mutex_free(&trx_sys->mutex);
+	mutex_free(&trx_sys->trx_memory_mutex);
 
 	mem_free(trx_sys);
+
+#ifdef UNIV_DEBUG_VALGRIND
+	trx_free_trx_pool();
+#endif
 
 	trx_sys = NULL;
 }
