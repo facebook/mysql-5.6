ae8d246637c2bf405ee9d0d4f3bb49f726a3b77e
Santosh Praneeth Banda <santoshb@fb.com>
2014-10-28 06:43:48 -0700

    send heartbeat event when a GTID group is skipped
    Summary:
    when using GTID and low slave_net_timeouts on slave, slave
    io_thread may get stuck in connecting phase without ever
    progressing. This is due to dump thread skipping GTID groups
    without sending anything through the network.
    
    Send heartbeat event when a GTID group is skipped so the connection is up
    and the slave io_thread doesn't hit net read timeout.
    
    public bug report: http://bugs.mysql.com/bug.php?id=74607
    
    Test Plan:
    use GTIDs. Stop slave at high master_log_pos (800MB). drop binlog caches in a loop
    (echo 3 > /proc/sys/vm/drop_caches). Verify io_thread is stuck in connecting phase
    without ever progressing.
    
    With this patch, io_thread receives binlog events.
    
    Reviewers: jtolmer
    
    Reviewed By: jtolmer
    
    Subscribers: webscalesql-eng@, yoshinori
    
    Differential Revision: https://phabricator.fb.com/D1644545
    
    Tasks: 5438249
    
    Signature: t1:1644545:1415080541:89efbc950880e649df6f372a2363b74eae39971a
    (cherry picked from commit d973294c41feae0bff1c390112c97e74e85160db)

--- sql/rpl_master.cc
+++ sql/rpl_master.cc
@@ -1594,6 +1594,17 @@ void mysql_binlog_send(THD* thd, char* log_ident, my_off_t pos,
       {
         p_last_skip_coord->pos= p_coord->pos;
         strcpy(p_last_skip_coord->file_name, p_coord->file_name);
+        if (reset_transmit_packet(thd, flags, &ev_offset, &errmsg,
+                                  observe_transmission,
+                                  packet, packet_buffer, packet_buffer_size,
+                                  semi_sync_slave))
+          GOTO_ERR;
+        if (send_heartbeat_event(net, packet, p_coord, current_checksum_alg))
+        {
+          errmsg = "Failed on my_net_write()";
+          my_errno= ER_UNKNOWN_ERROR;
+          GOTO_ERR;
+        }
       }
 
       if (!skip_group && last_skip_group
