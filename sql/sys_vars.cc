2989fb5fff3d470859aaa61a116fedfcd31d2734
Andreea Selagea <sela@dev246.lla1.facebook.com>
2014-12-15 19:44:40 -0800

    Make sql_log_bin session only
    Summary: Change sql_log_bin to be SESSION_ONLY instead of SESSION_VAR
    
    Test Plan:
    Created a new test file with validation for sql_log_bin.
    mysqltest.sh mysql-test/t/sql_log_bin_tests.test
    
    Spent 0.002 of 15 seconds executing testcases
    
    Completed: All 1 tests were successful.
    
    Reviewers: kuko, rongrong
    
    Reviewed By: rongrong

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -3753,7 +3753,7 @@ static bool check_sql_log_bin(sys_var *self, THD *thd, set_var *var)
 
 static Sys_var_mybool Sys_log_binlog(
        "sql_log_bin", "Controls whether logging to the binary log is done",
-       SESSION_VAR(sql_log_bin), NO_CMD_LINE, DEFAULT(TRUE),
+       SESSION_ONLY(sql_log_bin), NO_CMD_LINE, DEFAULT(TRUE),
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(check_sql_log_bin),
        ON_UPDATE(fix_sql_log_bin_after_update));
 
