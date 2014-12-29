301e9e49a753c1985b43496c75506d1746809175
Chip Turner <chip@fb.com>
2014-12-29 13:51:27 -0800

    Add facility to re-use SSL_CTX across connections
    Summary:
    Every connection, MySQL recreates the SSL_CTX object.  This is
    bad and unnecessary.  This diff adds the ability to re-use an SSL
    context object between connections.  This is roughly a 50% increase in
    throughput making SSL connections.  In combination with re-using SSL
    sessions, the net speedup is roughly 7-8x.
    
    This facility can be used in one of two ways; one is to "take ownership"
    of the SSL_CTX in an existing MySQL connection (simple) or to create a
    context by hand, therefore bypassing many of the MySQL "options" fields
    relating to verification, certs, etc.  This is not ideal but avoids a
    larger refactoring and public API change to support some kind of
    "initialize an OpenSSL context with these options" which would be
    complicated and potentially difficult to reconcile with yassl.
    
    Test Plan: runtests, also timed with 'mysql' command line tool
    
    Reviewers: anca, santoshb
    
    Reviewed By: anca, santoshb

--- client/mysql.cc
+++ client/mysql.cc
@@ -42,6 +42,10 @@
 #include <signal.h>
 #include <violite.h>
 
+#if defined(HAVE_OPENSSL) && !defined(EMBEDDED_LIBRARY)
+#include <openssl/ssl.h>
+#endif
+
 #include <algorithm>
 
 using std::min;
@@ -1395,10 +1399,14 @@ int main(int argc,char *argv[])
   if (opt_outfile)
     end_tee();
   mysql_end(0);
+
 #ifndef _lint
   DBUG_RETURN(0);				// Keep compiler happy
 #endif
 }
+
+/* The SSL context that will be reused across invocations. */
+static void* ssl_context;
 
 sig_handler mysql_end(int sig)
 {
@@ -1457,6 +1465,9 @@ sig_handler mysql_end(int sig)
   mysql_server_end();
   free_defaults(defaults_argv);
   my_end(my_end_arg);
+#if defined(HAVE_OPENSSL) && !defined(EMBEDDED_LIBRARY)
+  SSL_CTX_free((SSL_CTX*)ssl_context);
+#endif
   exit(status.exit_status);
 }
 
@@ -4761,6 +4772,9 @@ sql_real_connect(char *host,char *database,char *user,char *password,
       mysql_options4(&mysql, MYSQL_OPT_SSL_SESSION,
                      ssl_session_buffer, (void*)ssl_session_len);
     }
+    if (ssl_context) {
+      mysql_options(&mysql, MYSQL_OPT_SSL_CONTEXT, ssl_context);
+    }
   }
   mysql_options(&mysql,MYSQL_OPT_SSL_VERIFY_SERVER_CERT,
                 (char*)&opt_ssl_verify_server_cert);
@@ -4844,6 +4858,7 @@ sql_real_connect(char *host,char *database,char *user,char *password,
     DBUG_PRINT("error", ("unable to save SSL session buffer, ret: %ld",
                          ssl_session_len));
   }
+  ssl_context = mysql_take_ssl_context_ownership(&mysql);
 
 #ifdef __WIN__
   /* Convert --execute buffer from UTF8MB4 to connection character set */
