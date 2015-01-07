f106cd827265da3f7755b2874551de47600b3445
Jonah Cohen <jonah@fb.com>
2015-01-07 15:21:43 -0800

    Fast drop table
    Summary:
    When a table is dropped, we use a compaction filter to filter out all
    its indices.  These are tracked and persisted via the new class
    Dropped_indices_manager.
    
    This is much faster than explicitly deleting a table's rows when it's
    dropped, and should be crash safe as well.
    
    Test Plan: rocksdb.drop_table
    
    Reviewers: spetrunia, MarkCallaghan, maykov, yoshinorim, hermanlee4
    
    Reviewed By: hermanlee4
    
    Subscribers: jtolmer
    
    Differential Revision: https://reviews.facebook.net/D33069
    Differential Revision: https://reviews.facebook.net/D42909

---
+++ storage/rocksdb/rdb_dropped_indices.h
@@ -0,0 +1,61 @@
+/*
+   Copyright (c) 2015, Facebook, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#include <unordered_map>
+#include <unordered_set>
+
+#include "rocksdb/db.h"
+
+class Table_ddl_manager;
+class RDBSE_KEYDEF;
+
+typedef std::unordered_map<uint32, RDBSE_KEYDEF*> Dropped_Index_Map;
+
+class Dropped_indices_manager
+{
+  Dropped_Index_Map di_map;
+  rocksdb::DB* rdb;
+  Table_ddl_manager* ddl_manager;
+  mutable mysql_mutex_t dim_mutex;
+
+public:
+  Dropped_indices_manager()
+    : rdb(nullptr), ddl_manager(nullptr) {}
+  void init(rocksdb::DB* rdb_, Table_ddl_manager* ddl_manager_);
+  void cleanup();
+
+  // check if we have anything to filter
+  bool empty() const;
+
+  // check if key's prefix is in the map
+  bool has_index(const rocksdb::Slice& key) const;
+
+  // get entire list of indices to check if they're finished
+  Dropped_Index_Map get_indices() const;
+
+  // add new indices to the drop index manager
+  void add_indices(RDBSE_KEYDEF** key_descr, uint32 n_keys);
+
+  // remove indices once all their data is gone
+  void remove_indices(const std::unordered_set<uint32> & indices);
+
+private:
+  bool initialized() const { return rdb != nullptr; }
+
+  // internal thread-unsafe function for populating map
+  void map_insert(RDBSE_KEYDEF** key_descr, uint32 n_keys,
+                  const char* log_action);
+};
