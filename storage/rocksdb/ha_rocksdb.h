c41cefaeb54369cb83982038950cd0f62888c4b7
Jonah Cohen <jonah@fb.com>
2015-01-07 15:21:43 -0800

    Fast drop table
    Summary:
    When a table is dropped, we use a compaction filter to filter out all
    its indices.  These are tracked and persisted via the new class
    Dropped_indices_manager.
    
    This is much faster than explicitly deleting a table's rows when it's
    dropped, and should be crash safe as well.
    
    Differential Revision: https://reviews.facebook.net/D42909
    
    fbshipit-source-id: 2379a8d13a3

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -55,8 +55,11 @@ inline bool looks_like_per_index_cf_typo(const char *name)
   return (name && name[0]=='$' && strcmp(name, PER_INDEX_CF_NAME));
 }
 
-//#ifdef HAVE_PSI_INTERFACE
 extern PSI_stage_info stage_waiting_on_row_lock;
+
+#ifdef HAVE_PSI_INTERFACE
+extern PSI_mutex_key key_mutex_dropped_indices_manager;
+#endif
 
 extern "C"
 void thd_enter_cond(MYSQL_THD thd, mysql_cond_t *cond, mysql_mutex_t *mutex,
