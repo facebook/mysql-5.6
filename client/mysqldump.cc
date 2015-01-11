b382d88796b42b4b9f0955727373396ab322bcb7
Rongrong Zhong <rongrong@fb.com>
2015-01-10 22:17:53 -0800

    Port Add --timeout option for mysqldump to 5.6
    Summary:
    Allow per-session control of server user variables for mysqldump, thus
    avoiding session timeouts on write blockign (mostly in NFS environments)
    
    This adds --timeout option, which will set:
    	* net_write_timeout
    	* wait_timeout
    for mysqldump's connection to MySQL server
    
    Reference patch: https://github.com/facebook/mysql-5.6/commit/0b35262
    
    Reviewed By: Tema
    
    Differential Revision: D6253820
    
    fbshipit-source-id: 094594adee6

--- client/mysqldump.cc
+++ client/mysqldump.cc
@@ -161,6 +161,7 @@ static uint opt_enable_cleartext_plugin = 0;
 static bool using_opt_enable_cleartext_plugin = 0;
 static uint opt_mysql_port = 0, opt_master_data;
 static uint opt_slave_data;
+static ulong opt_timeout = 0;
 static uint my_end_arg;
 static char *opt_mysql_unix_port = 0;
 static char *opt_bind_addr = NULL;
@@ -537,6 +538,11 @@ static struct my_option my_long_options[] = {
      &path, &path, 0, GET_STR, REQUIRED_ARG, 0, 0, 0, 0, 0, 0},
     {"tables", OPT_TABLES, "Overrides option --databases (-B).", 0, 0, 0,
      GET_NO_ARG, NO_ARG, 0, 0, 0, 0, 0, 0},
+    {"timeout", OPT_TIMEOUT,
+     "Set data transfer timeouts for server-side session "
+     "(default server setting, if 0)",
+     &opt_timeout, &opt_timeout, 0, GET_ULONG, REQUIRED_ARG, 0, 0, 3600 * 12, 0,
+     0, 0},
     {"triggers", OPT_TRIGGERS, "Dump triggers for each dumped table.",
      &opt_dump_triggers, &opt_dump_triggers, 0, GET_BOOL, NO_ARG, 1, 0, 0, 0, 0,
      0},
@@ -1524,6 +1530,15 @@ static int connect_to_db(char *host, char *user, char *passwd) {
   snprintf(buff, sizeof(buff), "/*!40100 SET @@SQL_MODE='%s' */",
            ansi_mode ? "ANSI" : "");
   if (mysql_query_with_error_report(mysql, 0, buff)) DBUG_RETURN(1);
+
+  if (opt_timeout) {
+    snprintf(buff, sizeof(buff),
+                "SET wait_timeout=%lu, "
+                "net_write_timeout=%lu",
+                opt_timeout, opt_timeout);
+    if (mysql_query_with_error_report(mysql, 0, buff)) DBUG_RETURN(1);
+  }
+
   /*
     set time_zone to UTC to allow dumping date types between servers with
     different time zone settings
