25230c57193c2427bfbf9128d7005bdac65f55c8
Santosh Praneeth Banda <santoshb@fb.com>
2015-02-06 16:20:12 -0800

    Crash safe slave when using GTID and MTS
    Summary:
    Gtid information is stored in a transaction table
    (slave_gtid_info). The table contains three columns
    
      Id  | Database name  | Last_gtid
    
    Every database contains a row in the table storing the last gtid
    in the replication stream for that database. On every commit by the
    sql_thread or slave worker thread in case of MTS, the corresponding
    row in the table slave_gtid_info is updated using XA. The table schema
    slave_worker_info is changed to include last_gtid executed by that worker.
    This extra information helps in finding out the group where worker stopped
    in case of an error.
    
    Every group gtid is checked with the last_gtid seen for that database to
    decide whether it should be executed or skipped in which case only a empty
    group of that gtid is printed in the binary log. Empty group in the binlog looks like
    this
    
      SET @@session.GTID_NEXT='';
      BEGIN;
      // Followup of this diff (either wormhole comments or the whole event)
      COMMIT;
    
    Current setting for crash safe slave with gtid and mts requires
    
      --relay_log_info_repository='TABLE'
      (currently this won't work with MTS, a restart with relay-log-recovery=0 followed by
      START SLAVE UNTIL SQL_AFTER_MTS_GAPS is required)
      --relay_log_recovery=1
      --gtid_mode=ON
    
    without --relay_log_recovery=1 a transaction can get lost if the relay log ends with GTID
    event. With this transactional table the new crash safe slave parameters with gtid and
    MTS requires.
    
      --relay_log_recovery=1
      --gtid_mode=ON
    
    This avoids the need for START SLAVE UNTIL SQL_AFTER_MTS_GAPS command and sync_relay_log=1.
    The disadvantage is to copy binlogs again from master if slave is severely lagged.
    
    A slave running with sync_binlog=0 may have missing binlog events in case
    of a crash, but the events are committed in the storage engine. With new gtid crash
    safe functionality using a transaction table slave_gtid_info, the missing events
    can be detected. This diff also adds the functionality to write the missing events into
    the binlog, only when using statement based replication.
    
    coordinator thread doesn't need to store gtid_info in MTS mode, the same
    work is done by worker threads and worker threads flushes the
    information to mysql.slave_gtid_info table on xid commit.
    
    Fixed a bug in in insert_dynamic_set() where it fails to de-duplicate
    leading to increased size of dynamic array rli->gtid_infos.
    
    We need at least once semantic for binlog transactions which is strictly
    required for taking backup from row based binary logs.
    
    With less durable settings (sync_binlog=0) a slave may not have synced
    the transactions to disk before crashing. This will leave some
    transactions committed in storage engine but not in binlog. After
    slave's crash recovery these transactions are skipped to commit inside
    innodb using the slave_gtid_info table. These skipped transactions are
    logged to binlog to achieve at least once semantic.
    
    A flag binlog_only_row_event is used to skip applying event in innodb
    for a Row_log_event.
    
    Test Plan:
    Added crash safe tests to make sure slave_gtid_info is updated
    correctly even in cases of crashes at specific debug points. Added crash
    recovery tests both in MTS and single threaded replication and tested sql_thread
    doesn't fail with duplicate key error and missed gtids are printed in the binary
    log. Tested by repeatedly crashing a slave with new crash safe parameters
    (need to add a rpm test).
    
    verified the output of show binlog events to find the missing events
    on a crashed slave. Simulated missing events on a slave in a mtr_test
    and re-executed all the events of the slave binlog after resetting
    the slave. Finally a consistency check of all the tables between master
    and slave is done to verify the functionality.
    
    mtr rpl.rpl_gtid_mts.test
    
    Manual testing of method explained in rpl_slave_gtid_info.inc file.
    
    Reviewers: jtolmer, steaphan
    
    Reviewed By: jtolmer, steaphan

---
+++ sql/rpl_gtid_info.h
@@ -0,0 +1,75 @@
+#ifndef RPL_GTID_INFO_H
+#define RPL_GTID_INFO_H
+
+#ifdef HAVE_REPLICATION
+
+#include <my_global.h>
+#include <sql_priv.h>
+
+#include "my_sys.h"
+#include "rpl_gtid.h"
+#include "rpl_info_handler.h"
+#include "rpl_info.h"
+
+class Gtid_info : public Rpl_info
+{
+friend class Rpl_info_factory;
+
+private:
+  uint id; // Internal id of the row in the slave_gitd_info table.
+  char database_name[65];
+  // Last gtid seen in the replication stream of the database_name.
+  char last_gtid_string[Gtid::MAX_TEXT_LENGTH + 1];
+  // Used to store the sidno and gno of the last_gtid_string.
+  Gtid last_gtid;
+  // Local sid_map of this class to compare Uuid strings.
+  Sid_map *sid_map;
+public:
+  inline void set_database_name(const char* db_arg)
+  {
+    strmake(database_name, db_arg, sizeof(database_name) -1);
+  }
+
+  inline void set_last_gtid(const char* gtid_string)
+  {
+    strmake(last_gtid_string, gtid_string, sizeof(last_gtid_string) - 1);
+  }
+
+  inline const char *get_database_name()
+  {
+    return database_name;
+  }
+
+  inline const char *get_last_gtid_string()
+  {
+    return last_gtid_string;
+  }
+
+  static size_t get_number_info_gtid_fields();
+  int gtid_init_info();
+  bool flush_info(bool force);
+  void end_info();
+  bool skip_event(const char *gtid);
+
+  Gtid_info(uint param_id
+#ifdef HAVE_PSI_INTERFACE
+            ,PSI_mutex_key *param_key_info_run_lock,
+            PSI_mutex_key *param_key_info_data_lock,
+            PSI_mutex_key *param_key_info_sleep_lock,
+            PSI_mutex_key *param_key_info_data_cond,
+            PSI_mutex_key *param_key_info_start_cond,
+            PSI_mutex_key *param_key_info_stop_cond,
+            PSI_mutex_key *param_key_info_sleep_cond
+#endif
+            );
+  Gtid_info(const Gtid_info& info);
+  Gtid_info& operator = (const Gtid_info& info);
+  virtual ~Gtid_info();
+
+private:
+  bool read_info(Rpl_info_handler *from);
+  bool write_info(Rpl_info_handler *to);
+};
+
+#endif /* HAVE_REPLICATION */
+#endif /* RPL_GTID_INFO_H */
