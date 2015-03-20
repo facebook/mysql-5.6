bfee27c12e276bb5ddea286a4e142f6e9a12c77d
Alexey Maykov <maykov@fb.com>
2015-03-20 10:23:03 -0700

    Implementation for indexes cardinalities
    Summary: Implemented index cardinalities by storeing per-index cardinality in each SST file. Then merging cardinalities from each index by summing them up. analyze_table currently finds the stats for all tables. I will move that procedure to the background thread.
    
    Differential Revision: https://reviews.facebook.net/D35925
    
    fbshipit-source-id: f284751fed8

--- storage/rocksdb/properties_collector.h
+++ storage/rocksdb/properties_collector.h
@@ -6,8 +6,27 @@
 class MyRocksTablePropertiesCollector
     : public rocksdb::TablePropertiesCollector {
  public:
-  virtual rocksdb::Status Add(const rocksdb::Slice& key, const rocksdb::Slice& value) override;
+  struct IndexStats {
+    enum {
+      INDEX_STATS_VERSION= 1,
+    };
+    uint32_t index_number;
+    int64_t data_size, rows;
+    std::vector<int64_t> distinct_keys_per_prefix;
+    std::string name; // name is not persisted
 
+    static std::string materialize(std::vector<IndexStats>);
+    static int unmaterialize(const std::string& s, std::vector<IndexStats>&);
+    IndexStats() : index_number(0), data_size(0), rows(0) {}
+    IndexStats(uint32_t _index_number) : index_number(_index_number), data_size(0), rows(0) {}
+    void merge(const IndexStats& s);
+  };
+
+  MyRocksTablePropertiesCollector(Table_ddl_manager* ddl_manager) :
+      ddl_manager_(ddl_manager) {
+  }
+
+  virtual rocksdb::Status Add(const rocksdb::Slice& key, const rocksdb::Slice& value) override;
   virtual rocksdb::Status Finish(rocksdb::UserCollectedProperties* properties) override;
 
   virtual const char* Name() const override {
@@ -16,31 +35,38 @@ class MyRocksTablePropertiesCollector
 
   rocksdb::UserCollectedProperties GetReadableProperties() const override;
 
-  static std::size_t GetRows(
-    uint32_t index_number,
-    const rocksdb::TablePropertiesCollection&
+  static std::map<uint32_t, IndexStats> GetStats(
+    const rocksdb::TablePropertiesCollection& collection
   );
 
  private:
-  struct IndexStats {
-    uint32_t index_number;
-    std::ptrdiff_t data_size, rows;
-  };
+  std::unique_ptr<RDBSE_KEYDEF> keydef_;
+  Table_ddl_manager* ddl_manager_;
+
   std::vector<IndexStats> stats_;
   static const char* INDEXSTATS_KEY;
+
+  // last added
+  std::string last_key_;
 };
 
 
 class MyRocksTablePropertiesCollectorFactory
     : public rocksdb::TablePropertiesCollectorFactory {
  public:
+  MyRocksTablePropertiesCollectorFactory(Table_ddl_manager* ddl_manager) :
+      ddl_manager_(ddl_manager) {
+  }
+
   virtual rocksdb::TablePropertiesCollector* CreateTablePropertiesCollector() override {
-    return new MyRocksTablePropertiesCollector();
+    return new MyRocksTablePropertiesCollector(ddl_manager_);
   }
 
   virtual const char* Name() const override {
     return "MyRocksTablePropertiesCollectorFactory";
   }
+ private:
+  Table_ddl_manager* ddl_manager_;
 };
 
 #endif
