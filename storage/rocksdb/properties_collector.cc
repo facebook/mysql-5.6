cc9f27a08cce7d631773e2caf16f42111a98d045
Alexey Maykov <maykov@fb.com>
2015-03-20 10:23:03 -0700

    Implementation for indexes cardinalities
    Summary: Implemented index cardinalities by storeing per-index cardinality in each SST file. Then merging cardinalities from each index by summing them up. analyze_table currently finds the stats for all tables. I will move that procedure to the background thread.
    
    Differential Revision: https://reviews.facebook.net/D35925

--- storage/rocksdb/properties_collector.cc
+++ storage/rocksdb/properties_collector.cc
@@ -10,18 +10,53 @@
 #include "rdb_datadic.h"
 #include "properties_collector.h"
 
+/*
+  This function is called by RocksDB for every key in the SST file
+*/
 rocksdb::Status
 MyRocksTablePropertiesCollector::Add(
-  const rocksdb::Slice& key,
-  const rocksdb::Slice& value
+  const rocksdb::Slice& key, const rocksdb::Slice& value
 ) {
   if (key.size() >= 4) {
     uint32_t index_number = read_big_uint4((const uchar*)key.data());
     if (stats_.empty() || index_number != stats_.back().index_number) {
-      stats_.push_back( IndexStats {index_number, 0l, 0l});
+      keydef_ = NULL;
+      // starting a new table
+      // add the new element into stats_
+      stats_.push_back(IndexStats(index_number));
+      keydef_ = ddl_manager_->find(index_number);
+      if (keydef_) {
+        // resize the array to the number of columns.
+        // It will be initialized with zeroes
+        stats_.back().distinct_keys_per_prefix.resize(
+          keydef_->get_m_key_parts());
+        stats_.back().name = keydef_->get_name();
+      }
+      last_key_.clear();
     }
-    stats_.back().data_size += key.size()+value.size();
-    stats_.back().rows++;
+    auto& stats = stats_.back();
+    stats.data_size += key.size()+value.size();
+    stats.rows++;
+    if (keydef_) {
+      std::size_t column = 0;
+      rocksdb::Slice last(last_key_.data(), last_key_.size());
+      if (last_key_.empty()
+          || (keydef_->compare_keys(&last, &key, &column) == 0)) {
+        assert(column <= stats.distinct_keys_per_prefix.size());
+        for (std::size_t i=column;
+             i < stats.distinct_keys_per_prefix.size(); i++) {
+          stats.distinct_keys_per_prefix[i]++;
+        }
+
+        // assign new last_key for the next call
+        // however, we only need to change the last key
+        // if one of the first n-1 columns is different
+        // If the n-1 prefix is the same, no sense in storing
+        // the new key
+        if (column < stats.distinct_keys_per_prefix.size())
+          last_key_.assign(key.data(), key.size());
+      }
+    }
   }
 
   return rocksdb::Status::OK();
@@ -29,55 +64,154 @@ MyRocksTablePropertiesCollector::Add(
 
 const char* MyRocksTablePropertiesCollector::INDEXSTATS_KEY = "__indexstats__";
 
+/*
+  This function is called by RocksDB to compute properties to store in sst file
+*/
 rocksdb::Status
 MyRocksTablePropertiesCollector::Finish(
   rocksdb::UserCollectedProperties* properties
 ) {
-  // TODO: support machine-independent format
-  std::string s((char*)stats_.data(), stats_.size()*sizeof(stats_[0]));
-  properties->insert({INDEXSTATS_KEY, s});
+  properties->insert({INDEXSTATS_KEY, IndexStats::materialize(stats_)});
   return rocksdb::Status::OK();
 }
 
+/*
+  Returns the same as above, but in human-readable way for logging
+*/
 rocksdb::UserCollectedProperties
 MyRocksTablePropertiesCollector::GetReadableProperties() const {
-  rocksdb::UserCollectedProperties ret;
+  std::string s;
+#ifdef DBUG_OFF
+  s.append("[...");
+  s.append(std::to_string(stats_.size()));
+  s.append("  records...]");
+#else
+  bool first = true;
   for (auto it : stats_) {
-    ret.insert(
-      {
-        std::to_string(it.index_number),
-        std::string("size:")+
-          std::to_string(it.data_size)+
-          std::string(", rows:") + std::to_string(it.rows)
-      }
-    );
+    if (first) {
+      first = false;
+    } else {
+      s.append(",");
+    }
+    s.append(std::to_string(it.index_number));
+    s.append(":{name:");
+    s.append(it.name);
+    s.append(", number:");
+    s.append(std::to_string(it.index_number));
+    s.append(", size:");
+    s.append(std::to_string(it.data_size));
+    s.append(", rows:");
+    s.append(std::to_string(it.rows));
+    s.append(", distincts per prefix: [");
+    for (auto num : it.distinct_keys_per_prefix) {
+      s.append(std::to_string(num));
+      s.append(" ");
+    }
+    s.append("]}");
   }
-  return ret;
+ #endif
+  return rocksdb::UserCollectedProperties{{INDEXSTATS_KEY, s}};
 }
 
-std::size_t
-MyRocksTablePropertiesCollector::GetRows(
-  uint32_t index_number,
+/*
+  Given properties stored on a bunch of SST files, reads the stats from them
+  and returns one IndexStats struct per index
+*/
+std::map<uint32_t, MyRocksTablePropertiesCollector::IndexStats>
+MyRocksTablePropertiesCollector::GetStats(
   const rocksdb::TablePropertiesCollection& collection
 ) {
-  std::size_t ret = 0ul;
+  std::map<uint32_t, IndexStats> ret;
   for (auto it : collection) {
     const auto& user_properties = it.second->user_collected_properties;
     auto it2 = user_properties.find(std::string(INDEXSTATS_KEY));
     if (it2 != user_properties.end()) {
-      auto s = it2->second;
-      const IndexStats* p = (const IndexStats*) s.data();
-      const IndexStats* p2 = (const IndexStats*) (s.data()+s.size());
-      auto it3 = std::lower_bound(
-        p, p2, index_number,
-        [](const IndexStats& s, uint32_t index_number) {
-          return s.index_number < index_number;
+      std::vector<IndexStats> stats;
+      if (IndexStats::unmaterialize(it2->second, stats) == 0) {
+        for (auto it3 : stats) {
+          ret[it3.index_number].merge(it3);
         }
-      );
-      if (it3 != p2 && it3->index_number == index_number) {
-          ret += it3->rows;
       }
     }
   }
   return ret;
+}
+
+/*
+  Stores an array on IndexStats in string
+*/
+std::string MyRocksTablePropertiesCollector::IndexStats::materialize(
+  std::vector<IndexStats> stats
+) {
+  String ret;
+  write_short(&ret, INDEX_STATS_VERSION);
+  for (auto i : stats) {
+    write_int(&ret, i.index_number);
+    assert(sizeof i.data_size <= 8);
+    write_int64(&ret, i.data_size);
+    write_int64(&ret, i.rows);
+    write_int64(&ret, i.distinct_keys_per_prefix.size());
+    for (auto num_keys : i.distinct_keys_per_prefix) {
+      write_int64(&ret, num_keys);
+    }
+  }
+
+  return std::string((char*) ret.ptr(), ret.length());
+}
+
+/*
+  Reads an array of IndexStats from a string
+*/
+int MyRocksTablePropertiesCollector::IndexStats::unmaterialize(
+  const std::string& s, std::vector<IndexStats>& ret
+) {
+  const char* p = s.data();
+  const char* p2 = s.data() + s.size();
+
+  if (p+2 > p2 || read_short(&p) != INDEX_STATS_VERSION) {
+    return 1;
+  }
+
+  while (p < p2) {
+    IndexStats stats;
+    if (p+
+       sizeof(stats.index_number)+
+       sizeof(stats.data_size)+
+       sizeof(stats.rows)+
+       sizeof(uint64) > p2)
+    {
+      return 1;
+    }
+    stats.index_number = read_int(&p);
+    stats.data_size = read_int64(&p);
+    stats.rows = read_int64(&p);
+    stats.distinct_keys_per_prefix.resize(read_int64(&p));
+    if (p+stats.distinct_keys_per_prefix.size()
+        *sizeof(stats.distinct_keys_per_prefix[0]) > p2)
+    {
+      return 1;
+    }
+    for (std::size_t i=0; i < stats.distinct_keys_per_prefix.size(); i++) {
+      stats.distinct_keys_per_prefix[i] = read_int64(&p);
+    }
+    ret.push_back(stats);
+  }
+
+  return 0;
+}
+
+/*
+  Merges one IndexStats into another. Can be used to come up with the stats
+  for the index based on stats for each sst
+*/
+void MyRocksTablePropertiesCollector::IndexStats::merge(const IndexStats& s) {
+  index_number = s.index_number;
+  rows += s.rows;
+  data_size += s.data_size;
+  if (distinct_keys_per_prefix.size() < s.distinct_keys_per_prefix.size()) {
+    distinct_keys_per_prefix.resize(s.distinct_keys_per_prefix.size());
+  }
+  for (std::size_t i=0; i < s.distinct_keys_per_prefix.size(); i++) {
+    distinct_keys_per_prefix[i] += s.distinct_keys_per_prefix[i];
+  }
 }
