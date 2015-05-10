bbaf55e72d24881cecf2c29cccc31e58bd672904
Sergei Petrunia <psergey@askmonty.org>
2015-05-10 19:42:44 +0530

    Issue #44: SELECT ... LOCK IN SHARE MODE
    Summary:
    Add support for read and read write locks in RocksDB's LockTable.
    The implementation is more concerned with correctness than with
    concurrency.
    
    Test Plan: Added unit test, enabled MTR tests
    
    Reviewers: maykov, jtolmer, yoshinorim, hermanlee4
    
    Reviewed By: hermanlee4
    
    Differential Revision: https://reviews.facebook.net/D38265

--- storage/rocksdb/rdb_locks.h
+++ storage/rocksdb/rdb_locks.h
@@ -21,60 +21,55 @@
 #include "my_sys.h"
 #include "lf.h"
 
-/*
-  A row lock that one gets from LockTable.
+class Row_lock_impl;
 
-  note: the structure is stored in LF_HASH, which will copy a part of
-  structure with memcpy(). See LockTable::init().
+
+/*
+  A Row Lock Handle.
+
+  @brief
+    A handle is either a read lock handle, or a write lock handle.
+
+  @detail
+    Internally, there is one Row_lock_impl object for a given key value.
+    The object stores info about all read/write locks that were obtained
+    for this key value.
+
+    When the user calls LockTable::release_lock(), we need to know whether
+    this he is releasing a read lock, or a write lock (he may have both at the
+    same time).
+
+    In order to achieve that, Row_lock_impl has a read Row_lock and a write
+    Row_lock. LockTable can check the type of lock, and then
+    Row_lock::get_impl() uses offsetof() pointer arithmetic to get the
+    underlying Row_lock.
 */
+
 class Row_lock
 {
 public:
-  char *rowkey; /* The key this lock is for */
-  int len; /* length of the rowkey */
+  char is_write_handle;
 
-  /* TRUE - this row_lock is being deleted */
-  bool deleted;
-
-  /* How many are waiting for the lock */
-  int waiters;
-
-  /*
-    busy==0 - means free
-    busy>=1 - means the lock is occupied, the number tells how many rows the
-              lock was acquired
-  */
-  int busy;
-
-  /*
-    Some opaque data that identifies the lock owner.  This is needed so we can
-    tell if this is the lock owner requesting the lock the second time, or
-    somebody else.
-  */
-  void *owner_data;
-
-  /*
-    One must hold this mutex
-     - when marking lock as busy or free
-     - when adding/removing himself from waiters
-    the mutex is also associated with the condition when waiting for the lock.
-  */
-  mysql_mutex_t mutex;
-  mysql_cond_t cond;
+  Row_lock_impl *get_impl(bool *is_write_handle);
 };
 
 
 /*
-  A table of locks. It is backed by a lock-free hash.
+  @brief
+  A table of row locks. It is backed by a lock-free hash.
 
-  INTERNALS
-  - Locks are exclusive.
-  - If a thread has an element in the hashtable, it has a lock.
+  @detail
+  There are two types of locks:
+  - Read lock (compatible with other read locks)
+  - Write lock (not compatible with either read or write locks).
+
+  Locks are recursive: the same thread can hold both read and write lock on the
+  same row. It can also acquire multiple read or write locks. Each get_lock()
+  call must have a matching release_lock() call.
 */
 
 class LockTable
 {
-public:
   LF_HASH lf_hash;
 
 public:
@@ -82,13 +77,18 @@ public:
             lf_hashfunc_t hashfunc);
 
   void cleanup();
-  /*
-    Before using the LockTable, each thread should get its own "pins".
-  */
+  /* Before using the LockTable, each thread should get its own "pins". */
   LF_PINS* get_pins() { return lf_hash_get_pins(&lf_hash); }
   void put_pins(LF_PINS *pins) { return lf_hash_put_pins(pins); }
 
   Row_lock* get_lock(LF_PINS *pins, const uchar* key, size_t keylen,
-                     int timeout_sec);
+                     int timeout_sec, bool is_write_lock);
+
   void release_lock(LF_PINS *pins, Row_lock *own_lock);
+
+private:
+  bool do_locking_action(LF_PINS *pins, Row_lock_impl *found_lock,
+                         int timeout_sec, bool is_write_lock);
 };
+
+
