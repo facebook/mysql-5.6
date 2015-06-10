77fafbcc7e08a36bec5abe273827510870c50274
Herman Lee <herman@fb.com>
2015-06-10 09:59:45 -0700

    Enable coredumps
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #
    
    Blame Rev:

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -777,10 +777,10 @@ static bool fix_binlog_format_after_update(sys_var *self, THD *thd,
   return false;
 }
 
-my_bool opt_core_file = FALSE;
+my_bool opt_core_file = TRUE;
 static Sys_var_mybool Sys_core_file(
        "core_file", "write a core-file on crashes",
-       GLOBAL_VAR(opt_core_file), NO_CMD_LINE, DEFAULT(FALSE));
+       GLOBAL_VAR(opt_core_file), NO_CMD_LINE, DEFAULT(TRUE));
 
 static Sys_var_enum Sys_binlog_format(
        "binlog_format", "What form of binary logging the master will "
