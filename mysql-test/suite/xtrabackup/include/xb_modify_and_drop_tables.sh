f178db08b525a7e6ae8332566e9b3e4219e04bf7
Justin Tolmer <jtolmer@fb.com>
2015-06-15 10:21:31 -0700

    Fix xtrabackup suite to not require mysql to be installed
    Summary:
    1) As currently written, the tests rely on mysql being installed on the
    server and accessible via $PATH. Change the tests to use $MYSQL instead.
    2) While at it, simplify the tests by making use of existing configuration
    rather than querying and passing around things like the server's
    sockets and ports.
    3) Improve debugging support by tailing the logs when there is a failure.
    
    Test Plan:
    Jenkins
    
    Reviewers: santoshb
    
    Reviewed By: santoshb

--- mysql-test/suite/xtrabackup/include/xb_modify_and_drop_tables.sh
+++ mysql-test/suite/xtrabackup/include/xb_modify_and_drop_tables.sh
@@ -1,6 +1,5 @@
 set -e
 
-socket=$1
 stmt='insert into db1.t_99 values (100)';
 stmt='drop table db1.t_99';
-mysql --user=root --socket=$socket -e "$stmt"
+$MYSQL --defaults-group-suffix=.1 -e "$stmt"
