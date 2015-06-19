cca6e85400919888d1f4f89c9cb78265f2dc029d
Jason Shao <jshao@dev3104.prn1.facebook.com>
2015-06-19 10:44:15 -0700

    Add new document item type and DOCUMENT() function
    Summary:
    This diff introduces a new function DOCUMENT() which takes in a JSON literal string as a parameter and the return type is the document type. Internally, it is represented as a instance of class Item_func_document. This function will validate the JSON literal; if it's not a valid JSON, it will throw a syntax error. If it is a valid JSON, the FBSON will be stored and the function will be a valid expression of document type in SQL.
    The difference between this new type and a literal string is that you can add new operators that only make sense for the new document type. It also guarantees that a document item is parsed correctly as valid JSON. Note that all document items must be created using the DOCUMENT() function.
    
    For example, SELECT DOCUMENT('{"k": "v"}') will create a document item successfully where SELECT DOCUMENT('{"k"}') will fail due to invalid JSON.
    
    Test Plan:
    New test added:
    json.type_document_value.test
    
    Reviewers: pengt
    
    Reviewed By: pengt

---
+++ sql/item_document.cc
@@ -0,0 +1,83 @@
+/*
+   Copyright (c) 2015, Facebook. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "sql_class.h"
+#include "set_var.h"
+#include "mysqld.h"
+
+Item_func_document::Item_func_document(Item *a) :Item_func(a)
+{
+  /* Check for valid JSON */
+  fbson::FbsonJsonParser parser;
+  if (a->type() != STRING_ITEM || !parser.parse(a->str_value.c_ptr()))
+    my_error(ER_INVALID_JSON, MYF(0), a->str_value.c_ptr());
+
+  length = parser.getWriter().getOutput()->getSize();
+  fbson_blob = new char[length];
+  memcpy(fbson_blob, parser.getWriter().getOutput()->getBuffer(), length);
+}
+
+Item::Type Item_func_document::type() const
+{
+  return DOCUMENT_ITEM;
+}
+
+double Item_func_document::val_real()
+{
+  return 0.0;
+}
+
+long long Item_func_document::val_int()
+{
+  return 0ll;
+}
+
+String *Item_func_document::val_str(String *str)
+{
+  fbson::FbsonToJson tojson;
+  fbson::FbsonValue *val = fbson::FbsonDocument::createValue(
+      fbson_blob, length);
+
+  const char *output = tojson.json(val);
+  char *buffer = new char[strlen(output) + 1];
+  strcpy(buffer, output);
+  str->set(buffer, strlen(output), str->charset());
+
+  return str;
+}
+
+const char *Item_func_document::val_fbson_blob() const
+{
+  fbson::FbsonValue *val = fbson::FbsonDocument::createValue(
+      fbson_blob, length);
+  return (const char*) val;
+}
+
+my_decimal *Item_func_document::val_decimal(my_decimal *decimal_buffer)
+{
+  return decimal_buffer;
+}
+
+bool Item_func_document::get_date(MYSQL_TIME *ltime, uint fuzzydate)
+{
+  return false;
+}
+
+bool Item_func_document::get_time(MYSQL_TIME *ltime)
+{
+  return false;
+}
---
+++ sql/item_document.h
@@ -0,0 +1,46 @@
+#ifndef ITEM_DOCUMENT_INCLUDED
+#define ITEM_DOCUMENT_INCLUDED
+
+/* Copyright (c) 2015, Facebook. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+
+class Item_func_document :public Item_func
+{
+private:
+  char *fbson_blob;
+  int length;
+
+public:
+  Item_func_document(Item *a);
+  ~Item_func_document() {}
+  Item::Type type() const;
+  double val_real();
+  longlong val_int();
+  String *val_str(String*);
+  my_decimal *val_decimal(my_decimal*);
+  const char *val_fbson_blob () const;
+
+  void fix_length_and_dec() {}
+  const char *func_name() const {return "document";}
+
+  bool get_date(MYSQL_TIME*, uint);
+  bool get_time(MYSQL_TIME*);
+  Item_result result_type() const { return STRING_RESULT; }
+  enum_field_types field_type() const { return MYSQL_TYPE_DOCUMENT_VALUE; }
+};
+
+#endif /* ITEM_DOCUMENT_INCLUDED */
