af6ba5e785f24d28b5a228328b0da003cec33355
Alexey Maykov <maykov@fb.com>
2015-06-19 23:13:49 -0700

    Compile rocksdb library with mysql compiler settings
    Summary:
    Instead of linking with the separately compiled rocksdb, building rocksdb sources as a part of the storage plugin. There are quite a few of benefits to doing so:
    - xtrabackup builds now
    - rocksdb has same compiler options
    - rocksdb can be compiled in the debug mode, with ASAN, jemalloc, etc...
    - overall, faster compilation
    - enabled asserts in rocksdb and found some issues
    
    This change also requires a corresponging change in our build scripts (remove -fnoexceptions and -fno-rtti). I will send a separate diff for that
    
    Reviewed By: jtolmer
    
    Differential Revision: https://reviews.facebook.net/D49671
    
    fbshipit-source-id: 6c9495187fd

--- mysql-test/suite/rocksdb/t/sst_count_rows.sh
+++ mysql-test/suite/rocksdb/t/sst_count_rows.sh
@@ -1,4 +1,5 @@
-sst_dump=../rocksdb/sst_dump
+# deduce path to sst_dump from mysqld path
+sst_dump=$2
 for f in `ls $1/mysqld.1/data/.rocksdb/*.sst`
 do
 DELETED=`$sst_dump --command=scan --output_hex --file=$f | grep " : 0" | wc -l`
