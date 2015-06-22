eb91018b8e57c5ac1ab664a5ef76a66af19071b9
Alexey Maykov <maykov@fb.com>
2015-06-22 14:19:08 -0700

    Using sliding window to count a number of sequential deletes
    Summary: Just counting a number of sequential deletes doesn't always result in the correct detection of when we need to run compaction. This diff implements a sliding window method.
    
    Differential Revision: https://reviews.facebook.net/D48063

--- mysql-test/suite/rocksdb/t/sst_count_rows.sh
+++ mysql-test/suite/rocksdb/t/sst_count_rows.sh
@@ -1,8 +1,24 @@
-# deduce path to sst_dump from mysqld path
 sst_dump=$2
+TOTAL_D=0
+TOTAL_E=0
 for f in `ls $1/mysqld.1/data/.rocksdb/*.sst`
 do
 DELETED=`$sst_dump --command=scan --output_hex --file=$f | grep " : 0" | wc -l`
 EXISTS=`$sst_dump --command=scan --output_hex --file=$f | grep " : 1" | wc -l`
-echo "${f##*/} $DELETED $EXISTS"
+TOTAL_D=$(($TOTAL_D+$DELETED))
+TOTAL_E=$(($TOTAL_E+$EXISTS))
+# echo "${f##*/} $DELETED $EXISTS"
 done
+
+if [ "$TOTAL_E" = "0" ]
+then
+  echo "No records in the database"
+  exit
+fi
+
+if [ "$TOTAL_D" = "0" ]
+then
+  echo "No more deletes left"
+else
+  echo "There are deletes left"
+fi
