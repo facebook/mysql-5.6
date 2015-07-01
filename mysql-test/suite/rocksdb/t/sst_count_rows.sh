8b213d56ea8d241c5ff3d780413edb5213f2046f
Alexey Maykov <maykov@fb.com>
2015-07-01 11:50:14 -0700

    Rerecorded and fixed some tests on rocksdb suite
    Summary:
    - made the entire suite pass
    - added check for rocksdb being the default engine to each test
    
    Differential Revision: https://reviews.facebook.net/D48939

--- mysql-test/suite/rocksdb/t/sst_count_rows.sh
+++ mysql-test/suite/rocksdb/t/sst_count_rows.sh
@@ -3,8 +3,8 @@ TOTAL_D=0
 TOTAL_E=0
 for f in `ls $1/mysqld.1/data/.rocksdb/*.sst`
 do
-DELETED=`$sst_dump --command=scan --output_hex --file=$f | grep " : 0" | wc -l`
-EXISTS=`$sst_dump --command=scan --output_hex --file=$f | grep " : 1" | wc -l`
+DELETED=`$sst_dump --command=scan --output_hex --file=$f | grep ": 0" | wc -l`
+EXISTS=`$sst_dump --command=scan --output_hex --file=$f | grep ": 1" | wc -l`
 TOTAL_D=$(($TOTAL_D+$DELETED))
 TOTAL_E=$(($TOTAL_E+$EXISTS))
 # echo "${f##*/} $DELETED $EXISTS"
