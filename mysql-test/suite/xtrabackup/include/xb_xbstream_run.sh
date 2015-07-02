8adc3ba2c81f64b761749bb9ba9c583a5ee27d63
Alexey Maykov <maykov@fb.com>
2015-07-02 14:06:12 -0700

    Fixes for the xtrabackup suite
    Summary:
    Fixed two items:
     it turned out, I broke the xtrabackup build due to compression libraries
     changed how data directory is cleaned to account for .rocksdb
    
    Test Plan: ran xtrabackup suite
    
    Reviewers: jtolmer, yoshinorim, hermanlee4
    
    Reviewed By: hermanlee4
    
    Differential Revision: https://reviews.facebook.net/D41091

--- mysql-test/suite/xtrabackup/include/xb_xbstream_run.sh
+++ mysql-test/suite/xtrabackup/include/xb_xbstream_run.sh
@@ -13,7 +13,9 @@ PATH=$mysql_dir:$PATH
 
 mkdir -p $backup_dir
 rm -rf $backup_dir/*
-rm -rf $dest_data_dir/*
+# delete and recreate the dest dir to make sure all hidden files and directories (such as .rocksdb) are blown away
+rm -rf $dest_data_dir/
+mkdir $dest_data_dir
 
 echo "innobackupex copy phase"
 if ! $MYSQL_INNOBACKUPEX $defaults_file_opt --defaults-group=mysqld.1 $ibbackup_opt $xbstream_opt $backup_dir 2> ${MYSQL_TMP_DIR}/xtrabackup_copy_log | $MYSQL_XBSTREAM -x --directory=$backup_dir
