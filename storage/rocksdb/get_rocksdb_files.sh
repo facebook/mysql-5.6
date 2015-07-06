5565d2689ea0978abc58dbf612ed626a572e7a58
Alexey Maykov <maykov@fb.com>
2015-07-06 14:36:00 -0700

    Fixing the MyRocksd build
    Summary: It turned out that build_version.cc file is automatically generated by Rocksdb makefile. Adding the generation of the file to the MyRocks build.
    
    Test Plan: ran mysqlbuild.sh
    
    Reviewers: jtolmer, hermanlee4
    
    Reviewed By: hermanlee4
    
    Differential Revision: https://reviews.facebook.net/D41271

--- storage/rocksdb/get_rocksdb_files.sh
+++ storage/rocksdb/get_rocksdb_files.sh
@@ -1,7 +1,7 @@
 MKFILE=`mktemp`
 # create and run a simple makefile
 # include rocksdb make file relative to the path of this script
-echo "include $(dirname $(readlink -f $0))/../../rocksdb/src.mk
+echo "include rocksdb/src.mk
 all:
 	@echo \$(LIB_SOURCES)" > $MKFILE
 for f in `make --makefile $MKFILE`
@@ -9,3 +9,12 @@ do
   echo ../../rocksdb/$f
 done
 rm $MKFILE
+
+date=$(date +%F)
+git_sha=$(pushd rocksdb >/dev/null && git rev-parse  HEAD 2>/dev/null && popd >/dev/null)
+echo "#include \"build_version.h\"
+const char* rocksdb_build_git_sha =
+\"rocksdb_build_git_sha:$git_sha\";
+const char* rocksdb_build_git_date =
+\"rocksdb_build_git_date:$date\";
+const char* rocksdb_build_compile_date = __DATE__;" > rocksdb/util/build_version.cc
