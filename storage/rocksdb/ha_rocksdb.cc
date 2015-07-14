bd844933deee0dfa67a00257b6f43d94aa16fce6
Herman Lee <herman@fb.com>
2015-07-14 13:57:12 -0700

    Add rudimentary ldb support for MyRocks
    Summary:
    The ldb tool can be used to dump out contents of the MyRocks sst files
    directly. The default tool doesn't understand the MyRocks comparators
    being used, so refactor this code to be shared by the ldb tool and
    MyRocks.
    
    ldb currently only supports the default column family. Dumping out
    contents of other column families and of reverse column families would
    need changes to the ldb tool to support.
    
    Test Plan:
    run unit tests; use the mysql_ldb tool to dump out the keys of an
    instance
    
    Reviewers: spetrunia, maykov, yoshinorim
    
    Reviewed By: yoshinorim
    
    Differential Revision: https://reviews.facebook.net/D42183

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -35,6 +35,7 @@
 #include "rdb_locks.h"
 #include "rdb_rowmods.h"
 
+#include "rdb_comparator.h"
 #include "rdb_cf_options.h"
 #include "rdb_cf_manager.h"
 #include "rdb_dropped_indices.h"
@@ -886,65 +887,6 @@ static void init_rocksdb_psi_keys()
   mysql_thread_register(category, all_rocksdb_threads, count);
 }
 #endif
-
-static int bytewise_compare(const rocksdb::Slice& a, const rocksdb::Slice& b)
-{
-  size_t a_size= a.size();
-  size_t b_size= b.size();
-  size_t len= (a_size < b_size) ? a_size : b_size;
-  int res;
-
-  if ((res= memcmp(a.data(), b.data(), len)))
-    return res;
-
-  /* Ok, res== 0 */
-  if (a_size != b_size)
-  {
-    return a_size < b_size? -1 : 1;
-  }
-  return 0;
-}
-
-
-/*
-  The keys are in form: {index_number} {mem-comparable-key}
-
-  (todo: knowledge about this format is shared between this class and
-   RDBSE_KEYDEF)
-*/
-
-class Primary_key_comparator : public rocksdb::Comparator
-{
-public:
-  int Compare(const rocksdb::Slice& a, const rocksdb::Slice& b) const
-  {
-    return bytewise_compare(a,b);
-  }
-
-  /* The following is not needed by RocksDB, but conceptually should be here: */
-  static ulong get_hashnr(const char *key, size_t key_len);
-  const char* Name() const { return "RocksDB_SE_v3.7"; }
-
-  //TODO: advanced funcs:
-  // - FindShortestSeparator
-  // - FindShortSuccessor
-  // for now, do-nothing implementations:
-  void FindShortestSeparator(std::string* start, const rocksdb::Slice& limit) const {}
-  void FindShortSuccessor(std::string* key) const {}
-};
-
-
-class Reverse_comparator : public rocksdb::Comparator
-{
-  int Compare(const rocksdb::Slice& a, const rocksdb::Slice& b) const
-  {
-    return -bytewise_compare(a,b);
-  }
-  const char* Name() const { return "rev:RocksDB_SE_v3.7"; }
-  void FindShortestSeparator(std::string* start, const rocksdb::Slice& limit) const {}
-  void FindShortSuccessor(std::string* key) const {}
-};
-
 
 Primary_key_comparator rocksdb_pk_comparator;
 Reverse_comparator     rocksdb_rev_pk_comparator;
