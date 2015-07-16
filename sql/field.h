b5a0985262552069760fdeb5c8a931170ce6acb3
Tian Xia <tianx@fb.com>
2015-07-15 17:47:05 -0700

    [DocStore] Improve error messages during document parsing
    Summary:
    Improved DocStore's error message significantly to include character position
    and specific error reason. It is much easier now for users to fix invalid
    documents using the error info returned.
    
    Example:
      Invalid document value at column 'json' row 20: '{"key"}', pos 7, error 'Invalid JSON object'
    
    Test Plan:
    Mostly updating the existing test results. Also added a long key string (invalid) case.
    
    Jenkins and fbson's gtest (ran separately).
    
    Reviewers: pengt
    
    Reviewed By: pengt

--- sql/field.h
+++ sql/field.h
@@ -3666,9 +3666,11 @@ public:
 
 class Field_document :public Field_blob {
   bool validate(const char *from, uint length, const CHARSET_INFO *cs);
-  void push_warning_invalid(const char *from);
+  void push_warning_invalid(const char *from,
+                            const fbson::FbsonErrInfo *err_info);
   void push_warning_too_big();
-  void push_error_invalid(const char *from);
+  void push_error_invalid(const char *from,
+                          const fbson::FbsonErrInfo *err_info);
   void push_error_too_big();
   virtual type_conversion_status store_internal(const char *from, uint length,
                                                 const CHARSET_INFO *cs);
