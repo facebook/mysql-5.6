579769a25176ca25c9484b69aa4017e2cbe991c0
Yoshinori Matsunobu <yoshinori@fb.com>
2015-07-30 16:43:52 -0700

    Store and update maximum index id into data dictionary (issue#95)
    Summary:
    This diff stores current maximum index id into data dictionary so that
    MyRocks never assigns lower values for new index ids.
    Before this diff, MyRocks may assigne same index id for new tables,
    if previous tables were completely dropped. This diff changes the
    behavior. This diff never assigns the same index so it avoids potential
    bugs to assign the same index id between existing tables and new tables.
    But it adds new limitation that it is not possible to create more than
    4,294,967,295 indexes since creating a new instance.
    
    This diff also changes initial index id from 3 to 1.
    
    Differential Revision: https://reviews.facebook.net/D43299
    
    fbshipit-source-id: 2ccf3195b13

--- mysql-test/suite/rocksdb/t/drop_table_compactions.pl
+++ mysql-test/suite/rocksdb/t/drop_table_compactions.pl
@@ -1,4 +1,36 @@
+sub print_array {
+  $str = shift;
+  @arr = @_;
+  $prev= 0;
+  foreach (@arr) {
+    if ($prev) {
+      $dummy_idx = $_ - $prev;
+    }else {
+      $dummy_idx = 0;
+    }
+    $prev= $_;
+    print "$str $dummy_idx\n";
+  }
+}
+
 while (<>) {
   $a{$1} += $2 if /Compacting away elements from dropped index (\d+): (\d+)/;
+  if (/Begin filtering dropped index (\d+)/) {
+    push @b, $1;
+  }
+  if (/Finished filtering dropped index (\d+)/) {
+    push @c, $1;
+  }
 }
-print "Compacted $_: $a{$_}\n" foreach (sort keys %a);
+$prev= 0;
+foreach (sort {$a <=> $b} keys %a){
+  if ($prev) {
+    $dummy_idx= $_ - $prev;
+  }else {
+    $dummy_idx= 0;
+  }
+  $prev= $_;
+  print "Compacted+ $dummy_idx: $a{$_}\n";
+}
+print_array("Begin filtering dropped index+", sort {$a <=> $b} @b);
+print_array("Finished filtering dropped index+", sort {$a <=> $b} @c);
