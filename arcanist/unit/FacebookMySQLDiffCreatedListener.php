36c432a91413f3c0be3452f53abd1564e891d316
Justin Tolmer <jtolmer@fb.com>
2015-09-08 17:00:20 -0700

    Replace WebScaleSQL arcanist / phabricator configuration
    Summary:
    Switch to Facebook arcanist and phabricator configuration so that
    we can move our primary development to GitHub.
    
    Test Plan:
    arc diff --plan-changes
    arc diff --big-test-queue
    
    Reviewers: santoshb
    
    Differential Revision: https://reviews.facebook.net/D46503

---
+++ arcanist/unit/FacebookMySQLDiffCreatedListener.php
@@ -0,0 +1,50 @@
+<?php
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+final class FacebookMySQLDiffCreatedListener extends PhutilEventListener {
+
+  public function register() {
+    $this->listen(ArcanistEventType::TYPE_DIFF_WASCREATED);
+  }
+
+  public function handleEvent(PhutilEvent $event) {
+    if ($event->getValue('unitResult') == ArcanistUnitWorkflow::RESULT_SKIP) {
+      return;
+    }
+
+    $workflow = $event->getValue('workflow');
+    $diffID = $event->getValue('diffID');
+
+    $big = $workflow->getArgument('big-test-queue');
+
+    $this->startJenkinsBuilds($big, $diffID);
+  }
+
+  // Enqueue Jenkins build & test
+  function startJenkinsBuilds($big, $diffID) {
+    $console = PhutilConsole::getConsole();
+
+    $server = "ci-builds.fb.com";
+    $project = "github-mysql-precommit";
+    if ($big === true) {
+      $console->writeOut("Tests will use the 'big' queue.\n");
+      $project = "github-mysql-precommit-big";
+    }
+
+    $console->writeOut(
+      '%s',
+      "Launching a build for $diffID on the Jenkins server...\n");
+
+    // Push the source up to the master repo so that Jenkins
+    // can pull it down and build it
+    $repository = "git@github.com:facebook/mysql-5.6.git";
+    $gitcmd = "git push {$repository} HEAD:refs/autobuilds/{$diffID}";
+    $git_future = new ExecFuture($gitcmd);
+    $git_future->resolvex();
+
+    // Initiate a Jenkins build for exactly this revision
+    exec("wget -q -O - "
+      . "'http://$server/job/$project"
+      . "/buildWithParameters?token=ARC&DIFF_ID={$diffID}'");
+  }
+}
