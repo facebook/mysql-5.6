03c90fd90bb8599d7ac3f7ae8cd57f63a3fe61e9
Alexey Maykov <maykov@fb.com>
2015-09-18 17:24:50 -0700

    Sped up the build
    Summary:
    We were rebuilding the rocksdb/build_version.cc and relinking
    every time. This diff changes the build process so that the
    file is recreated only if needed. The file will still be recreated
    once per day. It is fine.
    squash with https://reviews.facebook.net/D41271
    
    Test Plan:
    built twice, the file wasn't recreated,
    pulled rocksdb, the file got recreated.
    
    Reviewers: yoshinorim, jtolmer
    
    Reviewed By: jtolmer
    
    Differential Revision: https://reviews.facebook.net/D47271

--- storage/rocksdb/get_rocksdb_files.sh
+++ storage/rocksdb/get_rocksdb_files.sh
@@ -10,11 +10,17 @@ do
 done
 rm $MKFILE
 
+# create build_version.cc file. Only create one if it doesn't exists or if it is different
+# this is so that we don't rebuild mysqld every time
+bv=rocksdb/util/build_version.cc
 date=$(date +%F)
 git_sha=$(pushd rocksdb >/dev/null && git rev-parse  HEAD 2>/dev/null && popd >/dev/null)
+if [ ! -f $bv ] || [ ! `grep $git_sha $bv` ]
+then
 echo "#include \"build_version.h\"
 const char* rocksdb_build_git_sha =
 \"rocksdb_build_git_sha:$git_sha\";
 const char* rocksdb_build_git_date =
 \"rocksdb_build_git_date:$date\";
-const char* rocksdb_build_compile_date = __DATE__;" > rocksdb/util/build_version.cc
+const char* rocksdb_build_compile_date = __DATE__;" > $bv
+fi
