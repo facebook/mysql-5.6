f64a84edf2b2fda115b69d3f870aed0458cb1031
Yoshinori Matsunobu <yoshinori@fb.com>
2015-09-25 13:27:15 -0700

    Adding initial version of MyRocks online backup tool
    Summary:
    This diff adds initial version of MyRocks online backup tool
    that covers the following. It is written in Python.
    - Streaming online backup
    - Moving checkpoints
    - move-back mode so that *.logs are moved to proper locations
    "make install" installs myrockshotbackup into the same location as other
    client programs (i.e. /usr/bin).
    
    The tool has not supported configuring replication yet. More documentation
    and features will be available later.
    
    Test Plan: manually tested
    
    Reviewers: jtolmer, jkedgar, maykov, spetrunia, rven, santoshb, hermanlee4
    
    Reviewed By: hermanlee4
    
    Differential Revision: https://reviews.facebook.net/D47373

---
+++ mysql-test/suite/rocksdb_hotbackup/include/load_data.sh
@@ -0,0 +1,28 @@
+set -e
+
+# Insert 100 batches of 100 records each to a table with following schema:
+# create table db1.t1 (
+#   `id` int(10) not null auto_increment,
+#   `k` int(10),
+#   `data` varchar(255),
+#   primary key (`id`),
+#   key (`k`)
+# ) engine=innodb;
+
+MAX_INSERTS=100
+MAX_ROWS_PER_INSERT=100
+for ((i=1; i<=$MAX_INSERTS; i++));
+do
+    stmt='INSERT INTO db1.t1 values'
+    for ((j=1; j<=$MAX_ROWS_PER_INSERT; j++));
+    do
+        k=$RANDOM
+        data=$(head -c 255 /dev/urandom|tr -cd 'a-zA-Z0-9')
+        stmt=$stmt' (NULL, '$k', "'$data'")'
+        if [ $j -lt $MAX_ROWS_PER_INSERT ]; then
+            stmt=$stmt','
+        fi
+    done
+    stmt=$stmt';'
+    $MYSQL --defaults-group-suffix=.1 -e "$stmt"
+done
