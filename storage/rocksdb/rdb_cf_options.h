d33633d8f8000e4e11b19e220ab97837a77d8fe2
Jay Edgar <jkedgar@fb.com>
2015-10-01 16:10:42 -0700

    Fix lint errors in the current code.
    Summary:
    We are adding linting to the MyRocks code.  The linters only show warnings on changed lines,
    but all errors are shown whether the line has changed or not.  This diff is to fix all the errors that
    lint finds so that everybody else doesn't have to deal with them.
    
    Many of the errors that showed up were related to the header files so I also cleaned them up, reorganizing
    them so that they had a consistent order.
    
    Test Plan:
    Many of the changes are validated by the system simply building.  A few require MTR.  I didn't
    change any functionality, so no new tests are needed.
    
    Reviewers: jtolmer, hermanlee4, maykov
    
    Differential Revision: https://reviews.facebook.net/D48021

--- storage/rocksdb/rdb_cf_options.h
+++ storage/rocksdb/rdb_cf_options.h
@@ -14,6 +14,9 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
+#pragma once
+
+/* C++ system header files */
 #include <string>
 #include <unordered_map>
 
