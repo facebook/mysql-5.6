c5d1f53039ee73c93792ae0221a182f9b5c60d37
Jay Edgar <jkedgar@fb.com>
2015-10-01 16:10:42 -0700

    Fix lint errors in the current code.
    Summary:
    We are adding linting to the MyRocks code.  The linters only show warnings on changed lines,
    but all errors are shown whether the line has changed or not.  This diff is to fix all the errors that
    lint finds so that everybody else doesn't have to deal with them.
    
    Many of the errors that showed up were related to the header files so I also cleaned them up, reorganizing
    them so that they had a consistent order.
    
    Test Plan:
    Many of the changes are validated by the system simply building.  A few require MTR.  I didn't
    change any functionality, so no new tests are needed.
    
    Reviewers: maykov, jtolmer, hermanlee4
    
    Reviewed By: hermanlee4
    
    Differential Revision: https://reviews.facebook.net/D48021

--- storage/rocksdb/rdb_mutex_wrapper.h
+++ storage/rocksdb/rdb_mutex_wrapper.h
@@ -1,9 +1,32 @@
+/*
+   Copyright (c) 2015, Facebook, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#pragma once
+
+/* C++ standard header file */
 #include <chrono>
 #include <condition_variable>
 #include <functional>
 #include <mutex>
 
-#include "rocksdb/status.h"
+/* MySQL header files */
+#include "./my_sys.h"
+#include "mysql/plugin.h"
+
+/* RocksDB header files */
 #include "rocksdb/utilities/transaction_db_mutex.h"
 
 class Wrapped_mysql_mutex: public rocksdb::TransactionDBMutex {
