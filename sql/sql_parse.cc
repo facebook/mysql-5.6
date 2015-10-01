170b0c5399db453cbc4adad3bbc2e00fac6be7a7
Alexey Maykov <maykov@fb.com>
2015-10-01 14:17:19 -0700

    Undoing the hack we had to do to use jemalloc
    Summary:
    The previous version of jemalloc didn't correctly specify exception handling for some stl functions. Undoing the hack we had to get around it.
    squash with https://reviews.facebook.net/D40461
    
    Test Plan: built
    
    Reviewers: jtolmer
    
    Reviewed By: jtolmer
    
    Differential Revision: https://reviews.facebook.net/D47967

--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -113,15 +113,7 @@
 
 #ifdef HAVE_JEMALLOC
 #ifndef EMBEDDED_LIBRARY
-// TODO: uncomment below once https://github.com/jemalloc/jemalloc/issues/237
-// is fixed
-//#include <jemalloc/jemalloc.h>
-extern "C" {
-int	mallctl(const char *name, const void *oldp,
-    size_t *oldlenp, void *newp, size_t newlen);
-void malloc_stats_print(void (*write_cb)(void *,
-    const char *), void *je_cbopaque, const char *opts);
-}
+#include <jemalloc/jemalloc.h>
 #endif
 #endif
 
