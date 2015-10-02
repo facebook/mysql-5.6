59784fa0671e6d7aa4da38001414dcfc08906b40
Jay Edgar <jkedgar@fb.com>
2015-10-02 14:56:30 -0700

    Fix lint error: one variable constructor needs 'explicit'
    Summary:
    The constructor for MyRocksSnapshot takes a single parameter which could inadvertently be used as
    a type conversion constructor
    
    Squash with https://reviews.facebook.net/D45945
    
    Test Plan: MTR
    
    Reviewers: hermanlee4, jtolmer
    
    Subscribers:

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -934,7 +934,7 @@ private:
   THD* thd_= NULL;
 
 public:
-  MyRocksSnapshot(THD* thd) : thd_(thd) {
+  explicit MyRocksSnapshot(THD* thd) : thd_(thd) {
     rdb->GetEnv()->GetCurrentTime(&timestamp_);
   }
 
