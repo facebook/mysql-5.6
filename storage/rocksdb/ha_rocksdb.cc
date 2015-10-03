bdcd576486f97bd5486afdf5738b16bc1fa427aa
Yoshinori Matsunobu <yoshinori@fb.com>
2015-10-02 19:29:20 -0700

    Fixing another START TRANSACTION WITH CONSISTENT ROCKSDB SNAPSHOT inconsistency
    Summary:
    START TRANSACTION WITH CONSISTENT ROCKSDB SNAPSHOT has to create
    consistent read view (snapshot) after mysql_bin_log_lock_commits() and
    before mysql_bin_log_unlock_commits(). This means get_or_create_trx()
    has to be called there.
    
    Test Plan:
    mtr. This bug was verified by intentionally adding sleep just
    before calling mysql_bin_log_lock_commits().
    
    Reviewers: jtolmer, santoshb, hermanlee4, maykov, spetrunia, jkedgar
    
    Reviewed By: jkedgar
    
    Differential Revision: https://reviews.facebook.net/D48105

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -1671,8 +1671,6 @@ static int rocksdb_start_trx_and_assign_read_view(
     return 1;
   }
 
-  Rdb_transaction* trx= get_or_create_trx(thd);
-
   if (binlog_file)
   {
     if (binlog_pos && mysql_bin_log_is_open())
@@ -1681,6 +1679,7 @@ static int rocksdb_start_trx_and_assign_read_view(
       return 1;
   }
 
+  Rdb_transaction* trx= get_or_create_trx(thd);
   rocksdb_register_trx(hton, thd, trx);
 
   if (binlog_file)
