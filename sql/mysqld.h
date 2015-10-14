a3037b80c02736bc015e79ed578f9147b1823c93
Manuel Ung <mung@fb.com>
2015-10-14 12:33:47 -0700

    Added global statistic on number of bytes written to temporary tables data files
    Summary:
    This change adds a global statistic called 'Tmp_table_bytes_written' on the number of bytes written to temporary tables data files. This was done mostly by checking in the debugger what called my_pwrite/my_write during writes/updates/deletes.
    
    The key files were trickier to handle, because they are only written to on eviction from the key cache, so this wasn't done. I could either make some data structure that mapped file descriptors to whether they belong to temporary tables or not, or I could tag every page in the key cache with a flag, so that I would know what to do on eviction.
    
    Test Plan: mysqltest.sh temp_table_size_counter.test
    
    Reviewers: pengt, weiwei, jtolmer
    
    Reviewed By: jtolmer
    
    Subscribers: jkedgar, webscalesql-eng, domas
    
    Differential Revision: https://reviews.facebook.net/D47727

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -852,6 +852,7 @@ extern ulonglong total_query_rejected, write_query_rejected;
 extern int32 write_query_running;
 extern my_atomic_rwlock_t write_query_running_lock;
 extern ulonglong tmp_table_rpl_max_file_size;
+extern ulonglong tmp_table_bytes_written;
 
 /** The size of the host_cache. */
 extern uint host_cache_size;
