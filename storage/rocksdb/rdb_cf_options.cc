inikep/percona-server

Replace cf options file with MySQL system variable
Summary:
Currently to override the default settings for a particular column family you have to create a
separate file and point a MySQL variable (rocksdb_cf_options_file) at the file.  It would be simpler to
have a MySQL system variable that allows you to override the settings on a per-column family basis.  The
new variable is rocksdb_override_cf_options and is in the format <cf>={<options}[;<cf>={<options>}]...

Each column family should only be specified once (it would be an error to include it again) and the options
must be surrounded by curly braces.  The options inside follow the format <key>=<value>[;<key>=<value>]...
like in rocksdb_default_cf_options and will be validated by RocksDB.

Test Plan: MTR (rocksdb_cf_options has been converted from rocksdb_cf_options_file to rocksdb_override_cf_options.

Reviewers: maykov, yoshinorim, spetrunia, hermanlee4

Reviewed By: hermanlee4

Subscribers: webscalesql-eng

Differential Revision: https://reviews.facebook.net/D48627
Differential Revision: https://reviews.facebook.net/D49293

Forward-ported from Percona Server 5.7 commit dffc51725eeed4e64c9ee0680dc729b7216037b5 as part of PS-269.
Produced by the PS-269 history split.

storage/rocksdb/rdb_cf_options.cc
@@ -1,0 +1,344 @@
+/*
+   Copyright (c) 2014, SkySQL Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifdef USE_PRAGMA_IMPLEMENTATION
+#pragma implementation  // gcc: Class implementation
+#endif
+
+/* This C++ files header file */
+#include "./rdb_cf_options.h"
+
+/* C++ system header files */
+#include <string>
+
+/* MySQL header files */
+#include "./log.h"
+
+/* RocksDB header files */
+#include "rocksdb/utilities/convenience.h"
+
+/* MyRocks header files */
+#include "./ha_rocksdb.h"
+#include "./rdb_cf_manager.h"
+#include "./rdb_compact_filter.h"
+
+namespace myrocks {
+
+Rdb_pk_comparator Rdb_cf_options::s_pk_comparator;
+Rdb_rev_comparator Rdb_cf_options::s_rev_pk_comparator;
+
+bool Rdb_cf_options::init(
+    const rocksdb::BlockBasedTableOptions &table_options,
+    std::shared_ptr<rocksdb::TablePropertiesCollectorFactory> prop_coll_factory,
+    const char *const default_cf_options,
+    const char *const override_cf_options) {
+  DBUG_ASSERT(default_cf_options != nullptr);
+  DBUG_ASSERT(override_cf_options != nullptr);
+
+  m_default_cf_opts.comparator = &s_pk_comparator;
+  m_default_cf_opts.compaction_filter_factory.reset(
+      new Rdb_compact_filter_factory);
+
+  m_default_cf_opts.table_factory.reset(
+      rocksdb::NewBlockBasedTableFactory(table_options));
+
+  if (prop_coll_factory) {
+    m_default_cf_opts.table_properties_collector_factories.push_back(
+        prop_coll_factory);
+  }
+
+  if (!set_default(std::string(default_cf_options)) ||
+      !set_override(std::string(override_cf_options))) {
+    return false;
+  }
+
+  return true;
+}
+
+void Rdb_cf_options::get(const std::string &cf_name,
+                         rocksdb::ColumnFamilyOptions *const opts) {
+  DBUG_ASSERT(opts != nullptr);
+
+  // Get defaults.
+  rocksdb::GetColumnFamilyOptionsFromString(*opts, m_default_config, opts);
+
+  // Get a custom confguration if we have one.
+  Name_to_config_t::iterator it = m_name_map.find(cf_name);
+
+  if (it != m_name_map.end()) {
+    rocksdb::GetColumnFamilyOptionsFromString(*opts, it->second, opts);
+  }
+}
+
+void Rdb_cf_options::update(const std::string &cf_name,
+                            const std::string &cf_options) {
+  DBUG_ASSERT(!cf_name.empty());
+  DBUG_ASSERT(!cf_options.empty());
+
+  // Always update. If we didn't have an entry before then add it.
+  m_name_map[cf_name] = cf_options;
+
+  DBUG_ASSERT(!m_name_map.empty());
+}
+
+bool Rdb_cf_options::set_default(const std::string &default_config) {
+  rocksdb::ColumnFamilyOptions options;
+
+  if (!default_config.empty() && !rocksdb::GetColumnFamilyOptionsFromString(
+                                      options, default_config, &options)
+                                      .ok()) {
+    fprintf(stderr, "Invalid default column family config: %s\n",
+            default_config.c_str());
+    return false;
+  }
+
+  m_default_config = default_config;
+  return true;
+}
+
+// Skip over any spaces in the input string.
+void Rdb_cf_options::skip_spaces(const std::string &input, size_t *const pos) {
+  DBUG_ASSERT(pos != nullptr);
+
+  while (*pos < input.size() && isspace(input[*pos]))
+    ++(*pos);
+}
+
+// Find a valid column family name.  Note that all characters except a
+// semicolon are valid (should this change?) and all spaces are trimmed from
+// the beginning and end but are not removed between other characters.
+bool Rdb_cf_options::find_column_family(const std::string &input,
+                                        size_t *const pos,
+                                        std::string *const key) {
+  DBUG_ASSERT(pos != nullptr);
+  DBUG_ASSERT(key != nullptr);
+
+  const size_t beg_pos = *pos;
+  size_t end_pos = *pos - 1;
+
+  // Loop through the characters in the string until we see a '='.
+  for (; *pos < input.size() && input[*pos] != '='; ++(*pos)) {
+    // If this is not a space, move the end position to the current position.
+    if (input[*pos] != ' ')
+      end_pos = *pos;
+  }
+
+  if (end_pos == beg_pos - 1) {
+    // NO_LINT_DEBUG
+    sql_print_warning("No column family found (options: %s)", input.c_str());
+    return false;
+  }
+
+  *key = input.substr(beg_pos, end_pos - beg_pos + 1);
+  return true;
+}
+
+// Find a valid options portion.  Everything is deemed valid within the options
+// portion until we hit as many close curly braces as we have seen open curly
+// braces.
+bool Rdb_cf_options::find_options(const std::string &input, size_t *const pos,
+                                  std::string *const options) {
+  DBUG_ASSERT(pos != nullptr);
+  DBUG_ASSERT(options != nullptr);
+
+  // Make sure we have an open curly brace at the current position.
+  if (*pos < input.size() && input[*pos] != '{') {
+    // NO_LINT_DEBUG
+    sql_print_warning("Invalid cf options, '{' expected (options: %s)",
+                      input.c_str());
+    return false;
+  }
+
+  // Skip the open curly brace and any spaces.
+  ++(*pos);
+  skip_spaces(input, pos);
+
+  // Set up our brace_count, the begin position and current end position.
+  size_t brace_count = 1;
+  const size_t beg_pos = *pos;
+
+  // Loop through the characters in the string until we find the appropriate
+  // number of closing curly braces.
+  while (*pos < input.size()) {
+    switch (input[*pos]) {
+    case '}':
+      // If this is a closing curly brace and we bring the count down to zero
+      // we can exit the loop with a valid options string.
+      if (--brace_count == 0) {
+        *options = input.substr(beg_pos, *pos - beg_pos);
+        ++(*pos);  // Move past the last closing curly brace
+        return true;
+      }
+
+      break;
+
+    case '{':
+      // If this is an open curly brace increment the count.
+      ++brace_count;
+      break;
+
+    default:
+      break;
+    }
+
+    // Move to the next character.
+    ++(*pos);
+  }
+
+  // We never found the correct number of closing curly braces.
+  // Generate an error.
+  // NO_LINT_DEBUG
+  sql_print_warning("Mismatched cf options, '}' expected (options: %s)",
+                    input.c_str());
+  return false;
+}
+
+bool Rdb_cf_options::find_cf_options_pair(const std::string &input,
+                                          size_t *const pos,
+                                          std::string *const cf,
+                                          std::string *const opt_str) {
+  DBUG_ASSERT(pos != nullptr);
+  DBUG_ASSERT(cf != nullptr);
+  DBUG_ASSERT(opt_str != nullptr);
+
+  // Skip any spaces.
+  skip_spaces(input, pos);
+
+  // We should now have a column family name.
+  if (!find_column_family(input, pos, cf))
+    return false;
+
+  // If we are at the end of the input then we generate an error.
+  if (*pos == input.size()) {
+    // NO_LINT_DEBUG
+    sql_print_warning("Invalid cf options, '=' expected (options: %s)",
+                      input.c_str());
+    return false;
+  }
+
+  // Skip equal sign and any spaces after it
+  ++(*pos);
+  skip_spaces(input, pos);
+
+  // Find the options for this column family.  This should be in the format
+  // {<options>} where <options> may contain embedded pairs of curly braces.
+  if (!find_options(input, pos, opt_str))
+    return false;
+
+  // Skip any trailing spaces after the option string.
+  skip_spaces(input, pos);
+
+  // We should either be at the end of the input string or at a semicolon.
+  if (*pos < input.size()) {
+    if (input[*pos] != ';') {
+      // NO_LINT_DEBUG
+      sql_print_warning("Invalid cf options, ';' expected (options: %s)",
+                        input.c_str());
+      return false;
+    }
+
+    ++(*pos);
+  }
+
+  return true;
+}
+
+bool Rdb_cf_options::parse_cf_options(const std::string &cf_options,
+                                      Name_to_config_t *option_map) {
+  std::string cf;
+  std::string opt_str;
+  rocksdb::ColumnFamilyOptions options;
+
+  DBUG_ASSERT(option_map != nullptr);
+  DBUG_ASSERT(option_map->empty());
+
+  // Loop through the characters of the string until we reach the end.
+  size_t pos = 0;
+
+  while (pos < cf_options.size()) {
+    // Attempt to find <cf>={<opt_str>}.
+    if (!find_cf_options_pair(cf_options, &pos, &cf, &opt_str)) {
+      return false;
+    }
+
+    // Generate an error if we have already seen this column family.
+    if (option_map->find(cf) != option_map->end()) {
+      // NO_LINT_DEBUG
+      sql_print_warning(
+          "Duplicate entry for %s in override options (options: %s)",
+          cf.c_str(), cf_options.c_str());
+      return false;
+    }
+
+    // Generate an error if the <opt_str> is not valid according to RocksDB.
+    if (!rocksdb::GetColumnFamilyOptionsFromString(options, opt_str, &options)
+             .ok()) {
+      // NO_LINT_DEBUG
+      sql_print_warning(
+          "Invalid cf config for %s in override options (options: %s)",
+          cf.c_str(), cf_options.c_str());
+      return false;
+    }
+
+    // If everything is good, add this cf/opt_str pair to the map.
+    (*option_map)[cf] = opt_str;
+  }
+
+  return true;
+}
+
+bool Rdb_cf_options::set_override(const std::string &override_config) {
+  Name_to_config_t configs;
+
+  if (!parse_cf_options(override_config, &configs)) {
+    return false;
+  }
+
+  // Everything checked out - make the map live
+  m_name_map = configs;
+
+  return true;
+}
+
+const rocksdb::Comparator *
+Rdb_cf_options::get_cf_comparator(const std::string &cf_name) {
+  if (Rdb_cf_manager::is_cf_name_reverse(cf_name.c_str())) {
+    return &s_rev_pk_comparator;
+  } else {
+    return &s_pk_comparator;
+  }
+}
+
+std::shared_ptr<rocksdb::MergeOperator>
+Rdb_cf_options::get_cf_merge_operator(const std::string &cf_name) {
+  return (cf_name == DEFAULT_SYSTEM_CF_NAME)
+             ? std::make_shared<Rdb_system_merge_op>()
+             : nullptr;
+}
+
+void Rdb_cf_options::get_cf_options(const std::string &cf_name,
+                                    rocksdb::ColumnFamilyOptions *const opts) {
+  DBUG_ASSERT(opts != nullptr);
+
+  *opts = m_default_cf_opts;
+  get(cf_name, opts);
+
+  // Set the comparator according to 'rev:'
+  opts->comparator = get_cf_comparator(cf_name);
+  opts->merge_operator = get_cf_merge_operator(cf_name);
+}
+
+}  // namespace myrocks

facebook/mysql-5.6

(cherry picked from commit 53042e4710e9e3c4b8a99dc994375bf8bbafba2f)

storage/rocksdb/rdb_cf_options.cc
@@ -22,11 +22,10 @@
 #include "./rdb_cf_options.h"
 
 /* C++ system header files */
-#include <fstream>
-#include <algorithm>
-#include <functional>
-#include <cctype>
-#include <locale>
+#include <string>
+
+/* MySQL header files */
+#include "./log.h"
 
 /* RocksDB header files */
 #include "rocksdb/utilities/convenience.h"
@@ -48,20 +47,6 @@ void Cf_options::Get(const std::string &cf_name,
   }
 }
 
-static void trim(std::string &s) {
-  //ltrim
-  s.erase(s.begin(),
-          std::find_if(s.begin(),
-                       s.end(),
-                       std::not1(std::ptr_fun<int, int>(std::isspace))));
-  //rtrim
-  s.erase(std::find_if(s.rbegin(),
-                       s.rend(),
-                       std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
-          s.end());
-}
-
-
 bool Cf_options::SetDefault(const std::string &default_config) {
   rocksdb::ColumnFamilyOptions options;
 
@@ -79,72 +64,195 @@ bool Cf_options::SetDefault(const std::string &default_config) {
   return true;
 }
 
-bool Cf_options::ParseConfigFile(const std::string &path) {
-  // TODO: support updates?
+// Skip over any spaces in the input string.
+static void skip_spaces(const std::string& input, size_t* pos)
+{
+  while (*pos < input.size() && isspace(input[*pos]))
+    ++(*pos);
+}
 
-  if (path.empty()) {
-    return true;
+// Find a valid column family name.  Note that all characters except a
+// semicolon are valid (should this change?) and all spaces are trimmed from
+// the beginning and end but are not removed between other characters.
+static bool find_column_family(const std::string& input, size_t* pos,
+                              std::string* key)
+{
+  size_t beg_pos = *pos;
+  size_t end_pos = *pos - 1;
+
+  // Loop through the characters in the string until we see a '='.
+  for ( ; *pos < input.size() && input[*pos] != '='; ++(*pos))
+  {
+    // If this is not a space, move the end position to the current position.
+    if (input[*pos] != ' ')
+      end_pos = *pos;
   }
 
-  NameToConfig configs;
-
-  std::ifstream input(path);
-  if (!input) {
-    fprintf(stderr,
-            "Couldn't open CF config file %s\n",
-            path.c_str());
+  if (end_pos == beg_pos - 1)
+  {
+    // NO_LINT_DEBUG
+    sql_print_warning("No column family found (options: %s)", input.c_str());
     return false;
   }
 
-  rocksdb::ColumnFamilyOptions options;
+  *key = input.substr(beg_pos, end_pos - beg_pos + 1);
+  return true;
+}
 
-  std::string line;
-  while (getline(input, line)) {
-    // strip out comments
-    size_t i = line.find_first_of('#');
-    if (i != std::string::npos) {
-      line = line.substr(0, i);
-    }
-    // remove whitespace
-    trim(line);
-    if (line.empty()) {
-      continue;
-    }
-
-    i = line.find_first_of('=');
-    if (i == std::string::npos) {
-      fprintf(stderr,
-              "Invalid cf entry in file %s: %s\n",
-              path.c_str(),
-              line.c_str());
-      return false;
-    }
-    std::string cf = line.substr(0, i);
-    std::string config = line.substr(i+1);
-    trim(cf);
-    trim(config);
-
-    if (configs.find(cf) != configs.end()) {
-      fprintf(stderr,
-              "Duplicate entry for %s in file %s\n",
-              cf.c_str(),
-              path.c_str());
-      return false;
-    }
-
-    if (!rocksdb::GetColumnFamilyOptionsFromString(
-        options, config, &options).ok()) {
-      fprintf(stderr,
-              "Invalid column family config for %s in file %s: %s\n",
-              cf.c_str(),
-              path.c_str(),
-              config.c_str());
-      return false;
-    }
-
-    configs[cf] = config;
+// Find a valid options portion.  Everything is deemed valid within the options
+// portion until we hit as many close curly braces as we have seen open curly
+// braces.
+static bool find_options(const std::string& input, size_t* pos,
+                         std::string* options)
+{
+  // Make sure we have an open curly brace at the current position.
+  if (*pos < input.size() && input[*pos] != '{')
+  {
+    // NO_LINT_DEBUG
+    sql_print_warning("Invalid cf options, '{' expected (options: %s)",
+        input.c_str());
+    return false;
   }
 
+  // Skip the open curly brace and any spaces.
+  ++(*pos);
+  skip_spaces(input, pos);
+
+  // Set up our brace_count, the begin position and current end position.
+  size_t brace_count = 1;
+  size_t beg_pos = *pos;
+
+  // Loop through the characters in the string until we find the appropriate
+  // number of closing curly braces.
+  while (*pos < input.size())
+  {
+    switch (input[*pos])
+    {
+      case '}':
+        // If this is a closing curly brace and we bring the count down to zero
+        // we can exit the loop with a valid options string.
+        if (--brace_count == 0)
+        {
+          *options = input.substr(beg_pos, *pos - beg_pos);
+          ++(*pos);  // Move past the last closing curly brace
+          return true;
+        }
+
+        break;
+
+      case '{':
+        // If this is an open curly brace increment the count.
+        ++brace_count;
+        break;
+
+      default:
+        break;
+    }
+
+    // Move to the next character.
+    ++(*pos);
+  }
+
+  // We never found the correct number of closing curly braces.
+  // Generate an error.
+  // NO_LINT_DEBUG
+  sql_print_warning("Mismatched cf options, '}' expected (options: %s)",
+      input.c_str());
+  return false;
+}
+
+static bool find_cf_options_pair(const std::string& input, size_t* pos,
+                                 std::string* cf, std::string* opt_str)
+{
+  // Skip any spaces.
+  skip_spaces(input, pos);
+
+  // We should now have a column family name.
+  if (!find_column_family(input, pos, cf))
+    return false;
+
+  // If we are at the end of the input then we generate an error.
+  if (*pos == input.size())
+  {
+    // NO_LINT_DEBUG
+    sql_print_warning("Invalid cf options, '=' expected (options: %s)",
+        input.c_str());
+    return false;
+  }
+
+  // Skip equal sign and any spaces after it
+  ++(*pos);
+  skip_spaces(input, pos);
+
+  // Find the options for this column family.  This should be in the format
+  // {<options>} where <options> may contain embedded pairs of curly braces.
+  if (!find_options(input, pos, opt_str))
+    return false;
+
+  // Skip any trailing spaces after the option string.
+  skip_spaces(input, pos);
+
+  // We should either be at the end of the input string or at a semicolon.
+  if (*pos < input.size())
+  {
+    if (input[*pos] != ';')
+    {
+      // NO_LINT_DEBUG
+      sql_print_warning("Invalid cf options, ';' expected (options: %s)",
+          input.c_str());
+      return false;
+    }
+
+    ++(*pos);
+  }
+
+  return true;
+}
+
+bool Cf_options::SetOverride(const std::string &override_config)
+{
+  // TODO(???): support updates?
+
+  std::string cf;
+  std::string opt_str;
+  rocksdb::ColumnFamilyOptions options;
+  NameToConfig configs;
+
+  // Loop through the characters of the string until we reach the end.
+  size_t pos = 0;
+  while (pos < override_config.size())
+  {
+    // Attempt to find <cf>={<opt_str>}.
+    if (!find_cf_options_pair(override_config, &pos, &cf, &opt_str))
+      return false;
+
+    // Generate an error if we have already seen this column family.
+    if (configs.find(cf) != configs.end())
+    {
+      // NO_LINT_DEBUG
+      sql_print_warning(
+          "Duplicate entry for %s in override options (options: %s)",
+          cf.c_str(), override_config.c_str());
+      return false;
+    }
+
+    // Generate an error if the <opt_str> is not valid according to RocksDB.
+    if (!rocksdb::GetColumnFamilyOptionsFromString(
+          options, opt_str, &options).ok())
+    {
+      // NO_LINT_DEBUG
+      sql_print_warning(
+          "Invalid cf config for %s in override options (options: %s)",
+          cf.c_str(), override_config.c_str());
+      return false;
+    }
+
+    // If everything is good, add this cf/opt_str pair to the map.
+    configs[cf] = opt_str;
+  }
+
+  // Everything checked out - make the map live
   name_map_ = configs;
+
   return true;
 }