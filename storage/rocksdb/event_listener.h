f995f6ed85e289b681b6e11ed120b215d4ba9c82
Alexey Maykov <maykov@fb.com>
2015-10-27 11:30:54 -0700

    Improving stats collection
    Summary:
    Changing the stats computation logic to adjust stats every time a new sst file is flushed or whenever compaction is performed.
    Changes in stats are stored into data dictionary inside a separate myrocks thread. Since there is a danger of losing stats on restart, I implemented analyze table.
    @update-submodule: rocksdb
    
    Test Plan: ran stats-specific mtr tests for rocksdb
    
    Reviewers: yoshinorim, hermanlee4
    
    Reviewed By: hermanlee4
    
    Subscribers: jkedgar, webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D48495

---
+++ storage/rocksdb/event_listener.h
@@ -0,0 +1,39 @@
+/*
+   Copyright (c) 2012, Facebook, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifndef EVENT_LISTENER_H
+#define EVENT_LISTENER_H
+
+struct Table_ddl_manager;
+
+#include "rocksdb/listener.h"
+
+class MyRocksEventListener : public rocksdb::EventListener {
+ public:
+  explicit MyRocksEventListener(Table_ddl_manager* ddl_manager) :
+      ddl_manager_(ddl_manager) {
+  }
+
+  void OnCompactionCompleted(
+    rocksdb::DB *db, const rocksdb::CompactionJobInfo& ci);
+  void OnFlushCompleted(
+    rocksdb::DB* db, const rocksdb::FlushJobInfo& flush_job_info);
+
+ private:
+  Table_ddl_manager* ddl_manager_;
+};
+
+#endif
