0b7fa31c83075e9ab7900aa0f8c69588aab2ef1c
Alex Yang <alexyang@fb.com>
2015-11-02 11:17:26 -0800

    added functionality to split rocksdb errors/warn/etc to mysql error log
    Summary:
    update-submodule: rocksdb
    
    added functionality to split rocksdb errors/warn/etc to mysql error log.
    Did this by embedding default rocksdb logger inside the MyRocks logger class.
    
    Differential Revision: https://reviews.facebook.net/D50025
    
    fbshipit-source-id: 18abc47eb2c

--- storage/rocksdb/logger.h
+++ storage/rocksdb/logger.h
@@ -18,17 +18,43 @@
 #define LOGGER_H
 
 #include <log.h>
+#include <sstream>
+#include <string>
 
 class Logger : public rocksdb::Logger {
  public:
   using rocksdb::Logger::Logv;
 
-   // Write an entry to the log file with the specified format.
-  void Logv(const char* format, va_list ap) {
-    std::string f("LibRocksDB:");
-    f.append(format);
-    error_log_print(INFORMATION_LEVEL, f.c_str(), ap);
+  void Logv(const rocksdb::InfoLogLevel log_level,
+            const char* format,
+            va_list ap) {
+    if (log_level < GetInfoLogLevel()) {
+      return;
+    }
+
+    rocksdb::Logger::Logv(log_level, format, ap);
+
+    // log to MySQL if not 'info' level
+    if (log_level >= rocksdb::InfoLogLevel::WARN_LEVEL) {
+      std::string f("LibRocksDB:");
+      f.append(format);
+      error_log_print(INFORMATION_LEVEL, f.c_str(), ap);
+    }
   }
+
+  // Write an entry to the log file using the proper logger.
+  void Logv(const char* format, va_list ap) {
+    if (rocksdb_logger_) {
+      rocksdb_logger_->Logv(format, ap);
+    }
+  }
+
+  void SetRocksDBLogger(std::shared_ptr<rocksdb::Logger> logger) {
+    rocksdb_logger_ = logger;
+  }
+
+ private:
+  std::shared_ptr<rocksdb::Logger> rocksdb_logger_;
 };
 
 #endif
