71552d3caefe7e9819d8271680dd5b604718426a
Alex Yang <alexyang@fb.com>
2015-11-02 11:17:26 -0800

    added functionality to split rocksdb errors/warn/etc to mysql error log
    Summary:
    added functionality to split rocksdb errors/warn/etc to mysql error log.
    Did this by embedding default rocksdb logger inside the MyRocks logger class.
    
    Test Plan:
    Tested by running rocksdb.1st
    Noted that logs created for both rocksdb, located in
    ~/mysql/5.6/_build-5.6-Debug/mysql-test/var/mysqld.1/data/.rocksdb/LOG
    
    as well as mysql error logs inside
    ~/mysql/5.6/_build-5.6-Debug/mysql-test/var/log/mysqld.1.err
    
    Noted that we could split the logs to both logs for varying log levels.
    
    Reviewers: hermanlee4 vasilep

--- storage/rocksdb/logger.h
+++ storage/rocksdb/logger.h
@@ -18,17 +18,42 @@
 #define LOGGER_H
 
 #include <log.h>
+#include <string>
 
 class Logger : public rocksdb::Logger {
  public:
   using rocksdb::Logger::Logv;
 
-   // Write an entry to the log file with the specified format.
-  void Logv(const char* format, va_list ap) {
-    std::string f("LibRocksDB:");
-    f.append(format);
-    error_log_print(INFORMATION_LEVEL, f.c_str(), ap);
+  void Logv(const rocksdb::InfoLogLevel log_level,
+            const char* format,
+            va_list ap) {
+    static const char* kInfoLogLevelNames[5] = { "DEBUG", "INFO", "WARN",
+      "ERROR", "FATAL" };
+    if (log_level < GetInfoLogLevel()) {
+      return;
+    }
+
+    rocksdb::Logger::Logv(log_level, format, ap);
+
+    // log to MySQL if not 'info' level
+    if (log_level != rocksdb::InfoLogLevel::INFO_LEVEL) {
+      char new_format[500];
+      snprintf(new_format, sizeof(new_format) - 1, "[%s] %s",
+        kInfoLogLevelNames[log_level], format);
+
+      std::string f("LibRocksDB:");
+      f.append(new_format);
+      error_log_print(INFORMATION_LEVEL, f.c_str(), ap);
+    }
   }
+
+
+  // Write an entry to the log file using the proper logger.
+  void Logv(const char* format, va_list ap) {
+    rocksdb_logger->Logv(format, ap);
+  }
+
+  std::shared_ptr<rocksdb::Logger> rocksdb_logger;
 };
 
 #endif
