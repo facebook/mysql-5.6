64f9a3be6ab79dff2897399c1129097943bad387
Reid Horuff <reidhoruff@gmail.com>
2015-11-09 16:40:13 -0800

    Add drop indexes to information_schema
    Summary: Add drop indexes to information_schema
    
    Test Plan: none currently
    
    Reviewers: jkedgar, hermanlee4
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D50457

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -192,6 +192,24 @@ rocksdb_drop_index_wakeup_thread(THD* thd,
 }
 
 static void
+rocksdb_set_pause_background_work(THD* thd,
+                                 struct st_mysql_sys_var* var,
+                                 void* var_ptr,
+                                 const void* save)
+{
+  static bool is_paused = false;
+  bool pause_requested = *static_cast<const bool*>(save);
+  if (is_paused != pause_requested) {
+    if (pause_requested) {
+      rdb->PauseBackgroundWork();
+    } else {
+      rdb->ContinueBackgroundWork();
+    }
+  }
+  is_paused = pause_requested;
+}
+
+static void
 set_compaction_options(THD* thd,
                        struct st_mysql_sys_var* var,
                        void* var_ptr,
@@ -227,6 +245,7 @@ static uint32_t rocksdb_wal_recovery_mode;
 static char * compact_cf_name;
 static char * snapshot_dir_name;
 static my_bool rocksdb_signal_drop_index_thread;
+static my_bool rocksdb_pause_background_work = 0;
 static my_bool rocksdb_strict_collation_check = 1;
 static char * rocksdb_strict_collation_exceptions;
 static my_bool rocksdb_collect_sst_properties = 1;
@@ -659,6 +678,12 @@ static MYSQL_SYSVAR_BOOL(signal_drop_index_thread,
   "Wake up drop index thread",
   NULL, rocksdb_drop_index_wakeup_thread, FALSE);
 
+static MYSQL_SYSVAR_BOOL(pause_background_work,
+  rocksdb_pause_background_work,
+  PLUGIN_VAR_RQCMDARG,
+  "Disable all rocksdb background operations",
+  nullptr, rocksdb_set_pause_background_work, FALSE);
+
 static MYSQL_SYSVAR_BOOL(strict_collation_check,
   rocksdb_strict_collation_check,
   PLUGIN_VAR_RQCMDARG,
@@ -819,6 +844,7 @@ static struct st_mysql_sys_var* rocksdb_system_variables[]= {
 
   MYSQL_SYSVAR(compact_cf),
   MYSQL_SYSVAR(signal_drop_index_thread),
+  MYSQL_SYSVAR(pause_background_work),
   MYSQL_SYSVAR(strict_collation_check),
   MYSQL_SYSVAR(strict_collation_exceptions),
   MYSQL_SYSVAR(collect_sst_properties),
