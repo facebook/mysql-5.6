784815cacb7e96e7b4902f935ab2f51c393bf7d5
Yoshinori Matsunobu <yoshinori@fb.com>
2015-11-11 17:16:41 -0800

    Adding xbstream option into myrockshotbackup
    Summary:
    myrockshotbackup supported only tar streaming as a backup option.
    This diff adds xbstream as another option. xbstream is useful
    in some cases, especially when people want to prevent burst writes
    on destination server.
    
    Test Plan: mtr rocksdb_hotbackup, new test case xbstream.test
    
    Reviewers: santoshb, nathacof, hermanlee4
    
    Reviewed By: hermanlee4
    
    Subscribers: jtolmer, jkedgar, webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D50619

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -2596,7 +2596,6 @@ sub environment_setup {
   # ----------------------------------------------------
   my $exe_myrockshotbackup= "$bindir/scripts/myrockshotbackup";
   $ENV{'MYSQL_MYROCKSHOTBACKUP'}= native_path($exe_myrockshotbackup);
-  $ENV{'MYSQL_MYROCKSSTREAM'}= "tar -xi -C";
 
   # ----------------------------------------------------
   # Setup env so childs can execute myisampack and myisamchk
