d26f639e68b9a4ca0e55f175225c8f3509488ed1
Yoshinori Matsunobu <yoshinori@fb.com>
2015-12-10 11:31:16 -0800

    Renaming myrockshotbackup to myrocks_hotbackup
    Summary: myrocks_hotbackup sounds better naming so this diff changes it.
    
    Test Plan: mtr --suite=rocksdb_hotbackup
    
    Reviewers: hermanlee4, jkedgar, alexyang
    
    Reviewed By: alexyang
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D51825

--- mysql-test/suite/rocksdb_hotbackup/include/stream_run.sh
+++ mysql-test/suite/rocksdb_hotbackup/include/stream_run.sh
@@ -1,6 +1,7 @@
 set -e
 
-# Takes a full backup from server_1 to server_2 using myrockshotbackup streaming
+# Takes a full backup from server_1 to server_2
+# using myrocks_hotbackup streaming
 
 checkpoint_dir="${MYSQLTEST_VARDIR}/checkpoint"
 backup_dir="${MYSQLTEST_VARDIR}/backup"
@@ -12,26 +13,27 @@ PATH=$mysql_dir:$PATH
 
 mkdir -p $backup_dir
 rm -rf $backup_dir/*
-# delete and recreate the dest dir to make sure all hidden files and directories (such as .rocksdb) are blown away
+# delete and recreate the dest dir to make sure all hidden files
+# and directories (such as .rocksdb) are blown away
 rm -rf $dest_data_dir/
 mkdir $dest_data_dir
 
-echo "myrockshotbackup copy phase"
-if ! $MYSQL_MYROCKSHOTBACKUP --user='root' --port=${MASTER_MYPORT} $stream_opt \
-  --checkpoint_dir=$backup_dir 2> ${MYSQL_TMP_DIR}/myrockshotbackup_copy_log \
+echo "myrocks_hotbackup copy phase"
+if ! $MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} $stream_opt\
+  --checkpoint_dir=$backup_dir 2> ${MYSQL_TMP_DIR}/myrocks_hotbackup_copy_log \
   | tar -xi -C $backup_dir
 then
-  tail ${MYSQL_TMP_DIR}/myrockshotbackup_copy_log
+  tail ${MYSQL_TMP_DIR}/myrocks_hotbackup_copy_log
   exit 1
 fi
 mkdir ${backup_dir}/test      # TODO: Fix skipping empty directories
 
-echo "myrockshotbackup move-back phase"
-if ! $MYSQL_MYROCKSHOTBACKUP --move_back --datadir=$dest_data_dir \
+echo "myrocks_hotbackup move-back phase"
+if ! $MYSQL_MYROCKS_HOTBACKUP --move_back --datadir=$dest_data_dir \
   --rocksdb_datadir="$dest_data_dir/.rocksdb" \
   --rocksdb_waldir="$dest_data_dir/.rocksdb" --backup_dir=$backup_dir > \
-  ${MYSQL_TMP_DIR}/myrockshotbackup_moveback_log 2>&1
+  ${MYSQL_TMP_DIR}/myrocks_hotbackup_moveback_log 2>&1
 then
-  tail ${MYSQL_TMP_DIR}/myrockshotbackup_moveback_log
+  tail ${MYSQL_TMP_DIR}/myrocks_hotbackup_moveback_log
   exit 1
 fi
