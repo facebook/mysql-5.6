2b99116af96a9c2e082e200d22a9830d84618ac9
Sergei Petrunia <psergey@askmonty.org>
2015-12-22 10:20:13 -0800

    Issue#111: mysqld sometimes crashes
    Summary:
    When get_row_by_rowid() call in ha_rocksdb::secondary_index_read
    returns an error, then the error must be returned from
    ha_rocksdb::secondary_index_read, too.
    
    Differential Revision: https://reviews.facebook.net/D52113

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -3750,6 +3750,9 @@ int ha_rocksdb::secondary_index_read(int keyno, uchar *buf)
 {
   stats.rows_requested++;
 
+  /* Use STATUS_NOT_FOUND when record not found or some error occurred */
+  table->status= STATUS_NOT_FOUND;
+
   if (scan_it->Valid())
   {
     rocksdb::Slice key= scan_it->key();
@@ -3783,11 +3786,10 @@ int ha_rocksdb::secondary_index_read(int keyno, uchar *buf)
         stats.rows_read++;
         stats.rows_index_next++;
         update_row_stats(ROWS_READ);
-        return rc;
       }
+      return rc;
     }
   }
-  table->status= STATUS_NOT_FOUND;
   return HA_ERR_END_OF_FILE;
 }
 
@@ -4186,7 +4188,7 @@ int ha_rocksdb::index_read_map_impl(uchar *buf, const uchar *key,
 
   if (rc)
   {
-    table->status= STATUS_NOT_FOUND; /* Only not found error is possible */
+    table->status= STATUS_NOT_FOUND; /* This status is returned on any error */
     io_perf_end_and_record();
     DBUG_RETURN(rc);
   }
@@ -4627,11 +4629,13 @@ int ha_rocksdb::get_row_by_rowid(uchar *buf, const char *rowid, uint rowid_size)
   }
   found= !s.IsNotFound();
 
+  table->status= STATUS_NOT_FOUND;
   if (found)
   {
     last_rowkey.copy((const char*)rowid, rowid_size, &my_charset_bin);
     rc= convert_record_from_storage_format(&key_slice, buf);
-    table->status= 0;
+    if (!rc)
+      table->status= 0;
   }
   else
   {
@@ -4639,7 +4643,6 @@ int ha_rocksdb::get_row_by_rowid(uchar *buf, const char *rowid, uint rowid_size)
       Note: we don't need to unlock the row. It is intentional that we keep
       locks on rows that don't exist.
     */
-    table->status= STATUS_NOT_FOUND;
     rc= HA_ERR_KEY_NOT_FOUND;
   }
 
