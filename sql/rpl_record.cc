66a0cb387d28d89d2767c9c7f26e0ee41f3a76e8
Santosh Praneeth Banda <santoshb@fb.com>
2016-01-07 03:30:44 -0800

    Skip removed columns when creating slave conversion table
    Summary:
    Removed columns on slave were not skipped in slave conversion table. This caused
    issues when slave has a missing column along with a column whose type is different than master. Fix this.
    
    Squash with 9b723d3 Add column names to rbr table map log events.
    
    Test Plan: Added mtr tests
    
    Reviewers: jtolmer, hermanlee4, yoshinorim, jkedgar
    
    Reviewed By: jkedgar
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D52635

--- sql/rpl_record.cc
+++ sql/rpl_record.cc
@@ -202,6 +202,7 @@ int unpack_row_with_column_info(TABLE *table, uint const colcnt,
   size_t const master_null_byte_count= (bitmap_bits_set(cols) + 7) / 8;
   uchar const *pack_ptr= row_data + master_null_byte_count;
   uint null_bit_index = 0;
+  uint conv_table_col = 0;
 
   for (uint i = 0; i < tabledef->size(); ++i)
   {
@@ -210,14 +211,15 @@ int unpack_row_with_column_info(TABLE *table, uint const colcnt,
       continue;
     const char* col_name = tabledef->get_column_name(i);
     DBUG_ASSERT(col_name);
-    // use conversion table if present.
-    Field *conv_field = conv_table ? conv_table->field[i] : NULL;
-    Field *const field = conv_field ? conv_field :
-      find_field_in_table_sef(table, col_name);
+    Field *actual_field = find_field_in_table_sef(table, col_name);
     int is_null= (null_bits[null_bit_index / 8]
                   >> (null_bit_index % 8))  & 0x01;
-    if (field)
+    if (actual_field)
     {
+      // use conversion table if present.
+      Field *conv_field = conv_table ?
+                          conv_table->field[conv_table_col++] : NULL;
+      Field *const field = conv_field ? conv_field : actual_field;
       if (is_null)
       {
         // Handle null column case.
@@ -250,7 +252,7 @@ int unpack_row_with_column_info(TABLE *table, uint const colcnt,
       if (conv_field)
       {
         Copy_field copy;
-        copy.set(find_field_in_table_sef(table, col_name), conv_field, TRUE);
+        copy.set(actual_field, conv_field, TRUE);
         (*copy.do_copy)(&copy);
       }
     }
