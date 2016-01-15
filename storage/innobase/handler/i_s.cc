92a65b540877a4bca9afcd611f91e52087924055
Jay Edgar <jkedgar@fb.com>
2016-01-15 15:01:45 -0800

    Disable InnoDB information schema plugins when InnoDB is disabled
    Summary:
    Currently disabling InnoDB does not automatically disable all the INFORMATION_SCHEMA tables that are associated with InnoDB and attempting to access one of them causes a segfault.  Make sure all InnoDB INFORMATION_SCHEMA tables are disabled when InnoDB is disabled.
    
    https://github.com/facebook/mysql-5.6/issues/129
    
    Test Plan: MTR with new test that attempts to SELECT from all INNODB INFORMATION_SCHEMA tables with InnoDB disabled.
    
    Reviewers: hermanlee4, yoshinorim
    
    Reviewed By: yoshinorim
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D52959

--- storage/innobase/handler/i_s.cc
+++ storage/innobase/handler/i_s.cc
@@ -3171,6 +3171,8 @@ i_s_metrics_fill_table(
 		DBUG_RETURN(0);
 	}
 
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
+
 	i_s_metrics_fill(thd, tables->table);
 
 	DBUG_RETURN(0);
@@ -3279,6 +3281,8 @@ i_s_stopword_fill(
 	TABLE*	table = (TABLE*) tables->table;
 
 	DBUG_ENTER("i_s_stopword_fill");
+
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
 
 	fields = table->field;
 
@@ -3463,6 +3467,7 @@ i_s_fts_deleted_fill(
 	Item*		)	/*!< in: condition (ignored) */
 {
 	DBUG_ENTER("i_s_fts_deleted_fill");
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
 
 	DBUG_RETURN(i_s_fts_deleted_generic_fill(thd, tables, FALSE));
 }
@@ -3551,6 +3556,8 @@ i_s_fts_being_deleted_fill(
 {
 	DBUG_ENTER("i_s_fts_being_deleted_fill");
 
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
+
 	DBUG_RETURN(i_s_fts_deleted_generic_fill(thd, tables, TRUE));
 }
 
@@ -3563,7 +3570,8 @@ i_s_fts_being_deleted_init(
 /*=======================*/
 	void*	p)	/*!< in/out: table schema object */
 {
-	DBUG_ENTER("i_s_fts_deleted_init");
+	DBUG_ENTER("i_s_fts_being_deleted_init");
+
 	ST_SCHEMA_TABLE* schema = (ST_SCHEMA_TABLE*) p;
 
 	schema->fields_info = i_s_fts_doc_fields_info;
@@ -3817,6 +3825,8 @@ i_s_fts_index_cache_fill(
 	if (check_global_access(thd, PROCESS_ACL)) {
 		DBUG_RETURN(0);
 	}
+
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
 
 	if (!fts_internal_tbl_name) {
 		DBUG_RETURN(0);
@@ -4263,6 +4273,8 @@ i_s_fts_index_table_fill(
 		DBUG_RETURN(0);
 	}
 
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
+
 	if (!fts_internal_tbl_name) {
 		DBUG_RETURN(0);
 	}
@@ -4415,6 +4427,8 @@ i_s_fts_config_fill(
 	if (check_global_access(thd, PROCESS_ACL)) {
 		DBUG_RETURN(0);
 	}
+
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
 
 	if (!fts_internal_tbl_name) {
 		DBUG_RETURN(0);
@@ -5313,6 +5327,7 @@ i_s_innodb_buffer_page_fill(
 	Field**			fields;
 
 	DBUG_ENTER("i_s_innodb_buffer_page_fill");
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
 
 	table = tables->table;
 
@@ -5730,6 +5745,8 @@ i_s_innodb_buffer_page_fill_table(
 	if (check_global_access(thd, PROCESS_ACL)) {
 		DBUG_RETURN(0);
 	}
+
+	RETURN_IF_INNODB_NOT_STARTED(tables->schema_table_name);
 
 	trx_t*	trx = thd_to_trx_func(thd);
 	buf_pool_reference_start(trx);
