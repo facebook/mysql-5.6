611113d49ba3f95ad25d9550f15c83e36a9abc25
Tian Xia <tianx@fb.com>
2016-01-28 16:21:03 -0800

    Adding more logging for xtrabackup.xb_with_logs and changing log ouput location
    Summary:
    xtrabackup.xb_with_logs occasionally failed with data corruption
    detected during copy phase. The failure happened only on 32K page size,
    and often on the first page of a new chunk (we saw it happens at page
    256, 257, 384, 448, and 512).
    
    I add some extra logging only for XtraBackup so we can have more
    insights on the cause of page corruption.
    
    I also change the log output directory to MYSQLTEST_VARDIR/log. There
    are four log files: xtrabackup_copy_log, xtrabackup_restore_log,
    xtrabackup_moveback_log, gtid_stmt,  which should really go into the log
    directory instead of tmp directory. This also let Jenkins to save those
    log files in test failures.
    
    Test Plan: Sanity tests on xtrabackup suite
    
    Reviewers: jtolmer, santoshb
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D53559

--- mysql-test/suite/xtrabackup/include/xb_setup_replication.sh
+++ mysql-test/suite/xtrabackup/include/xb_setup_replication.sh
@@ -1,6 +1,6 @@
 set -e
 
-binlog_line=($(grep -m 1 -o "Last MySQL binlog file position 0 [0-9]*, file name .*\.[0-9]*" ${MYSQL_TMP_DIR}/xtrabackup_restore_log))
+binlog_line=($(grep -m 1 -o "Last MySQL binlog file position 0 [0-9]*, file name .*\.[0-9]*" ${MYSQLTEST_VARDIR}/log/xtrabackup_restore_log))
 binlog_pos=${binlog_line[6]%?}
 binlog_file=${binlog_line[9]}
 sql="change master to master_host='127.0.0.1', master_port=${MASTER_MYPORT}, master_user='root', master_log_file='$binlog_file', master_log_pos=$binlog_pos, master_connect_retry=1; start slave"
--- xtrabackup/src/xtrabackup.cc
+++ xtrabackup/src/xtrabackup.cc
@@ -2280,6 +2280,14 @@ read_retry:
 						    "%s seems to be "
 						    "corrupted.\n",
 						    thread_n, node->name);
+						msg("[%02u] Dump of the corrupted page %lu:\n",
+								thread_n,
+								(ulint)((offset +
+									(IB_INT64)chunk_offset)
+									>> page_size_shift));
+						buf_page_print(page+chunk_offset,
+								info.zip_size,
+								BUF_PAGE_PRINT_NO_CRASH);
 						goto error;
 					}
 					msg("[%02u] xtrabackup: "
