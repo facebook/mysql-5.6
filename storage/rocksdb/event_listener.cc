5d998107cab31569228af464e127bbbe2e5c2369
Gunnar Kudrjavets <gunnarku@fb.com>
2016-02-02 09:28:22 -0800

    Unify assertion usage under storage/rocksdb
    Summary:
    This set of trivial changes is meant to achieve two outcomes:
    
      - Unify assertion usage, //i.e.//, use just one macro for assertions and prefer MySQL's debugging infrastructure and `DBUG_*` family of macros.
      - Preemptively make sure that we don't end up in a situation where assertion macros will behave differently. Though semantically current behavior is identical between `DBUG_ASSERT()` and `assert()`, any incoming change may introduce different behavior and bugs like this are notoriously hard to track down.
    
    Test Plan:
    arc lint
    mysqlbuild.sh
    mysqlbuild.sh --release
    mysqltest.sh
    
    Reviewers: mung
    
    Reviewed By: mung
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D53613

--- storage/rocksdb/event_listener.cc
+++ storage/rocksdb/event_listener.cc
@@ -38,7 +38,7 @@ extract_index_stats(
   std::vector<MyRocksTablePropertiesCollector::IndexStats> ret;
   for (auto fn : files) {
     auto it = props.find(fn);
-    assert(it != props.end());
+    DBUG_ASSERT(it != props.end());
     std::vector<MyRocksTablePropertiesCollector::IndexStats> stats =
       MyRocksTablePropertiesCollector::GetStatsFromTableProperties(
         it->second);
