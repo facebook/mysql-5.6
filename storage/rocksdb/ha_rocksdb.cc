5ef11ab102b918d7c6c3ab56995295316b57f375
Jay Edgar <jkedgar@fb.com>
2016-02-02 10:04:10 -0800

    Fix handling of auto increment columns that are secondary keys
    Summary: The current code assumes that an auto increment column is the first column of a primary key, but MySQL allows for them to be the first column in a secondary key as well.  Fix the code to handle this scenario.
    
    https://github.com/facebook/mysql-5.6/issues/153
    
    Test Plan: MTR
    
    Reviewers: hermanlee4, alexyang, yoshinorim
    
    Subscribers:

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -2722,7 +2722,7 @@ std::vector<std::string> get_share_names(void)
 void ha_rocksdb::load_auto_incr_value()
 {
   int save_active_index= active_index;
-  active_index= table->s->primary_key;
+  active_index= table->s->next_number_index;
   uint8 save_table_status= table->status;
 
   // Do a lookup.
@@ -2745,12 +2745,12 @@ void ha_rocksdb::load_auto_incr_value()
 /* Get PK value from table->record[0]. */
 void ha_rocksdb::update_auto_incr_val()
 {
-  Field *pk_field;
+  Field *field;
   longlong new_val;
-  pk_field= table->key_info[table->s->primary_key].key_part[0].field;
+  field= table->key_info[table->s->next_number_index].key_part[0].field;
 
   my_bitmap_map *old_map= dbug_tmp_use_all_columns(table, table->read_set);
-  new_val= pk_field->val_int() + 1;
+  new_val= field->val_int() + 1;
   dbug_tmp_restore_column_map(table->read_set, old_map);
 
   /* TODO: switch to compare-and-swap? */
