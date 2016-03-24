cf074fe0a3bfdb2649b26cfff0413af36c7eaff3
Laurent Demailly <ldemailly@fb.com>
2016-03-24 13:46:28 -0700

    adding wdt support
    Summary:
    Adding script to transfer myrocks snapshot using WDT.
    Myrocks snapshot transfer is currently done using tar/xbstream. WDT is much
    faster than both of this and has many other features like rate limiting,
    download resumption, progress reporting etc.
    I measured transfer speed between a prn host and a lla host. Tar over ssh gives
    about 55 MBytes/sec. xbstream is even slower. WDT can reach upto 700 MBytes/sec.
    So, speed wise there is no comparison.
    WDT has rate limiting support. And WDT calls sync_file_range after each disk
    write, thereby not affecting disk performance. Regarding rate limiting at rack
    and cluster level, we have used BTM successfully for other deployments inside
    facebook for this purpose. Also, WDT transfers are encrypted with AES GCM.
    Implementation details -
    We use the "create-checkpoint" api of rocksdb to create a snapshot. For files in
    other directories like mtr, performance_... etc we create a hard link to the
    snapshot directory. I am assuming that this files are immutable. We then start a
    WDT transfer with timeout of 5 minutes. If the transfer does not finish within
    the timeout, it is aborted and a new snapshot is generated. Download resumption
    is built into WDT. So, we don't have to remember how much we have sent already.
    WDT's download resumption works for append-only files. CURRENT is the only file
    in rocksdb which is not append-only. So, we remove it before every transfer to
    ensure that it is resend every time.
    WDT also deletes extra files left over from previous transfers. So, if a file is
    no longer needed in the latest snapshot, it is removed.
    
    Differential Revision: https://reviews.facebook.net/D54153

--- mysql-test/suite/rocksdb_hotbackup/include/stream_run.sh
+++ mysql-test/suite/rocksdb_hotbackup/include/stream_run.sh
@@ -1,3 +1,11 @@
+if [ "$STREAM_TYPE" == 'wdt' ]; then
+  which wdt >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    # change to tar if wdt is not installed
+    STREAM_TYPE='tar'
+  fi
+fi
+
 set -e
 
 # Takes a full backup from server_1 to server_2
@@ -6,10 +14,12 @@ set -e
 checkpoint_dir="${MYSQLTEST_VARDIR}/checkpoint"
 backup_dir="${MYSQLTEST_VARDIR}/backup"
 dest_data_dir="${MYSQLTEST_VARDIR}/mysqld.2/data/"
-stream_opt="--stream=tar"
 
 mysql_dir=$(echo $MYSQL | awk '{print $1}' | xargs dirname)
 PATH=$mysql_dir:$PATH
+
+mkdir -p $checkpoint_dir
+rm -rf $checkpoint_dir/*
 
 mkdir -p $backup_dir
 rm -rf $backup_dir/*
@@ -18,22 +28,44 @@ rm -rf $backup_dir/*
 rm -rf $dest_data_dir/
 mkdir $dest_data_dir
 
+COPY_LOG="${MYSQL_TMP_DIR}/myrocks_hotbackup_copy_log"
+
+if [ "$STREAM_TYPE" == 'tar' ]; then
+  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} \
+    --stream=tar --checkpoint_dir=$checkpoint_dir 2> \
+    $COPY_LOG | tar -xi -C $backup_dir"
+elif [ "$STREAM_TYPE" == 'xbstream' ]; then
+  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} \
+    --stream=xbstream --checkpoint_dir=$checkpoint_dir 2> \
+    $COPY_LOG | xbstream -x \
+    --directory=$backup_dir"
+else
+  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --stream=wdt \
+    --port=${MASTER_MYPORT} --destination=localhost --backup_dir=$backup_dir \
+    --avg_mbytes_per_sec=10 --interval=5 \
+    --extra_wdt_sender_options='--block_size_mbytes=1' \
+    --checkpoint_dir=$checkpoint_dir 2> \
+    $COPY_LOG"
+fi
+
 echo "myrocks_hotbackup copy phase"
-if ! $MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} $stream_opt\
-  --checkpoint_dir=$backup_dir 2> ${MYSQL_TMP_DIR}/myrocks_hotbackup_copy_log \
-  | tar -xi -C $backup_dir
-then
-  tail ${MYSQL_TMP_DIR}/myrocks_hotbackup_copy_log
+eval "$BACKUP_CMD"
+if [ $? -ne 0 ]; then
+  tail $COPY_LOG
   exit 1
 fi
+
 mkdir ${backup_dir}/test      # TODO: Fix skipping empty directories
 
+MOVEBACK_LOG="${MYSQL_TMP_DIR}/myrocks_hotbackup_moveback_log"
+
 echo "myrocks_hotbackup move-back phase"
-if ! $MYSQL_MYROCKS_HOTBACKUP --move_back --datadir=$dest_data_dir \
-  --rocksdb_datadir="$dest_data_dir/.rocksdb" \
-  --rocksdb_waldir="$dest_data_dir/.rocksdb" --backup_dir=$backup_dir > \
-  ${MYSQL_TMP_DIR}/myrocks_hotbackup_moveback_log 2>&1
-then
-  tail ${MYSQL_TMP_DIR}/myrocks_hotbackup_moveback_log
+$MYSQL_MYROCKS_HOTBACKUP --move_back --datadir=$dest_data_dir \
+  --rocksdb_datadir=$dest_data_dir/.rocksdb \
+  --rocksdb_waldir=$dest_data_dir/.rocksdb \
+  --backup_dir=$backup_dir > $MOVEBACK_LOG 2>&1
+
+if [ $? -ne 0 ]; then
+  tail $MOVEBACK_LOG
   exit 1
 fi
