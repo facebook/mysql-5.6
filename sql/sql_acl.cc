5aaae0322a1972dfa1998db7ab02d88359d91507
Alex Yang <alexyang@fb.com>
2016-04-08 13:32:41 -0700

    change handshake to include rpm version
    Summary:
    Changes the handshake sent over by mysqld to "version_comment" rather than
    "version", which contains more information.
    
    Fixes t10435427.
    
    Test Plan:
    Used ngrep to verify.
    Before
    https://phabricator.fb.com/P56262853
    
    After
    https://phabricator.fb.com/P56276294
    
    Reviewers: jtolmer, anca
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D56547

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -9598,7 +9598,7 @@ static bool send_server_handshake_packet(MPVIO_EXT *mpvio,
     data_len= SCRAMBLE_LENGTH;
   }
 
-  end= strnmov(end, server_version, SERVER_VERSION_LENGTH) + 1;
+  end= strnmov(end, MYSQL_COMPILATION_COMMENT, SERVER_VERSION_LENGTH) + 1;
   int4store((uchar*) end, mpvio->thread_id);
   end+= 4;
 
