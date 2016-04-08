244c391ffd10d770e9f4f2d54a2090b36fe1f72b
Jay Edgar <jkedgar@fb.com>
2016-04-08 14:24:58 -0700

    Fix a crash with a subquery when using MyRocks and partitions
    Summary: Testing found a crash on a particular query that involves subqueries when using partitions.  The problem turned out to be that we were not setting table->status in a case when we went passed the last key.
    
    Test Plan: MTR
    
    Reviewers: yoshinorim, hermanlee4
    
    Reviewed By: hermanlee4
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D56475

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -6355,6 +6355,7 @@ int ha_rocksdb::rnd_next_with_direction(uchar *buf, bool move_forward)
   int rc;
   DBUG_ENTER("ha_rocksdb::rnd_next");
 
+  table->status= STATUS_NOT_FOUND;
   stats.rows_requested++;
 
   if (!scan_it || !scan_it->Valid())
@@ -6367,7 +6368,6 @@ int ha_rocksdb::rnd_next_with_direction(uchar *buf, bool move_forward)
 
       In this case, we should return EOF.
     */
-    table->status= STATUS_NOT_FOUND;
     DBUG_RETURN(HA_ERR_END_OF_FILE);
   }
 
@@ -6388,7 +6388,6 @@ int ha_rocksdb::rnd_next_with_direction(uchar *buf, bool move_forward)
     if (!pk_descr->covers_key(key))
     {
       rc= HA_ERR_END_OF_FILE;
-      table->status= STATUS_NOT_FOUND;
     }
     else
     {
