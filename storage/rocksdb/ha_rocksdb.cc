ad94402c5d167fe220e66cdae8615e05ba1a3b90
Anirban Rahut <arahut@fb.com>
2016-06-02 17:24:35 -0700

    Output nlocks and nwrites to rocksdb transaction status
    Summary:
    Added a line per snapshot status displaying nlocks and nwrites.
    Public functions had to be added to the Rdb_Transaction class to make
    them available. One get function was not const correct, made it so.
    
    Test Plan:
    1. updated rocksdb.show_engine test with new output. test passes now
    2. tested by creating a transaction and seeing that nwrites and nlocks
       is output correctly
    
    Reviewers: jkedgar, vasilep
    
    Reviewed By: vasilep
    
    Subscribers: dhruba, pritamdamania, yoshinorim, lgalanis, webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D59115

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -1532,7 +1532,9 @@ public:
       m_rocksdb_tx->SetLockTimeout(m_timeout_sec * 1000);
   }
 
-  ulonglong get_n_writes() { return m_write_count; }
+  ulonglong get_write_count() const { return m_write_count; }
+
+  ulonglong get_lock_count() const { return m_lock_count; }
 
   void release_lock(rocksdb::ColumnFamilyHandle* column_family,
                     const std::string &rowkey)
@@ -1558,7 +1560,7 @@ public:
 
   bool commit()
   {
-    if (get_n_writes() == 0)
+    if (get_write_count() == 0)
     {
       rollback();
       return false;
@@ -1709,7 +1711,7 @@ public:
   */
   bool flush_batch()
   {
-    if (get_n_writes() == 0)
+    if (get_write_count() == 0)
       return false;
 
     /* Commit the current transaction */
@@ -2318,9 +2320,11 @@ class Rdb_snapshot_status : public Rdb_tx_list_walker
       THD* thd = tx->get_thd();
 
       m_data += format_string("---SNAPSHOT, ACTIVE %lld sec\n"
-                              "MySQL thread id %lu, OS thread handle %p\n",
+                              "MySQL thread id %lu, OS thread handle %p\n"
+                              "lock count %llu, write count %llu\n",
                               curr_time - snapshot_timestamp,
-                              my_core::thd_get_thread_id(thd), thd);
+                              my_core::thd_get_thread_id(thd), thd,
+                              tx->get_lock_count(), tx->get_write_count());
     }
   }
 };
@@ -6079,7 +6083,7 @@ bool ha_rocksdb::do_bulk_commit(Rdb_transaction *tx)
 {
   DBUG_ASSERT(tx != nullptr);
   return commit_in_the_middle() &&
-         tx->get_n_writes() >= THDVAR(table->in_use, bulk_load_size) &&
+         tx->get_write_count() >= THDVAR(table->in_use, bulk_load_size) &&
          tx->flush_batch();
 }
 
