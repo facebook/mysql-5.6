fbc8e064c2ade16cc922e6963ed68189518073d2
Justin Tolmer <jtolmer@fb.com>
2016-07-29 12:29:17 -0700

    Switch to new determinator with stack diff support
    Summary:
    When working with stacked diffs, we get better test behavior
    from the new determinator.
    
    Test Plan: Check Phabricator to see if tests ran correctly.
    
    Reviewers: gunnarku
    
    Reviewed By: gunnarku
    
    Subscribers: webscalesql-eng
    
    Differential Revision: https://reviews.facebook.net/D61329

--- arcanist/unit/FacebookMySQLDiffCreatedListener.php
+++ arcanist/unit/FacebookMySQLDiffCreatedListener.php
@@ -1,68 +1,41 @@
-<?php
+<?hh
 // Copyright 2004-present Facebook. All Rights Reserved.
-
-// Internal diff determinator for Sandcastle. The file will exist only in the
-// FB-specific environment.
-define("MYSQL_INTERNAL_DIFF_DETERMINATOR",
-       "../tools/sandcastle/mysql_diff_determinator.php");
 
 final class FacebookMySQLDiffCreatedListener extends PhutilEventListener {
 
-  public function register() {
-    assert_options(ASSERT_BAIL, true);
+  // File containing the implementation of the event handler that is shared
+  // across our multiple repositories.
+  const string
+    MYSQL_DIFF_HANDLER_FILE = "../tools/sandcastle/MysqlDiffHandler.php";
+
+  public function register(): void {
     $this->listen(ArcanistEventType::TYPE_DIFF_WASCREATED);
   }
 
-  public function handleEvent(PhutilEvent $event) {
-    if ($event->getValue('unitResult') == ArcanistUnitWorkflow::RESULT_SKIP) {
-      return;
-    }
+  public function handleEvent(PhutilEvent $event): void {
     $workflow = $event->getValue('workflow');
-    assert($workflow);
+    $working_copy = $workflow->getWorkingCopy();
 
-    $diffID = $event->getValue('diffID');
-    assert(is_numeric($diffID));
+    $full_diff_handler_path = $working_copy->getProjectPath(
+      FacebookMySQLDiffCreatedListener::MYSQL_DIFF_HANDLER_FILE,
+    );
 
-    if ($this->shouldStartBuilds($workflow)) {
-      $this->startSandcastleBuilds($workflow, $diffID);
+    // If we have acccess to the shared handler, call it to start tests.
+    if (Filesystem::pathExists($full_diff_handler_path)) {
+      // Do the magic that allows us to call the handler shared by
+      // multiple repositories.
+      require_once ($full_diff_handler_path);
+      $handler = new MysqlDiffHandler(
+        $event,
+        MysqlLegoConstants::MYSQL_GITHUB,
+        './tools/',
+      );
+      $handler->handleEvent();
     } else {
       $console = PhutilConsole::getConsole();
-      $console->writeOut("Skipping launch of tests. Ask a Facebook " .
-                         "reviewer to launch tests for %d.\n", $diffID);
+      $console->writeOut(
+        "Failed to detect Facebook-internal tools. Skipping launch of tests.\n",
+      );
     }
-  }
-
-  // Should we try to launch CI builds / tests?
-  function shouldStartBuilds($workflow) {
-    // If this sentinel file exists, assume that we have access to
-    // the CI servers.
-    $sentinel = MYSQL_INTERNAL_DIFF_DETERMINATOR;
-    $working_copy = $workflow->getWorkingCopy();
-    assert($working_copy);
-
-    return Filesystem::pathExists($working_copy->getProjectPath($sentinel));
-  }
-
-  // Start Sandcastle build and test execution.
-  function startSandcastleBuilds($workflow, $diffID) {
-    assert($workflow);
-    assert(strlen($diffID) > 0);
-    assert(is_numeric($diffID));
-
-    $username = $workflow->getUserName();
-    assert(strlen($username) > 0);
-
-    $working_copy = $workflow->getWorkingCopy();
-    assert($working_copy);
-
-    $full_diff_det_path = $working_copy->getProjectPath(
-        MYSQL_INTERNAL_DIFF_DETERMINATOR);
-
-    // All the real work will be done by the code in the file referenced
-    // below. File will be there because shouldStartBuilds() guards the
-    // execution of this section of code.
-    require($full_diff_det_path);
-
-    StartSandCastleBuild($diffID, $username);
   }
 }
