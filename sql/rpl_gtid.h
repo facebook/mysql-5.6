f38db41e14ae6f8d641fcc95566ad1d4155775e1
Abhinav Sharma <abhinavsharma@fb.com>
2016-11-01 14:01:43 -0700

    RBR's idempotent mode to make slave crash safe
    Summary:
    Currently, to ensure crash safety on slaves a DB->GTID map is maintained
    which is updated after every commit. The map is required to identify holes
    in the sequence of gtids caused by using MTS. In RBR we can make use of
    idempotency of row events to handle holes instead. We only track the max gtid
    encountered and after a crash we turn on idempotency for all transactions which
    have gtid <= max gtid. If any trx was already executed in this range it's okay
    because we've turned on idempotent mode.
    
    Test Plan:
    Created a new suite rpl_recovery with some existing and some new test cases.
    Added a new combination in this suite to run in idempotent recovery mode.

--- sql/rpl_gtid.h
+++ sql/rpl_gtid.h
@@ -817,6 +817,9 @@ struct Gtid
   /// Returns true if this Gtid has the same sid and gno as 'other'.
   bool equals(const Gtid &other) const
   { return sidno == other.sidno && gno == other.gno; }
+  /// Returns true if this Gtid is greater than 'other'.
+  bool greater_than(const Gtid &other) const
+  { return sidno == other.sidno && gno > other.gno; }
   /**
     Parses the given string and stores in this Gtid.
 
