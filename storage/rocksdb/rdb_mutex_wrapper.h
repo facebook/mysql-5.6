ff87bfc320ff5915e702ba5d5c06f418d2af6ca7
Sidi Fu <sidifu@devvm27355.prn1.facebook.com>
2016-12-16 14:14:43 -0800

    const fix for storage/rocksdb (first half) (#478) (#478)
    Summary:
    First harf of the const fix for all of the files under storage/rocksdb folder.
    
    This pull request includes the changes from the previous two requests that are canceled.
    Closes https://github.com/facebook/mysql-5.6/pull/478
    
    Differential Revision: D4341822
    
    Pulled By: gunnarku

--- storage/rocksdb/rdb_mutex_wrapper.h
+++ storage/rocksdb/rdb_mutex_wrapper.h
@@ -64,7 +64,7 @@ class Rdb_mutex: public rocksdb::TransactionDBMutex {
   friend class Rdb_cond_var;
 
 #ifndef STANDALONE_UNITTEST
-  void set_unlock_action(PSI_stage_info *old_stage_arg);
+  void set_unlock_action(const PSI_stage_info* const old_stage_arg);
   std::unordered_map<THD*, std::shared_ptr<PSI_stage_info>> m_old_stage_info;
 #endif
 };
@@ -87,7 +87,7 @@ class Rdb_cond_var: public rocksdb::TransactionDBCondVar {
   // Returns non-OK if TransactionDB should stop waiting and fail the operation.
   // May return OK spuriously even if not notified.
   virtual rocksdb::Status
-  Wait(std::shared_ptr<rocksdb::TransactionDBMutex> mutex) override;
+  Wait(const std::shared_ptr<rocksdb::TransactionDBMutex> mutex) override;
 
   // Block current thread until condition variable is notifiesd by a call to
   // Notify() or NotifyAll(), or if the timeout is reached.
@@ -102,7 +102,7 @@ class Rdb_cond_var: public rocksdb::TransactionDBCondVar {
   //  fail the operation.
   // May return OK spuriously even if not notified.
   virtual rocksdb::Status
-  WaitFor(std::shared_ptr<rocksdb::TransactionDBMutex> mutex,
+  WaitFor(const std::shared_ptr<rocksdb::TransactionDBMutex> mutex,
           int64_t timeout_time) override;
 
   // If any threads are waiting on *this, unblock at least one of the
