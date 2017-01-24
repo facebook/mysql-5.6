percona/percona-server

Add gcc function attributes to MyRocks code with a sprinkle of assertions
Summary:
Annotate MyRocks to document the current contracts using function attributes. We don't have the full power of SAL (Standard Annotation Language), but this is still better than nothing. Both Clang and GCC understand the attributes and `include/my_attribute.h` has the magic to disable them for compilers which don't support those directives.

The most useful thing we get out of these changes are the annotations using `warn_unused_result` because that's the only thing which `gcc` can reasonably enforce.

Using `nonnull` will help compiler to only catch the basic cases (passing `nullptr` or `NULL` literally) and rest of the attributes have the main benefit of guiding the compiler to apply more complex optimizations. Enhanced error deduction will require running a dedicated static analysis tool on the codebase (`scan-build` at some point in future?).

Where applicable add more assertions to code to increase the amount of internal consistency checks. Fix a couple of small bugs found by `warn_unused_result` as well.

Test Plan:
Classics:

```
mysqlbuild.sh
mysqltest.sh
mysqltest.sh --testset=RocksDB

mysqlbuild.sh --release
mysqltest.sh --release
mysqltest.sh --release --testset=RocksDB
```

Reviewers: alexyang, jkedgar

Reviewed By: jkedgar

Subscribers: MarkCallaghan, webscalesql-eng

Differential Revision: https://reviews.facebook.net/D55521

storage/rocksdb/ha_rocksdb_proto.h
@@ -26,26 +26,35 @@ class Column_family_manager;
 rocksdb::DB *rocksdb_get_rdb();
 Column_family_manager& rocksdb_get_cf_manager();
 rocksdb::BlockBasedTableOptions& rocksdb_get_table_options();
-void get_cf_options(const std::string &cf_name, rocksdb::ColumnFamilyOptions *opts);
+
+void get_cf_options(const std::string &cf_name,
+  rocksdb::ColumnFamilyOptions *opts) __attribute__((__nonnull__));
 int rocksdb_normalize_tablename(const char *tablename,
-                                StringBuffer<256> *strbuf);
+  StringBuffer<256> *strbuf)
+  __attribute__((__nonnull__, __warn_unused_result__));
 int rocksdb_split_normalized_tablename(const char *fullname,
                                        StringBuffer<256> *dbbuf,
                                        StringBuffer<256> *tablebuf,
-                                       StringBuffer<256> *partitionbuf);
+                                       StringBuffer<256> *partitionbuf)
+  __attribute__((__nonnull__, __warn_unused_result__));
 std::vector<std::string> get_share_names(void);
 
 int rocksdb_get_share_perf_counters(const char *tablename,
-                                    SHARE_PERF_COUNTERS *counters);
+  SHARE_PERF_COUNTERS *counters) __attribute__((__nonnull__(2)));
 
 void request_save_stats();
 
 class Dict_manager;
-Dict_manager *get_dict_manager(void);
+Dict_manager *get_dict_manager(void)
+  __attribute__((__returns_nonnull__, __warn_unused_result__));
+
 class Table_ddl_manager;
-Table_ddl_manager *get_ddl_manager(void);
+Table_ddl_manager *get_ddl_manager(void)
+  __attribute__((__returns_nonnull__, __warn_unused_result__));
+
 class Binlog_info_manager;
-Binlog_info_manager *get_binlog_manager(void);
+Binlog_info_manager *get_binlog_manager(void)
+  __attribute__((__returns_nonnull__, __warn_unused_result__));
 
 }  // namespace myrocks
 

facebook/mysql-5.6

storage/rocksdb/ha_rocksdb_proto.h
@@ -26,26 +26,35 @@ class Column_family_manager;
 rocksdb::DB *rocksdb_get_rdb();
 Column_family_manager& rocksdb_get_cf_manager();
 rocksdb::BlockBasedTableOptions& rocksdb_get_table_options();
-void get_cf_options(const std::string &cf_name, rocksdb::ColumnFamilyOptions *opts);
+
+void get_cf_options(const std::string &cf_name,
+  rocksdb::ColumnFamilyOptions *opts) MY_ATTRIBUTE((__nonnull__));
 int rocksdb_normalize_tablename(const char *tablename,
-                                StringBuffer<256> *strbuf);
+  StringBuffer<256> *strbuf)
+  MY_ATTRIBUTE((__nonnull__, __warn_unused_result__));
 int rocksdb_split_normalized_tablename(const char *fullname,
                                        StringBuffer<256> *dbbuf,
                                        StringBuffer<256> *tablebuf,
-                                       StringBuffer<256> *partitionbuf);
+                                       StringBuffer<256> *partitionbuf)
+  MY_ATTRIBUTE((__nonnull__, __warn_unused_result__));
 std::vector<std::string> get_share_names(void);
 
 int rocksdb_get_share_perf_counters(const char *tablename,
-                                    SHARE_PERF_COUNTERS *counters);
+  SHARE_PERF_COUNTERS *counters) MY_ATTRIBUTE((__nonnull__(2)));
 
 void request_save_stats();
 
 class Dict_manager;
-Dict_manager *get_dict_manager(void);
+Dict_manager *get_dict_manager(void)
+  MY_ATTRIBUTE((__returns_nonnull__, __warn_unused_result__));
+
 class Table_ddl_manager;
-Table_ddl_manager *get_ddl_manager(void);
+Table_ddl_manager *get_ddl_manager(void)
+  MY_ATTRIBUTE((__returns_nonnull__, __warn_unused_result__));
+
 class Binlog_info_manager;
-Binlog_info_manager *get_binlog_manager(void);
+Binlog_info_manager *get_binlog_manager(void)
+  MY_ATTRIBUTE((__returns_nonnull__, __warn_unused_result__));
 
 }  // namespace myrocks
 