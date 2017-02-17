facebook/mysql-5.6

commit 3b47daef57f7429894bd655141272ee528485669

Remove special formatting for user name when writing to audit plugin log
Summary: As titled

Reviewed By: cevans87

Differential Revision: D4562120

fbshipit-source-id: 56bcd66

sql/sql_audit.h
@@ -78,15 +78,14 @@ void mysql_audit_general_log(THD *thd, const char *cmd, uint cmdlen,
     ha_rows resultrows= 0;
     longlong affectrows= 0;
     int error_code= 0; 
-    char user_buff[MAX_USER_HOST_SIZE + 1];
     uint userlen, databaselen, queryattrlen;
     const char *user, *database, *queryattr;
     time_t time= (time_t) thd->start_time.tv_sec;
 
     if (thd)
     {
-      user= user_buff;
-      userlen= make_user_name(thd, user_buff);
+      user= thd->security_ctx->user;
+      userlen= strlen(thd->security_ctx->user);
       if (!query_len)
       {
         /* no query specified, fetch from THD */
@@ -165,7 +164,6 @@ void mysql_audit_general(THD *thd, uint event_subtype,
     uint msglen= msg ? strlen(msg) : 0;
     uint userlen, databaselen, queryattrlen;
     const char *user, *database, *queryattr;
-    char user_buff[MAX_USER_HOST_SIZE];
     CSET_STRING query;
     MYSQL_LEX_STRING ip, host, external_user, sql_command;
     // Result rows will hold the number of rows sent to the client.
@@ -187,8 +185,8 @@ void mysql_audit_general(THD *thd, uint event_subtype,
                            thd->rewritten_query.charset());
       else
         query= thd->query_string;
-      user= user_buff;
-      userlen= make_user_name(thd, user_buff);
+      user= thd->security_ctx->user;
+      userlen= strlen(thd->security_ctx->user);
       affectrows= thd->get_row_count_func();
       resultrows= thd->get_sent_row_count();
       ip.str= (char *) thd->security_ctx->get_ip()->ptr();

commit 590a66de797b3a32cf4ed2786aee97c64da25932

Fix crash in audit plugin path
Summary:
This commit https://github.com/facebook/mysql-5.6/commit/3b47dae introduced a crash in the audit plugin code path during server initialization, because it was calling strlen on a nullptr. Fix by doing a check first.

Squash with: D4562120

Test Plan: Server does not crash.

Reviewers: gunnarku, camerone, abalsara

Reviewed By: abalsara

Subscribers: abalsara, webscalesql-eng@fb.com

Differential Revision: https://phabricator.intern.facebook.com/D4580019

Signature: t1:4580019:1487358685:c17947e68d6e76288f3ee1d1d1ff77e9e0c0a63a

sql/sql_audit.h
@@ -85,7 +85,7 @@ void mysql_audit_general_log(THD *thd, const char *cmd, uint cmdlen,
     if (thd)
     {
       user= thd->security_ctx->user;
-      userlen= strlen(thd->security_ctx->user);
+      userlen= thd->security_ctx->user ? strlen(thd->security_ctx->user) : 0;
       if (!query_len)
       {
         /* no query specified, fetch from THD */
@@ -186,7 +186,7 @@ void mysql_audit_general(THD *thd, uint event_subtype,
       else
         query= thd->query_string;
       user= thd->security_ctx->user;
-      userlen= strlen(thd->security_ctx->user);
+      userlen= thd->security_ctx->user ? strlen(thd->security_ctx->user) : 0;
       affectrows= thd->get_row_count_func();
       resultrows= thd->get_sent_row_count();
       ip.str= (char *) thd->security_ctx->get_ip()->ptr();