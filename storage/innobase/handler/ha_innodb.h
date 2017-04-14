55a3af861ce107e9e1c5f96f266d63cb6228ae40
Jay Edgar <jkedgar@fb.com>
2017-04-14 12:37:02 -0700

    FB8-106: Add more information to duplicate key error
    Jira issue: https://jira.percona.com/browse/FB8-106
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/2c7f717
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/f3a0ed0
    
    Summary: The current error information for duplicate key lists the key value and key name.  In some scenarios this may not be sufficient.  Add a variable that, when enabled, includes the tableame for the key and the query text (or up to 512 bytes of it) into the error message.  The new variable, improved_dup_key_error, will change the output error message to contain this new information.
    
    Originally Reviewed By: hermanlee
    
    fbshipit-source-id: 1c0cf40

--- storage/innobase/handler/ha_innodb.h
+++ storage/innobase/handler/ha_innodb.h
@@ -67,6 +67,8 @@ struct INNOBASE_SHARE {
 /** Prebuilt structures in an InnoDB table handle used within MySQL */
 struct row_prebuilt_t;
 
+struct ha_innobase_inplace_ctx;
+
 namespace dd {
 namespace cache {
 class Dictionary_client;
@@ -409,6 +411,10 @@ class ha_innobase : public handler {
                                   bool commit, const dd::Table *old_dd_tab,
                                   dd::Table *new_dd_tab);
   /** @} */
+  MY_ATTRIBUTE((warn_unused_result)) bool commit_try_rebuild(
+      Alter_inplace_info *ha_alter_info, ha_innobase_inplace_ctx *ctx,
+      TABLE *altered_table, const TABLE *old_table, trx_t *trx,
+      const char *table_name);
 
   bool check_if_incompatible_data(HA_CREATE_INFO *info, uint table_changes);
 
