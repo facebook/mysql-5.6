e84f58ca2bb50e4afc94647e3757009a894b4a91
Archit Mishra <armishra@fb.com>
2017-07-21 11:23:32 -0700

    added lock_wait_timeout and deadlock stats to rocksdb (#653)
    Summary:
    Addresses #299. Changes:
    * Fixed InnoDB table_stats bug that increments weirdly when consecutively querying for lock_waits
    * added rocksdb stat to count lock_wait_timeouts
    * added test case that queries this stat in various ways.
    Closes https://github.com/facebook/mysql-5.6/pull/653
    
    Differential Revision: D5409795
    
    fbshipit-source-id: 7567b2acdff

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -286,6 +286,8 @@ struct Rdb_table_handler {
   char *m_table_name;
   uint m_table_name_length;
   int m_ref_count;
+  atomic_stat<int> m_lock_wait_timeout_counter;
+  atomic_stat<int> m_deadlock_counter;
 
   my_core::THR_LOCK m_thr_lock; ///< MySQL latch needed by m_db_lock
 
