27149c8ba5e77496dab539e85d955f7b5dbf9be2
Andrew Kryczka <andrewkr@fb.com>
2017-09-21 14:32:46 -0700

    address comments

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -408,6 +408,10 @@ static void rocksdb_set_collation_exception_list(THD *thd,
                                                  void *var_ptr,
                                                  const void *save);
 
+static int rocksdb_validate_update_cf_options(THD *thd,
+                                              struct st_mysql_sys_var *var,
+                                              void *save,
+                                              st_mysql_value *value);
 void rocksdb_set_update_cf_options(THD *thd,
                                    struct st_mysql_sys_var *var,
                                    void *var_ptr,
@@ -1164,7 +1168,8 @@ static MYSQL_SYSVAR_STR(override_cf_options, rocksdb_override_cf_options,
 static MYSQL_SYSVAR_STR(update_cf_options, rocksdb_update_cf_options,
                         PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_MEMALLOC |
                             PLUGIN_VAR_ALLOCATED,
-                        "Option updates per column family for RocksDB", nullptr,
+                        "Option updates per column family for RocksDB",
+                        rocksdb_validate_update_cf_options,
                         rocksdb_set_update_cf_options, nullptr);
 
 enum rocksdb_flush_log_at_trx_commit_type : unsigned int {
@@ -11961,23 +11966,30 @@ static void rocksdb_set_max_background_jobs(THD *thd,
   RDB_MUTEX_UNLOCK_CHECK(rdb_sysvars_mutex);
 }
 
+static int rocksdb_validate_update_cf_options(THD *thd,
+                                              struct st_mysql_sys_var *var,
+                                              void *save,
+                                              st_mysql_value *value) {
+  // just disallow setting to null; otherwise same as default string checker
+  char buff[STRING_BUFFER_USUAL_SIZE];
+  const char *str;
+  int length;
+
+  length= sizeof(buff);
+  if ((str= value->val_str(value, buff, &length)))
+    str= thd->strmake(str, length);
+  *(const char**)save= str;
+  return str == nullptr;
+}
+
 void rocksdb_set_update_cf_options(THD *const /* unused */,
                                    struct st_mysql_sys_var *const /* unused */,
                                    void *const var_ptr,
                                    const void *const save) {
   const char *const val = *static_cast<const char *const *>(save);
-
-  if (!val) {
-    // NO_LINT_DEBUG
-    sql_print_warning("MyRocks: NULL is not a valid option for updates to "
-                      "column family settings.");
-    *reinterpret_cast<char**>(var_ptr) = nullptr;
-    return;
-  }
+  DBUG_ASSERT(val != nullptr);
 
   RDB_MUTEX_LOCK_CHECK(rdb_sysvars_mutex);
-
-  DBUG_ASSERT(val != nullptr);
 
   // Do the real work of applying the changes.
   Rdb_cf_options::Name_to_config_t option_map;
