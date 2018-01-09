45d38a3d5a8de33cd2056eafe10707c86e5092ba
Joseph (Jy) Yaworski <jyw@surefirelocal.com>
2018-01-09 14:51:20 -0500

    Support building with libressl

--- vio/viosslfactories.c
+++ vio/viosslfactories.c
@@ -83,7 +83,7 @@ static DH *get_dh2048(void)
   {
     BIGNUM *p = BN_bin2bn(dh2048_p,sizeof(dh2048_p),NULL);
     BIGNUM *g = BN_bin2bn(dh2048_g,sizeof(dh2048_g),NULL);
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
     dh->p=p;
     dh->g=g;
     if (! dh->p || ! dh->g)
