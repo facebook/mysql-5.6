f20f55b3980e4c1025cab6f7fd46678771e22cff
Abhinav Sharma <abhinavsharma@fb.com>
2018-04-04 15:25:48 -0700

    Fix memory leak in --print-ordering-key
    Summary: Resources were not cleaned up properly when using --print-ordering-key
    
    Reviewed By: anirbanr-fb
    
    Differential Revision: D7506203
    
    fbshipit-source-id: d2bc469

--- client/mysqldump.c
+++ client/mysqldump.c
@@ -4190,6 +4190,9 @@ static void dump_table(char *table, char *db)
       }
       fprintf(md_result_file, "/* ORDERING KEY%s : %s */;\n",
               opt_order_by_primary_desc ? " (DESC)" : "", row[5]);
+      if(res)
+        mysql_free_result(res);
+      dynstr_free(&explain_query_string);
     }
 
     if (!opt_xml && !opt_compact)
