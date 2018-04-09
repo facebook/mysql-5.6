13ffff1ed0ad684fb4d8009c0d8ce5f92c960856
Manuel Ung <mung@fb.com>
2018-04-09 13:39:13 -0700

    Change default character set used by client to latin1
    Summary:
    The original diff changed the behaviour of "auto" to mean default, so that latin1 character set could be used. However, this no longer works because the default in 8.0 has changed to utf8mb4.
    
    Instead, we can just directly change the mysql default to latin1.
    
    Reference patch: https://github.com/facebook/mysql-5.6/commit/142d7b5
    
    Differential Revision: D7539956

--- client/mysql.cc
+++ client/mysql.cc
@@ -186,7 +186,7 @@ static char *current_user = nullptr;
 static char *current_prompt = nullptr;
 static char *delimiter_str = nullptr;
 static char *opt_init_command = nullptr;
-static const char *default_charset = MYSQL_AUTODETECT_CHARSET_NAME;
+static const char *default_charset = (const char *)"latin1";
 #ifdef HAVE_READLINE
 static char *histfile;
 static char *histfile_tmp;
