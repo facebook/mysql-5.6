c32d21b592e473dd4e5e03e5abeb86c209c14066
chouxi <qizheng1993hit@gmail.com>
2018-07-03 13:59:49 -0700

    Integerate deadlock timestamp info to MySQL

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -31,6 +31,7 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <inttypes.h>
 
 /* MySQL includes */
 #include "./debug_sync.h"
@@ -3629,6 +3630,7 @@ private:
     if (!path_entry.path.empty() && !path_entry.limit_exceeded) {
       auto deadlocking_txn = *(path_entry.path.end() - 1);
       deadlock_info.victim_trx_id = deadlocking_txn.m_txn_id;
+      deadlock_info.deadlock_time = path_entry.deadlock_time;
     }
     return deadlock_info;
   }
@@ -3675,6 +3677,7 @@ private:
         path_data += "\n*** DEADLOCK PATH\n"
                      "=========================================\n";
         const auto dl_info = get_dl_path_trx_info(path_entry);
+        const auto deadlock_time = dl_info.deadlock_time;
         for (auto it = dl_info.path.begin(); it != dl_info.path.end(); it++) {
           const auto trx_info = *it;
           path_data += format_string(
@@ -3684,10 +3687,12 @@ private:
               "LOCK TYPE: %s\n"
               "INDEX NAME: %s\n"
               "TABLE NAME: %s\n",
+              "Timestamp: %" PRId64 "\n",
               trx_info.trx_id, trx_info.cf_name.c_str(),
               trx_info.waiting_key.c_str(),
               trx_info.exclusive_lock ? "EXCLUSIVE" : "SHARED",
-              trx_info.index_name.c_str(), trx_info.table_name.c_str());
+              trx_info.index_name.c_str(), trx_info.table_name.c_str(),
+              deadlock_time);
           if (it != dl_info.path.end() - 1) {
             path_data += "---------------WAITING FOR---------------\n";
           }
