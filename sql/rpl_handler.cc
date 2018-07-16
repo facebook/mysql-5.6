40c607e48d850cd9772c29585b0631794e1767fa
Manuel Ung <mung@fb.com>
2018-07-16 15:27:01 -0700

    Log semi-sync tailer's host/ip information
    Summary:
    Log semi-sync tailer's host/ip information
    
    Reference patch: https://github.com/facebook/mysql-5.6/commit/dfeef11
    
    Reviewed By: asandryh
    
    Differential Revision: D8867279
    
    fbshipit-source-id: df8c227d0f3

--- sql/rpl_handler.cc
+++ sql/rpl_handler.cc
@@ -707,6 +707,7 @@ int Binlog_transmit_delegate::transmit_start(THD *thd, ushort flags,
   Binlog_transmit_param param;
   param.flags = flags;
   param.server_id = thd->server_id;
+  param.host_or_ip = thd->security_context()->host_or_ip().str;
 
   int ret = 0;
   FOREACH_OBSERVER(ret, transmit_start, (&param, log_file, log_pos));
@@ -718,6 +719,7 @@ int Binlog_transmit_delegate::transmit_stop(THD *thd, ushort flags) {
   Binlog_transmit_param param;
   param.flags = flags;
   param.server_id = thd->server_id;
+  param.host_or_ip = thd->security_context()->host_or_ip().str;
 
   DBUG_EXECUTE_IF("crash_binlog_transmit_hook", DBUG_SUICIDE(););
 
@@ -739,6 +741,7 @@ int Binlog_transmit_delegate::reserve_header(THD *thd, ushort flags,
   Binlog_transmit_param param;
   param.flags = flags;
   param.server_id = thd->server_id;
+  param.host_or_ip = thd->security_context()->host_or_ip().str;
 
   DBUG_EXECUTE_IF("crash_binlog_transmit_hook", DBUG_SUICIDE(););
 
@@ -778,6 +781,7 @@ int Binlog_transmit_delegate::before_send_event(THD *thd, ushort flags,
   Binlog_transmit_param param;
   param.flags = flags;
   param.server_id = thd->server_id;
+  param.host_or_ip = thd->security_context()->host_or_ip().str;
 
   DBUG_EXECUTE_IF("crash_binlog_transmit_hook", DBUG_SUICIDE(););
 
@@ -796,6 +800,7 @@ int Binlog_transmit_delegate::after_send_event(THD *thd, ushort flags,
   Binlog_transmit_param param;
   param.flags = flags;
   param.server_id = thd->server_id;
+  param.host_or_ip = thd->security_context()->host_or_ip().str;
 
   DBUG_EXECUTE_IF("crash_binlog_transmit_hook", DBUG_SUICIDE(););
 
@@ -813,6 +818,7 @@ int Binlog_transmit_delegate::after_reset_master(THD *thd, ushort flags)
   Binlog_transmit_param param;
   param.flags = flags;
   param.server_id = thd->server_id;
+  param.host_or_ip = thd->security_context()->host_or_ip().str;
 
   int ret = 0;
   FOREACH_OBSERVER(ret, after_reset_master, (&param));
