347d1870914c948a6eb40521f43d174b1481b5c2
Aliaksei Sandryhaila <aliaksei@fb.com>
2018-07-17 10:13:56 -0700

    Remove mysql client warning about passwords
    Summary:
    This warning annoys people, and it's pretty stupid since it just makes
    you do it in even less secure ways (like env vars).
    
    Reference patch: https://github.com/facebook/mysql-5.6/commit/ec66b6e
    
    Reviewed By: lloyd
    
    Differential Revision: D8881245

--- client/mysqladmin.cc
+++ client/mysqladmin.cc
@@ -1020,7 +1020,6 @@ static int execute_commands(MYSQL *mysql, int argc, char **argv) {
                                    (ulong)password_len);
           typed_password = tmp;
         } else {
-          print_cmdline_password_warning();
           typed_password = argv[1];
         }
 
