17856f068821611ebbf79ce21c643af0bb14b24a
Raheel Shahzad <raheels@fb.com>
2018-08-17 10:33:01 -0700

    Add performance_schema table for query attributes.
    Summary:
    Currently, query attributes sent to MySQL Server are not
    visible. Adding 'session_query_attrs' table in performance_schema.
    
    Differential Revision: D9385165
    
    fbshipit-source-id: e5af09faff9

--- storage/perfschema/pfs_dd_version.h
+++ storage/perfschema/pfs_dd_version.h
@@ -45,8 +45,8 @@
 
   The numbering to use is the MySQL version number
   of the first MySQL version that published a given database schema.
-  The format is Mmmdd with M=Major, m=minor, d=dot,
-  so that MySQL 8.0.4 is encoded as 80004.
+  The format is Mmmdd with M=Major, m=minor, d=dot, and last three digits
+  for facebook specific schema changes so that MySQL 8.0.4 is encoded as 80004.
 
   In case of -dash version numbers, encode MySQL 8.12.34-56 as 8123456.
 
@@ -85,8 +85,12 @@
   performance_schema tables changed in MySQL 8.0.11 are
   - instance_log_resource was renamed to log_resource.
 
-  Version published is now 80011.
+  80011001:
+   performance_schema tables changed:
+   - session_query_attrs added.
+   - The last three digits reprents Facebook specific MySQL Schema changes.
+   Version published is now 80011001. i.e. 8.0.11 facebook schema change no. 1.
 */
-static const uint PFS_DD_VERSION = 80011;
+static const uint PFS_DD_VERSION = 80011001;
 
 #endif /* PFS_DD_VERSION_H */
