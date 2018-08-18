26a71d5c842f1cf9149ef00196ab4636fe7458cb
Artem Danilov <rtm@fb.com>
2018-08-17 17:52:52 -0700

    add path to ctest and symbolizer
    Summary: unit_tests will start to fail after we move to clang as symbols will be missing in asan leak report so suppress file will stio to work. This diff allow to configure symbolizer so that llvm-symbolizer can be provided for clang builds.
    
    Differential Revision: D9390164

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -204,6 +204,9 @@ our $opt_client_ddd;
 our $opt_client_debugger;
 our $opt_client_gdb;
 our $opt_client_lldb;
+our $opt_ctest_exclude;
+our $opt_ctest_preload;
+our $opt_ctest_path;
 our $opt_ctest_report;
 our $opt_dbx;
 our $opt_ddd;
@@ -229,6 +232,7 @@ our $opt_skip_combinations;
 our $opt_suites;
 our $opt_suite_opt;
 our $opt_summary_report;
+our $opt_symbolizer_path;
 our $opt_vardir;
 our $opt_xml_report;
 
@@ -1697,6 +1701,9 @@ sub command_line_setup {
     'charset-for-testdb=s'  => \$opt_charset_for_testdb,
     'colored-diff'          => \$opt_colored_diff,
     'comment=s'             => \$opt_comment,
+    'ctest_exclude=s'       => \$opt_ctest_exclude,
+    'ctest_path=s'          => \$opt_ctest_path,
+    'ctest_preload=s'       => \$opt_ctest_preload,
     'default-myisam!'       => \&collect_option,
     'disk-usage!'           => \&report_option,
     'enable-disabled'       => \&collect_option,
@@ -1721,6 +1728,7 @@ sub command_line_setup {
     'stress=s'              => \$opt_stress,
     'suite-opt=s'           => \$opt_suite_opt,
     'suite-timeout=i'       => \$opt_suite_timeout,
+    'symbolizer_path=s'     => \$opt_symbolizer_path,
     'testcase-timeout=i'    => \$opt_testcase_timeout,
     'timediff'              => \&report_option,
     'timer!'                => \&report_option,
@@ -7451,9 +7459,29 @@ sub run_ctest() {
   # the MTR tests.
   mtr_report("Running ctest parallel=$opt_parallel");
   $ENV{CTEST_PARALLEL_LEVEL} = $opt_parallel;
-  my $ctest_out = `ctest --test-timeout $opt_ctest_timeout $ctest_vs 2>&1`;
+  my $ctest = $opt_ctest_path || "ctest";
+  my $ctest_options = "";
+  if ($opt_ctest_exclude) {
+    $ctest_options = $ctest_options . "-E $opt_ctest_exclude ";
+  }
+  my $old_ld_preload = $ENV{'LD_PRELOAD'};
+  if ($opt_ctest_preload) {
+    $ENV{'LD_PRELOAD'} = $ENV{'LD_PRELOAD'} . ":$opt_ctest_preload";
+  }
+
+  my $asan_symbolizer_path = "";
+  if ($opt_symbolizer_path) {
+    $asan_symbolizer_path = "ASAN_SYMBOLIZER_PATH=$opt_symbolizer_path";
+  }
+
+  # For ASan builds, add in the leak suppression file
+  my $ctest_cmd =
+      join(" ", $asan_symbolizer_path, $ctest,
+           "--test-timeout $opt_ctest_timeout $ctest_vs $ctest_options 2>&1");
+  my $ctest_out = `$ctest_cmd`;
   if ($? == $no_ctest && ($opt_ctest == -1 || defined $ENV{PB2WORKDIR})) {
     chdir($olddir);
+    $ENV{'LD_PRELOAD'} = $old_ld_preload;
     return;
   }
 
@@ -7503,6 +7531,7 @@ sub run_ctest() {
   mark_time_used('test');
   mtr_report_test($tinfo);
   chdir($olddir);
+  $ENV{'LD_PRELOAD'} = $old_ld_preload;
   return $tinfo;
 }
 
