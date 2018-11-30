8b4fa007e5ed21888275e63e733383a331d8f722
Herman Lee <herman@fb.com>
2018-11-29 20:25:17 -0800

    Allow mtr to list --skip-test-file multiple times
    Summary:
    Add support for mtr --skip-test-file=FILE1 --skip-test-file=FILE2. This
    functionality was removed in 8.0.13 but has been useful in our test
    setups.
    
    Differential Revision: D13273093

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -115,7 +115,7 @@ my $opt_platform_exclude;
 my $opt_ps_protocol;
 my $opt_report_features;
 my $opt_skip_core;
-my $opt_skip_test_list;
+my @opt_skip_test_list;
 my $opt_sp_protocol;
 my $opt_start;
 my $opt_start_dirty;
@@ -654,7 +654,7 @@ sub main {
 
   mtr_report("Collecting tests");
   my $tests = collect_test_cases($opt_reorder, $opt_suites,
-                                 \@opt_cases,  $opt_skip_test_list);
+                                 \@opt_cases,  \@opt_skip_test_list);
   mark_time_used('collect');
   # A copy of the tests list, that will not be modified even after the tests
   # are executed.
@@ -1821,7 +1821,7 @@ sub command_line_setup {
     # list-options is internal, not listed in help
     'do-test-list=s'   => \$opt_do_test_list,
     'list-options'     => \$opt_list_options,
-    'skip-test-list=s' => \$opt_skip_test_list,
+    'skip-test-list=s' => \@opt_skip_test_list,
     'summary-report=s' => \$opt_summary_report,
     'xml-report=s'     => \$opt_xml_report);
 
@@ -7808,6 +7808,8 @@ Options to control what test suites or cases to run
   skip-test-list=FILE   Skip the tests listed in FILE. Each line in the file
                         is an entry and should be formatted as:
                         <TESTNAME> : <COMMENT>
+                        Multiple files can be passed in, each with a separate
+                        skip-test-list option.
   skip-test=PREFIX or REGEX
                         Skip test cases which name are prefixed with PREFIX
                         or fulfills REGEX.
