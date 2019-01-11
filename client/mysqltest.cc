facebook/mysql-5.6

commit cec752d027838c5b9d0a129e6472f7b17433aa9b

mysqld sends error 2006 to socket before closing the connection due to timeout
Summary:
Currently in mysqld when a connection is timed out (defined by wait_timeout) mysqld close the socket directly. Clients won't be able to find out the causes of the lost connection. This diff introduces a new feature to mysqld that when the new added global sys var "send_error_before_closing_timed_out_connection" is truned on server error 2006 with message "Connection closed due to timeout." will be pushed into socket before mysqld closing the connection due to timeout. This error will be delivered and stay in the socket buffer on client side after the connection is closed on server side. Client can read this error and find out the reason of the closed connection is timeout. This feature will help clients improving error handling, to distinguish if it was caused by timeout, or other reasons, e.g. crash, etc.

The purpose of forcing the error code to be 2006 is for compatibility since (client) error code 2006 has been used for this error.

A new global sys var "send_error_before_closing_timed_out_connection" is added in this diff, its default value is false so by default the timeout error message won't be sent, we will turn it on for testing and once all the clients can correctly consume this error message, we will set the default value to true.

To test this feature, a new mysqltest command "dump_timed_out_connection_socket_buffer" is added. Use this command to dump the socket buffer on cilent side after a connection is timeout and verify if the error code / message is valid. Example is as below:

set @global.wait_timeout = 3;
set @global.interactive_timeout = 3;

connect (con1,localhost,root,,);

--echo sleeping 6 sec to make sure the connection get timed out and closed by server...
sleep 6;

--echo dump the client side socket buffer and verify error 2006
dump_timed_out_connection_socket_buffer;

Reference patch:
https://github.com/facebook/mysql-5.6/commit/e04c9e1

client/mysqltest.cc
@@ -496,6 +496,7 @@ enum enum_commands {
   Q_QUERY_ATTRS_ADD,
   Q_QUERY_ATTRS_DELETE,
   Q_QUERY_ATTRS_RESET,
+  Q_DUMP_TIMED_OUT_CONNECTION_SOCKET_BUFFER,
   Q_UNKNOWN, /* Unknown command.   */
   Q_COMMENT, /* Comments, ignored. */
   Q_COMMENT_WITH_COMMAND,
@@ -532,7 +533,7 @@ const char *command_names[] = {
     "send_shutdown", "shutdown_server", "result_format", "move_file",
     "remove_files_wildcard", "copy_files_wildcard", "send_eval", "output",
     "reset_connection", "query_attrs_add", "query_attrs_delete",
-    "query_attrs_reset",
+    "query_attrs_reset", "dump_timed_out_connection_socket_buffer",
 
     0};
 
@@ -6066,6 +6067,123 @@ static void do_close_connection(struct st_command *command) {
   DBUG_VOID_RETURN;
 }
 
+/* Append a line to a dynamic string. */
+void dynstr_append_line(DYNAMIC_STRING *ds, char *msg, int len) {
+  dynstr_append_mem(ds, (const char *)msg, len);
+  dynstr_append_mem(ds, "\n", 1);
+}
+
+/*
+  When mysql global sys var "send_error_before_closing_timed_out_connection"
+  is turned on, mysqld will push error 2006 with the following message:
+  "Closed connection: idle timeout after <wait_timeout>s"
+  into socket before closing it due to timeout. This error will sit in the
+  socket buffer on client side so that client will be able to find out the
+  reason of lost connection or mysql server has gone away. This function will
+  try to read this error from socket buffer and verify if the packet is valid.
+*/
+void dump_timed_out_connection_socket_buffer(struct st_connection *con) {
+  if (!con->mysql.net.vio) return;
+  DYNAMIC_STRING ds;
+  init_dynamic_string(&ds, "", 2048, 2048);
+  /* vio read buffer size is 16KB */
+  uchar buf[VIO_READ_BUFFER_SIZE + 1];
+  int sz = vio_read(con->mysql.net.vio, (uchar *)buf, VIO_READ_BUFFER_SIZE);
+  bool ok = true;
+  char err_msg[256];
+  buf[sz] = 0;
+  // buf[0..2] is the length of the message, will check it later
+  int message_len = sint3korr(&buf[0]);
+  // buf[3] is the packet serial number, which has been forced to 1
+  int packet_serial_num = buf[3];
+  if (packet_serial_num != 1) {
+    ok = false;
+    sprintf(err_msg,
+            "Error: packet serial number in buf[3] is %d, "
+            "but 1 is expected;",
+            packet_serial_num);
+    dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+  }
+  // buf[4] is 255
+  if (buf[4] != 255) {
+    ok = false;
+    sprintf(err_msg, "Error: buf[4] != 255;");
+    dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+  }
+  // buf[5..6] is the error code 2006
+  int err_code = sint2korr(&buf[5]);
+  if (err_code != 2006) {
+    ok = false;
+    sprintf(err_msg,
+            "Error: error code in buf[5..6] is %d, "
+            "but 2006 is expected;",
+            err_code);
+    dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+  }
+  // buf[7] is '#'
+  if (buf[7] != '#') {
+    ok = false;
+    sprintf(err_msg, "Error: buf[7] != #;");
+    dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+  }
+  // Starting from buf[8] is the sql state "HY000" + message
+  // The message is as below, wait_timeout is a unsigned integer
+  // HY000Closed connection: idle timeout after <wait_timeout>s
+  static const char *msg_body = "HY000Closed connection: idle timeout after ";
+  int msg_body_len = strlen(msg_body);
+  uchar *end = NULL;
+  if (strncmp((const char *)&buf[8], msg_body, msg_body_len) == 0) {
+    // Skip the wait_timeout value, which is a unsigned integer,
+    // followed by 's', which is the last character of the string
+    end = &buf[8] + msg_body_len;
+    while (*end >= '0' && *end <= '9') ++end;
+    if (*end != 's' || *(++end) != '\0') ok = false;
+  } else
+    ok = false;
+  if (ok) {
+    // It is time to verify the packet and message length
+    DBUG_ASSERT(*end == '\0');
+    int packet_len = end - &buf[0];
+    // packet_len is the length of the whole network packet
+    // that includes 4 bytes of header (3 bytes of payload
+    // length followed by 1 byte of serial number) plus 42
+    // bytes of payload (the error message string)
+    if (sz != packet_len) {
+      sprintf(err_msg,
+              "Error: packet length is %d, "
+              "but %d is expected;",
+              sz, packet_len);
+      dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+      log_file.write(&ds);
+      log_file.flush();
+      dynstr_free(&ds);
+      return;
+    }
+    // The header length is 4 bytes
+    if (message_len + 4 != packet_len) {
+      ok = false;
+      sprintf(err_msg,
+              "Error: message length in buf[0..2] is %d, "
+              "but %d is expected;",
+              message_len, packet_len - 4);
+      dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+    }
+  }
+  if (ok) {
+    // Print the error message if no error was found
+    const char *err_str = "Error 2006: ";
+    replace_dynstr_append_mem(&ds, err_str, strlen(err_str));
+    replace_dynstr_append_mem(&ds, (const char *)&buf[8], sz - 8);
+    dynstr_append_mem(&ds, "\n", 1);
+  } else {
+    sprintf(err_msg, "Error message is wrong: %s", &buf[8]);
+    dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+  }
+  log_file.write(&ds);
+  log_file.flush();
+  dynstr_free(&ds);
+}
+
 /*
   Connect to a server doing several retries if needed.
 
@@ -9439,6 +9557,9 @@ int main(int argc, char **argv) {
           break;
         case Q_DISABLE_SESSION_TRACK_INFO:
           set_property(command, P_SESSION_TRACK, 0);
+          break;
+        case Q_DUMP_TIMED_OUT_CONNECTION_SOCKET_BUFFER:
+          dump_timed_out_connection_socket_buffer(cur_con);
           break;
         case Q_ENABLE_METADATA:
           set_property(command, P_META, 1);

include/violite.h
@@ -378,6 +378,7 @@ struct Vio {
   int kq_fd = {-1};
   std::atomic_flag kevent_wakeup_flag = ATOMIC_FLAG_INIT;
 #endif
+  const char *timeout_err_msg = {nullptr}; /* Timeout error message. */
 
   /*
      VIO vtable interface to be implemented by VIO's like SSL, Socket,

mysql-test/r/all_persisted_variables.result
@@ -44,7 +44,7 @@ include/assert.inc ['Expect 500+ variables in the table. Due to open Bugs, we ar
 
 # Test SET PERSIST
 
-include/assert.inc ['Expect 384 persisted variables in the table. Due to open Bugs, we are checking for 378']
+include/assert.inc ['Expect 385 persisted variables in the table. Due to open Bugs, we are checking for 379']
 
 ************************************************************
 * 3. Restart server, it must preserve the persisted variable
@@ -52,9 +52,9 @@ include/assert.inc ['Expect 384 persisted variables in the table. Due to open Bu
 ************************************************************
 # restart
 
-include/assert.inc ['Expect 378 persisted variables in persisted_variables table.']
-include/assert.inc ['Expect 378 persisted variables shown as PERSISTED in variables_info table.']
-include/assert.inc ['Expect 378 persisted variables with matching peristed and global values.']
+include/assert.inc ['Expect 379 persisted variables in persisted_variables table.']
+include/assert.inc ['Expect 379 persisted variables shown as PERSISTED in variables_info table.']
+include/assert.inc ['Expect 379 persisted variables with matching peristed and global values.']
 
 ************************************************************
 * 4. Test RESET PERSIST IF EXISTS. Verify persisted variable

mysql-test/r/connection_timeout_server_error.result
@@ -1,0 +1,73 @@
+### mysqld writes error 2006 to the socket before closing a connection
+### due to timeout so that client can find out the cause of the lost
+### connection by reading the message.
+drop table if exists t1;
+create table t1 (a1 int primary key, x1 int) engine = innodb;
+insert into t1 values (1, 1);
+select @@wait_timeout;
+@@wait_timeout
+28800
+select @@interactive_timeout;
+@@interactive_timeout
+28800
+set @orig_wait_timeout = @@wait_timeout;
+set @orig_interactive_timeout = @@interactive_timeout;
+set @@global.wait_timeout to 3 seconds
+set @@global.wait_timeout = 3;
+set @@global.interactive_timeout to 3 seconds
+set @@global.interactive_timeout = 3;
+### (1) Connection con1 non-SSL
+select @@session.wait_timeout;
+@@session.wait_timeout
+3
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+3
+### Connection gets timed out without executing any queries
+sleeping 18 sec to make sure the connection get timed out and closed by server...
+waking up...
+dump and verify the error 2006 from socket buffer...
+Error 2006: HY000Closed connection: idle timeout after 3s
+### (2) Connection con2 non-SSL
+select @@session.wait_timeout;
+@@session.wait_timeout
+3
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+3
+### Query results and timeout error will sit in client socket buffer in order
+select * from t1;
+sleeping 18 sec to make sure the connection get timed out and closed by server...
+waking up...
+con2 has been closed on server side but the results of 'select * from t1'
+should be in cilent buffer already and can be received by 'reap'
+a1	x1
+1	1
+dump and verify the error 2006 from socket buffer...
+Error 2006: HY000Closed connection: idle timeout after 3s
+### (3) Connection con3 non-SSL
+select @@session.wait_timeout;
+@@session.wait_timeout
+3
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+3
+set wait_timeout to 2 seconds
+set wait_timeout = 2;
+set interactive_timeout to 2 seconds
+set interactive_timeout = 2;
+select @@session.wait_timeout;
+@@session.wait_timeout
+2
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+2
+sleeping 12 sec to make sure the connection get timed out and closed by server...
+waking up...
+dump and verify the error 2006 from socket buffer
+the timeout value in error message should refect
+the latest session variable overrides
+Error 2006: HY000Closed connection: idle timeout after 2s
+set @@global.wait_timeout = @orig_wait_timeout;
+set @@global.interactive_timeout = @orig_interactive_timeout;
+drop table t1;

mysql-test/r/connection_timeout_server_error_ssl.result
@@ -1,0 +1,73 @@
+### mysqld writes error 2006 to the socket before closing a connection
+### due to timeout so that client can find out the cause of the lost
+### connection by reading the message.
+drop table if exists t1;
+create table t1 (a1 int primary key, x1 int) engine = innodb;
+insert into t1 values (1, 1);
+select @@wait_timeout;
+@@wait_timeout
+28800
+select @@interactive_timeout;
+@@interactive_timeout
+28800
+set @orig_wait_timeout = @@wait_timeout;
+set @orig_interactive_timeout = @@interactive_timeout;
+set @@global.wait_timeout to 3 seconds
+set @@global.wait_timeout = 3;
+set @@global.interactive_timeout to 3 seconds
+set @@global.interactive_timeout = 3;
+### (1) Connection con1 SSL
+select @@session.wait_timeout;
+@@session.wait_timeout
+3
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+3
+### Connection gets timed out without executing any queries
+sleeping 18 sec to make sure the connection get timed out and closed by server...
+waking up...
+dump and verify the error 2006 from socket buffer...
+Error 2006: HY000Closed connection: idle timeout after 3s
+### (2) Connection con2 SSL
+select @@session.wait_timeout;
+@@session.wait_timeout
+3
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+3
+### Query results and timeout error will sit in client socket buffer in order
+select * from t1;
+sleeping 18 sec to make sure the connection get timed out and closed by server...
+waking up...
+con2 has been closed on server side but the results of 'select * from t1'
+should be in cilent buffer already and can be received by 'reap'
+a1	x1
+1	1
+dump and verify the error 2006 from socket buffer...
+Error 2006: HY000Closed connection: idle timeout after 3s
+### (3) Connection con3 SSL
+select @@session.wait_timeout;
+@@session.wait_timeout
+3
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+3
+set wait_timeout to 2 seconds
+set wait_timeout = 2;
+set interactive_timeout to 2 seconds
+set interactive_timeout = 2;
+select @@session.wait_timeout;
+@@session.wait_timeout
+2
+select @@session.interactive_timeout;
+@@session.interactive_timeout
+2
+sleeping 12 sec to make sure the connection get timed out and closed by server...
+waking up...
+dump and verify the error 2006 from socket buffer
+the timeout value in error message should refect
+the latest session variable overrides
+Error 2006: HY000Closed connection: idle timeout after 2s
+set @@global.wait_timeout = @orig_wait_timeout;
+set @@global.interactive_timeout = @orig_interactive_timeout;
+drop table t1;

mysql-test/r/mysqld--help-notwin.result
@@ -1121,6 +1121,9 @@ The following options may be given as the first argument:
  --secure-file-priv=name 
  Limit LOAD DATA, SELECT ... OUTFILE, and LOAD_FILE() to
  files within specified directory
+ --send-error-before-closing-timed-out-connection 
+ Send error before closing connections due to timeout.
+ (Defaults to on; use --skip-send-error-before-closing-timed-out-connection to disable.)
  --server-id=#       Uniquely identifies the server instance in the community
  of replication partners
  --server-id-bits=#  Set number of significant bits in server-id
@@ -1674,6 +1677,7 @@ rpl-send-buffer-size 2097152
 rpl-stop-slave-timeout 31536000
 safe-user-create FALSE
 schema-definition-cache 256
+send-error-before-closing-timed-out-connection TRUE
 server-id 1
 server-id-bits 32
 session-track-gtids OFF

mysql-test/suite/sys_vars/r/send_error_before_closing_timed_out_connection_basic.result
@@ -1,0 +1,69 @@
+SET @start_global_send_error_before_closing_timed_out_connection = @@global.send_error_before_closing_timed_out_connection;
+SELECT @start_global_send_error_before_closing_timed_out_connection;
+@start_global_send_error_before_closing_timed_out_connection
+1
+SET @@global.send_error_before_closing_timed_out_connection = true;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+1
+SET @@global.send_error_before_closing_timed_out_connection = DEFAULT;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+1
+SET @@global.send_error_before_closing_timed_out_connection = false;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+0
+SET @@global.send_error_before_closing_timed_out_connection = false;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+0
+SET @@global.send_error_before_closing_timed_out_connection = true;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+1
+SET @@global.send_error_before_closing_timed_out_connection = 1;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+1
+SET @@global.send_error_before_closing_timed_out_connection = 0;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+0
+SET @@global.send_error_before_closing_timed_out_connection = -1;
+ERROR 42000: Variable 'send_error_before_closing_timed_out_connection' can't be set to the value of '-1'
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+0
+SET @@global.send_error_before_closing_timed_out_connection = 100;
+ERROR 42000: Variable 'send_error_before_closing_timed_out_connection' can't be set to the value of '100'
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+0
+SET @@global.send_error_before_closing_timed_out_connection = 1000.01;
+ERROR 42000: Incorrect argument type to variable 'send_error_before_closing_timed_out_connection'
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+0
+SET @@session.send_error_before_closing_timed_out_connection = 10;
+ERROR HY000: Variable 'send_error_before_closing_timed_out_connection' is a GLOBAL variable and should be set with SET GLOBAL
+SELECT @@session.send_error_before_closing_timed_out_connection;
+ERROR HY000: Variable 'send_error_before_closing_timed_out_connection' is a GLOBAL variable
+SELECT @@global.send_error_before_closing_timed_out_connection = VARIABLE_VALUE
+FROM performance_schema.global_variables
+WHERE VARIABLE_NAME='send_error_before_closing_timed_out_connection';
+@@global.send_error_before_closing_timed_out_connection = VARIABLE_VALUE
+1
+Warnings:
+Warning	1292	Truncated incorrect DOUBLE value: 'OFF'
+SELECT @@send_error_before_closing_timed_out_connection = VARIABLE_VALUE
+FROM performance_schema.session_variables
+WHERE VARIABLE_NAME='send_error_before_closing_timed_out_connection';
+@@send_error_before_closing_timed_out_connection = VARIABLE_VALUE
+1
+Warnings:
+Warning	1292	Truncated incorrect DOUBLE value: 'OFF'
+SET @@global.send_error_before_closing_timed_out_connection = @start_global_send_error_before_closing_timed_out_connection;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+@@global.send_error_before_closing_timed_out_connection
+1

mysql-test/suite/sys_vars/t/send_error_before_closing_timed_out_connection_basic.test
@@ -1,0 +1,51 @@
+--source include/load_sysvars.inc
+
+SET @start_global_send_error_before_closing_timed_out_connection = @@global.send_error_before_closing_timed_out_connection;
+SELECT @start_global_send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = true;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = DEFAULT;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = false;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = false;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = true;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = 1;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+SET @@global.send_error_before_closing_timed_out_connection = 0;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+--Error ER_WRONG_VALUE_FOR_VAR
+SET @@global.send_error_before_closing_timed_out_connection = -1;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+--Error ER_WRONG_VALUE_FOR_VAR
+SET @@global.send_error_before_closing_timed_out_connection = 100;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+--Error ER_WRONG_TYPE_FOR_VAR
+SET @@global.send_error_before_closing_timed_out_connection = 1000.01;
+SELECT @@global.send_error_before_closing_timed_out_connection;
+
+--ERROR ER_GLOBAL_VARIABLE
+SET @@session.send_error_before_closing_timed_out_connection = 10;
+--ERROR ER_INCORRECT_GLOBAL_LOCAL_VAR
+SELECT @@session.send_error_before_closing_timed_out_connection;
+
+SELECT @@global.send_error_before_closing_timed_out_connection = VARIABLE_VALUE
+FROM performance_schema.global_variables
+WHERE VARIABLE_NAME='send_error_before_closing_timed_out_connection';
+
+SELECT @@send_error_before_closing_timed_out_connection = VARIABLE_VALUE
+FROM performance_schema.session_variables
+WHERE VARIABLE_NAME='send_error_before_closing_timed_out_connection';
+
+SET @@global.send_error_before_closing_timed_out_connection = @start_global_send_error_before_closing_timed_out_connection;
+SELECT @@global.send_error_before_closing_timed_out_connection;

commit e239e60021ddbe7ef0d27141a868e632328eb8aa

Fixed FB8-212 (Potential buffer overrun in dump_timed_out_connection_socket_buffer() in mysqltest.cc)
https://jira.percona.com/browse/FB8-212

Reorganized the way how a buffer containing a wrong error message is appended
to the 'ds' DYNAMIC_STRING.

This patch fixes a problem introduced in commit cec752d
"mysqld sends error 2006 to socket before closing the connection due to
timeout".

client/mysqltest.cc
@@ -6068,8 +6068,8 @@ static void do_close_connection(struct st_command *command) {
 }
 
 /* Append a line to a dynamic string. */
-void dynstr_append_line(DYNAMIC_STRING *ds, char *msg, int len) {
-  dynstr_append_mem(ds, (const char *)msg, len);
+void dynstr_append_line(DYNAMIC_STRING *ds, const char *msg, int len) {
+  dynstr_append_mem(ds, msg, len);
   dynstr_append_mem(ds, "\n", 1);
 }
 
@@ -6176,8 +6176,9 @@ void dump_timed_out_connection_socket_buffer(struct st_connection *con) {
     replace_dynstr_append_mem(&ds, (const char *)&buf[8], sz - 8);
     dynstr_append_mem(&ds, "\n", 1);
   } else {
-    sprintf(err_msg, "Error message is wrong: %s", &buf[8]);
-    dynstr_append_line(&ds, err_msg, sizeof(err_msg));
+    const char *err_str = "Error message is wrong: ";
+    dynstr_append_mem(&ds, err_str, std::strlen(err_str));
+    dynstr_append_line(&ds, reinterpret_cast<char*>(&buf[8]), sz - 8);
   }
   log_file.write(&ds);
   log_file.flush();