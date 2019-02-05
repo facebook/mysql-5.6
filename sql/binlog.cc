c2dbc1e586e2d0252296fb067dce71d485fbcba7
Jay Edgar <jkedgar@fb.com>
2019-02-05 10:58:25 -0800

    FB8-100: Handle out-of-space on binlog cache file (#938) (#938)
    Summary:
    JIRA: https://jira.percona.com/browse/FB8-100
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/e29ff14
    
    The binlog cache file code is designed to handle an error that would cause binlog corruption.  It does this by checking the error for a set of values that it expects and when it finds one it marks the cache to indicate an incident has occurred and this mark keeps it from writing corrupt entries to the binlog.  Unfortunately, during an out-of-space event, there is a lower level error that gets set which is not being overwritten when we set the expected error.  This means that when the code checks for the expected error it is not found - the original lower level error is found.  This diff modifies the checking code to scan through all associated conditions (which is a list of all encountered errors) to see if the expected error is present anywhere in the list.
    
    This diff can be reverted if/when https://bugs.mysql.com/bug.php?id=72457 is resolved upstream.
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/938
    
    Reviewed By: lloyd
    
    Differential Revision: D13924297 (https://github.com/facebook/mysql-5.6/commit/3603beb2acc2e2fb1d6a4426fb6a415be34a1095)
    
    Pulled By: lth
    
    fbshipit-source-id: 200d9123c1e

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -5363,14 +5363,20 @@ bool MYSQL_BIN_LOG::check_write_error(const THD *thd) {
 
   if (!thd->is_error()) return checked;
 
-  switch (thd->get_stmt_da()->mysql_errno()) {
-    case ER_TRANS_CACHE_FULL:
-    case ER_STMT_CACHE_FULL:
-    case ER_ERROR_ON_WRITE:
-    case ER_BINLOG_LOGGING_IMPOSSIBLE:
-      checked = true;
-      break;
+  // Check all conditions for one that matches the expected error
+  const Sql_condition *err;
+  auto it = thd->get_stmt_da()->sql_conditions();
+  while ((err = it++) != nullptr && !checked) {
+    switch (err->mysql_errno()) {
+      case ER_TRANS_CACHE_FULL:
+      case ER_STMT_CACHE_FULL:
+      case ER_ERROR_ON_WRITE:
+      case ER_BINLOG_LOGGING_IMPOSSIBLE:
+        checked = true;
+        break;
+    }
   }
+
   DBUG_PRINT("return", ("checked: %s", YESNO(checked)));
   return checked;
 }
