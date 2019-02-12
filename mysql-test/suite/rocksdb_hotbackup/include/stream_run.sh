584a14efbdcccf40393dc2fe3bbd923fca110a55
Manuel Ung <mung@fb.com>
2019-02-11 16:09:08 -0800

    Use xbstream instead of wdt by default
    Summary: Most tests do not specify stream type, and end up choosing wdt which isn't installed, causing the test to fail. Use xbstream by default instead.
    
    Differential Revision: D14034100

--- mysql-test/suite/rocksdb_hotbackup/include/stream_run.sh
+++ mysql-test/suite/rocksdb_hotbackup/include/stream_run.sh
@@ -48,23 +48,23 @@ if [ "$STREAM_TYPE" == 'tar' ]; then
   BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} \
     --stream=tar --checkpoint_dir=$checkpoint_dir $SIGNAL_CONDITION 2> \
     $COPY_LOG | tar -xi -C $backup_dir"
-elif [ "$STREAM_TYPE" == 'xbstream' ]; then
-  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} \
-    --stream=xbstream --checkpoint_dir=$checkpoint_dir $SIGNAL_CONDITION 2> \
-    $COPY_LOG | xbstream -x \
-    --directory=$backup_dir"
-elif [ "$STREAM_TYPE" == "xbstream_socket" ]; then
-  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --socket=${MASTER_MYSOCK} \
-    --stream=xbstream --checkpoint_dir=$checkpoint_dir $SIGNAL_CONDITION 2> \
-    $COPY_LOG | xbstream -x \
-    --directory=$backup_dir"
-else
+elif [ "$STREAM_TYPE" == 'wdt' ]; then
   BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --stream=wdt \
     --port=${MASTER_MYPORT} --destination=localhost --backup_dir=$backup_dir \
     --avg_mbytes_per_sec=10 --interval=5 $SIGNAL_CONDITION \
     --extra_wdt_sender_options='--block_size_mbytes=1' \
     --checkpoint_dir=$checkpoint_dir 2> \
     $COPY_LOG"
+elif [ "$STREAM_TYPE" == "xbstream_socket" ]; then
+  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --socket=${MASTER_MYSOCK} \
+    --stream=xbstream --checkpoint_dir=$checkpoint_dir $SIGNAL_CONDITION 2> \
+    $COPY_LOG | xbstream -x \
+    --directory=$backup_dir"
+else
+  BACKUP_CMD="$MYSQL_MYROCKS_HOTBACKUP --user='root' --port=${MASTER_MYPORT} \
+    --stream=xbstream --checkpoint_dir=$checkpoint_dir $SIGNAL_CONDITION 2> \
+    $COPY_LOG | xbstream -x \
+    --directory=$backup_dir"
 fi
 
 echo "myrocks_hotbackup copy phase"
