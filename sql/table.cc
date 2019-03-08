504416abc9c45b509faac0ec3004c88759d8fe0f
Abhinav Sharma <abhinavsharma@fb.com>
2019-03-08 09:15:30 -0800

    FB8-82: Removing unchanged columns from AI when when row image is not FULL (#942)
    Summary:
    JIRA: https://jira.percona.com/browse/FB8-82
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/b1fec60
    Some update and upsert (insert ... on duplicate key update) queries
    used to log columns which have not changed in the after image (when using RBR)
    even when the image format is not FULL.
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/942
    
    Reviewed By: lth
    
    Differential Revision: D13930763
    
    Pulled By: lth
    
    fbshipit-source-id: 7cac72255d1

--- sql/table.cc
+++ sql/table.cc
@@ -2952,7 +2952,7 @@ int open_table_from_share(THD *thd, TABLE_SHARE *share, const char *alias,
   */
 
   bitmap_size = share->column_bitmap_size;
-  if (!(bitmaps = (uchar *)alloc_root(root, bitmap_size * 7))) goto err;
+  if (!(bitmaps = (uchar *)alloc_root(root, bitmap_size * 8))) goto err;
   bitmap_init(&outparam->def_read_set, (my_bitmap_map *)bitmaps, share->fields,
               false);
   bitmap_init(&outparam->def_write_set,
@@ -2969,6 +2969,9 @@ int open_table_from_share(THD *thd, TABLE_SHARE *share, const char *alias,
               false);
   bitmap_init(&outparam->pack_row_tmp_set,
               (my_bitmap_map *)(bitmaps + bitmap_size * 6), share->fields,
+              false);
+  bitmap_init(&outparam->tmp_write_set,
+              (my_bitmap_map *)(bitmaps + bitmap_size * 7), share->fields,
               false);
   outparam->default_column_bitmaps();
 
