00813611c4971ed80dbfceced120f726f8a3169a
Tian Xia <tianx@fb.com>
2019-03-12 02:49:07 -0700

    FB8-84: Add global variable `max_nonsuper_connections` (#950) (#950)
    Summary:
    JIRA: https://jira.percona.com/browse/FB8-84
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/478fa2a
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/8a269ee4f97
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/c53bf248a27
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/ae0d38ae0c8
    
    The new global variable `max_nonsuper_connections` will enforce the limit for
    the sum of all non-admin connections. This will be useful to limit regular user
    connections while still allow super users to connect to server.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/950
    
    Reviewed By: lloyd
    
    Differential Revision: D13987618
    
    Pulled By: lth

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -344,6 +344,8 @@ extern ulong tablespace_def_size;
 extern MYSQL_PLUGIN_IMPORT ulong max_connections;
 extern ulong max_digest_length;
 extern ulong max_connect_errors, connect_timeout;
+extern ulong max_nonsuper_connections;
+extern std::atomic<ulong> nonsuper_connections;
 extern bool opt_replica_allow_batching;
 extern ulong slave_trans_retries;
 extern uint replica_net_timeout;
