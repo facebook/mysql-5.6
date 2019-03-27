7575f38fbdc2aae5b4d8239a75c309d7df6c9f72
Jay Edgar <jkedgar@fb.com>
2019-03-27 13:23:39 -0700

    FB8-106: Add more information to duplicate key error (#998)
    Summary:
    Jira issue: https://jira.percona.com/browse/FB8-106
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/2c7f717
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/f3a0ed0
    
    The current error information for duplicate key lists the key value and key name.  In some scenarios this may not be sufficient.  Add a variable that, when enabled, includes the tableame for the key and the query text (or up to 512 bytes of it) into the error message.  The new variable, improved_dup_key_error, will change the output error message to contain this new information.
    
    Originally Reviewed By: hermanlee
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/998
    
    Reviewed By: lth
    
    Differential Revision: D14652434
    
    Pulled By: lth
    
    fbshipit-source-id: d6167df3c9a

--- storage/innobase/handler/handler0alter.cc
+++ storage/innobase/handler/handler0alter.cc
@@ -6090,7 +6090,7 @@ oom:
               &ha_alter_info->key_info_buffer[m_prebuilt->trx->error_key_num];
         }
       }
-      print_keydup_error(altered_table, dup_key, MYF(0));
+      print_keydup_error(altered_table, dup_key, MYF(0), m_user_thd);
       break;
     case DB_ONLINE_LOG_TOO_BIG:
       DBUG_ASSERT(ctx->online);
@@ -6662,10 +6662,12 @@ when rebuilding the table.
 @retval true Failure
 @retval false Success
 */
-inline MY_ATTRIBUTE((warn_unused_result)) bool commit_try_rebuild(
-    Alter_inplace_info *ha_alter_info, ha_innobase_inplace_ctx *ctx,
-    TABLE *altered_table, const TABLE *old_table, trx_t *trx,
-    const char *table_name) {
+MY_ATTRIBUTE((warn_unused_result))
+bool ha_innobase::commit_try_rebuild(Alter_inplace_info *ha_alter_info,
+                                     ha_innobase_inplace_ctx *ctx,
+                                     TABLE *altered_table,
+                                     const TABLE *old_table, trx_t *trx,
+                                     const char *table_name) {
   dict_table_t *rebuilt_table = ctx->new_table;
   dict_table_t *user_table = ctx->old_table;
 
@@ -6753,7 +6755,8 @@ inline MY_ATTRIBUTE((warn_unused_result)) bool commit_try_rebuild(
             dup_key = &ha_alter_info->key_info_buffer[err_key];
           }
         }
-        print_keydup_error(altered_table, dup_key, MYF(0));
+        print_keydup_error(altered_table, dup_key, MYF(0), m_user_thd,
+                           old_table->s->table_name.str);
         DBUG_RETURN(true);
       case DB_ONLINE_LOG_TOO_BIG:
         my_error(ER_INNODB_ONLINE_LOG_TOO_BIG, MYF(0),
