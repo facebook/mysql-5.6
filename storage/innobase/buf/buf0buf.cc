d9bd6aa7cd95bdd4f40ebc0ebdd01c7e10a1a9b2
Zsolt Parragi <zsolt.parragi@percona.com>
2019-03-27 13:21:57 -0700

    FB8-132: NUMA support (#987)
    Summary:
    Jira ticket: https://jira.percona.com/browse/FB8-132
    
    Reference commit: https://github.com/facebook/mysql-5.6/commit/e50c754879828c1d0b4f40cc13475ec7adea9afc
    Reference commit: https://github.com/facebook/mysql-5.6/commit/286e975
    
    WebScaleSQL Feature: NUMA Support
    
    Credits for research and implementation: Jeremy Cole and Davi Arnaut
    
    This patch provides startup options:
    * flush-caches: Flush and purge buffers/caches
    * numa-nodebind: Run mysqld with CPU affinity
    
    Note, the original patch provided numa-interleave, but that is supported natively by 8.0 now.
    
    It also provides a config option:
    * innodb_buffer_pool_populate: pre-allocation of buffer pool
    memory at start up:
      -- Use MAP_POPULATE if supported (Linux 2.6.23 and higher)
      -- Forced pre-allocation using memset
    
    Automation will pass in the right value to this option if
    it needs to bind mysqld to a particular socket. That will happen
    via systemd scripts.
    
    This is only adding the capability on the mysqld_safe side to be able to
    pass in numactl to the server. The turning on of this feature will need more
    testing and slow roll, and that will happen in different diffs on the ops repo.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/987
    
    Reviewed By: lth
    
    Differential Revision: D14652411
    
    Pulled By: lth
    
    fbshipit-source-id: 4d8d1f96598

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -869,9 +869,10 @@ itself succeeded.
 @param[in,out]  chunk     mem and mem_pfx fields of this chunk will be updated
                           to contain information about allocated memory region
 @return true iff allocated successfully */
-bool buf_pool_t::allocate_chunk(ulonglong mem_size, buf_chunk_t *chunk) {
+bool buf_pool_t::allocate_chunk(ulonglong mem_size, buf_chunk_t *chunk,
+                                bool populate) {
   ut_ad(mutex_own(&chunks_mutex));
-  chunk->mem = allocator.allocate_large(mem_size, &chunk->mem_pfx);
+  chunk->mem = allocator.allocate_large(mem_size, &chunk->mem_pfx, populate);
   if (chunk->mem == NULL) {
     return false;
   }
@@ -978,7 +979,8 @@ static buf_chunk_t *buf_chunk_init(
     buf_pool_t *buf_pool, /*!< in: buffer pool instance */
     buf_chunk_t *chunk,   /*!< out: chunk of buffers */
     ulonglong mem_size,   /*!< in: requested size in bytes */
-    std::mutex *mutex)    /*!< in,out: Mutex protecting chunk map. */
+    std::mutex *mutex,    /*!< in,out: Mutex protecting chunk map. */
+    bool populate)        /*!< in: virtual page preallocation */
 {
   buf_block_t *block;
   byte *frame;
@@ -996,7 +998,7 @@ static buf_chunk_t *buf_chunk_init(
 
   DBUG_EXECUTE_IF("ib_buf_chunk_init_fails", return (NULL););
 
-  if (!buf_pool->allocate_chunk(mem_size, chunk)) {
+  if (!buf_pool->allocate_chunk(mem_size, chunk, populate)) {
     return (NULL);
   }
 
@@ -1193,8 +1195,8 @@ static void buf_pool_set_sizes(void) {
 @param[in,out]  mutex     Mutex to protect common data structures
 @param[out] err           DB_SUCCESS if all goes well */
 static void buf_pool_create(buf_pool_t *buf_pool, ulint buf_pool_size,
-                            ulint instance_no, std::mutex *mutex,
-                            dberr_t &err) {
+                            ulint instance_no, std::mutex *mutex, dberr_t &err,
+                            bool populate) {
   ulint i;
   ulint chunk_size;
   buf_chunk_t *chunk;
@@ -1262,7 +1264,7 @@ static void buf_pool_create(buf_pool_t *buf_pool, ulint buf_pool_size,
     chunk = buf_pool->chunks;
 
     do {
-      if (!buf_chunk_init(buf_pool, chunk, chunk_size, mutex)) {
+      if (!buf_chunk_init(buf_pool, chunk, chunk_size, mutex, populate)) {
         while (--chunk >= buf_pool->chunks) {
           buf_block_t *block = chunk->blocks;
 
@@ -1431,7 +1433,7 @@ static void buf_pool_free() {
 @param[in]  total_size    Size of the total pool in bytes.
 @param[in]  n_instances   Number of buffer pool instances to create.
 @return DB_SUCCESS if success, DB_ERROR if not enough memory or error */
-dberr_t buf_pool_init(ulint total_size, ulint n_instances) {
+dberr_t buf_pool_init(ulint total_size, ulint n_instances, bool populate) {
   ulint i;
   const ulint size = total_size / n_instances;
 
@@ -1490,7 +1492,7 @@ dberr_t buf_pool_init(ulint total_size, ulint n_instances) {
 
     for (ulint id = i; id < n; ++id) {
       threads.emplace_back(std::thread(buf_pool_create, &buf_pool_ptr[id], size,
-                                       id, &m, std::ref(errs[id])));
+                                       id, &m, std::ref(errs[id]), populate));
     }
 
     for (ulint id = i; id < n; ++id) {
@@ -2297,7 +2299,8 @@ withdraw_retry:
       while (chunk < echunk) {
         ulonglong unit = srv_buf_pool_chunk_unit;
 
-        if (!buf_chunk_init(buf_pool, chunk, unit, nullptr)) {
+        if (!buf_chunk_init(buf_pool, chunk, unit, nullptr,
+                            srv_buf_pool_populate)) {
           ib::error(ER_IB_MSG_65) << "buffer pool " << i
                                   << " : failed to allocate"
                                      " new memory.";
