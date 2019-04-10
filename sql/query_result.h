3f41610405981035af525795b825793a0e7f6da4
Zsolt Parragi <zsolt.parragi@percona.com>
2019-04-10 16:02:11 -0700

    FB8-75: Periodic fsync in select into outfile controlled by sysvar (#992)
    Summary:
    Jira ticket: https://jira.percona.com/browse/FB8-75
    
    Reference commit: https://github.com/facebook/mysql-5.6/commit/14b1086
    
    Two variables are added, GLOBAL variables with SESSION override.
    select_into_file_fsync_size, which controls the size of the buffer
    after which a fsync will be done. Needs to be a multiple of 1024
    
    After each fsync you can optionally sleep for
    select_into_file_fsync_timeout milliseconds.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/992
    
    Reviewed By: yizhang82
    
    Differential Revision: D14883775
    
    Pulled By: yizhang82
    
    fbshipit-source-id: c3d419adf82

--- sql/query_result.h
+++ sql/query_result.h
@@ -238,6 +238,10 @@ class Query_result_export final : public Query_result_to_file {
   bool is_unsafe_field_sep;
   bool fixed_row_size;
   const CHARSET_INFO *write_cs;  // output charset
+  my_off_t last_fsync_off = 0L;
+#ifndef DBUG_OFF
+  uint n_fsyncs = 0;
+#endif
  public:
   Query_result_export(THD *thd, sql_exchange *ex)
       : Query_result_to_file(thd, ex) {}
