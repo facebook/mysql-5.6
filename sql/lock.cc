777d0e27bc991550dc383cb253c5c7d342879962
Zsolt Parragi <zsolt.parragi@percona.com>
2019-05-15 13:37:59 -0700

    FB8-221: Change SET GLOBAL [super_]read_only to not acquire the global read lock (#1008) (#1008)
    Summary:
    Jira ticket: https://jira.percona.com/browse/FB8-221
    
    Reference commit: https://github.com/facebook/mysql-5.6/commit/d8657c3d
    Reference commit: https://github.com/facebook/mysql-5.6/commit/a3f808d
    Reference commit: https://github.com/facebook/mysql-5.6/commit/f2dfbd0
    
    Change SET GLOBAL [super_]read_only to not acquire the global read lock
    
    Calling SET GLOBAL [super_]read_only acquires both the global read lock and
    the global commit lock before setting the flag. Acquiring the global read
    lock will cause the SET statement to wait on long running DML transactions.
    We think it is safe in our environment to not acquire this lock, so that SET
    GLOBAL [super_]read_only does not need to wait on long running DML
    transactions.  The side effect of not acquiring the global read lock is some
    DDL statements may complete after super_read_only is set, but that behavior
    is acceptable.
    
    This behavior can be changed back to the original by SET
    global.legacy_global_read_lock_mode=1
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1008
    
    Differential Revision: D15360477
    
    Pulled By: yizhang82

--- sql/lock.cc
+++ sql/lock.cc
@@ -1104,15 +1104,17 @@ bool Global_read_lock::lock_global_read_lock(THD *thd) {
 void Global_read_lock::unlock_global_read_lock(THD *thd) {
   DBUG_TRACE;
 
-  assert(m_mdl_global_shared_lock && m_state);
+  assert(m_state);
 
   if (m_mdl_blocks_commits_lock) {
     thd->mdl_context.release_lock(m_mdl_blocks_commits_lock);
     m_mdl_blocks_commits_lock = nullptr;
   }
-  thd->mdl_context.release_lock(m_mdl_global_shared_lock);
-  Global_read_lock::m_atomic_active_requests--;
-  m_mdl_global_shared_lock = nullptr;
+  if (m_mdl_global_shared_lock) {
+    thd->mdl_context.release_lock(m_mdl_global_shared_lock);
+    Global_read_lock::m_atomic_active_requests--;
+    m_mdl_global_shared_lock = nullptr;
+  }
   m_state = GRL_NONE;
 }
 
@@ -1135,10 +1137,14 @@ bool Global_read_lock::make_global_read_lock_block_commit(THD *thd) {
   MDL_request mdl_request;
   DBUG_TRACE;
   /*
-    If we didn't succeed lock_global_read_lock(), or if we already succeeded
+    If we didn't succeed lock_global_read_lock() and are running in legacy
+    global lock mode, or if we already succeeded
     make_global_read_lock_block_commit(), do nothing.
   */
-  if (m_state != GRL_ACQUIRED) return false;
+  if ((legacy_global_read_lock_mode && m_state == GRL_NONE) ||
+      m_state == GRL_ACQUIRED_AND_BLOCKS_COMMIT) {
+    return false;
+  }
 
   MDL_REQUEST_INIT(&mdl_request, MDL_key::COMMIT, "", "", MDL_SHARED,
                    MDL_EXPLICIT);
