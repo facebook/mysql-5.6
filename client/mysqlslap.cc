ff6cc9f5dd9e7e913e804fcd9ab33895a49729c2
Yi Zhang <yzha@fb.com>
2019-06-26 13:27:00 -0700

    Properly support SSL options in mysqlslap
    Summary: Mysqlslap doesn't apply options the same way for the initial connection and the workers - as a result the workers missing a lot of options passed from command line, including SSL. This fixes the SSL part only. I'll address the bigger issue with probably a bit refactoring with a separate fix (which is going to require quite a bit more testing).
    
    Reviewed By: hermanlee
    
    Differential Revision: D16015149
    
    fbshipit-source-id: 2fe741ed535

--- client/mysqlslap.cc
+++ client/mysqlslap.cc
@@ -1674,6 +1674,7 @@ extern "C" void *run_task(void *p) {
     exit(0);
   }
 
+  SSL_SET_OPTIONS(mysql);
   if (opt_protocol)
     mysql_options(mysql, MYSQL_OPT_PROTOCOL, (char *)&opt_protocol);
 
