1397f7162cdfc7a6773979939e67f16763dd541e
Yura Sorokin <yura.sorokin@percona.com>
2019-07-15 09:15:32 -0700

    Fix FB8-231 (abi_check CMake target fails after introducing SESSION_TRACK_RESP_ATTR) (#1032)
    Summary:
    https://jira.percona.com/browse/FB8-231
    
    Fixed problem with 'SESSION_TRACK_RESP_ATTR' not added to 'include/mysql.h.pp' that used to cause CMake error.
    
    Squash with D16076659
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1032
    
    Reviewed By: lth
    
    Differential Revision: D16262719
    
    Pulled By: lth
    
    fbshipit-source-id: 0c38a426f65

--- include/mysql.h.pp
+++ include/mysql.h.pp
@@ -210,7 +210,8 @@ enum enum_session_state_type {
   SESSION_TRACK_STATE_CHANGE,
   SESSION_TRACK_GTIDS,
   SESSION_TRACK_TRANSACTION_CHARACTERISTICS,
-  SESSION_TRACK_TRANSACTION_STATE
+  SESSION_TRACK_TRANSACTION_STATE,
+  SESSION_TRACK_RESP_ATTR = 32
 };
 bool my_net_init(struct NET *net, struct Vio * vio);
 void my_net_local_init(struct NET *net);
