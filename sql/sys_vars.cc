c7913be5a4e3c5a732a311301d07f9e443cbb2b8
Yi Zhang <yzha@fb.com>
2020-01-27 18:21:33 -0800

    Fix replication type conversion tests timeout by reverting default of rbr type mismatch whitelist to empty
    Summary:
    A lot of type conversion related tests are failing because we flipped the RBR type mismatch whitelist to .* but many tests are expecting the conversion to fail and as a result stuck in waiting for the failure, hence the timeout.
    
    The fix is to revert the default back to empty.
    
    Another option is to change the tests / suite options and only set it to empty in those cases, but it makes more sense to me for the defaults to map to 'official' MySQL 8.0 behavior and change to FB specific value in prod instead.
    
    Squash with D19415898
    
    Reviewed By: abhinav04sharma
    
    Differential Revision: D19595460
    
    fbshipit-source-id: 56d0625b285

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -3907,10 +3907,10 @@ static Sys_var_charptr Sys_rbr_column_type_mismatch_whitelist(
     "rbr_column_type_mismatch_whitelist",
     "List of db.table.col (comma separated) where type mismatches are "
     "expected. The slave will not fail it the conversion is lossless."
-    "This variable is overridden by slave_type_conversions. Default: '.*', "
-    "which means all tables are in the whitelist.",
+    "This variable is overridden by slave_type_conversions. Default: ''. "
+    "A value of '.*' means all cols are in the whitelist.",
     GLOBAL_VAR(opt_rbr_column_type_mismatch_whitelist), CMD_LINE(OPT_ARG),
-    IN_FS_CHARSET, DEFAULT(".*"), NO_MUTEX_GUARD, NOT_IN_BINLOG);
+    IN_FS_CHARSET, DEFAULT(""), NO_MUTEX_GUARD, NOT_IN_BINLOG);
 
 static Sys_var_ulonglong Sys_slave_dump_thread_wait_sleep_usec(
     "slave_dump_thread_wait_sleep_usec",
