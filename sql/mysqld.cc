4d29a3f276e7195c5ea7554fae4ab3c8b75be59e
Abhinav Sharma <abhinavsharma@fb.com>
2020-02-06 14:01:23 -0800

    FB8-94: Stop slave if master's binlog format is not ROW but some features that depend on it are enabled on the slave (#1102) (#1102)
    Summary:
    Jira issue: https://jira.percona.com/browse/FB8-94
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/1834a84
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/eba8a27
    
    Stop slave if master's binlog format is not ROW but some features that
    depend on it are enabled on the slave Idempotent recovery and rocksdb's
    skip trx api should be enabled only when master's binlog format is ROW.
    We detect master's binlog format by checking if it executes any DML
    statements. This tells us that the binlog format is not ROW and we stop
    the slave.
    
    Also, I moved rpl_skip_tx_api from rocksdb plugin to global mysqld so
    that it can be retrieved without using a handler.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1102
    
    Reviewed By: luqun
    
    Differential Revision: D19727603 (https://github.com/facebook/mysql-5.6/commit/9544441adf42caed9491b0a56f5448012ab0898a)
    
    Pulled By: abhinav04sharma
    
    fbshipit-source-id: 3f9a4046ae1

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1280,6 +1280,7 @@ std::atomic<ulong> nonsuper_connections(0);
 ulong opt_max_running_queries = 0, opt_max_waiting_queries = 0;
 ulong rpl_stop_slave_timeout = LONG_TIMEOUT;
 bool log_bin_use_v1_row_events = false;
+bool rpl_skip_tx_api = false;
 bool rpl_slave_flow_control = true;
 bool thread_cache_size_specified = false;
 bool host_cache_size_specified = false;
