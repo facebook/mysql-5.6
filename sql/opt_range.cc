5daf3ab4f94e995fe2a174ff7a43fcc3ca06eaa7
Yi Zhang <yzha@fb.com>
2020-02-11 20:52:02 -0800

    Early terminate if storage engine returns critical/serious errors in group by plans
    Summary:
    For certain GROUP BY plans, the QUICK_RANGE_SELECT class is used to iterate through all possible prefixes that form a group. The current iteration process calls read_range_first with both a start and end range, and then iterates until we've moved past the end range. However, the storage engine may choose to honour the end range (because of index condition pushdown, rocksdb iterator bounds, or rocksdb prefix seek mode) and return HA_ERR_KEY_NOT_FOUND or HA_ERR_END_OF_FILE instead. Instead of moving onto the next range, this causes the QUICK_RANGE_SELECT::get_next_prefix to return early without actually advancing onto the next prefix. This can cause an infinite loop in QUICK_GROUP_MIN_MAX_SELECT::get_next, or QUICK_GROUP_MIN_MAX_SELECT::get_next to return HA_ERR_END_OF_FILE too early.
    
    The fix is to detect if HA_ERR_KEY_NOT_FOUND or HA_ERR_END_OF_FILE is returned, and continue onto the next range instead of returning early.
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/98b0d40af27
    
    -------
    
    Porting Notes: some tests need rebaseline due to 8.0 behavior change. In particular group by min/max are returning stale rows in some cases when storage engine returns error, which is a 8.0 upstream bug that will be addressed in a future change.
    
    Reviewed By: lloyd
    
    Differential Revision: D19851080 (https://github.com/facebook/mysql-5.6/commit/79dee57ae8e1edfc5981b6b3630dcdbc786c945a)
    
    fbshipit-source-id: 630676cd403

--- sql/opt_range.cc
+++ sql/opt_range.cc
@@ -3172,6 +3172,7 @@ int test_quick_select(THD *thd, Key_map keys_to_use, table_map prev_tables,
     KEY_PART *key_parts;
     KEY *key_info;
     PARAM param;
+    bool force_group_by = false;
 
     /*
       Use the 3 multiplier as range optimizer allocates big PARAM structure
@@ -3179,6 +3180,7 @@ int test_quick_select(THD *thd, Key_map keys_to_use, table_map prev_tables,
       TODO During the optimization phase we should evaluate only inexpensive
            single-lookup subqueries.
     */
+    DBUG_EXECUTE_IF("force_group_by", force_group_by = true;);
     uchar buff[STACK_BUFF_ALLOC];
     if (check_stack_overrun(thd, 3 * STACK_MIN_SIZE + sizeof(PARAM), buff))
       return 0;  // Fatal error flag is set
@@ -3367,10 +3369,13 @@ int test_quick_select(THD *thd, Key_map keys_to_use, table_map prev_tables,
                                    Opt_trace_context::RANGE_OPTIMIZER);
       if (unlikely(trace->is_started()))
         group_trp->trace_basic_info(&param, &grp_summary);
-      if (group_trp->cost_est < best_cost) {
+      if (group_trp->cost_est < best_cost || force_group_by) {
         grp_summary.add("chosen", true);
         best_trp = group_trp;
         best_cost = best_trp->cost_est;
+        if (force_group_by) {
+          goto force_plan;
+        }
       } else
         grp_summary.add("chosen", false).add_alnum("cause", "cost");
     }
@@ -3474,6 +3479,7 @@ int test_quick_select(THD *thd, Key_map keys_to_use, table_map prev_tables,
       }
     }
 
+  force_plan:
     thd->mem_root = param.old_root;
 
     /*
@@ -11003,12 +11009,27 @@ int QUICK_RANGE_SELECT::get_next_prefix(uint prefix_length,
       DBUG_ASSERT(cur_prefix != nullptr);
       result = file->ha_index_read_map(record, cur_prefix, keypart_map,
                                        HA_READ_AFTER_KEY);
-      if (result || last_range->max_keypart_map == 0) return result;
-
-      key_range previous_endpoint;
-      last_range->make_max_endpoint(&previous_endpoint, prefix_length,
-                                    keypart_map);
-      if (file->compare_key(&previous_endpoint) <= 0) return 0;
+      if (result || last_range->max_keypart_map == 0) {
+        /*
+          Only return if actual failure occurred. For HA_ERR_KEY_NOT_FOUND
+          or HA_ERR_END_OF_FILE, we just want to continue to reach the next
+          set of ranges. It is possible for the storage engine to return
+          HA_ERR_KEY_NOT_FOUND/HA_ERR_END_OF_FILE even when there are more
+          keys if it respects the end range set by the read_range_first call
+          below.
+        */
+        if (result != HA_ERR_KEY_NOT_FOUND && result != HA_ERR_END_OF_FILE)
+          return result;
+      } else {
+        /*
+          For storage engines that don't respect end range, check if we've
+          moved past the current range.
+        */
+        key_range previous_endpoint;
+        last_range->make_max_endpoint(&previous_endpoint, prefix_length,
+                                      keypart_map);
+        if (file->compare_key(&previous_endpoint) <= 0) return 0;
+      }
     }
 
     const size_t count = ranges.size() - (cur_range - ranges.begin());
