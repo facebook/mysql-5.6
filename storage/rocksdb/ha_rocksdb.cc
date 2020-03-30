5feccbd9c65c75813548f350993501b6bfab0a0d
Yura Sorokin <yura.sorokin@percona.com>
2020-03-30 14:30:29 -0700

    FB8-246 main.implicit_commit fails in debug compilation (#1117)
    Summary:
    `main.implicit_commit` crashes in debug build
    
    Crashes due to the fact that `Diagnostics_area::m_status` is not set to other
    than the default value (`DA_EMPTY`) on error. The error occurs due to the lack
    of implementation of `Env::GetThreadList()` for rockdb.
    Any `Diagnostics_area::m_status` must be set in `mysql_execute_command()` after
    executing sql commands. In case of any error `Diagnostics_area::m_status` should
    be set to `DA_ERROR` using `my_error()`.
    
    Review note: MyRocks now calls my_error() and added a test plan.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1117
    
    Reviewed By: luqun
    
    Differential Revision: D20746941
    
    Pulled By: luqun
    
    fbshipit-source-id: e7c7e25cd21

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -5007,7 +5007,9 @@ static bool rocksdb_show_status(handlerton *const hton, THD *const thd,
     std::vector<rocksdb::ThreadStatus> thread_list;
     rocksdb::Status s = rdb->GetEnv()->GetThreadList(&thread_list);
 
-    if (!s.ok()) {
+    // GetThreadList() may return Status::NotSupported when
+    // ROCKSDB_USING_THREAD_STATUS is not defined
+    if (!s.ok() && !s.IsNotSupported()) {
       // NO_LINT_DEBUG
       sql_print_error("RocksDB: Returned error (%s) from GetThreadList.\n",
                       s.ToString().c_str());
@@ -5051,6 +5053,9 @@ static bool rocksdb_show_status(handlerton *const hton, THD *const thd,
     res |= rocksdb_show_snapshot_status(hton, thd, stat_print);
   }
 
+  if (res) {
+    my_error(ER_UNKNOWN_ERROR, MYF(0), "SHOW ENGINE");
+  }
   return res;
 }
 
