504b73af00b0d034ab84dba99d8f7658171b2f83
Herman Lee <herman@fb.com>
2020-04-07 16:54:56 -0700

    Call NewJemallocNodumpAllocator only if jemalloc is used
    Summary:
    The server fails to come up if MyRocks is built without jemalloc, but
    the rocksdb_cache_dump feature is enabled. For asan/valgrind builds,
    jemalloc should be disabled, but the test might still enable
    rocksdb_cache_dump. Allow the asan builds to run with the feature
    enabled, but generate a warning to the log instead.
    
    Reviewed By: lloyd
    
    Differential Revision: D20907021

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -5641,6 +5641,7 @@ static int rocksdb_init_func(void *const p) {
   if (!rocksdb_tbl_options->no_block_cache) {
     std::shared_ptr<rocksdb::MemoryAllocator> memory_allocator;
     if (!rocksdb_cache_dump) {
+#ifdef HAVE_JEMALLOC
       size_t block_size = rocksdb_tbl_options->block_size;
       rocksdb::JemallocAllocatorOptions alloc_opt;
       // Limit jemalloc tcache memory usage. The range
@@ -5658,6 +5659,11 @@ static int rocksdb_init_func(void *const p) {
         memory_allocator = nullptr;
         DBUG_RETURN(HA_EXIT_FAILURE);
       }
+#else
+      // NO_LINT_DEBUG
+      sql_print_warning(
+          "Ignoring rocksdb_cache_dump because jemalloc is missing.");
+#endif  // HAVE_JEMALLOC
     }
     std::shared_ptr<rocksdb::Cache> block_cache =
         rocksdb_use_clock_cache
