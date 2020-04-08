2bfbb8d1166ca4361a0b72713a6f3a3a4cbcdc92
Atish Agrawal <atishag@devvm2765.prn2.facebook.com>
2020-04-08 15:34:44 -0700

    Rename sync_binlog_pos_threshold to update_binlog_pos_threshold
    Summary:
    Rename sync_binlog_pos_threshold to update_binlog_pos_threshold
    Fix all code reference to use *update* instead of *sync*
    
    Differential Revision: D20930517

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -205,7 +205,7 @@ static void binlog_prepare_row_images(const THD *thd, TABLE *table,
 static bool is_loggable_xa_prepare(THD *thd);
 static int check_instance_backup_locked();
 
-extern int ha_sync_binlog_pos(const char *, my_off_t, Gtid *);
+extern int ha_update_binlog_pos(const char *, my_off_t, Gtid *);
 
 bool normalize_binlog_name(char *to, const char *from, bool is_relay_log) {
   DBUG_TRACE;
@@ -3835,7 +3835,7 @@ MYSQL_BIN_LOG::MYSQL_BIN_LOG(uint *sync_period, bool relay_log)
       file_id(1),
       sync_period_ptr(sync_period),
       sync_counter(0),
-      ha_last_synced_binlog_pos(0),
+      ha_last_updated_binlog_pos(0),
       non_xid_trxs(0),
       is_relay_log(relay_log),
       checksum_alg_reset(binary_log::BINLOG_CHECKSUM_ALG_UNDEF),
@@ -9526,8 +9526,9 @@ void MYSQL_BIN_LOG::process_commit_stage_queue(THD *thd, THD *first) {
      it doesn't have to be updated exactly per 'threshold', we're ok with
      ignoring this case.
     */
-    if (binlog_pos >= (ha_last_synced_binlog_pos + sync_binlog_pos_threshold) ||
-        binlog_pos < ha_last_synced_binlog_pos) {
+    if (binlog_pos >=
+            (ha_last_updated_binlog_pos + update_binlog_pos_threshold) ||
+        binlog_pos < ha_last_updated_binlog_pos) {
       Gtid max_gtid{0, 0};
       if (max_gtid_var != nullptr) {
         global_sid_lock->rdlock();
@@ -9535,8 +9536,8 @@ void MYSQL_BIN_LOG::process_commit_stage_queue(THD *thd, THD *first) {
         global_sid_lock->unlock();
       }
 
-      if (!ha_sync_binlog_pos(binlog_file, binlog_pos, &max_gtid)) {
-        ha_last_synced_binlog_pos = binlog_pos;
+      if (!ha_update_binlog_pos(binlog_file, binlog_pos, &max_gtid)) {
+        ha_last_updated_binlog_pos = binlog_pos;
       }
     }
   }
