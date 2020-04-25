93ef8798a77c4624e1f0943e561023683e9e2a54
Atish Agrawal <atishag@fb.com>
2020-04-24 20:10:13 -0700

    Optimize pack_with_varchar_space_pad handling for empty data
    Summary:
    The current packing algorithm uses 9 bytes (or 10 bytes for some collations) to encode
    empty data. The fix proposal adds a lead segment byte in packing. The content of the lead byte is  same as end of segment byte. The details for the algorithm is explained as
    part of Rdb_key_def::pack_with_varchar_space_pad function comment.
    After this modification, empty data can be represented with 1 byte. There is an extra byte overhead for non-empty data.
    
    Reviewed By: lloyd
    
    Differential Revision: D21304394

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -7678,6 +7678,14 @@ int ha_rocksdb::create_key_def(const TABLE *const table_arg, const uint i,
     }
   });
 
+  DBUG_EXECUTE_IF("MYROCKS_NO_LEAD_BYTE_VARCHAR_SPACE_PADDED", {
+    if (index_type == Rdb_key_def::INDEX_TYPE_SECONDARY) {
+      kv_version = Rdb_key_def::SECONDARY_FORMAT_VERSION_TTL;
+    } else {
+      kv_version = Rdb_key_def::PRIMARY_FORMAT_VERSION_TTL;
+    }
+  });
+
   uint32 index_flags = (ttl_duration > 0 ? Rdb_key_def::TTL_FLAG : 0);
 
   uint32 ttl_rec_offset =
