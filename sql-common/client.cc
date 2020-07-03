ee9416ccec8e5bf7e7714335aefda932e4395c70
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2020-07-03 07:16:46 -0700

    Change default auth method back to mysql_native_password
    Summary:
    MySQL 8.0 has changed the default authentication method to caching_sha2_password, but we still expect to use mysql_native_password on the server side for compatibility.
    
    To avoid extra round trips from renegotiation, the client library will default to mysql_native_password. For mtr tests though, we will explicitly set the method to caching_sha2_password to avoid changing too many tests.
    
    Differential Revision: D7062516

--- sql-common/client.cc
+++ sql-common/client.cc
@@ -5524,8 +5524,10 @@ static mysql_state_machine_status authsm_begin_plugin_auth(
   if (ctx->auth_plugin_name == nullptr || ctx->auth_plugin == nullptr) {
     /*
       If everything else fail we use the built in plugin
+      FB changed default to mysql_native_password to avoid auth
+      renegotiation.
     */
-    ctx->auth_plugin = &caching_sha2_password_client_plugin;
+    ctx->auth_plugin = &native_password_client_plugin;
     ctx->auth_plugin_name = ctx->auth_plugin->name;
   }
 
@@ -6748,7 +6750,11 @@ static mysql_state_machine_status csm_parse_handshake(
       }
     } else {
       ctx->scramble_data_len = (int)(pkt_end - ctx->scramble_data);
-      ctx->scramble_plugin = caching_sha2_password_plugin_name;
+      /*
+        FB changed default to mysql_native_password to avoid auth
+        renegotiation.
+      */
+      ctx->scramble_plugin = native_password_plugin_name;
     }
   } else {
     set_mysql_error(mysql, CR_MALFORMED_PACKET, unknown_sqlstate);
