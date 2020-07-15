db60520df7dccc278aff2ce14ab5fbd7a6557693
Yi Zhang <yzha@fb.com>
2020-07-15 09:11:38 -0700

    Better MyRocks init failure cleanup
    Summary:
    I'm seeing crashes in prod (merlindb only so far) where rocksdb is failing to initialize due to some WritePrepared conflict (to be investigated further later), and at process shutdown we crash when calling my_rocksdb::BlockBasedTableOptions::~BlockBasedTableOptions at __run_exit_handlers, and jemalloc is in a bad state. This is because when a plugin fail to initialize, MySQL doesn't call uninitialize function, which probably makes sense as the uninitialize would crash, unprepared to clean up a partial initialized state. I've also see similar issues in the past when we are cleaning up DBOptions at process shutdown leads to a similar crash. In general it is a bad idea to depend on clean up of global variable cleanup  in destructors because the orders aren't guaranteed.
    
    Unfortunately it's hard to fix this once and for all - ideally the best approach is to just call rocksdb_done_func to cleanup everything but it's hard to guarantee that cleanup works correctly in partial states, especially for the thread objects and the various manager code we have. Even just deleting rdb by itself leads to a crash in some cases as it asserts it holds to the only reference to some ColumnFamily objects that are held by something else.
    
    For now I'm just going the safe route - just clean up the rdb_*_options / stats variables that we are currently seeing issues and are safe to clean up.
    
    Differential Revision: D22551000

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -5454,7 +5454,7 @@ static rocksdb::Status check_rocksdb_options_compatibility(
   Storage Engine initialization function, invoked when plugin is loaded.
 */
 
-static int rocksdb_init_func(void *const p) {
+static int rocksdb_init_internal(void *const p) {
   DBUG_ENTER_FUNC();
 
   if (rdb_check_rocksdb_corruption()) {
@@ -5673,6 +5673,10 @@ static int rocksdb_init_func(void *const p) {
   rocksdb::Status status;
   status = rocksdb::DB::ListColumnFamilies(*rocksdb_db_options, rocksdb_datadir,
                                            &cf_names);
+  DBUG_EXECUTE_IF("rocksdb_init_failure_list_cf", {
+    // Simulate ListColumnFamilies failure
+    status = rocksdb::Status::Corruption();
+  });
   if (!status.ok()) {
     /*
       When we start on an empty datadir, ListColumnFamilies returns IOError,
@@ -5853,14 +5857,17 @@ static int rocksdb_init_func(void *const p) {
 
   // NO_LINT_DEBUG
   sql_print_information("RocksDB: Opening TransactionDB...");
-
   status = rocksdb::TransactionDB::Open(
       main_opts, tx_db_options, rocksdb_datadir, cf_descr, &cf_handles, &rdb);
-
+  DBUG_EXECUTE_IF("rocksdb_init_failure_open_db", {
+    // Simulate opening TransactionDB failure
+    status = rocksdb::Status::Corruption();
+  });
   if (!status.ok()) {
     rdb_log_status_error(status, "Error opening instance");
     DBUG_RETURN(HA_EXIT_FAILURE);
   }
+
   cf_manager.init(std::move(cf_options_map), &cf_handles);
 
   // NO_LINT_DEBUG
@@ -6048,6 +6055,27 @@ static int rocksdb_init_func(void *const p) {
 }
 
 /*
+  Cleanup rocksdb variables that need to be deleted even in case of
+  engine initialization failure. It's dangerous to clean them up
+  at process shutdown in __run_exit_handlers
+*/
+static void rocksdb_shutdown_safe() {
+  rocksdb_db_options = nullptr;
+  rocksdb_tbl_options = nullptr;
+  rocksdb_stats = nullptr;
+}
+
+static int rocksdb_init_func(void *const p) {
+  int ret = rocksdb_init_internal(p);
+  if (ret) {
+    // Ideally we should call rocksdb_done_func but we are not quite
+    // there yet so this only cleans up the safe ones
+    rocksdb_shutdown_safe();
+  }
+  return ret;
+}
+
+/*
   Storage Engine deinitialization function, invoked when plugin is unloaded.
 */
 
@@ -6164,9 +6192,7 @@ static int rocksdb_done_func(void *const p MY_ATTRIBUTE((__unused__))) {
   }
 #endif /* HAVE_VALGRIND */
 
-  rocksdb_db_options = nullptr;
-  rocksdb_tbl_options = nullptr;
-  rocksdb_stats = nullptr;
+  rocksdb_shutdown_safe();
 
   my_error_unregister(HA_ERR_ROCKSDB_FIRST, HA_ERR_ROCKSDB_LAST);
 
