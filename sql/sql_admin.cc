be68b9e8e82ee701503a5a214cea3550e14981e1
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2020-08-12 23:06:28 -0700

    Fix bug 1704195 / 87065 / TDB-83 (Stop ANALYZE TABLE from flushing table definition cache) [non-RocksDB part]
    Summary:
    This is adapted from https://github.com/percona/percona-server/pull/1977
    
      Make ANALYZE TABLE stop flushing affected tables from the table
      definition cache, which has the effect of not blocking any subsequent
      new queries involving the table if there's a parallel long-running
      query:
    
      - new table flag HA_ONLINE_ANALYZE, return it for InnoDB and TokuDB
        tables;
      - in mysql_admin_table, if we are performing ANALYZE TABLE, and the
        table flag is set, do not remove the table from the table
        definition cache, do not invalidate query cache;
      - in partitioning handler, refresh the query optimizer statistics
        after ANALYZE if the underlying handler supports HA_ONLINE_ANALYZE;
      - new testcases main.percona_nonflushing_analyze_debug,
        parts.percona_nonflushing_abalyze_debug and a supporting debug sync
        point.
    
      For TokuDB, this change exposes bug TDB-83 (Index cardinality stats
      updated for handler::info(HA_STATUS_CONST), not often enough for
      tokudb_cardinality_scale_percent). TokuDB may return different
      rec_per_key values depending on dynamic variable
      tokudb_cardinality_scale_percent value. The server does not have a way
      of knowing that changing this variable invalidates the previous
      rec_per_key values in any opened table shares, and so does not call
      info(HA_STATUS_CONST) again. Fix by updating rec_per_key for both
      HA_STATUS_CONST and HA_STATUS_VARIABLE. This also forces a re-record
      of tokudb.bugs.db756_card_part_hash_1_pick, with the new output
      seeming to be more correct.
    
    Differential Revision: D8822745

--- sql/sql_admin.cc
+++ sql/sql_admin.cc
@@ -1285,6 +1285,9 @@ static bool mysql_admin_table(
       }
     }
     if (table->table) {
+      const bool skip_flush =
+          (operator_func == &handler::ha_analyze) &&
+          (table->table->file->ha_table_flags() & HA_ONLINE_ANALYZE);
       if (table->table->s->tmp_table) {
         /*
           If the table was not opened successfully, do not try to get
@@ -1292,7 +1295,7 @@ static bool mysql_admin_table(
         */
         if (open_for_modify && !open_error)
           table->table->file->info(HA_STATUS_CONST);
-      } else if (open_for_modify || fatal_error) {
+      } else if ((!skip_flush && open_for_modify) || fatal_error) {
         if (operator_func == &handler::ha_analyze)
           /*
             Force update of key distribution statistics in rec_per_key array and
