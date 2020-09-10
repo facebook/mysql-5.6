4fe65b553f823b5b2ab275e1c2af8911cca5003a
Yi Zhang <yzha@fb.com>
2020-09-09 21:03:59 -0700

    Partitions with TTL should be idempotent by default
    Summary:
    With MyRocks tables that has TTL, we already tolerate replication failures by making them idempotent by default. However, for tables with partitions, we didn't override has_ttl_column in ha_partition, so they were still non-idempotent by default. For tables that has TTL partitions and non-TTL / regular partitions, we had to manually make the whole table idempotent (through rbr_idempotent_tables) but this might hide real replication issues from non-TTL partitions, which may lead to silent data loss.
    
    To address this, we would have to override has_ttl_column correctly for partitions. One naive implementation would be just calling all partitions' has_ttl_column and return true if any of them are true, but that would only make the whole table idempotent by default (so effectively making rbr_idempotent_tables automatic) but still hide the replication errors. The correct way is to only check the partition that shows the error.
    
    Ideally, we would refactor the replication thread to identify the error and the corresponding partition, but doing so involves significant changes that may end up being more risky, and it would also end up exposing some internal details of partitions, or having to redo the partition lookup based on the before/after image (which has already been done during the lookup phase).
    
    The simplest fix, is to leverage the m_last_part that are already being set in partition code. Typically whenever you go through the ha_partition code it would first identify the correct partition, remember in m_last_part and delegate the call to the correct partition, so assuming the structure of the row based replication code won't change, it should be relatively safe to assume the last partition touched is where the error is happening, and only check that partition. It's obviously not ideal but should work well in practice.
    
    I've renamed has_ttl_column to last_part_has_ttl_column to avoid confusion. It's not super accurate but at least specific enough to give people pause if they are trying to use it elsewhere.
    
    Added a new test that verifies TTL partitions are idempotent by default and non-TTL partitions are not idempotent.
    
    -----
    
    Porting Notes:
    * ha_partition.* has moved to partition_base.*
    * a few messages have changed and requiring updating tests/rebaseline
    
    Differential Revision: D24213880

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -1033,7 +1033,7 @@ class ha_rocksdb : public my_core::handler {
 
   virtual bool use_read_free_rpl() const override;
 
-  virtual bool has_ttl_column() const override;
+  virtual bool last_part_has_ttl_column() const override;
 
   /*
     Returns the buffer of size(current_size) which will be used
