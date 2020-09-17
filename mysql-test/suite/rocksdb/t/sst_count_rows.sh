fe163f54548452b61d488dceb860bb1cbf08eed4
Yoshinori Matsunobu <yoshinori@fb.com>
2020-09-17 09:21:47 -0700

    Triggering additional compactions if SST file is filled with deletions
    Summary:
    MyRocks has a feature to trigger additional compactions if
    a SST file has many deletions within a specific window -- configured by rocksdb_compaction_sequential_deletes_window and rocksdb_compaction_sequential_deletes. However, there were cases that SST files were filled with deletions but file size was so small that number of entries did not reach rocksdb_compaction_sequential_deletes. In that case, additional compaction did not happen.
    
    This diff triggers an additional compaction if (number of deletions
    / number of entries) > (rocksdb_compaction_sequential_deletes /
    rocksdb_compaction_sequential_deletes_window) in the SST file. For
    example, if rocksdb_compaction_sequential_deletes=99999 and
    rocksdb_compaction_sequential_deletes_window=100000, this diff will
    trigger a compaction if a sst file has only 50000 entries and all of
    them are deletions. Prior to the diff, compaction was not triggered to the file.
    
    This diff also fixed a test case file
    suite/rocksdb/t/sst_count_rows.sh which got unnecessarily stuck because
    it did not exclude all system cf index ids from calculation.
    
     This diff is a port from diffs in 5.6
    (https://github.com/facebook/mysql-5.6/commit/dcfa07dd46feb8c730938e52cea637faf27e07a1 and
    https://github.com/facebook/mysql-5.6/commit/249bc6e58c3955bb3215dd6e5f8f0ac620ea2461)
    
    Differential Revision: D23760639

--- mysql-test/suite/rocksdb/t/sst_count_rows.sh
+++ mysql-test/suite/rocksdb/t/sst_count_rows.sh
@@ -2,6 +2,7 @@
 
 sst_dump=$2
 wait_for_no_more_deletes=$3
+wait_for_no_more_rows=$4
 num_retries=240
 retry=0
 
@@ -14,16 +15,16 @@ while : ; do
   do
     # excluding system cf
     DELETED=`$sst_dump --command=scan --output_hex --file=$f | \
-      perl -ne 'print  if(/''(\d\d\d\d\d\d\d\d)/ && $1 >= 8)' | \
+      perl -ne 'print  if(/''(\d\d\d\d\d\d\d\d)/ && $1 >= 11)' | \
       grep -e ", type:0" -e ", type:7" | wc -l`
     EXISTS=`$sst_dump --command=scan --output_hex --file=$f | \
-      perl -ne 'print  if(/''(\d\d\d\d\d\d\d\d)/ && $1 >= 8)' | \
+      perl -ne 'print  if(/''(\d\d\d\d\d\d\d\d)/ && $1 >= 11)' | \
       grep ", type:1" | wc -l`
     TOTAL_D=$(($TOTAL_D+$DELETED))
     TOTAL_E=$(($TOTAL_E+$EXISTS))
     # echo "${f##*/} $DELETED $EXISTS"
   done
-  if [ $TOTAL_E != "0" ]
+  if [ $TOTAL_E != "0" ] || ([ $wait_for_no_more_rows = "1" ] && [ $TOTAL_E = "0" ])
   then
     if [ $TOTAL_D = "0" ] || [ $wait_for_no_more_deletes = "0" ]
     then
