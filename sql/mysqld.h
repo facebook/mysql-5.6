a4c23524a6bed965652e1a93a1abfde98e50aa5a
Luqun Lou <luqun@fb.com>
2020-10-08 22:17:02 -0700

    Adding option to enable global commit ordering on secondaries
    Summary:
    Port D23366447 (https://github.com/facebook/mysql-5.6/commit/79e3b753db6fe09f34600f9cb22fe1c93eeed843)
    
    Extending the variable slave_preserve_commit_order to allow
    three values NONE, DB and GLOBAL. When the variabale it set to GLOBAL we
    order commits even across databases.
    
    Reviewed By: abhinav04sharma
    
    Differential Revision: D24213844

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -380,6 +380,7 @@ extern ulonglong opt_mts_dependency_size;
 extern double opt_mts_dependency_refill_threshold;
 extern ulonglong opt_mts_dependency_max_keys;
 extern ulonglong opt_mts_pending_jobs_size_max;
+extern ulong opt_mts_dependency_order_commits;
 extern ulong rpl_stop_replica_timeout;
 extern bool rpl_skip_tx_api;
 extern bool log_bin_use_v1_row_events;
--- sql/sql_class.h
+++ sql/sql_class.h
@@ -211,6 +211,12 @@ enum enum_mts_dependency_replication {
   DEP_RPL_STATEMENT,
 };
 
+enum enum_mts_dependency_order_commits {
+  DEP_RPL_ORDER_NONE,
+  DEP_RPL_ORDER_DB,
+  DEP_RPL_ORDER_GLOBAL,
+};
+
 enum enum_i_s_engine {
   I_S_MEMORY,
   I_S_TEMPTABLE,
