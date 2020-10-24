4385689a6543e98fa0b060ef851b7f3e46ed16a4
Yi Zhang <yzha@fb.com>
2020-10-24 12:54:37 -0700

    Faster value decoding
    Summary:
    Similar to integer key decoding improvement, but for values:
    * Remove dependency to field* and cache those information in Rdb_field_encoder
    * No more Field::move_field - just write to buffer directly
    
    We were originally planning to use Rdb_protocol_value_decoder for MySQL bypass project to eliminate overhead of decoding to integer then convert to string, but in practice it probably makes little difference and it is not going to be needed when we go with bypass RPC (which is thrift binary protocol). So this changes removes Rdb_protocol_value_decoder completely.
    
    One thing that worth mentioning is I've unified pack_length and pack_length_in_rec because they are essentially identical in our scenarios and in the case they could be different (more packed BITS format that borrows bits from null byte when storage engine supports it) we don't really support it at all, so unifying it to avoid confusion and assert it.
    
    With this MyRocks is now either faster or on par with InnoDB when it comes to SELECT integer columns stored in value.
    
    NOTE:FDO data needs to be updated for this diff in order to take full advantage of this change.
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/f4cefbaca8753c582ee1b372be9be9c6fc9915b4
    
    Reviewed By: Pushapgl
    
    Differential Revision: D25880543

--- storage/rocksdb/rdb_datadic.cc
+++ storage/rocksdb/rdb_datadic.cc
@@ -1599,7 +1599,7 @@ int Rdb_key_def::unpack_record(TABLE *const table, uchar *const buf,
       has_covered_bitmap ? &covered_bitmap : nullptr, buf);
   while (iter.has_next()) {
     err = iter.next();
-    if (err) {
+    if (unlikely(err)) {
       return err;
     }
   }
