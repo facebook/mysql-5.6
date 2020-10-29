8ab112f4918a08bceb21ba0e6647239802ad523a
Luqun Lou <luqun@fb.com>
2020-10-28 23:29:29 -0700

    clear and set additional fields during promotion
    Summary:
    Multiple fixes to correctly maintain mysqld state during promotion/demotion.
    1. is_slave: This flag is used to check if the instance is in a mysql 'slave'
    role. Among other things, unsetting read-only relies on this flag. Unsetting
    read-only fails on slave instances. This was not being cleared on promotion to a
    master causing issues when automation tries to unset read-only after disabling
    raft.
    2. Flush active_mi to make it durable during promotion/demotion
    
    Differential Revision: D24616131

--- sql/rpl_slave.cc
+++ sql/rpl_slave.cc
@@ -1671,9 +1671,12 @@ int raft_reset_slave(THD *) {
   mi->inited = false;
   mysql_mutex_lock(&mi->rli->data_lock);
   mi->rli->inited = false;
+  mi->flush_info(true);
   mysql_mutex_unlock(&mi->rli->data_lock);
   mysql_mutex_unlock(&mi->data_lock);
 
+  // no longer a slave. will be set again during change master
+  is_slave = false;
   channel_map.unlock();
   DBUG_RETURN(error);
 }
@@ -1695,7 +1698,11 @@ int raft_change_master(
   mi->port = master_instance.second;
   mi->set_auto_position(true);
   mi->inited = true;
+  mi->flush_info(true);
   mysql_mutex_unlock(&mi->data_lock);
+
+  // changing to a slave. set the is_slave flag
+  is_slave = true;
 end:
   channel_map.unlock();
   DBUG_RETURN(error);
