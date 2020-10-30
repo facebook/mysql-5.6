21cd29edc8b5893064310bd05cbcd2d9b7e855cb
Satya Valluri <satyav@fb.com>
2020-10-30 11:20:47 -0700

    Fix --new-tests to find the suite correctly
    Summary:
    This diff fixes the following issues with "--new-tests" option in mysql-test-run.pl:
    
    * Includes files that end with extension ".result" while collecting the test files that need to be executed.
    
    * Parses the suite name from the path of the .test or .result file. This will fix the issue of not being able to locate the files in suites that are not considered by mysqltest.sh. Also, if two tests from different suites share the same name then parsing the suite name will disambiguate to the correct test file.
    
    * If "--new-tests" is specified with no additional test names and if the current commit contains no newly added or modified tests then the test will exit without running any tests with the following warning message:
    ```
    mysql-test-run: WARNING: --new-tests is specified but no tests are added/modified in the current commit
    ```
    
    Reviewed By: luqun, yizhang82
    
    Differential Revision: D24656782 (https://github.com/facebook/mysql-5.6/commit/d392eb557669a8bd5c3062bad028aaa01808180d)
    
    fbshipit-source-id: aaf069b8053

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -175,6 +175,7 @@ my $exe_ndbmtd_counter = 0;
 my $ports_per_thread   = 20;
 my $source_dist        = 0;
 my $valgrind_reports   = 0;
+my $new_test_option = 0; # is '--new-tests' option specified
 
 my @valgrind_args;
 
@@ -405,6 +406,15 @@ sub main {
     die "Can't open file $build_thread_id_file: $!";
   print FH "# Unique id file paths\n";
   close(FH);
+
+  # if the option '--new-tests' is specified and if no test/result files are
+  # added or modified or specified as part of the command then exit
+  if ($new_test_option && $#opt_cases < 0)
+  {
+    mtr_warning("--new-tests is specified but no tests are added/modified" .
+                " in the current commit");
+    exit(0);
+  }
 
   # --help will not reach here, so now it's safe to assume we have binaries
   My::SafeProcess::find_bin($bindir);
@@ -1728,27 +1738,45 @@ sub command_line_setup {
     } elsif ( $arg eq "--new-tests") {
       # Run tests that are modified from the last commit
       # find the files that are modified in the current commit
-      my $git_cmd = "git diff --name-only HEAD^|grep '\.test\$'";
+      # include .result file that are modified
+      my $git_cmd = "git diff --name-only HEAD^|grep '\.test\$\\\|\.result\$'";
       my @modified_files = `$git_cmd`;
 
       my $mfile;
       my $file_list="";
+      my %uniq_opt_cases;
+      # there could be duplicates due to same file appearing twice
+      # because of changes to .test and .result files. So do duplicate
+      # elimination using a hash
       foreach $mfile (@modified_files)
       {
         chomp($mfile);
         my @split_mfile = split(/\//, $mfile);
-        my $last_commit_filename = $split_mfile[$#split_mfile];
-        push(@opt_cases, $last_commit_filename);
+        # split the file name to elimination extension since it could include
+        # ".result" string
+        my @split_last_filename = split(/\./, $split_mfile[$#split_mfile]);
+        my $last_commit_filename = $split_last_filename[0];
+        if ($split_mfile[1] eq "suite")
+        {
+            $last_commit_filename = $split_mfile[2].".".$last_commit_filename;
+        }
+        $uniq_opt_cases{$last_commit_filename} = 1;
+      }
+      my $test_file_name;
+      foreach $test_file_name (keys %uniq_opt_cases)
+      {
+        push(@opt_cases, $test_file_name);
         if (length($file_list) > 0)
         {
           $file_list = $file_list.", ";
         }
-        $file_list = $file_list.$last_commit_filename;
+        $file_list = $file_list.$test_file_name;
       }
       if (length($file_list) > 0)
       {
         print "Tests added/modified in the current commit: $file_list\n";
       }
+      $new_test_option = 1;
     } elsif ($arg =~ /^-/) {
       usage("Invalid option \"$arg\"");
     } else {
