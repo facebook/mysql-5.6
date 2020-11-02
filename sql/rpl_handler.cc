fd29ba8537dbb7d36d510b0acfd665144f60ef51
Luqun Lou <luqun@fb.com>
2020-11-02 09:35:21 -0800

    always exit raft queue thread
    Summary:
    During RaftListenerQueue destructor, check to see whether exist raft queue thread
    also during exit raft queue thread, wait until the exit event is finished
    
    Differential Revision: D24684936
    
    fbshipit-source-id: a497a2bafcb

--- sql/rpl_handler.cc
+++ sql/rpl_handler.cc
@@ -1501,6 +1501,8 @@ extern "C" void *process_raft_queue(void *) {
   if (thd_added) thd_manager->remove_thd(thd);
   delete thd;
   my_thread_end();
+  // NO_LINT_DEBUG
+  sql_print_information("Raft listener queue aborted");
   pthread_exit(0);
   return 0;
 }
@@ -1517,9 +1519,7 @@ int start_raft_listener_thread() {
   return 0;
 }
 
-RaftListenerQueue::~RaftListenerQueue() {
-  // TODO : Need to fix the destruction construct.
-}
+RaftListenerQueue::~RaftListenerQueue() { deinit(); }
 
 int RaftListenerQueue::add(QueueElement element) {
   std::unique_lock<std::mutex> lock(queue_mutex_);
@@ -1567,10 +1567,14 @@ void RaftListenerQueue::deinit() {
 
   // Queue an exit event in the queue. The listener thread will eventually pick
   // this up and exit
+
+  std::promise<RaftListenerCallbackResult> prms;
+  auto fut = prms.get_future();
   QueueElement element;
   element.type = RaftListenerCallbackType::RAFT_LISTENER_THREADS_EXIT;
+  element.result = &prms;
   add(element);
-
+  fut.get();
   inited_ = false;
   return;
 }
