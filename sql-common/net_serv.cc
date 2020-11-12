b74220d59f3f7a0f5e32ba0a0eaa5de19d1c9a6d
Manuel Ung <mung@fb.com>
2020-11-11 20:18:50 -0800

    Exit admission control during queries
    Summary:
    Currently, a query holds an admission control slot for the
        entire duration of the query. This causes a few problems:
    
        1. This can lead to deadlocks. A transaction holding an admission control slot could be waiting on a row lock held by a transaction waiting for admission control, leading to a deadlock cycle. A possible solution is to exit admission control before waiting on a lock.
        2. Admission control is mostly used to manage cpu usage. When a query is blocked for whatever reason, eg. waiting on row lock or writing to network, it is not consuming any cpu so it is artificially constraining the amount of resources a workload can consume. Generally speaking, we can allow other queries to run if a query is blocked.
        3. Long running queries are generally bad queries. The current admission control system rewards them as they can hold onto their admission control slots until they complete. Instead we should preempt them to let better queries have a chance of running. Eventually, we should also deprioritize them.
    
        To solve this, queries will now exit and re-enter admission control
        during a query.
    
        A query will exit admission control if it is blocked, or after it has
        done a certain amount of work (tracked by `THD::check_yield`).
        Currently, work is counted only during fetching of rows from the storage
        engine. Eventually, this could also cover many more cases.
    
        When a query tries to re-enter admission control, it will either be
        queued at the front or at the back depending on why the query released
        its slot. If it was due to being blocked, then it is queued at the front
        on the assumption that it should finish its work quickly. This reduces
        the risk of hitting query timeout and wasting cpu by throwing away work,
        and if the query was holding onto locks, then it would be able to
        release them more quickly, allowing other queries to run.
    
        If the query released its slot because it has done too much work, then
        it queues itself at the back, to let other queries get a chance to run.
        It's possible that a long running query is holding onto row locks as
        well, but this is currently unhandled. If these are due to large
        transactions, then this could be handled differently in order to prevent
        replication lag.
    
        Two new global variables are added to control this:
        `admission_control_wait_events` controls which events the query will exit admission control for.
        `admission_control_yield_freq` controls how many units of work a query will do until a query yields to other queries.
    
    Reference patch: https://github.com/facebook/mysql-5.6/commit/0399d0a259ecbf3fd79392ef24b37a0d32ff8614
    
    Porting notes:
    - `USER_LOCK` has been removed in favour of `META_DATA_LOCK` since in 8.0 user locks are now implemented as metadata locks.
    - pfs instrumentation is now done at server startup instead of on demand.
    - Do not yield if we're dealing with a system table. It's possible that the thread is holding onto important locks while reading from system tables, so yielding would be a bad idea.
    
    Reviewed By: george-reynya
    
    Differential Revision: D24915097
    
    fbshipit-source-id: 5f9e06b9018

--- sql-common/net_serv.cc
+++ sql-common/net_serv.cc
@@ -43,12 +43,14 @@
 #include <algorithm>
 
 #include <mysql/components/services/log_builtins.h>
+#include <mysql/plugin.h>
 #include "my_byteorder.h"
 #include "my_compiler.h"
 #include "my_dbug.h"
 #include "my_io.h"
 #include "my_macros.h"
 #include "my_sys.h"
+#include "my_systime.h"
 #include "mysql.h"
 #include "mysql/service_mysql_alloc.h"
 #include "mysql_async.h"
@@ -97,6 +99,8 @@ extern long lz4f_net_compression_level;
 #define net_compression_level 6
 #define zstd_net_compression_level 0
 #define lz4f_net_compression_level 0
+#define thd_wait_begin(A, B)
+#define thd_wait_end(A)
 #endif
 
 static bool net_write_buff(NET *, const uchar *, size_t);
@@ -1018,7 +1022,14 @@ static bool net_write_raw_loop(NET *net, const uchar *buf, size_t count) {
   unsigned int retry_count = 0;
 
   while (count) {
+    thd_wait_begin(thd_get_current_thd(), THD_WAIT_NET_IO);
+    DBUG_EXECUTE_IF("simulate_net_write_delay", {
+      // Sleep for 10 seconds.
+      my_sleep(10 * 1000 * 1000);
+    });
+
     ssize_t sentcnt = vio_write(net->vio, buf, count);
+    thd_wait_end(thd_get_current_thd());
 
     if (sentcnt == VIO_SOCKET_READ_TIMEOUT ||
         sentcnt == VIO_SOCKET_WRITE_TIMEOUT) {
