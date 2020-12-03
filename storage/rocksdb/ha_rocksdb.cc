acb9dfd545ee929097f42ea57f932113209a72b6
Yoshinori Matsunobu <yoshinori@fb.com>
2020-12-03 10:34:15 -0800

    Adding track_and_verify_wals_in_manifest RocksDB DB option
    Summary:
    This diff adds RocksDB's recent option track_and_verify_wals_in_manifest
    to track WAL size in MANIFEST and verify size correctness. It also updates RocksDB version to 6.15.1 to include related fixes, notably RocksDB PR#7701.
    
    By setting track_and_verify_wals_in_manifest=ON, any WAL size mismatch
    will make MySQL server refuse to start.
    This prevents from an issue like silent data corruption
    when part of the flushed WAL tail entires are accidentally lost.
    
    This diff was a combination of the two 5.6 diffs https://github.com/facebook/mysql-5.6/commit/3262ec22e5455085b9bec16ebf67dcc9d83bc600  and https://github.com/facebook/mysql-5.6/commit/ccb50cf189d57adcede5b32cf426abdba8f49105 , and modifying existing 8.0 test cases to adjust new RocksDB behavior that it no longer allows to start instances if middle of WAL files are truncated.
    
    Differential Revision: D25310308

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -709,6 +709,7 @@ static uint32_t rocksdb_force_compute_memtable_stats_cachetime;
 static bool rocksdb_debug_optimizer_no_zero_cardinality;
 static uint32_t rocksdb_debug_cardinality_multiplier;
 static uint32_t rocksdb_wal_recovery_mode;
+static bool rocksdb_track_and_verify_wals_in_manifest;
 static uint32_t rocksdb_stats_level;
 static uint32_t rocksdb_access_hint_on_compaction_start;
 static char *rocksdb_compact_cf_name;
@@ -1403,6 +1404,13 @@ static MYSQL_SYSVAR_UINT(
     /* default */ (uint)rocksdb::WALRecoveryMode::kPointInTimeRecovery,
     /* min */ (uint)rocksdb::WALRecoveryMode::kTolerateCorruptedTailRecords,
     /* max */ (uint)rocksdb::WALRecoveryMode::kSkipAnyCorruptedRecords, 0);
+
+static MYSQL_SYSVAR_BOOL(
+    track_and_verify_wals_in_manifest,
+    *reinterpret_cast<bool *>(&rocksdb_track_and_verify_wals_in_manifest),
+    PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_READONLY,
+    "DBOptions::track_and_verify_wals_in_manifest for RocksDB", nullptr,
+    nullptr, true);
 
 static MYSQL_SYSVAR_UINT(
     stats_level, rocksdb_stats_level, PLUGIN_VAR_RQCMDARG,
@@ -2313,6 +2321,7 @@ static struct SYS_VAR *rocksdb_system_variables[] = {
     MYSQL_SYSVAR(enable_thread_tracking),
     MYSQL_SYSVAR(perf_context_level),
     MYSQL_SYSVAR(wal_recovery_mode),
+    MYSQL_SYSVAR(track_and_verify_wals_in_manifest),
     MYSQL_SYSVAR(stats_level),
     MYSQL_SYSVAR(access_hint_on_compaction_start),
     MYSQL_SYSVAR(compaction_readahead_size),
@@ -5661,6 +5670,9 @@ static int rocksdb_init_internal(void *const p) {
 
   rocksdb_db_options->wal_recovery_mode =
       static_cast<rocksdb::WALRecoveryMode>(rocksdb_wal_recovery_mode);
+
+  rocksdb_db_options->track_and_verify_wals_in_manifest =
+      rocksdb_track_and_verify_wals_in_manifest;
 
   rocksdb_db_options->access_hint_on_compaction_start =
       static_cast<rocksdb::Options::AccessHint>(
