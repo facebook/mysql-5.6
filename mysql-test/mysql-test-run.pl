6527c38e6df6efbfdac0dd6cdfdf31a3cf5da8f6
Manuel Ung <mung@fb.com>
2020-12-08 20:00:54 -0800

    [mysql-test-run] Add option to force tmpdir shortening
    Summary:
    In mysql-test-run.pl, there is a check to see if the socket name path is too long based on the currently configured tmpdir. However, it assumes a specific socket name, but some tests use a different socket name, or a different path for the socket.
    
    It seems like it's more resilient to always shorten the tmpdir path in our environment, so add a new parameter to mysql-test-run.pl that does this.
    
    Differential Revision: D25439366

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -125,6 +125,7 @@ my $opt_strace_server;
 my @opt_perf_servers;
 my $opt_stress;
 my $opt_tmpdir;
+my $opt_tmpdir_force_short;
 my $opt_tmpdir_pid;
 my $opt_trace_protocol;
 my $opt_user_args;
@@ -1813,6 +1814,7 @@ sub command_line_setup {
     'client-libdir=s' => \$path_client_libdir,
     'mem'             => \$opt_mem,
     'tmpdir=s'        => \$opt_tmpdir,
+    'tmpdir-force-short' => \$opt_tmpdir_force_short,
     'vardir=s'        => \$opt_vardir,
 
     # Misc
@@ -2215,7 +2217,7 @@ sub command_line_setup {
   if (!$opt_tmpdir) {
     $opt_tmpdir = "$opt_vardir/tmp" unless $opt_tmpdir;
 
-    my $res =
+    my $res = $opt_tmpdir_force_short or
       check_socket_path_length("$opt_tmpdir/mysqld.NN.sock", $opt_parallel);
 
     if ($res) {
@@ -3551,6 +3553,16 @@ sub environment_setup {
   my $pathsep = ":";
   $pathsep = ";" if IS_WINDOWS && !IS_CYGWIN;
   $ENV{'PATH'} = "$ENV{'PATH'}" . $pathsep . $perldir;
+
+  # ----------------------------------------------------
+  # sst_dump
+  # ----------------------------------------------------
+  my $exe_sst_dump=
+    mtr_exe_maybe_exists(
+           vs_config_dirs('storage/rocksdb', 'sst_dump'),
+           "$path_client_bindir/sst_dump",
+           "$basedir/storage/rocksdb/sst_dump");
+  $ENV{'MYSQL_SST_DUMP'}= native_path($exe_sst_dump);
 }
 
 sub remove_vardir_subs() {
