8344ff0902d2552f0cf5687b4c0b70c745c6870c
Manuel Ung <mung@fb.com>
2020-12-08 20:00:54 -0800

    Add option to force tmpdir shortening
    Summary:
    In mysql-test-run.pl, there is a check to see if the socket name path is too long based on the currently configured tmpdir. However, it assumes a specific socket name, but some tests use a different socket name, or a different path for the socket.
    
    It seems like it's more resilient to always shorten the tmpdir path in our environment, so add a new parameter to mysql-test-run.pl that does this.
    
    Reviewed By: hermanlee
    
    Differential Revision: D25439366
    
    fbshipit-source-id: c35786ee18a

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -111,6 +111,7 @@ my $opt_strace_client;
 my $opt_strace_server;
 my $opt_stress;
 my $opt_tmpdir;
+my $opt_tmpdir_force_short = 1;
 my $opt_tmpdir_pid;
 my $opt_trace_protocol;
 my $opt_user_args;
@@ -1580,6 +1581,7 @@ sub command_line_setup {
     'client-libdir=s' => \$path_client_libdir,
     'mem'             => \$opt_mem,
     'tmpdir=s'        => \$opt_tmpdir,
+    'tmpdir-force-short' => \$opt_tmpdir_force_short,
     'vardir=s'        => \$opt_vardir,
 
     # Misc
@@ -1938,8 +1940,8 @@ sub command_line_setup {
   if (!$opt_tmpdir) {
     $opt_tmpdir = "$opt_vardir/tmp" unless $opt_tmpdir;
 
-    my $res = check_socket_path_length("$opt_tmpdir/mysqld.NN.sock",
-                                       $opt_parallel);
+    my $res = $opt_tmpdir_force_short or
+      check_socket_path_length("$opt_tmpdir/mysqld.NN.sock", $opt_parallel);
 
     if ($res) {
       mtr_report("Too long tmpdir path '$opt_tmpdir'",
