4dd70a9f63adc47a46917c71609e06068adbd573
Luqun Lou <luqun@fb.com>
2021-01-10 11:34:39 -0800

    Re-register new log file after closing to solve heap-use-after-free
    Summary:
    ASANDEBUG complains heap-use-after-free issue due to raft will store IO_CACHE from binlog,  Later MYSQL_BIN_LOG will create a new binlog file and close its old binlog file(this also release old IO_CACHE).  raft may use stale IO_CACHE during commit trx.
    
    The change is to register new binlog file to raft to make sure raft always keep latest IO_CACHE.
    
    TODO: We should update raft plugin to pass message to mysqld, mysqld should responsible to write message into IO_CACHE --- raft plugin shouldn't know IO_CACHE.
    
    Differential Revision: D25865086

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -8294,7 +8294,13 @@ int MYSQL_BIN_LOG::new_file_impl(
     // pattern in most places of after_commit hook (TODO)
     (void)RUN_HOOK(raft_replication, after_commit, (current_thd));
   }
-
+  if (!error && enable_raft_plugin && (is_relay_log || rotate_via_raft)) {
+    // Register new log to raft
+    // Previous close(LOG_CLOSE_TO_BE_OPENED | LOG_CLOSE_INDEX,) will close
+    // binlog and its IO_CACHE.
+    register_log_entities(current_thd, /*context=*/0,
+                          /*need_lock=*/false, is_relay_log);
+  }
 end:
   if (error && close_on_error /* rotate, flush or reopen failed */) {
     /*
@@ -11424,7 +11430,7 @@ err:
   DBUG_RETURN(error);
 }
 
-int binlog_change_to_binlog() {
+int binlog_change_to_binlog(THD *thd) {
   DBUG_ENTER("binlog_change_to_binlog");
 
   // disable_raft_log_repointing for MTR integration tests
@@ -11529,6 +11535,11 @@ int binlog_change_to_binlog() {
   mysql_bin_log.is_apply_log = false;
   mysql_bin_log.apply_file_count.store(0);
 
+  // Register new log to raft
+  // Previous mysql_bin_log.close(LOG_CLOSE_INDEX) will also close binlog and
+  // its IO_CACHE.
+  mysql_bin_log.register_log_entities(thd, /*context=*/0, /*need_lock=*/false,
+                                      /*is_relay_log=*/false);
 err:
   mysql_bin_log.unlock_index();
   mysql_mutex_unlock(mysql_bin_log.get_log_lock());
