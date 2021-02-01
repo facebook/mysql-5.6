6c773a1609e34b735e2971807b7cb3a213d57eed
Luqun Lou <luqun@fb.com>
2021-01-31 18:29:58 -0800

    Raft abrupt stepdown and trim binlog file / gtid test
    Summary: binlog file should get trimmed for abrupt stepdown
    
    Reviewed By: Pushapgl, bhatvinay
    
    Differential Revision: D26169975
    
    fbshipit-source-id: e2b4d4add55

--- sql/rpl_rli.cc
+++ sql/rpl_rli.cc
@@ -1858,7 +1858,9 @@ int Relay_log_info::remove_logged_gtids(
       DBUG_PRINT("info",
                  ("Removing gtid(sidno:%d, gno:%lld) from rli logged gtids",
                   gtid.sidno, gtid.gno));
+      get_sid_lock()->wrlock();
       gtid_set->_remove_gtid(gtid);
+      get_sid_lock()->unlock();
     }
   }
 
