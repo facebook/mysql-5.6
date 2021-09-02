054f9004608b3cc2c7e4d1c2fe2cb7ad93bedf27
Manuel Ung <mung@fb.com>
2021-09-02 13:30:10 -0700

    Fix oss builds
    Summary: We should be using rocksdb instead of my_rocksdb because ROCKSDB_CUSTOM_NAMESPACE isn't always defined, so we should let ROCKSDB_CUSTOM_NAMESPACE control whether a different namespace name should be used or not.
    
    Reviewed By: yizhang82
    
    Differential Revision: D30737755
    
    fbshipit-source-id: 69f072ce726

--- storage/rocksdb/rdb_iterator.h
+++ storage/rocksdb/rdb_iterator.h
@@ -181,17 +181,17 @@ class Rdb_iterator_partial : public Rdb_iterator_base {
   int next_with_direction_in_group(bool direction);
   int next_with_direction(bool direction);
 
-  using Slice_pair = std::pair<my_rocksdb::Slice, my_rocksdb::Slice>;
+  using Slice_pair = std::pair<rocksdb::Slice, rocksdb::Slice>;
   using Records = std::vector<Slice_pair>;
 
   struct slice_comparator {
     explicit slice_comparator(const rocksdb::Comparator *c) : m_comparator(c) {}
     const rocksdb::Comparator *const m_comparator;
 
-    bool operator()(const my_rocksdb::Slice &lhs, const Slice_pair &rhs) {
+    bool operator()(const rocksdb::Slice &lhs, const Slice_pair &rhs) {
       return m_comparator->Compare(lhs, rhs.first) < 0;
     }
-    bool operator()(const Slice_pair &lhs, const my_rocksdb::Slice &rhs) {
+    bool operator()(const Slice_pair &lhs, const rocksdb::Slice &rhs) {
       return m_comparator->Compare(lhs.first, rhs) < 0;
     }
     bool operator()(const Slice_pair &lhs, const Slice_pair &rhs) {
