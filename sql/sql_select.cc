ea8922d99fb0da9a1b3262e909ee1d036b600fe3
Chi-I Huang <chii@fb.com>
2021-09-07 16:28:57 -0700

    Notify audit plugin when query statement is prepared before execution
    Summary:
    1. Notify audit plugin when query statement is prepared before execution with new event class `MYSQL_AUDIT_QUERY_STMT_PREPARED`
    2. Introduce `thd_process_select_lex_visitor` to allow a custom vistor to be passed in from plugin.
    
    We're not reusing the existing audit event because the select statement is not yet "prepared" at that moment, i.e. the column reference `Item_field` hasn't yet resolved the actual column name, table name and db name.
    
    So, we introduce another event name `MYSQL_AUDIT_QUERY_STMT_PREPARED` between the statement is prepared but before executed.
    
    Reviewed By: mzait
    
    Differential Revision: D31093855
    
    fbshipit-source-id: ecc5c3a4f05

--- sql/sql_select.cc
+++ sql/sql_select.cc
@@ -96,6 +96,7 @@
 #include "sql/row_iterator.h"
 #include "sql/set_var.h"
 #include "sql/sorting_iterator.h"
+#include "sql/sql_audit.h"
 #include "sql/sql_base.h"
 #include "sql/sql_class.h"
 #include "sql/sql_cmd.h"
@@ -601,6 +602,9 @@ bool Sql_cmd_dml::execute(THD *thd) {
   // slightly different from 5.6 and would need a little more investment to
   // tag joins properly.
   parse_column_usage_info(thd);
+
+  mysql_audit_notify(thd, MYSQL_AUDIT_QUERY_STMT_PREPARED,
+                     "MYSQL_AUDIT_QUERY_STMT_PREPARED");
 
   thd->validate_schema_info(lex->query_tables);
 
@@ -1686,8 +1690,9 @@ void JOIN::reset() {
 
   if (!executed) return;
 
-  unit->offset_limit_cnt = (ha_rows)(
-      select_lex->offset_limit ? select_lex->offset_limit->val_uint() : 0ULL);
+  unit->offset_limit_cnt =
+      (ha_rows)(select_lex->offset_limit ? select_lex->offset_limit->val_uint()
+                                         : 0ULL);
 
   group_sent = false;
   recursive_iteration_count = 0;
