f549a517ad466ba36bc8a5f40007f6a48a48934d
Jay Edgar <jkedgar@fb.com>
2021-09-23 14:53:52 -0700

    Make FB added client errors consistent across versions
    Summary: We have run into multiple problems when upgrading the MySQL client library in fbcode when the FB-added client error messages change their value because we have to adjust them when Oracle adds more client errors.  To try to avoid this continuing to happen in the future, add a bunch of place holder client error messages to push the FB-added ones way down the list.
    
    Reviewed By: aditya-jalan, lth
    
    Differential Revision: D31160326
    
    fbshipit-source-id: 3c5e4d064e2

--- include/mysql.h.pp
+++ include/mysql.h.pp
@@ -405,10 +405,16 @@ typedef struct MYSQL_TIME {
 void init_client_errs(void);
 void finish_client_errs(void);
 extern const char *client_errors[];
+static inline bool isPlaceHolder(int client_errno) {
+  return client_errno >= 2068 &&
+         client_errno <= 2199;
+}
 static inline const char *ER_CLIENT(int client_errno) {
-  if (client_errno >= 2000 && client_errno <= 2070)
+  if (client_errno >= 2000 && client_errno <= 2202 &&
+      !isPlaceHolder(client_errno)) {
     return client_errors[client_errno - 2000];
-  return client_errors[2000];
+  }
+  return client_errors[2000 - 2000];
 }
 extern unsigned int mysql_port;
 extern char *mysql_unix_port;
