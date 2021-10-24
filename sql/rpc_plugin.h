60881c95ee415289bc3afb9356076813e7bbf33f
Jupyung Lee (JP) <jupyung@fb.com>
2021-10-23 23:21:20 -0700

    Introduce a base protocol class and header file for communicating with thrift plugin
    Summary:
    This diff introduces a base protocol class, which can be derived as MySQL or RPC protocol class.
    Also, a new header file, sql/rpc_plugin.h, defines essential structs necessary for communicating
    between myrocks and thrift plugin. This header file will later be synced up and used by thrift plugin.
    
    Bypass RPC reference diff: D30493654
    Thrift plugin reference diff: D30692287
    
    Reviewed By: yizhang82
    
    Differential Revision: D31885126
    
    fbshipit-source-id: 42318e4eb85

---
+++ sql/rpc_plugin.h
@@ -0,0 +1,119 @@
+#ifndef RPC_PLUGIN_INCLUDED
+#define RPC_PLUGIN_INCLUDED
+
+#include <array>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+// These structs are defined for communication between thrift plugin and myrocks
+typedef enum {
+  _BOOL = 0,
+  _UNSIGNED_INT = 1,
+  _SIGNED_INT = 2,
+  _DOUBLE = 3,
+  _STRING = 4
+} myrocks_value_type;
+
+struct myrocks_column_cond_value {
+  myrocks_value_type type;
+  union {
+    bool boolVal;
+    uint64_t i64Val;
+    double doubleVal;
+    const char *stringVal;
+  };
+};
+
+struct myrocks_where_item {
+  typedef enum {
+    EQ = 0,
+    LT = 1,
+    GT = 2,
+    LE = 3,
+    GE = 4,
+  } where_op;
+
+  std::string column;
+  where_op op;
+  myrocks_column_cond_value value;
+};
+
+struct myrocks_order_by_item {
+  typedef enum {
+    ASC = 0,
+    DESC = 1,
+  } order_by_op;
+
+  std::string column;
+  order_by_op op;
+};
+
+struct myrocks_column_value {
+  myrocks_value_type type;
+  bool isNull = false;
+  union {
+    bool boolVal;
+    uint64_t i64Val;
+    int64_t signed_i64Val;
+    double doubleVal;
+    uchar *stringVal;
+  };
+};
+
+const int MAX_VALUES_PER_RPC_WHERE_ITEM = 30;
+struct myrocks_where_in_item {
+  std::string column;
+  std::array<myrocks_column_cond_value, MAX_VALUES_PER_RPC_WHERE_ITEM> values;
+  uint32_t num_values;
+};
+
+// This rpc buffer is allocated per each thrift io thread
+const int MAX_COLUMNS_PER_RPC_BUFFER = 200;
+using myrocks_columns =
+    std::array<myrocks_column_value, MAX_COLUMNS_PER_RPC_BUFFER>;
+
+struct myrocks_column_metadata {
+  std::string name;
+  myrocks_value_type type;
+};
+
+/*
+   myrocks calls this callback function whenever a row is ready.
+   rpc_buffer             : points to protocol-specific (eg. thrift) rpc buffer
+                            where myrocks_columns are converted to
+                            protocol-specific format and stored.
+   myrocks_columns_values : points to where myrocks_column_value typed values
+                            are stored.
+   num_columns            : stores the number of columns to be fetched from
+                            myrocks_column_values.
+*/
+using myrocks_bypass_rpc_send_row_fn =
+    void (*)(void *rpc_buffer, myrocks_columns *myrocks_columns_values,
+             uint64_t num_columns);
+
+/*
+   myrocks calls this callback function when the metadata of resultset is ready.
+   rpc_buffer   : points to protocol-specific (eg. thrift) rpc buffer where
+                  metadata in result are converted to protocol-specific format
+                  and stored.
+   result       : points to where column metadata are stored.
+*/
+using myrocks_bypass_rpc_send_metadata_fn =
+    void (*)(void *rpc_buffer, std::vector<myrocks_column_metadata> *result);
+
+// This is allocated and populated by thrift plugin
+struct myrocks_select_from_rpc {
+  std::string db_name;
+  std::string table_name;
+  std::vector<std::string> columns;
+  std::vector<myrocks_where_item> where;
+  uint64_t limit;
+  std::string force_index;
+  std::vector<myrocks_order_by_item> order_by;
+  void *rpc_buffer;
+  myrocks_bypass_rpc_send_row_fn send_row;
+  myrocks_bypass_rpc_send_metadata_fn send_metadata;
+};
+
+#endif /* RPC_PLUGIN_INCLUDED */
