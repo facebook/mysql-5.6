d75a64177eba95fe6efef7ed48b2018bfd2cc79e
Pushap Goyal <pgl@fb.com>
2021-12-15 13:10:12 -0800

    Fixing the ubsan build complaining for null attributes in rename table
    Summary:
    Recently started passing from_table_def and to_table_def as nullptr for rename_table when we truncate table as part of ha_rocksdb::delete_all_rows method. These two attributes were unused, so passing them as null won't break any functionality.
    
    Squash with D32272622.
    
    Reviewed By: hermanlee
    
    Differential Revision: D33140919
    
    fbshipit-source-id: 039eb0481f6

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -461,10 +461,11 @@ class ha_rocksdb : public my_core::handler {
 
   bool should_store_row_debug_checksums() const;
 
-  int rename_table(const char *const from, const char *const to,
-                   const dd::Table *from_table_def,
-                   dd::Table *to_table_def) override
-      MY_ATTRIBUTE((__nonnull__, __warn_unused_result__));
+  int rename_table(const char *const from MY_ATTRIBUTE((__nonnull__)),
+                   const char *const to MY_ATTRIBUTE((__nonnull__)),
+                   const dd::Table *from_table_def MY_ATTRIBUTE((__unused__)),
+                   dd::Table *to_table_def MY_ATTRIBUTE((__unused__))) override
+      MY_ATTRIBUTE((__warn_unused_result__));
 
   int convert_record_from_storage_format(const rocksdb::Slice *const key,
                                          const rocksdb::Slice *const value,
