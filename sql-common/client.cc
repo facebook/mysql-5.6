881ad761de8ba915063199a6c2dcb353272e0979
Pushap Goyal <pgl@fb.com>
2021-12-17 13:21:47 -0800

    Fixed heap-use-after-free failures for cli_read_query_result_nonblocking
    Summary:
    All test cases where the connection is killed and accessed after that shows  heap-use-after-free failures.
    
    main.loadxml.test is one such example.
    
    Reviewed By: hermanlee
    
    Differential Revision: D33196224

--- sql-common/client.cc
+++ sql-common/client.cc
@@ -7451,8 +7451,10 @@ static net_async_status cli_read_query_result_nonblocking(MYSQL *mysql) {
 
       /* TODO: Make LOAD DATA LOCAL INFILE asynchronous. */
       if ((length = cli_safe_read(mysql, nullptr)) == packet_error || error) {
-        net_async->async_read_query_result_status =
-            NET_ASYNC_READ_QUERY_RESULT_IDLE;
+        if (NET_ASYNC_DATA(net) != nullptr) {
+          net_async->async_read_query_result_status =
+              NET_ASYNC_READ_QUERY_RESULT_IDLE;
+        }
         async_context->async_op_status = ASYNC_OP_UNSET;
         async_context->async_query_state = QUERY_IDLE;
         async_context->async_query_length = 0;
