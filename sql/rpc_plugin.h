abc6684de663b719ce0615e3b61cf40aaf70b503
Jupyung Lee (JP) <jupyung@fb.com>
2022-01-18 16:53:55 -0800

    Add RPC select parser class
    Summary:
    This diff adds rpc select parser class, derived from base select parser class.
    This class reads select request sent from rpc plugin in the form of
    `myrocks_select_from_rpc` struct.
    This will be instantiated by an entry function called by rpc plugin.
    
    The previous attempt to add rpc parser class is in D32381747.
    
    Reviewed By: yizhang82
    
    Differential Revision: D33649009
    
    fbshipit-source-id: 0998e668fb1

--- sql/rpc_plugin.h
+++ sql/rpc_plugin.h
@@ -63,10 +63,13 @@ struct myrocks_column_value {
   };
 };
 
-const int MAX_VALUES_PER_RPC_WHERE_ITEM = 30;
+static const int MAX_VALUES_PER_RPC_WHERE_ITEM = 30;
 struct myrocks_where_in_item {
   std::string column;
   std::array<myrocks_column_cond_value, MAX_VALUES_PER_RPC_WHERE_ITEM> values;
+  // If the number of values is above the limit, then these values are stored in
+  // the following more_values.
+  myrocks_column_cond_value *more_values;
   uint32_t num_values;
 };
 
@@ -74,11 +77,6 @@ struct myrocks_where_in_item {
 const int MAX_COLUMNS_PER_RPC_BUFFER = 200;
 using myrocks_columns =
     std::array<myrocks_column_value, MAX_COLUMNS_PER_RPC_BUFFER>;
-
-struct myrocks_column_metadata {
-  std::string name;
-  myrocks_value_type type;
-};
 
 /*
    myrocks calls this callback function whenever a row is ready.
@@ -94,28 +92,25 @@ using myrocks_bypass_rpc_send_row_fn =
     void (*)(void *rpc_buffer, myrocks_columns *myrocks_columns_values,
              uint64_t num_columns);
 
-/*
-   myrocks calls this callback function when the metadata of resultset is ready.
-   rpc_buffer   : points to protocol-specific (eg. thrift) rpc buffer where
-                  metadata in result are converted to protocol-specific format
-                  and stored.
-   result       : points to where column metadata are stored.
-*/
-using myrocks_bypass_rpc_send_metadata_fn =
-    void (*)(void *rpc_buffer, std::vector<myrocks_column_metadata> *result);
-
 // This is allocated and populated by thrift plugin
 struct myrocks_select_from_rpc {
   std::string db_name;
   std::string table_name;
   std::vector<std::string> columns;
   std::vector<myrocks_where_item> where;
+  std::vector<myrocks_where_in_item> where_in;
   uint64_t limit;
+  uint64_t limit_offset;
   std::string force_index;
   std::vector<myrocks_order_by_item> order_by;
   void *rpc_buffer;
   myrocks_bypass_rpc_send_row_fn send_row;
-  myrocks_bypass_rpc_send_metadata_fn send_metadata;
+};
+
+struct bypass_rpc_exception {
+  uint32_t errnum = 0;
+  std::string sqlstate;
+  std::string message;
 };
 
 #endif /* RPC_PLUGIN_INCLUDED */
