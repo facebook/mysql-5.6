9d8dccc96244d13406302aee341b539fac8ef404
Abhinav Sharma <abhinavsharma@fb.com>
2022-03-25 09:03:10 -0700

    Ability to send binlog row image delta in response attrs
    Summary:
    Adding an ability to send the delta of column sizes between the
    before and after images in the binlog to clients through response
    attributes. This info will be useful to tell the clients the rough
    estimate of storage impact for every transaction.
    
    Reviewed By: anirbanr-fb
    
    Differential Revision: D34164720
    
    fbshipit-source-id: b355d19a2b92789ff3f314d0ca4a224a9e528756

--- sql/system_variables.h
+++ sql/system_variables.h
@@ -367,6 +367,8 @@ struct System_variables {
    */
   bool response_attrs_contain_hlc;
 
+  bool response_attrs_contain_binlog_row_image_delta;
+
   /*
     Should we set the server CPU time in response attribute?
    */
