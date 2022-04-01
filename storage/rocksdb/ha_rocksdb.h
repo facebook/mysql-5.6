2efce5da35ef3637729526a0cdf91dd24c712293
Luqun Lou <luqun@fb.com>
2022-04-01 14:45:37 -0700

    add rocksdb_column_default_value_as_expression and more testing
    Summary: add rocksdb_column_default_value_as_expressoin variable to control whether support column default value as expression/function and also add more testing
    
    Reviewed By: yizhang82
    
    Differential Revision: D35328339
    
    fbshipit-source-id: e0bf32e61f8a6b089b59364d06da3988adf03c00

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -81,7 +81,7 @@ class Rdb_field_encoder;
 extern PSI_rwlock_key key_rwlock_read_free_rpl_tables;
 #endif
 extern Regex_list_handler rdb_read_free_regex_handler;
-
+static bool rocksdb_column_default_value_as_expression = true;
 /**
   @brief
   Rdb_table_handler is a reference-counted structure storing information for
@@ -450,7 +450,9 @@ class ha_rocksdb : public my_core::handler, public blob_buffer {
                 (m_pk_can_be_decoded ? HA_PRIMARY_KEY_IN_READ_INDEX : 0) |
                 HA_PRIMARY_KEY_REQUIRED_FOR_POSITION | HA_NULL_IN_KEY |
                 HA_PARTIAL_COLUMN_READ | HA_ONLINE_ANALYZE |
-                HA_SUPPORTS_DEFAULT_EXPRESSION);
+                (rocksdb_column_default_value_as_expression
+                     ? HA_SUPPORTS_DEFAULT_EXPRESSION
+                     : 0));
   }
 
   bool init_with_fields() override;
