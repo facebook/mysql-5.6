2873d642cf0b157c63420f500c6d9ebb45e5ab73
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-08-23 14:16:06 -0700

    [macos] MyRocks: define BSD-style endian conversion functions (#1204)
    Summary:
    macOS does not have beXXtoh and htobeXX functions. Define them using macOS
    equivalents.
    
    Both downstreams have done the same: https://github.com/percona/percona-server/pull/4808/files#diff-083593616349ef36e9a39f1251813d7c0fe6a93883f131fe041f0d167dfe941f, https://github.com/MariaDB/server/commit/b64910ce27a4df33e3ad2e3f40764e8b3271a9aa#diff-083593616349ef36e9a39f1251813d7c0fe6a93883f131fe041f0d167dfe941f
    
    This patch is closer to MariaDB one instead of Percona : use general `__APPLE__` define instead of RocksDB core `OS_MACOSX`, define only used functions, and define them right after system header includes.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1204
    
    Reviewed By: Pushapgl
    
    Differential Revision: D38956497
    
    Pulled By: hermanlee

--- storage/rocksdb/rdb_buff.h
+++ storage/rocksdb/rdb_buff.h
@@ -20,6 +20,21 @@
 #include <string>
 #include <vector>
 
+#ifdef __APPLE__
+
+#include <libkern/OSByteOrder.h>
+
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define htobe16(x) OSSwapHostToBigInt16(x)
+
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define htobe32(x) OSSwapHostToBigInt32(x)
+
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define htobe64(x) OSSwapHostToBigInt64(x)
+
+#endif  // __APPLE__
+
 /* MySQL header files */
 
 /* MyRocks header files */
