29158e05458aa02fee0dadf4d6007a4246ad6fe9
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-09-05 10:59:52 +0300

    Disable DSCP functionality on macOS
    Because macOS does not support it.
    
    Squash with 946ab96defa464315c9fe1426ed77cb277a3b562

--- sql/conn_handler/connection_handler_per_thread.cc
+++ sql/conn_handler/connection_handler_per_thread.cc
@@ -294,7 +294,9 @@ static void *handle_connection(void *arg) {
     MYSQL_SOCKET socket = thd->get_protocol_classic()->get_vio()->mysql_socket;
     mysql_socket_set_thread_owner(socket);
     thd->set_thread_priority();
+#ifndef __APPLE__
     thd->set_dscp_on_socket();
+#endif
 
     thd_manager->add_thd(thd);
 
