3f604c9a4e44266be0ef58d35db5d4b1fce018f4
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2022-09-08 11:01:15 -0700

    =?UTF-8?q?[mysql-5.6][PR]=20Fix=20Oracle=20bug=20108317?= =?UTF-8?q?=20(clone=5Fos=5Fcopy=5Ffile=5Fto=5Fbuf=20partial=20read=20hand?= =?UTF-8?q?lin=E2=80=A6?= (#1242)
    Summary:
    …g completely broken)
    
    1) Pass the remaining bytes to read instead of total bytes to read_from_file. 2) Advance the read buffer pointer after a read.
    3) To avoid saving and restoring these in/out arguments to the correct out values, observe that no callers are using their out values and convert them to in-only arguments.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1242
    
    Reviewed By: yoshinorim
    
    Differential Revision: D39356718
    
    Pulled By: sunshine-Chun
    
    fbshipit-source-id: c5ab0f02ee809db4f9482541dce4bd0909044084

--- plugin/clone/include/clone_os.h
+++ plugin/clone/include/clone_os.h
@@ -42,12 +42,12 @@ const uint CLONE_DEF_CON = 16;
 
 /** Copy data from file to buffer
 @param[in]	from_file	source file descriptor
-@param[in,out]	to_buffer	buffer to copy data
-@param[in,out]	length		buffer/data length
+@param[in]	to_buffer	buffer to copy data
+@param[in]	length		buffer/data length
 @param[in]	src_name	source file name
 @return error code */
-int clone_os_copy_file_to_buf(Ha_clone_file from_file, uchar *&to_buffer,
-                              uint &length, const char *src_name);
+int clone_os_copy_file_to_buf(Ha_clone_file from_file, uchar *to_buffer,
+                              uint length, const char *src_name);
 
 /** Check zero copy support
 @return true if supports zero copy */
