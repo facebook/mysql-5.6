facebook/mysql-5.6

commit 78c1954822958d96a0ef527eeb3a0b8744be520d

Bug #34638573 Compile MySQL with clang 15 (#1302)
Summary:
Clang 15 comes with a few new warnings:

variable 'cnt' set but not used
-Wunused-but-set-variable

argument 'ptr' of type 'const LinearSectionPtr[3]' with mismatched bound -Warray-parameter

out-of-line definition of constexpr static data member is redundant in C++17 and is deprecated
-Wdeprecated

unqualified call to 'std::move'
-Wunqualified-std-cast-call

builtin __has_trivial_copy is deprecated; use __is_trivially_copyable instead -Wdeprecated-builtins

Fix these, to get a clean build also with Clang 15.

Change-Id: I315b7b77e2bf3a6bacf091aeb8887d0902d3f950

Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1302

Reviewed By: hermanlee

Differential Revision: D45446111

Pulled By: sunshine-Chun

fbshipit-source-id: f074287bb2043d6c7cc98fe6e5e136871f4bf71e

sql/auth/sql_auth_cache.cc
@@ -93,7 +93,6 @@
 
 using std::make_unique;
 using std::min;
-using std::move;
 using std::string;
 using std::unique_ptr;
 
@@ -2489,8 +2488,7 @@ bool acl_reload(THD *thd, bool mdl_locked) {
   old_acl_users = acl_users;
   old_acl_dbs = acl_dbs;
   old_acl_proxy_users = acl_proxy_users;
-  old_acl_restrictions = move(acl_restrictions);
-  old_acl_fast_lookup = acl_fast_lookup;
+  old_acl_restrictions = std::move(acl_restrictions);
   swap_role_cache();
   roles_init();
 
@@ -2509,7 +2507,7 @@ bool acl_reload(THD *thd, bool mdl_locked) {
   // acl_load() overwrites global_acl_memory, so we need to free it.
   // However, we can't do that immediately, because acl_load() might fail,
   // and then we'd need to keep it.
-  old_mem = move(global_acl_memory);
+  old_mem = std::move(global_acl_memory);
   delete acl_wild_hosts;
   acl_wild_hosts = nullptr;
   delete acl_check_hosts;
@@ -2531,8 +2529,8 @@ bool acl_reload(THD *thd, bool mdl_locked) {
     acl_dbs = old_acl_dbs;
     acl_proxy_users = old_acl_proxy_users;
     acl_fast_lookup = old_acl_fast_lookup;
-    global_acl_memory = move(old_mem);
-    acl_restrictions = move(old_acl_restrictions);
+    global_acl_memory = std::move(old_mem);
+    acl_restrictions = std::move(old_acl_restrictions);
     // Revert to the old role caches
     swap_role_cache();
     // Old caches must be pointing to the global role caches right now
@@ -2869,17 +2867,17 @@ static bool grant_reload_procs_priv(TABLE_LIST *table) {
   /* Save a copy of the current hash if we need to undo the grant load */
   unique_ptr<
       malloc_unordered_multimap<string, unique_ptr_destroy_only<GRANT_NAME>>>
-      old_proc_priv_hash(move(proc_priv_hash));
+      old_proc_priv_hash(std::move(proc_priv_hash));
   unique_ptr<
       malloc_unordered_multimap<string, unique_ptr_destroy_only<GRANT_NAME>>>
-      old_func_priv_hash(move(func_priv_hash));
+      old_func_priv_hash(std::move(func_priv_hash));
   bool return_val = false;
 
   if ((return_val = grant_load_procs_priv(table->table))) {
     /* Error; Reverting to old hash */
     DBUG_PRINT("error", ("Reverting to old privileges"));
-    proc_priv_hash = move(old_proc_priv_hash);
-    func_priv_hash = move(old_func_priv_hash);
+    proc_priv_hash = std::move(old_proc_priv_hash);
+    func_priv_hash = std::move(old_func_priv_hash);
   }
 
   return return_val;
@@ -2946,13 +2944,13 @@ bool grant_reload(THD *thd, bool mdl_locked) {
   {
     unique_ptr<
         malloc_unordered_multimap<string, unique_ptr_destroy_only<GRANT_TABLE>>>
-        old_column_priv_hash(move(column_priv_hash));
+        old_column_priv_hash(std::move(column_priv_hash));
 
     /*
       Create a new memory pool but save the current memory pool to make an
       undo opertion possible in case of failure.
     */
-    old_mem = move(memex);
+    old_mem = std::move(memex);
     init_sql_alloc(key_memory_acl_memex, &memex, ACL_ALLOC_BLOCK_SIZE, 0);
     /*
       tables[2].table i.e. procs_priv can be null if we are working with
@@ -2962,10 +2960,11 @@ bool grant_reload(THD *thd, bool mdl_locked) {
                        grant_reload_procs_priv(
                            &tables[2])))) {  // Error. Revert to old hash
       DBUG_PRINT("error", ("Reverting to old privileges"));
-      column_priv_hash = move(old_column_priv_hash); /* purecov: deadcode */
+      column_priv_hash =
+          std::move(old_column_priv_hash); /* purecov: deadcode */
       memex.Clear();
-      memex = move(old_mem); /* purecov: deadcode */
-    } else {                 // Reload successful
+      memex = std::move(old_mem); /* purecov: deadcode */
+    } else {                      // Reload successful
       old_column_priv_hash.reset();
       old_mem.Clear();
       grant_version++;
@@ -3542,13 +3541,13 @@ Acl_map::~Acl_map() {
 Acl_map::Acl_map(const Acl_map &&map) { operator=(map); }
 
 Acl_map &Acl_map::operator=(Acl_map &&map) {
-  m_db_acls = move(map.m_db_acls);
+  m_db_acls = std::move(map.m_db_acls);
   m_global_acl = map.m_global_acl;
   m_reference_count = map.m_reference_count.load();
-  m_table_acls = move(map.m_table_acls);
-  m_sp_acls = move(map.m_sp_acls);
-  m_func_acls = move(map.m_func_acls);
-  m_with_admin_acls = move(map.m_with_admin_acls);
+  m_table_acls = std::move(map.m_table_acls);
+  m_sp_acls = std::move(map.m_sp_acls);
+  m_func_acls = std::move(map.m_func_acls);
+  m_with_admin_acls = std::move(map.m_with_admin_acls);
   m_version = map.m_version;
   m_restrictions = map.m_restrictions;
   map.m_reference_count = 0;

commit d15a8ded8f67f1ad62f6672db19dd1aa345ea2b1

Restore an erroneously-removed assignment
A regression from 78c1954822958d96a0ef527eeb3a0b8744be520d

sql/auth/sql_auth_cache.cc
@@ -2489,6 +2489,7 @@ bool acl_reload(THD *thd, bool mdl_locked) {
   old_acl_dbs = acl_dbs;
   old_acl_proxy_users = acl_proxy_users;
   old_acl_restrictions = std::move(acl_restrictions);
+  old_acl_fast_lookup = std::move(acl_fast_lookup);
   swap_role_cache();
   roles_init();
 