996dabc9693805b3066ab9ece6e8dd7eb8558cc6
Abhinav Sharma <abhinavsharma@meta.com>
2023-10-24 12:32:48 -0700

    Flushing engine logs when switching from primary to secondary
    Summary:
    We need to make sure that we flush the engine logs (aka sync the engines)
    before we switch from binlog to apply log (i.e. switch from primary to
    secondary) to recover correctly in host restart case. Otherwise if host
    restarts after apply log has been created and the engine goes back in
    time on recovery, we'll use the apply log for recovery and assume that
    engine is caught up.
    
    Differential Revision: D50385170
    
    fbshipit-source-id: 1a0dff6b2586b36f7ee3e9d2322e403d63acc31a

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -12213,6 +12213,10 @@ int binlog_change_to_apply() {
     DBUG_RETURN(0);
   }
 
+  if (ha_flush_logs()) {
+    DBUG_RETURN(1);
+  }
+
   int error = 0;
   LOG_INFO linfo;
   mysql_mutex_lock(mysql_bin_log.get_log_lock());
@@ -12297,7 +12301,9 @@ int binlog_change_to_binlog(THD *thd) {
   // batch of transactions. This is important because the act of switching trx
   // logs from "apply-logs-*" to "binary-logs-*" looks like a rotation to other
   // parts of the system and rotation is always a 'sync' point
-  ha_flush_logs(NULL);
+  if (ha_flush_logs()) {
+    DBUG_RETURN(1);
+  }
 
   mysql_mutex_lock(mysql_bin_log.get_log_lock());
   const bool is_locked = dump_log.lock();
