93d5b46c0dbc23d912487a035d7b1830066e4e46
Alex Budovski <alexbud@meta.com>
2023-11-08 10:37:09 -0800

    mysql on clang15: fix -Wformat errors
    Summary:
    clang 15 comes with a few extra [formatting warnings](https://clang.llvm.org/docs/DiagnosticsReference.html#wformat) that we treat as
    errors. This diff fixes them in preparation for the upgrade.
    
    Differential Revision: D51122375
    
    fbshipit-source-id: 0d2ad3dff20e8d95566dfbfb590e09241c211030

--- storage/rocksdb/rdb_datadic.cc
+++ storage/rocksdb/rdb_datadic.cc
@@ -5545,12 +5545,14 @@ bool Rdb_dict_manager::get_index_info(
   }
 
   if (error) {
-    rdb_fatal_error("RocksDB: Found invalid key version number (%" PRIu16
-                    ", %hhu, %" PRIu16
-                    ", %llu) from data dictionary. This should never happen "
-                    "and it may be a bug.",
-                    index_info->m_index_dict_version, index_info->m_index_type,
-                    index_info->m_kv_version, index_info->m_ttl_duration);
+    rdb_fatal_error(
+        "RocksDB: Found invalid key version number (%hu"
+        ", %hhu, %hu"
+        ", %" PRIu64
+        ") from data dictionary. This should never happen "
+        "and it may be a bug.",
+        index_info->m_index_dict_version, index_info->m_index_type,
+        index_info->m_kv_version, index_info->m_ttl_duration);
   }
 
   return found;
