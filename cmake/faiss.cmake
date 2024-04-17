2e2e469533976e13553cacf5bd46248760fd8a13
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-04-17 15:46:28 +0300

    Fix vectordb build, including macOS portability fixes
    Fix some vectordb build issues, both general and macOS-specific:
    - cmake/faiss.cmake references a non-existing BUILD_DIR CMake variable. Since it
      gets expanded to nothing, the FAISS build dir becomes /faiss, and then, if the
      root dir is unwritable:
      -- Adding faiss and its dependencies
      CMake Error at _build-debug/FAISS_EXTERNAL-prefix/tmp/FAISS_EXTERNAL-mkdirs.cmake:6 (file):
        file failed to create directory:
    
          /faiss
    
        because: Read-only file system
      Call Stack (most recent call first):
        /opt/homebrew/Cellar/cmake/3.29.2/share/cmake/Modules/ExternalProject.cmake:1816 (include)
        /opt/homebrew/Cellar/cmake/3.29.2/share/cmake/Modules/ExternalProject.cmake:4392 (_ep_set_directories)
        cmake/faiss.cmake:14 (ExternalProject_Add)
        CMakeLists.txt:2214 (INCLUDE)
    - If Ninja is used as the CMake build generator, the build fails with:
      ninja: error: 'faiss/faiss/libfaiss.a', needed by 'runtime_output_directory/mysqld', missing and no known rule to make it
      This is fixed by setting BUILD_BYPRODUCTS option on ExternalProject_Add to the
      static library name.
    - Replace INCLUDE_DIRECTORIES for sql_main with more idiomatic and non-polluting
      globally TARGET_INCLUDE_DIRECTORIES
    - Fix trivial macOS specific build errors on storage/rocksdb/rdb_vector_db.cc:
    storage/rocksdb/rdb_vector_db.cc:90:26: error: cannot initialize a parameter of type 'uint64 *' (aka 'unsigned long long *') with an rvalue of type 'size_t *' (aka 'unsigned long *')
      if (reader.read_uint64(&actual_list_id)) {
                             ^~~~~~~~~~~~~~~
    storage/rocksdb/./rdb_buff.h:325:34: note: passing argument to parameter 'res' here
      bool read_uint64(uint64 *const res) {
                                     ^
    storage/rocksdb/rdb_vector_db.cc:144:63: error: format specifies type 'unsigned long' but the argument has type 'value_type' (aka 'long long') [-Werror,-Wformat]
                            "Failed to find matching pk for %lu", vector_id);
                                                            ~~~   ^~~~~~~~~
                                                            %lld
    include/mysql/components/services/log_builtins.h:838:66: note: expanded from macro 'LogPluginErrMsg'
          .message_quoted("Plugin " LOG_COMPONENT_TAG " reported", ##__VA_ARGS__)
                                                                     ^~~~~~~~~~~
    storage/rocksdb/rdb_vector_db.cc:145:63: error: format specifies type 'unsigned long' but the argument has type 'value_type' (aka 'long long') [-Werror,-Wformat]
                            "Failed to find matching pk for %lu", vector_id);
                                                            ~~~   ^~~~~~~~~
                                                            %lld
    include/mysql/components/services/log_builtins.h:838:66: note: expanded from macro 'LogPluginErrMsg'
          .message_quoted("Plugin " LOG_COMPONENT_TAG " reported", ##__VA_ARGS__)
                                                                     ^~~~~~~~~~~
    -- Fix an LLVM 14+ build error by adding the missing include:
    storage/rocksdb/rdb_cmd_srv_helper.cc:66:39: error: implicit instantiation of undefined template 'std::array<char, 512>'
      std::array<char, MYSQL_ERRMSG_SIZE> error_msg;
                                          ^
    /opt/homebrew/opt/llvm@14/bin/../include/c++/v1/__tuple:219:64: note: template is declared here
    template <class _Tp, size_t _Size> struct _LIBCPP_TEMPLATE_VIS array;
                                                                   ^

--- cmake/faiss.cmake
+++ cmake/faiss.cmake
@@ -4,7 +4,8 @@ INCLUDE(ExternalProject)
 
 MESSAGE(STATUS "Adding faiss and its dependencies")
 SET(FAISS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/faiss)
-SET(FAISS_BUILD_DIR ${BUILD_DIR}/faiss)
+SET(FAISS_BUILD_DIR ${CMAKE_BINARY_DIR}/faiss)
+SET(FAISS_LIBRARY ${FAISS_BUILD_DIR}/faiss/libfaiss.a)
 FIND_LIBRARY(OPENMP_LIBRARY REQUIRED
     NAMES libomp.so omp
     HINTS ${WITH_OPENMP}/lib)
@@ -18,6 +19,7 @@ ExternalProject_Add(
   PATCH_COMMAND ${CMAKE_COMMAND} -E echo "applying patch to faiss"
   COMMAND patch --no-backup-if-mismatch -f -r - -p1 -N < ${CMAKE_SOURCE_DIR}/extra/faiss.patch || true
   CMAKE_ARGS -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_RAFT=OFF -DFAISS_ENABLE_PYTHON=OFF -DFAISS_ENABLE_C_API=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DFAISS_OPT_LEVEL=generic -DRAFT_NVTX=OFF -DCMAKE_CUDA_ARCHITECTURES="NATIVE" -DOpenMP_CXX_LIB_NAMES=libomp -DOpenMP_libomp_LIBRARY=${OPENMP_LIBRARY} -DOpenMP_CXX_INCLUDE_DIR=${WITH_OPENMP}/include -DBLAS_LIBRARIES=${BLAS_LIBRARIES}
+  BUILD_BYPRODUCTS ${FAISS_LIBRARY}
   INSTALL_COMMAND ""
 )
 # revert patch in submodule so we have a clean git state
@@ -28,7 +30,6 @@ ExternalProject_Add_Step(
   COMMAND patch --no-backup-if-mismatch -f -r - -p1 -R < ${CMAKE_SOURCE_DIR}/extra/faiss.patch || true
   WORKING_DIRECTORY <SOURCE_DIR>
 )
-SET(FAISS_LIBRARY ${FAISS_BUILD_DIR}/faiss/libfaiss.a)
 ADD_LIBRARY(FAISS STATIC IMPORTED GLOBAL)
 SET_PROPERTY(TARGET FAISS PROPERTY IMPORTED_LOCATION ${FAISS_LIBRARY})
 # run the ExternalProject target
