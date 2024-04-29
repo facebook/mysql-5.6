f0108427fb4704ad203516284cf2614233f8253d
Luqun Lou <luqun@meta.com>
2024-04-29 12:09:27 -0700

    replace python with python3 for cpplint.py
    Summary: For cpplint.py, use python3 instead of python2
    
    Differential Revision: D56717362
    
    fbshipit-source-id: bc9309da7d0ea0827578d04cab6e25b3a9f86eca

--- arcanist/lint/cpp_linter/cpplint.py
+++ arcanist/lint/cpp_linter/cpplint.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
